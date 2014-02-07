89b7f19869fe3c93a9a06d9d4666aeedc6cda6a5
Dan Guzek <dguzek@gmail.com>
2014-02-07 11:58:22 -0500

    Replace ScreenTitleMenu Graphic
    The png that was here was broken in some way (I don’t know how/why) in
    at least one version of Arch Linux.  Though I’m unable to reproduce
    this problem on Mac OS, Windows, Debian, or Ubuntu, this hopefully
    fixes it.

--- BGAnimations/ScreenSimplyLove underlay.lua
+++ BGAnimations/ScreenSimplyLove underlay.lua
@@ -5,7 +5,7 @@ t[#t+1] = LoadActor(THEME:GetPathG("", "_logos/" .. GAMESTATE:GetCurrentGame():G
 	OnCommand=cmd(linear,0.5; diffusealpha, 1);
 };
 
-t[#t+1] = LoadActor(THEME:GetPathB("ScreenTitleMenu","underlay/SimplyLove-hq.png"))..{
+t[#t+1] = LoadActor(THEME:GetPathB("ScreenTitleMenu","underlay/SimplyLove.png"))..{
 	InitCommand=cmd(x, SCREEN_CENTER_X; y, SCREEN_CENTER_Y; diffusealpha, 0; zoom, 0.333);
 	OnCommand=cmd(linear,0.5; diffusealpha, 1);
 };
--- BGAnimations/ScreenTitleMenu underlay/default.lua
+++ BGAnimations/ScreenTitleMenu underlay/default.lua
@@ -5,7 +5,7 @@ t[#t+1] = LoadActor(THEME:GetPathG("", "_logos/" .. GAMESTATE:GetCurrentGame():G
 	OffCommand=cmd(linear,0.5; diffusealpha, 0;);
 };
 
-t[#t+1] = LoadActor("SimplyLove-hq.png") .. {
+t[#t+1] = LoadActor("SimplyLove.png") .. {
 	InitCommand=cmd(x, SCREEN_CENTER_X; y, SCREEN_CENTER_Y; diffusealpha, 1; zoom, 0.333);
 	OffCommand=cmd(linear,0.5; diffusealpha, 0;);
 };
