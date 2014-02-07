50702b5a7b5834e8d016e2b6e761e59bc1325a1b
Dan Guzek <dguzek@gmail.com>
2014-02-06 22:48:05 -0500

    Add semicolons to the GradeTier3 (double star)

--- Graphics/_grades/Grade_Tier03.lua
+++ Graphics/_grades/Grade_Tier03.lua
@@ -1,8 +1,8 @@
 return Def.ActorFrame{
 	LoadActor("graphics/star.png")..{
 		OnCommand=cmd(x,-39;y,40;zoom,0.6;pulse;effectmagnitude,1,0.9,0);
-	}
+	};
 	LoadActor("graphics/star.png")..{
 		OnCommand=cmd(x,39;y,-40;zoom,0.6;effectoffset,0.2;pulse;effectmagnitude,0.9,1,0);
-	}
+	};
 };
