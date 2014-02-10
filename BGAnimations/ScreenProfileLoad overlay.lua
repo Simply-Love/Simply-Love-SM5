2f9a3b90aa610b782fedf730bc2722a6bd6f72f5
Dan Guzek <dguzek@gmail.com>
2014-02-09 22:27:33 -0500

    Simplify ScreenProfileSave

--- BGAnimations/ScreenProfileLoad overlay.lua
+++ BGAnimations/ScreenProfileLoad overlay.lua
@@ -1,9 +1,15 @@
 local t = Def.ActorFrame{};
 
 t[#t+1] = Def.Quad{
-	InitCommand=cmd(Center; zoomto,SCREEN_WIDTH,50;diffuse,color("#FFFFFF"););
-	OnCommand=cmd(fadebottom,0.15;fadetop,0.15);
-	OffCommand=cmd(accelerate,0.15;zoomy,0);
+	InitCommand=cmd(Center; zoomto,SCREEN_WIDTH,SCREEN_HEIGHT;diffuse,Color.Black);
+};
+
+t[#t+1] = Def.Quad{
+	InitCommand=cmd(zoomto,SCREEN_WIDTH,0; Center; diffuse,Color.White);
+	OnCommand=cmd(decelerate, 0.3; zoomtoheight,50; sleep,0.5; sleep,0.1; queuecommand, "Load");
+	LoadCommand=function(self) 
+		SCREENMAN:GetTopScreen():Continue();
+	end;
 };
 
 
@@ -11,15 +17,6 @@ t[#t+1] = LoadFont("_wendy small")..{
 	Text=THEME:GetString("ScreenProfileLoad","Loading Profiles...");
 	InitCommand=cmd(Center;diffuse,color("#000000");shadowlength,0; zoom,0.6);
 	OffCommand=cmd(linear,0.2;diffusealpha,0);
-};
-
-
-t[#t+1] = Def.Actor {
-	BeginCommand=function(self)		
-		if SCREENMAN:GetTopScreen():HaveProfileToLoad() then self:sleep(0.15); end;
-		self:queuecommand("Load");
-	end;
-	LoadCommand=function() SCREENMAN:GetTopScreen():Continue(); end;
 };
 
 return t;
