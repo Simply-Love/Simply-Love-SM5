15ea900bd134fc75598ce8ea676a6d3c621b4b2f
Dan Guzek <dguzek@gmail.com>
2014-02-09 21:05:06 -0500

    Remove unused Lua
    This had been a failed experiment in loading custom data for local
    profiles.

--- BGAnimations/ScreenWithMenuElements decorations.lua
+++ BGAnimations/ScreenWithMenuElements decorations.lua
@@ -3,24 +3,4 @@ t =  Def.ActorFrame {};
 t[#t+1] = StandardDecorationFromFile( "Header", "Header" );
 t[#t+1] = StandardDecorationFromFileOptional( "Footer", "Footer" );
 
-
-t[#t+1] = Def.ActorFrame{
-	
-	-- PlayerJoinedMessageCommand=function(self,params)
-	-- 	
-	-- 	local slot;
-	-- 	
-	-- 	if params.Player == PLAYER_1 then
-	-- 		slot = "ProfileSlot_Player1";
-	-- 	elseif params.Player == PLAYER_2 then
-	-- 		slot = "ProfileSlot_Player2";
-	-- 	end
-	-- 	
-	-- 	local dir = PROFILEMAN:GetProfileDir(slot);		
-	-- 	local profile = PROFILEMAN:GetProfile(params.Player);
-	-- 	
-	-- 	LoadProfileCustom(profile,dir);
-	-- end
-	
-};
 return t; 
