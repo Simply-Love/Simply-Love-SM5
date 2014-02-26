c2b3f6bbae94d591e992b6d96a532ae62cfb5817
Dan Guzek <dguzek@gmail.com>
2014-02-25 19:41:35 -0500

    Minor aesthetic change to ScreenSelectMusic

--- BGAnimations/ScreenSelectMusic overlay/banner.lua
+++ BGAnimations/ScreenSelectMusic overlay/banner.lua
@@ -1,25 +1,12 @@
 local t = Def.ActorFrame{
 
+
 	LoadActor("colored_banners/banner"..SimplyLoveColor()..".png")..{
 		Name="FallbackBanner";
 		OnCommand=function(self)
 			self:xy((SCREEN_CENTER_X - SCREEN_WIDTH/WideScale(4, 4.75) ), 112);
 			self:zoom(WideScale(0.725,0.8));
 		end;
-		OffCommand=cmd(diffusealpha,0);
-		-- CurrentSongChangedMessageCommand=function(self)
-		-- 	
-		-- 	local song = GAMESTATE:GetCurrentSong();
-		-- 	if song then
-		-- 		local banner = song:HasBanner();
-		-- 	
-		-- 		if banner then
-		-- 			self:diffusealapha(0);
-		-- 		else
-		-- 			self:diffusealapha(1);
-		-- 		end
-		-- 	end
-		-- end;
 	};
 	
 
@@ -34,6 +21,7 @@ local t = Def.ActorFrame{
 			self:SetTarget(banner);
 		end;
 	};
+
 	
 };
 
--- BGAnimations/ScreenSelectMusic overlay/stepArtist.lua
+++ BGAnimations/ScreenSelectMusic overlay/stepArtist.lua
@@ -121,7 +121,7 @@ for p=1,2 do
 				
 				local song = GAMESTATE:GetCurrentSong();
 				local course = GAMESTATE:GetCurrentCourse();
-				self:visible(song or course)
+				self:visible(song ~= nil or course ~= nil)
 			end;
 		};
 		
