9985a63171258791c0e972ef91fb8a35d76c95a9
Dan Guzek <dguzek@gmail.com>
2014-02-27 16:37:45 -0500

    replace ScreenInit's SSC logo with the SM5 logo

--- BGAnimations/ScreenInit decorations/default.lua
+++ BGAnimations/ScreenInit decorations/default.lua
@@ -1,44 +1,51 @@
+local slc = SimplyLoveColor();
+local arrowData = {
+	zoom = { 0.05, 0.06, 0.075, 0.1, 0.075, 0.06, 0.05 },
+	sleep = {0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9},
+	xPos = {-150, -100, -50, 0, 50, 100, 150},
+	color = {slc-3, slc-2, slc-1, slc, slc+1, slc+2, slc+3}
+};
+
+local function RainbowArrows( x )
+	
+	return Def.ActorFrame {
+		InitCommand=cmd(Center);
+		
+		LoadActor("white_logo.png")..{
+			InitCommand=cmd(zoom,arrowData.zoom[x]; diffuse, GetHexColor(arrowData.color[x]%12+1); diffusealpha,0; x, arrowData.xPos[x] );
+			OnCommand=cmd(sleep,arrowData.sleep[x]; linear,0.75; diffusealpha,1; linear,0.75;diffusealpha,0);
+		};
+		LoadActor("highlight.png")..{
+			InitCommand=cmd(zoom,arrowData.zoom[x]; diffusealpha,0; x, arrowData.xPos[x]);
+			OnCommand=cmd(sleep,arrowData.sleep[x]; linear,0.75; diffusealpha,0.75; linear,0.75;diffusealpha,0);
+		};
+	};
+	
+end
+
+
+
 local t = Def.ActorFrame {};
 
+
 t[#t+1] = Def.ActorFrame {
-	
 	InitCommand=cmd(Center);
-  
-	Def.ActorFrame {
-		Def.Quad {
-			InitCommand=cmd(zoomto,SCREEN_WIDTH,0;);
-			OnCommand=cmd( accelerate,0.3; zoomtoheight,128; diffusealpha,1; sleep,1.5; linear,0.25);
-			OffCommand=cmd(accelerate,0.3; zoomtoheight,0;)
-		};
-		LoadActor("ssc") .. {
-			InitCommand=cmd(diffusealpha,0);
-			OnCommand=cmd(sleep,0.3; linear,1;diffusealpha,1;sleep,0.75;linear,0.25;diffusealpha,0);
-		};
+
+	Def.Quad {
+		InitCommand=cmd(zoomto,SCREEN_WIDTH,0;);
+		OnCommand=cmd( accelerate,0.3; zoomtoheight,128; diffusealpha,1; sleep,2.5; linear,0.25);
+		OffCommand=cmd(accelerate,0.3; zoomtoheight,0;)
 	};
-	
-	Def.ActorFrame {
-		OnCommand=cmd(playcommandonchildren,"ChildrenOn"; diffuse,color("0,0,0,1"););
-		ChildrenOnCommand=cmd(diffusealpha,0;sleep,2;linear,0.25;diffusealpha,1);
-		OffCommand=cmd(linear, 0.2; diffusealpha,0);
-	  
-		LoadFont("_misoreg hires") .. {
-			Text=ProductID();
-			InitCommand=cmd(y,-36; zoom, 0.9; );
-		};
-		LoadFont("_wendy small") .. {
-			Text=THEME:GetThemeDisplayName();
-			InitCommand=cmd(y, -10; zoom, 0.55; );
-		};
-		LoadFont("_misoreg hires") .. {
-			Text="Created by " .. THEME:GetThemeAuthor();
-			InitCommand=cmd(y,24; zoom, 0.9; );
-		};
-		
-		LoadFont("_misoreg hires") .. {
-			Text="SM5 port by dbk2";
-			InitCommand=cmd(y,42;zoom,0.75);
-		};
+  		
+	LoadFont("_misoreg hires")..{
+		Text="Theme by " .. THEME:GetThemeAuthor();
+		OnCommand=cmd(diffuse,color("0,0,0,0");sleep,3;linear,0.25;diffusealpha,1);
+		OffCommand=cmd(linear, 0.25; diffusealpha,0);
 	};
 };
+
+for i=1,7 do
+	t[#t+1] = RainbowArrows(i)
+end
 
 return t
