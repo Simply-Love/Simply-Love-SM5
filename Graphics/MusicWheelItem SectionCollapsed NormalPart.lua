438d363ac86490753b5777bfbf9d7d28d08deb2c
Dan Guzek <dguzek@gmail.com>
2014-03-05 19:58:38 -0500

    remove unused code

--- Graphics/MusicWheelItem SectionCollapsed NormalPart.lua
+++ Graphics/MusicWheelItem SectionCollapsed NormalPart.lua
@@ -5,15 +5,8 @@ local t = Def.ActorFrame{
 		Name="SectionBG";
 		InitCommand=cmd(zoomto,SCREEN_WIDTH/2,SCREEN_HEIGHT/15; horizalign,left; addx,-SCREEN_WIDTH/10);
 		SetMessageCommand=function(self,params)
-						
-			if group ~= "" or GAMESTATE:GetSortOrder() == 'SortOrder_ModeMenu' then
-				self:visible(true)
-			else
-				self:visible(false);
-			end
-	
 		end;
-	};
+	};	
 };
 
 return t;
