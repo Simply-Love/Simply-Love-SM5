b37612096bfd5ad990f1c05b612de8acfc316fb8
Dan Guzek <dguzek@gmail.com>
2014-03-14 16:29:45 -0400

    update Scripts/ThemeInfo

--- Scripts/00 themeInfo.lua
+++ Scripts/00 themeInfo.lua
@@ -3,6 +3,6 @@ themeInfo = {
 	ProductCode = "SL-001",
 	Name = "Simply Love",
 	Version = 0.2,
-	FriendlyVersion = "Beta 2",
-	Date = "20140226 16:00",
+	FriendlyVersion = "Beta 3",
+	Date = "20140314 16:30",
 };
