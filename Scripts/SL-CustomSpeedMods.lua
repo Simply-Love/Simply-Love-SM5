90365694f87a3fedf437fe2dea5b71fea1acb355
Dan Guzek <dguzek@gmail.com>
2014-03-14 16:06:11 -0400

    Significant rewrite of BPM display handlers
    Now supports course mode and is, in general, more modular.

--- Scripts/SL-CustomSpeedMods.lua
+++ Scripts/SL-CustomSpeedMods.lua
@@ -140,8 +140,13 @@ end
 
 
 function DisplaySpeedMod(speed)
-	local bpm = GAMESTATE:GetCurrentSong():GetDisplayBpms();
-	local display;
+	local bpm, display;
+	
+	if GAMESTATE:IsCourseMode() then
+		bpm = GetCourseModeBPMs();
+	else
+		bpm = GAMESTATE:GetCurrentSong():GetDisplayBpms();
+	end
 		
 	-- if using an x-mod
 	if string.sub(speed,-1) == "x" then
@@ -162,4 +167,86 @@ function DisplaySpeedMod(speed)
 	end
 
 	return display;
+end
+
+
+function GetCourseModeBPMs()
+	local Players, player, trail, trailEntries, lowest, highest, text;
+
+	Players = GAMESTATE:GetHumanPlayers();
+	player = Players[1];
+
+	if player then
+		trail = GAMESTATE:GetCurrentTrail(player);
+
+		if trail then
+			trailEntries = trail:GetTrailEntries();
+
+			for k,trailEntry in ipairs(trailEntries) do
+				local bpms = trailEntry:GetSong():GetDisplayBpms();
+
+				-- on the first iteration, lowest and highest will both be nil
+				-- so set lowest to this song's lower bpm
+				-- and highest to this song's higher bpm
+				if not lowest then
+					lowest = bpms[1];
+				end
+				if not highest then
+					highest = bpms[2];
+				end
+
+				-- on each subsequent iteration, compare
+				if lowest > bpms[1] then
+					lowest = bpms[1];
+				end
+				if highest < bpms[2] then
+					highest = bpms[2];
+				end
+			end
+			if lowest and highest then
+				range = {lowest, highest}
+				return range;
+			end
+		end
+	end
+end
+
+
+function GetDisplayBPMs()
+	local text = "";
+	
+	-- if in "normal" mode
+	if not GAMESTATE:IsCourseMode() then
+		local song = GAMESTATE:GetCurrentSong();
+		
+		if song then
+			local bpm = song:GetDisplayBpms();
+			
+			--if a single bpm suffices
+			if bpm[1] == bpm[2] then
+				text = round(bpm[1])
+		
+			-- if we have a range of bpms
+			else
+				text = round(bpm[1]) .. " - " .. round(bpm[2])
+			end
+		end
+		
+	-- if we ARE in CourseMode		
+	else
+		local range = GetCourseModeBPMs();
+		local lowest = range[1];
+		local highest = range[2];
+				
+				
+		if lowest and highest then
+			if lowest == highest then
+				text = round(lowest);
+			else
+				text = round(lowest) .. " - " .. round(highest)
+			end
+		end
+	end
+	
+	return text;
 end
