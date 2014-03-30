ffe6f4a5d0accac505f3606e834052bcfeef65b4
Dan Guzek <dguzek@gmail.com>
2014-03-30 17:07:03 -0400

    Have SSM use a "cancel" tween

--- BGAnimations/_fade out fast.lua
+++ BGAnimations/_fade out fast.lua
@@ -1,4 +1,4 @@
 return Def.Quad {
-	InitCommand=cmd(FullScreen);
+	InitCommand=cmd(FullScreen; diffuse, Color.Black);
 	StartTransitioningCommand=cmd(diffusealpha,0;sleep,0.1;linear,0.2;diffusealpha,1);
 };
