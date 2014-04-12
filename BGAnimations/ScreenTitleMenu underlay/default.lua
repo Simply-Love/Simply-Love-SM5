3c54e7331d228f2b627fda25473d2870a7ced40e
Dan Guzek <dguzek@gmail.com>
2014-04-11 21:29:38 -0400

    update transition for ScreenTitleMenu

--- BGAnimations/ScreenTitleMenu underlay/default.lua
+++ BGAnimations/ScreenTitleMenu underlay/default.lua
@@ -6,13 +6,19 @@ local t = Def.ActorFrame{
 };
 	
 t[#t+1] = LoadActor(THEME:GetPathG("", "_logos/" .. GAMESTATE:GetCurrentGame():GetName()))..{
-	InitCommand=cmd(x, SCREEN_CENTER_X; y, SCREEN_CENTER_Y; diffusealpha, 1;);
+	InitCommand=cmd(x, SCREEN_CENTER_X; y, SCREEN_CENTER_Y );
 	OffCommand=cmd(linear,0.5; diffusealpha, 0;);
 };
 
 t[#t+1] = LoadActor("SimplyLove.png") .. {
-	InitCommand=cmd(x, SCREEN_CENTER_X; y, SCREEN_CENTER_Y; diffusealpha, 1; zoom, 0.333);
+	InitCommand=cmd(x, SCREEN_CENTER_X; y, SCREEN_CENTER_Y; zoom, 0.333);
 	OffCommand=cmd(linear,0.5; diffusealpha, 0;);
 };
+
+
+t[#t+1] = Def.Quad{
+	InitCommand=cmd(zoomto,SCREEN_WIDTH,SCREEN_HEIGHT/2 - SCREEN_HEIGHT/4; xy, SCREEN_CENTER_X, SCREEN_BOTTOM-SCREEN_HEIGHT/8; MaskSource );
+	OnCommand=cmd(sleep, 0.1; linear, 0.3; addy, SCREEN_HEIGHT/2 - SCREEN_HEIGHT/4;);
+}
 
 return t;
