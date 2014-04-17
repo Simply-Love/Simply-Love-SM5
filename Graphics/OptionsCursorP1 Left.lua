819619a5fdea1f66934f9d988615080615ce225f
Dan Guzek <dguzek@gmail.com>
2014-04-17 16:45:00 -0400

    use lua cursors rather than png cursors

---
+++ Graphics/OptionsCursorP1 Left.lua
@@ -0,0 +1,5 @@
+return Def.ActorFrame {
+	Def.Quad {
+		InitCommand=cmd(zoomto,2,26;);
+	}
+};
---
+++ Graphics/OptionsCursorP1 Middle.lua
@@ -0,0 +1,10 @@
+return Def.ActorFrame {	
+	Def.Quad {
+		Name="P1CursorTop";
+		InitCommand=cmd(zoomto,1,2; y,-12;);
+	},
+	Def.Quad {
+		Name="P1CursorBottom";
+		InitCommand=cmd(zoomto,1,2; y,12; );
+	},	
+};
