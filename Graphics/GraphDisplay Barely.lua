c4dc9489a4b100ddc0b1902021c92dc426e1b203
Dan Guzek <dguzek@gmail.com>
2014-04-18 03:33:53 -0400

    add GraphDisplay Barely

---
+++ Graphics/GraphDisplay Barely.lua
@@ -0,0 +1,18 @@
+local t = Def.ActorFrame{
+	LoadFont("_misoreg hires")..{
+		Text="Barely!";
+		OnCommand=cmd(zoom,0.75;shadowlength,0;y,-4;diffusealpha,0;addy,-20;sleep,0.5;accelerate,.2;diffusealpha,1;addy,30;);
+	};
+	
+	LoadActor(THEME:GetPathB("ScreenSelectMusic", "overlay/cursor.png"))..{
+		InitCommand=cmd(rotationz,90; zoom,0.5; y,10; diffusealpha,0; addy,-20;);
+		OnCommand=cmd(sleep,0.5;
+ 			accelerate,.2; diffusealpha,1; addy,30;
+			diffuseshift; effectcolor1,1,1,1,1; effectcolor2,1,1,1,0.2;
+			decelerate,.3; addy,-10; accelerate,.3; addy,10;
+		);
+	};
+
+};
+
+return t;
