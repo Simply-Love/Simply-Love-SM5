7b9ebe7f3293caa54d458897a7cf193907355186
Dan Guzek <dguzek@gmail.com>
2014-04-20 22:22:56 -0400

    Graphics directory cleanup

--- Graphics/ScreenStageInformation header.lua
+++
@@ -1,6 +0,0 @@
-return Def.ActorFrame{
-	Def.Quad{
-		InitCommand=cmd(x, SCREEN_CENTER_X; zoomto,SCREEN_WIDTH,40; );
-		OnCommand=cmd(diffuse,color("0.65,0.65,0.65,0.8"); linear,0.15; diffusealpha,0;);
-	};
-};
