2ce7123f2e0b3b73748811bf47f4d56f5ddec3b2
Dan Guzek <dguzek@gmail.com>
2014-04-21 03:14:22 -0400

    color OptionRow titles by PlayerColor()

--- BGAnimations/ScreenPlayerOptions overlay.lua
+++ BGAnimations/ScreenPlayerOptions overlay.lua
@@ -8,21 +8,85 @@ local currentMod = {
 	P2 = getenv("SpeedModP2") or "1x";
 };
 
+local ScreenOptions = SCREENMAN:GetTopScreen();
 
 local t = Def.ActorFrame{
 	InitCommand=cmd(xy,SCREEN_CENTER_X,0);
-	OnCommand=cmd(linear,0.2;diffusealpha,1);
+	OnCommand=cmd(diffusealpha,0; linear,0.2;diffusealpha,1);
 	OffCommand=cmd(linear,0.2;diffusealpha,0);
+	
+	-- this is broadcast from [OptionRow] TitleGainFocusCommand in metrics.ini
+	-- we use it to color the active OptionRow's title appropriately by PlayerColor()
+	OptionRowChangedMessageCommand=function(self, params)
+				
+		local CurrentRowIndexP1, CurrentRowIndexP2;
+		local dummySpeedModTitle = self:GetChild("DummySpeedModTitle");
+		
+		-- there is always the possibility that a diffuseshift is still active
+		-- cancel it now (and re-apply below, if applicable)
+		params.Title:stopeffect();
+		dummySpeedModTitle:stopeffect();
+		dummySpeedModTitle:diffuse(Color.White);
+		
+		-- if ScreenOptions is nil, get it now
+		if not ScreenOptions then
+			ScreenOptions = SCREENMAN:GetTopScreen();
+		else			
+			-- get the index of PLAYER_1's current row
+			if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
+				CurrentRowIndexP1 = ScreenOptions:GetCurrentRowIndex(PLAYER_1);
+				if CurrentRowIndexP1 == 1 then
+					dummySpeedModTitle:diffuse(PlayerColor(PLAYER_1));
+				end
+			end
+			
+			-- get the index of PLAYER_2's current row
+			if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
+				CurrentRowIndexP2 = ScreenOptions:GetCurrentRowIndex(PLAYER_2);
+				if CurrentRowIndexP2 == 1 then
+					dummySpeedModTitle:diffuse(PlayerColor(PLAYER_2));
+				end
+			end
+		end
+			
+		local optionRow = params.Title:GetParent():GetParent();
+		
+		-- color the active optionrow's title appropriately
+		if optionRow:HasFocus(PLAYER_1) then
+			params.Title:diffuse(PlayerColor(PLAYER_1));
+		end
+		
+		if optionRow:HasFocus(PLAYER_2) then
+			params.Title:diffuse(PlayerColor(PLAYER_2));
+		end
+			
+		if CurrentRowIndexP1 == CurrentRowIndexP2 then
+			params.Title:diffuseshift();
+			params.Title:effectcolor1(PlayerColor(PLAYER_1));
+			params.Title:effectcolor2(PlayerColor(PLAYER_2));
+			
+			if CurrentRowIndexP1 == 1 then
+				dummySpeedModTitle:diffuseshift();
+				dummySpeedModTitle:effectcolor1(PlayerColor(PLAYER_1));
+				dummySpeedModTitle:effectcolor2(PlayerColor(PLAYER_2));
+			end
+		end
+
+	end;
+	
 	
 	LoadFont("_misoreg hires")..{
 		Name="DummySpeedModTitle";
 		Text="";
-		InitCommand=cmd(zoom,0.8;shadowlength,1;shadowcolor,color("0,0,0,0.8");strokecolor,color("0,0,0,0.2");halign, 0 );
+		InitCommand=cmd(diffusealpha,0; zoom,0.8; halign,0 );
 		OnCommand=function(self)
 			local x = WideScale(-215,-315);
 			local y = 112;
 			self:xy(x, y);		
 			self:settext( "(".. GetDisplayBPMs() .. ")" );
+			self:sleep(0.1);
+			self:linear(0.2);
+			self:diffusealpha(1);
 		end;
 	};
 };
--- BGAnimations/ScreenPlayerOptions2 overlay.lua
+++ BGAnimations/ScreenPlayerOptions2 overlay.lua
@@ -1,1 +1,54 @@
-return Def.ActorFrame{};
+local ScreenOptions = SCREENMAN:GetTopScreen();
+
+local t = Def.ActorFrame{
+
+	OnCommand=cmd(diffusealpha,0; linear,0.2;diffusealpha,1);
+	OffCommand=cmd(linear,0.2;diffusealpha,0);
+	
+	-- this is broadcast from [OptionRow] TitleGainFocusCommand in metrics.ini
+	-- we use it to color the active OptionRow's title appropriately by PlayerColor()
+	OptionRowChangedMessageCommand=function(self, params)
+		
+		-- there is always the possibility that a diffuseshift is still active
+		-- cancel it now (and re-apply below, if applicable)
+		params.Title:stopeffect();
+		
+		local CurrentRowIndexP1, CurrentRowIndexP2;
+		
+		-- if ScreenOptions is nil, get it now
+		if not ScreenOptions then
+			ScreenOptions = SCREENMAN:GetTopScreen();
+		else			
+			-- get the index of PLAYER_1's current row
+			if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
+				CurrentRowIndexP1 = ScreenOptions:GetCurrentRowIndex(PLAYER_1);
+			end
+			
+			-- get the index of PLAYER_2's current row
+			if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
+				CurrentRowIndexP2 = ScreenOptions:GetCurrentRowIndex(PLAYER_2);				
+			end
+		end
+			
+		local optionRow = params.Title:GetParent():GetParent();
+		
+		-- color the active optionrow's title appropriately
+		if optionRow:HasFocus(PLAYER_1) then
+			params.Title:diffuse(PlayerColor(PLAYER_1));
+		end
+		
+		if optionRow:HasFocus(PLAYER_2) then
+			params.Title:diffuse(PlayerColor(PLAYER_2));
+		end
+			
+		if CurrentRowIndexP1 == CurrentRowIndexP2 then
+			params.Title:diffuseshift();
+			params.Title:effectcolor1(PlayerColor(PLAYER_1));
+			params.Title:effectcolor2(PlayerColor(PLAYER_2));
+		end
+
+	end;	
+	
+};
+
+return t;
