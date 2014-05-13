510deb1d3124c822006d0fefcdc235f56c7c0724
Dan Guzek <dguzek@gmail.com>
2014-05-13 18:00:00 -0400

    fix Grade_Tier01
    We do, in fact, actually need to return ActorFrames after we're done
    manipulating them!

--- Graphics/_grades/Grade_Tier01.lua
+++ Graphics/_grades/Grade_Tier01.lua
@@ -1,35 +1,29 @@
 local function Spin(self)
-	r = math.min(math.random(3,51),36)
-	s = math.random()*7+1
+	r = math.min(math.random(3,51),36);
+	s = math.random()*7+1;
 	z = self:GetZ();
 	l = r/36;
+	
 	if z >= 36 then
-		z = z-36
-		self:z(z)
-		self:rotationz(z*10)
+		z = z-36;
+		self:z(z);
+		self:rotationz(z*10);
 	end
-	z = z + r
-	self:linear(l)
-	self:rotationz(z*10)
-	self:z(z)
-	self:sleep(s)
-	self:queuecommand('Spin')
+	
+	z = z + r;
+	self:linear(l);
+	self:rotationz(z*10);
+	self:z(z);
+	self:sleep(s);
+	self:queuecommand("Spin");
 end
 
-local speed;
 local t = Def.ActorFrame{
 
 	--top left
 	LoadActor("graphics/star.png")..{
 		OnCommand=cmd(x,-46;y,-46;zoom,0.5;pulse;effectmagnitude,1,0.9,0;sleep,60;queuecommand,"Spin");
-		SpinCommand=function(self)
-			if not speed then
-				 speed = 0;
-			else
-				speed + 1;
-			end
-			Spin(self);
-		end;
+		SpinCommand=function(self) Spin(self); end;
 	};
 
 	--top right
@@ -50,3 +44,5 @@ local t = Def.ActorFrame{
 		SpinCommand=function(self) Spin(self) end;
 	};
 };
+
+return t;
