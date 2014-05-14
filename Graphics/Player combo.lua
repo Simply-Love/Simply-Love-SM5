db802ce2e5a425024bb1cfcbda40bffb3c71a331
Dan Guzek <dguzek@gmail.com>
2014-05-13 22:09:50 -0400

    combo should zoom as it increases in count
    This also simplifies the combo logic.  This hasn't been extensively
    tested and may require slight visual adjusting in the future.

--- Graphics/Player combo.lua
+++ Graphics/Player combo.lua
@@ -2,8 +2,8 @@ local c, tc;
 local player = Var "Player";
 local ShowComboAt = THEME:GetMetric("Combo", "ShowComboAt");
 
-local NumberMinZoom = 1;
-local NumberMaxZoom = 0.8;
+local NumberMinZoom = 0.75;
+local NumberMaxZoom = 1.1;
 local NumberMaxZoomAt = THEME:GetMetric("Combo", "NumberMaxZoomAt");
 
 
@@ -26,12 +26,8 @@ local t = Def.ActorFrame {
 		Name="Number";
 		OnCommand = THEME:GetMetric("Combo", "NumberOnCommand");
 	};
+
 	LoadFont("_wendy small")..{
-		Name="Number2";
-		OnCommand = THEME:GetMetric("Combo", "NumberOnCommand");
-	};
-	LoadFont("_wendy small")..{
-		Text="Combo";
 		Name="Label";
 		InitCommand=cmd(zoom,0.25);
 		OnCommand = THEME:GetMetric("Combo", "LabelOnCommand");
@@ -49,7 +45,6 @@ local t = Def.ActorFrame {
 		local iCombo = param.Misses or param.Combo;
 		if not iCombo or iCombo < ShowComboAt then
 			c.Number:visible(false);
-			c.Number2:visible(false);
 			c.Label:visible(false);
 			return;
 		end
@@ -60,7 +55,6 @@ local t = Def.ActorFrame {
 
 		if param.Combo then
 			c.Number:diffuseshift();
-			c.Number2:diffuseshift();
 			Label:settext( "Combo" );
 			
 			bComboOrMiss = true;
@@ -68,7 +62,6 @@ local t = Def.ActorFrame {
 		elseif param.Misses then
 			Label:settext( "Misses" );
 			c.Number:stopeffect();
-			c.Number2:stopeffect();
 			
 			bComboOrMiss = true;
 			bMiss = true;
@@ -80,56 +73,44 @@ local t = Def.ActorFrame {
 
 		if bComboOrMiss then
 			c.Number:visible(true);
-			c.Number2:visible(true);
+			c.Number:zoom(param.Zoom);
 			Label:visible(true);
 		end;
 
-		c.Number:settext( string.format("%i", iCombo) );
-		c.Number2:settext( string.format("%i", iCombo) );
-		
+		c.Number:settext( string.format("%i", iCombo) );		
 
 		
 		local targetColor;
 		if param.FullComboW1 then
 			c.Number:effectcolor1(color("#C8FFFF"));
 			c.Number:effectcolor2(color("#6BF0FF"));
-			c.Number2:effectcolor1(color("#C8FFFF"));
-			c.Number2:effectcolor2(color("#6BF0FF"));
 			
 		elseif param.FullComboW2 then
 			c.Number:effectcolor1(color("#FDFFC9"));
 			c.Number:effectcolor2(color("#FDDB85"));
-			c.Number2:effectcolor1(color("#FDFFC9"));
-			c.Number2:effectcolor2(color("#FDDB85"));
 
 		elseif param.FullComboW3 then
 			c.Number:effectcolor1(color("#C9FFC9"));
 			c.Number:effectcolor2(color("#94FEC1"));
-			c.Number2:effectcolor1(color("#C9FFC9"));
-			c.Number2:effectcolor2(color("#94FEC1"));
 
 		elseif param.Combo then
 			c.Number:stopeffect();
-			c.Number2:stopeffect();
+			-- c.Number2:stopeffect();
 			targetColor = color("#FFFFFF");
 		else
 			targetColor = color("#FF0000");
 		end
-		c.Number:effectperiod(0.8);
-
-		c.Number2:blend('BlendMode_Add');
-		c.Number2:diffusealpha(0.1); -- was 0.15
-		
+		c.Number:effectperiod(0.8);		
 		Label:diffuse( color("#FFFFFF") );
 		
 	end;
 
-	JudgmentMessageCommand=function(self, param)
-		if param.Player ~= player then return end;
-		if not param.TapNoteScore then return end;
+	-- JudgmentMessageCommand=function(self, param)
+		-- if param.Player ~= player then return end;
+		-- if not param.TapNoteScore then return end;
 		-- if not UseToastyMeter then return end;
-		local tns = param.TapNoteScore;
-	end;
+		-- local tns = param.TapNoteScore;
+	-- end;
 };
 
 return t;
