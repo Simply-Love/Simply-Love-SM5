454fbfd22749ac1cbd6e16c89f129bf5fe5298fd
Dan Guzek <dguzek@gmail.com>
2014-05-14 16:47:48 -0400

    OptionsCursor cleanup
    The fewer PNGs, the better?

---
+++ Graphics/OptionsUnderlineP1 Middle.lua
@@ -0,0 +1,6 @@
+return Def.ActorFrame {	
+	Def.Quad {
+		Name="OptionsUnderlineRight";
+		InitCommand=cmd(zoomto,1,3;);
+	}
+};
---
+++ Graphics/OptionsUnderlineP1 Right.lua
@@ -0,0 +1,6 @@
+return Def.ActorFrame {	
+	Def.Quad {
+		Name="OptionsUnderlineMiddle";
+		InitCommand=cmd(zoomto,1,3;);
+	}
+};
