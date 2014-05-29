5e708353f75b3cae871b2007f6f129caec8b10e9
Dan Guzek <dguzek@gmail.com>
2014-05-29 14:40:00 -0400

    make SystemLayer behave
    As noted in my comments, most of this is copy/pasted from SM5's
    _fallback theme, with some minor aesthetic modifications.  This should
    mostly behave as expected now, though it hasn't been extensively tested.
    
    The credit text's overlap with the memorycard icons may cause me to
    look at this again in the near future.

--- BGAnimations/ScreenSystemLayer overlay.lua
+++ BGAnimations/ScreenSystemLayer overlay.lua
@@ -1,131 +1,99 @@
-local function UpdateVisible(self)
-	local screen = SCREENMAN:GetTopScreen();
-	local bShow = true;
-	if screen then
-		local sClass = screen:GetName();
-		bShow = THEME:GetMetric( sClass, "ShowCoinsAndCredits" );
-	end
-
-	self:visible( bShow );
-end
+-- This is mostly copy/pasted directly from SM5's _fallback theme with
+-- very minor modifications.
 
 local function CreditsText( pn )
-	function update(self)
-		local str = ScreenSystemLayerHelpers.GetCreditsMessage(pn);
-		local screen = SCREENMAN:GetTopScreen();
-		
-		if screen then
-			bAttract = screen:GetScreenType() == "ScreenType_Attract";
-		end
-		
-		if not GAMESTATE:IsSideJoined(pn) and not bAttract and GAMESTATE:EnoughCreditsToJoin() then
-			str = THEME:GetString("ScreenSystemLayer", "CreditsPressStart");
-		end
-			
-		if MEMCARDMAN:GetCardState(pn) ~= "MemoryCardState_none" then
-			if pn == PLAYER_1 then
-				self:x(56);
-			elseif pn == PLAYER_2 then
-				self:x(-56);
-			end
-		end
-			
-		self:settext(str);
-	end
-
-	local text = LoadFont(Var "LoadingScreen","credits") .. {
-		InitCommand=cmd(diffuse,color("#FFFFFF"););
-		RefreshCreditTextMessageCommand=update;
-		CoinInsertedMessageCommand=update;
-		PlayerJoinedMessageCommand=update;
-		ScreenChangedMessageCommand=function(self)
+	local text = LoadFont("_misoreg hires") .. {
+		InitCommand=function(self)
+			self:name("Credits" .. PlayerNumberToString(pn))
+			ActorUtil.LoadAllCommandsAndSetXY(self,Var "LoadingScreen");
+		end;
+		UpdateTextCommand=function(self)
+			local str = ScreenSystemLayerHelpers.GetCreditsMessage(pn);
+			self:settext(str);
+		end;
+		UpdateVisibleCommand=function(self)
 			local screen = SCREENMAN:GetTopScreen();
 			local bShow = true;
-			
 			if screen then
-				local sName = screen:GetName();
-				bShow = THEME:GetMetric( sName, "ShowCoinsAndCredits" );
+				local sClass = screen:GetName();
+				bShow = THEME:GetMetric( sClass, "ShowCreditDisplay" );
 			end
-			
-			self:visible(bShow);
+
+			self:visible( bShow );
 		end
 	};
 	return text;
-end
+end;
 
 
 
-local t = Def.ActorFrame {
-	Def.ActorFrame{
-		Def.Quad{
-			InitCommand=cmd(x,THEME:GetMetric(Var "LoadingScreen","MessageFrameX");y,THEME:GetMetric(Var "LoadingScreen","MessageFrameY");zoomto,SCREEN_WIDTH,SCREEN_HEIGHT/8; diffuse,Color.Black; diffusealpha,0);
-			OnCommand=cmd(draworder,-1);
-			SystemMessageMessageCommand=function(self,params)
-				self:finishtweening();
-				self:linear(0.5);
-				self:diffusealpha(0.5);
-				self:sleep(2.75);
-				self:linear(0.25);
-				self:diffusealpha(0);
-			end;
-			HideSystemMessageMessageCommand=cmd(finishtweening);
-		};
+local t = Def.ActorFrame {}
 
-		LoadFont("_misoreg hires") .. {
-			InitCommand=cmd(
-				maxwidth,750;
-				horizalign,left;
-				vertalign,top;
-				zoom,0.8;
-				shadowlength,1;
-				y,SCREEN_TOP+20;
-				diffusealpha,0;
-				strokecolor,color("0,0,0,0.25");
-			);
 
-			SystemMessageMessageCommand = function(self, params)
-				self:settext( params.Message );
-				local f = cmd(finishtweening;x,SCREEN_LEFT+20;diffusealpha,1); f(self);
-				self:playcommand( "On" );
-				if params.NoAnimate then
-					self:finishtweening();
-				end
-				f = cmd(sleep,3;linear,0.25;diffusealpha,0); f(self);
-				self:playcommand( "Off" );
-			end;
-			HideSystemMessageMessageCommand = cmd(finishtweening);
-		};
+-- Aux
+t[#t+1] = LoadActor(THEME:GetPathB("ScreenSystemLayer","aux"));
+
+
+-- Credits
+t[#t+1] = Def.ActorFrame {
+ 	CreditsText( PLAYER_1 );
+	CreditsText( PLAYER_2 ); 
+};
+
+
+-- SystemMessage Text
+t[#t+1] = Def.ActorFrame {
+	Def.Quad {
+		InitCommand=cmd(zoomtowidth,SCREEN_WIDTH;zoomtoheight,30;horizalign,left;vertalign,top;y,SCREEN_TOP;diffuse,color("0,0,0,0"));
+		OnCommand=cmd(finishtweening;diffusealpha,0.85;);
+		OffCommand=cmd(sleep,3;linear,0.5;diffusealpha,0;);
 	};
+	LoadFont("_misoreg hires") .. {
+		Name="Text";
+		InitCommand=cmd(maxwidth,750; horizalign,left; vertalign,top; xy,SCREEN_LEFT+10, 10; diffusealpha,0;);
+		OnCommand=cmd(finishtweening;diffusealpha,1;zoom,0.8);
+		OffCommand=cmd(sleep,3;linear,0.5;diffusealpha,0;);
+	};
+	SystemMessageMessageCommand = function(self, params)
+		self:GetChild("Text"):settext( params.Message );
+		self:playcommand( "On" );
+		if params.NoAnimate then
+			self:finishtweening();
+		end
+		self:playcommand( "Off" );
+	end;
+	HideSystemMessageMessageCommand = cmd(finishtweening);
 };
 
-
-t[#t+1] = CreditsText( PLAYER_1 ) .. {
-	InitCommand=cmd(x,THEME:GetMetric(Var "LoadingScreen","CreditsP1X");
-					y,THEME:GetMetric(Var "LoadingScreen","CreditsP1Y");
-					horizalign,left;shadowlength,0;strokecolor,color("0,0,0,0.5"););
-};
-
-t[#t+1] = CreditsText( PLAYER_2 ) .. {
-	InitCommand=cmd(x,THEME:GetMetric(Var "LoadingScreen","CreditsP2X");
-					y,THEME:GetMetric(Var "LoadingScreen","CreditsP2Y");
-					horizalign,right;shadowlength,0;strokecolor,color("0,0,0,0.5"));
-};
-
-
-
+-- Centered Credit Text
 t[#t+1] = LoadFont("_wendy small")..{
 	InitCommand=cmd(x,SCREEN_CENTER_X;
-					y,THEME:GetMetric(Var "LoadingScreen","CreditsP2Y");
+					y,SCREEN_BOTTOM-16;
 					zoom,0.5;horizalign,center;
 	);
 	OnCommand=cmd(queuecommand,"Refresh");
 	ScreenChangedMessageCommand=function(self)
-		UpdateVisible(self);
 		self:queuecommand("Refresh");
 	end;
 	CoinModeChangedMessageCommand=cmd(playcommand,"Refresh");
 	CoinsChangedMessageCommand=cmd(playcommand,"Refresh");
 	RefreshCommand=function(self)
+		
+		local screen = SCREENMAN:GetTopScreen();
+		local bShow = true;
+		if screen then
+			local sClass = screen:GetName();
+			bShow = THEME:GetMetric( sClass, "ShowCreditDisplay" );
+			
+			-- hide this centered credit text for certain screens,
+			-- where it would more likely just be distracting and superfluous
+			if sClass == "ScreenGameplay" or sClass == "ScreenPlayerOptions" or sClass == "ScreenStageInformation" or sClass == "ScreenEvaluationStage" or sClass == "ScreenEvaluationCourse" or sClass == "ScreenEvaluationSummary" or sClass == "ScreenNameEntryTraditional"  then
+				bShow = false
+			end
+		end
+
+		self:visible( bShow );
+
 		if GAMESTATE:IsEventMode() then
 			self:settext('EVENT MODE');
 		elseif GAMESTATE:GetCoinMode()== "CoinMode_Free" then
--- BGAnimations/ScreenTitleJoin overlay.lua
+++ BGAnimations/ScreenTitleJoin overlay.lua
@@ -4,7 +4,7 @@ return Def.ActorFrame{
 		InitCommand=cmd(queuecommand,"Refresh");
 		OnCommand=cmd(xy,SCREEN_CENTER_X,SCREEN_BOTTOM-100; zoom,0.95; diffuseblink; effectperiod,0.5; effectcolor1,1,1,1,0; effectcolor2,1,1,1,1);
 		CoinsChangedMessageCommand=cmd(playcommand,"Refresh");
-		RefreshCommand=function(self)
+		RefreshCommand=function(self)			
 			if GAMESTATE:GetCoinMode() == "CoinMode_Pay" then
 				local Credits = GetCredits();
 				if Credits["Credits"] < 1 then
