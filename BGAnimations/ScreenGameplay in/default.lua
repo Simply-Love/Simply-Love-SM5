66327027f03d586cf24d5d65c8ac5a21abdbfd91
Dan Guzek <dguzek@gmail.com>
2014-07-17 19:01:15 -0400

    minor cleanup to Gameplay in

--- BGAnimations/ScreenGameplay in/default.lua
+++ BGAnimations/ScreenGameplay in/default.lua
@@ -1,38 +1,38 @@
-local sStage = "";
+local sStage = ""
 
-if not PREFSMAN:GetPreference("EventMode") then
+if not GAMESTATE:IsEventMode() then
 	
-	local CurrentSong = GAMESTATE:GetCurrentSong();
-	local bIsLong = CurrentSong:IsLong();
-	local bIsMarathon = CurrentSong:IsMarathon();
-	local iAdditionalStagesThisSongCountsFor = bIsLong and 1 or bIsMarathon and 2 or 0;
+	local CurrentSong = GAMESTATE:GetCurrentSong()
+	local bIsLong = CurrentSong:IsLong()
+	local bIsMarathon = CurrentSong:IsMarathon()
+	local iAdditionalStagesThisSongCountsFor = bIsLong and 1 or bIsMarathon and 2 or 0
 
 	-- GAMESTATE:GetNumStagesLeft() asks for a playernumber because latejoin can
 	-- cause discrepencies between players.  For Simply Love, we only care about
 	-- which player has fewer stages remaining; we use that value for both.
-	local iStagesLeft = GAMESTATE:GetSmallestNumStagesLeftForAnyHumanPlayer();
+	local iStagesLeft = GAMESTATE:GetSmallestNumStagesLeftForAnyHumanPlayer()
 
-	local iSongsPerPlay = PREFSMAN:GetPreference("SongsPerPlay");
-	local iStageNumber = (iSongsPerPlay - iStagesLeft) + 1;
+	local iSongsPerPlay = PREFSMAN:GetPreference("SongsPerPlay")
+	local iStageNumber = (iSongsPerPlay - iStagesLeft) + 1
 
-	sStage = THEME:GetString("Stage", "Stage") .. " " .. tostring(iStageNumber);
+	sStage = THEME:GetString("Stage", "Stage") .. " " .. tostring(iStageNumber)
 
 	if iStageNumber + iAdditionalStagesThisSongCountsFor >= iSongsPerPlay then
-		sStage = THEME:GetString("Stage", "Final");
+		sStage = THEME:GetString("Stage", "Final")
 	end
 	
 else
-	sStage = THEME:GetString("Stage", "Event");
+	sStage = THEME:GetString("Stage", "Event")
 end
 
 
 
-local t = Def.ActorFrame {};
+local t = Def.ActorFrame {}
 
 t[#t+1] = Def.ActorFrame{
 
 	Def.Quad{
-		InitCommand=cmd(diffuse,color("0,0,0,1"); Center; FullScreen;);
+		InitCommand=cmd(diffuse,Color.Black; Center; FullScreen;);
 		OnCommand=cmd(sleep,1.4; accelerate,0.6; diffusealpha,0;);
 	};
 	
@@ -52,11 +52,8 @@ t[#t+1] = Def.ActorFrame{
 	
 	LoadFont("_wendy small")..{
 		InitCommand=cmd(Center; diffusealpha,0; shadowlength,1);
-		OnCommand=cmd(settext, sStage; accelerate, 0.5; diffusealpha, 1; sleep, 0.66; accelerate, 0.33; zoom, 0.4; y, SCREEN_HEIGHT-30);
+		OnCommand=cmd(settext, sStage; accelerate, 0.5; diffusealpha, 1; sleep, 0.66; accelerate, 0.33; zoom, 0.4; y, _screen.h-30);
 	};
-};
-
-
-
+}
 
 return t;
