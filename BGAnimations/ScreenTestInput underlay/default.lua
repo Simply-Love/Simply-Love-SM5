55954950447af5d8abe0b079b94adc6cfc19ea69
Dan Guzek <dguzek@gmail.com>
2014-07-17 21:16:50 -0400

    rework ScreenTestInput

--- BGAnimations/ScreenTestInput underlay/default.lua
+++ BGAnimations/ScreenTestInput underlay/default.lua
@@ -1,86 +1,25 @@
-local GameButtons = {
-	"Left", "Right", "Up", "Down",
-	"UpRight", "UpLeft", "DownRight", "DownLeft", "Center",
-	"Start", "Select", "MenuRight", "MenuLeft",
-};
-
-
-
-local function ProcessInputs(inputList)
-	local state = {{},{}};
-	
-	-- initialize state to be full of "Off" values
-	for i=1,2 do
-		for k,panel in ipairs(GameButtons) do
-			state[i][panel] =  "Off";
-		end
+local function input(event)
+	if not event.PlayerNumber or not event.button then
+		return false
 	end
-				
-				
-	-- 	loop through our list of input strings			
-	-- 	setting the corresponding state value to "On"
-	for i,v in ipairs(inputList) do
+	
+	local state = "Off"
+	if event.type ~= "InputEventType_Release" then
+		state = "On"		
+	end
 		
-		local temp = {};
-	
-		-- break each input string into three parts
-		-- ["Controller"] [pn] [button]
-		for input in string.gmatch(inputList[i], "[^- ]+") do
-			temp[#temp+1] = input;
-	    end
-		
-		local pn = tonumber(temp[2]);
-		local activeButton = tostring(temp[3]);
-				
-		-- an example here might be
-		--state[1]["Right"] = "On"
-		state[pn][activeButton] = "On";
-	end
-
-
-	-- broadcast EVERYTHING, whether it is On or Off
-	-- an example Message to be interpretted by ./visuals/default.lua might look like...
-	-- Player2CenterOffMessageCommand
-	for i=1,2 do		
-		for k,button in ipairs(GameButtons) do
-			MESSAGEMAN:Broadcast("Player"..i..button..state[i][button]);
-		end
-	end
-	
+	MESSAGEMAN:Broadcast(ToEnumShortString(event.PlayerNumber) .. event.button .. state)
+	return false
 end
 
 
 
-
 return Def.ActorFrame {
-	
+	InitCommand=cmd(queuecommand,"Capture");
+	CaptureCommand=function(self)
+		SCREENMAN:GetTopScreen():AddInputCallback(input)
+	end;
 	OffCommand=cmd(sleep,0.4);
-	
-	-- We use this to grab input strings from
-	Def.InputList {
-		Name="InputList";
-		Font=THEME:GetPathF("Common","Normal");
-		-- but it's distracting, so hide it
-		InitCommand=cmd(diffusealpha,0);
-		OnCommand=cmd(queuecommand,"Update");
-		UpdateCommand=function(self)
-			local inputList = {};
-			
-			local text = self:GetText();
-			
-			-- match for the pattern we are looking for
-			-- Controller [pn] [button]
-			-- and insert them all into a table
-			for input in string.gmatch(text, "Controller %d %w+") do
-				inputList[#inputList+1] = input;
-		    end
-
-			ProcessInputs(inputList);
-			
-			self:sleep(1/30);
-			self:queuecommand("Update");
-		end;
-	};
 	
 	Def.DeviceList {
 		Font=THEME:GetPathF("","_misoreg hires");
--- BGAnimations/ScreenTestInput underlay/visuals/default.lua
+++ BGAnimations/ScreenTestInput underlay/visuals/default.lua
@@ -1,234 +1,63 @@
-local t = Def.ActorFrame{};
-local game = GAMESTATE:GetCurrentGame():GetName();
+local t = Def.ActorFrame{}
+local game = GAMESTATE:GetCurrentGame():GetName()
+local Players = GAMESTATE:GetHumanPlayers()
 
+local Highlights = {
+	Start={		x=0, 	y=66, 	rotationz=0, 	zoom=0.5, graphic="highlightgreen.png" },
+	Select={	x=0, 	y=95,	rotationz=180, 	zoom=0.5, graphic="highlightred.png" },
+	MenuRight={	x=37, 	y=80, 	rotationz=0, 	zoom=0.5, graphic="highlightarrow.png" },
+	MenuLeft={	x=-37, 	y=80, 	rotationz=180,	zoom=0.5, graphic="highlightarrow.png" },
+	
+	UpLeft={	x=-67,	y=-148, rotationz=0,	zoom=0.8, graphic="highlight.png" },	
+	Up={		x=0, 	y=-148, rotationz=0,	zoom=0.8, graphic="highlight.png" },
+	UpRight={	x=67, 	y=-148, rotationz=0,	zoom=0.8, graphic="highlight.png" },
+	
+	Left={		x=-67,	y=-80,	rotationz=0,	zoom=0.8, graphic="highlight.png" },
+	Center={	x=0, 	y=-80,	rotationz=0,	zoom=0.8, graphic="highlight.png" },
+	Right={		x=67, 	y=-80,	rotationz=0,	zoom=0.8, graphic="highlight.png" },
 
---PLAYER_1
-t[#t+1] = Def.ActorFrame{
-	Name="Player1Buttons";
-	InitCommand=cmd(diffusealpha,0);
-	OnCommand=cmd(linear,0.3;diffusealpha,1);
-	OffCommand=cmd(linear,0.2;diffusealpha,0);
-	
-	LoadFont("_wendy small")..{
-		Text="PLAYER 1";
-		InitCommand=cmd(xy,_screen.cx-150,30;zoom,0.7)
-	};
-	
-	LoadActor(game..".png")..{
-		InitCommand=cmd(xy,_screen.cx-150,_screen.cy-80; zoom,0.8);
-	};
-	LoadActor("buttons.png")..{
-		InitCommand=cmd(xy,_screen.cx-150,_screen.cy+80; zoom,0.5);
-	};
-	
-	
-	
-	--menuleft highlight
-	LoadActor("highlightarrow.png")..{
-		InitCommand=cmd(xy,_screen.cx-187,_screen.cy+80; rotationz,180; zoom, 0.5;);
-		Player1MenuLeftOnMessageCommand=cmd(diffusealpha,1;);
-		Player1MenuLeftOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--menuright highlight
-	LoadActor("highlightarrow.png")..{
-		InitCommand=cmd(xy,_screen.cx-113,_screen.cy+80; zoom, 0.5;);
-		Player1MenuRightOnMessageCommand=cmd(diffusealpha,1;);
-		Player1MenuRightOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--start highlight
-	LoadActor("highlightgreen.png")..{
-		InitCommand=cmd(xy,_screen.cx-150,_screen.cy+66; zoom, 0.5;);
-		Player1StartOnMessageCommand=cmd(diffusealpha,1;);
-		Player1StartOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--select highlight
-	LoadActor("highlightred.png")..{
-		InitCommand=cmd(xy,_screen.cx-150,_screen.cy+94.5;rotationz,180; zoom, 0.5;);
-		Player1SelectOnMessageCommand=cmd(diffusealpha,1;);
-		Player1SelectOffMessageCommand=cmd(diffusealpha,0);
-	};
+	DownLeft={	x=-67,	y=-12,	rotationz=0,	zoom=0.8, graphic="highlight.png" },	
+	Down={		x=0,	y=-12,	rotationz=0,	zoom=0.8, graphic="highlight.png" },
+	DownRight={	x=67, 	y=-12,	rotationz=0,	zoom=0.8, graphic="highlight.png" }
+}
 
+for pn in ivalues(Players) do
 	
+ 	local PlayerPad = Def.ActorFrame{
+		InitCommand=function(self)
+			self:diffusealpha(0);
+			if pn == PLAYER_1 then
+				self:x(_screen.cx-150);
+			elseif pn == PLAYER_2 then
+				self:x(_screen.cx+150);
+			end
+			self:y(_screen.cy);
+		end;
+		OnCommand=cmd(linear,0.3;diffusealpha,1);
+		OffCommand=cmd(linear,0.2;diffusealpha,0);
 	
+		LoadFont("_wendy small")..{
+			Text="PLAYER "..ToEnumShortString(pn):gsub("P","");
+			InitCommand=cmd(y,-210;zoom,0.7)
+		};
+		LoadActor(game..".png")..{
+			InitCommand=cmd(y,-80; zoom,0.8);
+		};
+		LoadActor("buttons.png")..{
+			InitCommand=cmd(y,80; zoom,0.5);
+		};
+	}
 	
-	--upleft highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx-217,_screen.cy-148; zoom, 0.8; );
-		Player1UpLeftOnMessageCommand=cmd(diffusealpha,1;);
-		Player1UpLeftOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--up highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx-150,_screen.cy-148; zoom, 0.8; );
-		Player1UpOnMessageCommand=cmd(diffusealpha,1;);
-		Player1UpOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--upright highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx-83,_screen.cy-148; zoom, 0.8; );
-		Player1UpRightOnMessageCommand=cmd(diffusealpha,1;);
-		Player1UpRightOffMessageCommand=cmd(diffusealpha,0);
-	};
+	for panel,values in pairs(Highlights) do
+		PlayerPad[#PlayerPad+1] = LoadActor( values.graphic )..{
+			InitCommand=cmd(xy,values.x, values.y; rotationz,values.rotationz; zoom, values.zoom; diffusealpha,0);
+			[ToEnumShortString(pn) .. panel .."OnMessageCommand"]=cmd(diffusealpha,1);
+			[ToEnumShortString(pn) .. panel .."OffMessageCommand"]=cmd(diffusealpha,0);
+		}
+	end
 	
+	t[#t+1] = PlayerPad
 	
-
-	--left highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx-217,_screen.cy-80; zoom, 0.8; );
-		Player1LeftOnMessageCommand=cmd(diffusealpha,1);
-		Player1LeftOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--center highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx-150,_screen.cy-80; zoom, 0.8; );
-		Player1CenterOnMessageCommand=cmd(diffusealpha,1);
-		Player1CenterOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--right highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx-83,_screen.cy-80; zoom, 0.8; );
-		Player1RightOnMessageCommand=cmd(diffusealpha,1);
-		Player1RightOffMessageCommand=cmd(diffusealpha,0);
-	};
-
-
-
-	-- downleft highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx-217,_screen.cy-12; zoom, 0.8; );
-		Player1DownLeftOnMessageCommand=cmd(diffusealpha,1);
-		Player1DownLeftOffMessageCommand=cmd(diffusealpha,0);
-	};
-	-- down highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx-150,_screen.cy-12; zoom, 0.8; );
-		Player1DownOnMessageCommand=cmd(diffusealpha,1);
-		Player1DownOffMessageCommand=cmd(diffusealpha,0);
-	};
-	-- downright highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx-83,_screen.cy-12; zoom, 0.8; );
-		Player1DownRightOnMessageCommand=cmd(diffusealpha,1);
-		Player1DownRightOffMessageCommand=cmd(diffusealpha,0);
-	};
-
-};
-
-
-
-
-
-
-
-
-
---PLAYER_2
-t[#t+1] = Def.ActorFrame{
-	Name="Player2Buttons";
-	InitCommand=cmd(diffusealpha,0);
-	OnCommand=cmd(linear,0.3;diffusealpha,1);
-	OffCommand=cmd(linear,0.2;diffusealpha,0);
-		
-	LoadFont("_wendy small")..{
-		Text="PLAYER 2";
-		InitCommand=cmd(xy,_screen.cx+150,30;zoom,0.7)
-	};
-	
-	LoadActor(game..".png")..{
-		InitCommand=cmd(xy,_screen.cx+150,_screen.cy-80; zoom,0.8);
-	};
-	LoadActor("buttons.png")..{
-		InitCommand=cmd(xy,_screen.cx+150,_screen.cy+80; zoom,0.5);
-	};
-	
-	
-	
-	--menuleft highlight
-	LoadActor("highlightarrow.png")..{
-		InitCommand=cmd(xy,_screen.cx+113,_screen.cy+80;rotationz,180; zoom, 0.5);
-		Player2MenuLeftOnMessageCommand=cmd(diffusealpha,1;);
-		Player2MenuLeftOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--menuright highlight
-	LoadActor("highlightarrow.png")..{
-		InitCommand=cmd(xy,_screen.cx+187,_screen.cy+80; zoom, 0.5);
-		Player2MenuRightOnMessageCommand=cmd(diffusealpha,1;);
-		Player2MenuRightOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--start highlight
-	LoadActor("highlightgreen.png")..{
-		InitCommand=cmd(xy,_screen.cx+150,_screen.cy+66; zoom, 0.5);
-		Player2StartOnMessageCommand=cmd(diffusealpha,1;);
-		Player2StartOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--select highlight
-	LoadActor("highlightred.png")..{
-		InitCommand=cmd(xy,_screen.cx+150,_screen.cy+94.5; zoom, 0.5);
-		Player2SelectOnMessageCommand=cmd(diffusealpha,1;);
-		Player2SelectOffMessageCommand=cmd(diffusealpha,0);
-	};
-	
-	
-	--upleft highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx+83,_screen.cy-148; zoom,0.8; );
-		Player2UpLeftOnMessageCommand=cmd(diffusealpha,1;);
-		Player2UpLeftOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--up highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx+150,_screen.cy-148; zoom,0.8; );
-		Player2UpOnMessageCommand=cmd(diffusealpha,1;);
-		Player2UpOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--upright highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx+217,_screen.cy-148; zoom,0.8; );
-		Player2UpRightOnMessageCommand=cmd(diffusealpha,1;);
-		Player2UpRightOffMessageCommand=cmd(diffusealpha,0);
-	};
-	
-	
-	
-	--left highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx+83,_screen.cy-80; zoom,0.8; );
-		Player2LeftOnMessageCommand=cmd(diffusealpha,1;);
-		Player2LeftOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--center highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx+150,_screen.cy-80; zoom,0.8; );
-		Player2CenterOnMessageCommand=cmd(diffusealpha,1;);
-		Player2CenterOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--right highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx+217,_screen.cy-80; zoom,0.8; );
-		Player2RightOnMessageCommand=cmd(diffusealpha,1;);
-		Player2RightOffMessageCommand=cmd(diffusealpha,0);
-	};
-	
-	
-	
-	--down highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx+83,_screen.cy-12; zoom,0.8; );
-		Player2DownLeftOnMessageCommand=cmd(diffusealpha,1;);
-		Player2DownLeftOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--down highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx+150,_screen.cy-12; zoom,0.8; );
-		Player2DownOnMessageCommand=cmd(diffusealpha,1;);
-		Player2DownOffMessageCommand=cmd(diffusealpha,0);
-	};
-	--downright highlight
-	LoadActor("highlight.png")..{
-		InitCommand=cmd(xy,_screen.cx+217,_screen.cy-12; zoom,0.8; );
-		Player2DownRightOnMessageCommand=cmd(diffusealpha,1;);
-		Player2DownRightOffMessageCommand=cmd(diffusealpha,0);
-	};
-};
+end
 
 return t;
