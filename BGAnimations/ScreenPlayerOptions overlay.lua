c7eaa53e6d416aa02ebe923a7b0c7f300a180a56
Dan Guzek <dguzek@gmail.com>
2014-07-22 17:31:09 -0400

    make ScreenPlayerOptions more DRY
    This creates a "ScreenPlayerOptions common" that contains code shared by PlayerOptions and PlayerOptions2.  Previously, the code was duplicated across the two.

--- BGAnimations/ScreenPlayerOptions overlay.lua
+++ BGAnimations/ScreenPlayerOptions overlay.lua
@@ -1,5 +1,4 @@
 local Players = GAMESTATE:GetHumanPlayers()
-local ScreenOptions
 
 -- SpeedModItems is a table that will contain the BitMapText Actors
 -- for the SpeedModNew OptionRow for both P1 and P2
@@ -11,7 +10,7 @@ local t = Def.ActorFrame{
 	OffCommand=cmd(linear,0.2;diffusealpha,0);
 	CaptureCommand=function(self)
 		
-		ScreenOptions = SCREENMAN:GetTopScreen()
+		local ScreenOptions = SCREENMAN:GetTopScreen()
 		
 		-- reset for ScreenEditOptions
 		SpeedModItems = {P1 = nil, P2 = nil}
@@ -26,54 +25,10 @@ local t = Def.ActorFrame{
 		if SpeedModItems.P2 and GAMESTATE:IsPlayerEnabled(PLAYER_2) then
 			self:playcommand("SetP2")
 		end
-	end;
-	
-	-- this is broadcast from [OptionRow] TitleGainFocusCommand in metrics.ini
-	-- we use it to color the active OptionRow's title appropriately by PlayerColor()
-	OptionRowChangedMessageCommand=function(self, params)
-			
-		local CurrentRowIndex = {P1, P2}
-	
-		-- There is always the possibility that a diffuseshift is still active;
-		-- cancel it now (and re-apply below, if applicable).
-		params.Title:stopeffect()
-	
-		-- if ScreenOptions is nil, get it now
-		if not ScreenOptions then
-			ScreenOptions = SCREENMAN:GetTopScreen()
-		else			
-			-- get the index of PLAYER_1's current row
-			if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
-				CurrentRowIndex.P1 = ScreenOptions:GetCurrentRowIndex(PLAYER_1)
-			end
-		
-			-- get the index of PLAYER_2's current row
-			if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
-				CurrentRowIndex.P2 = ScreenOptions:GetCurrentRowIndex(PLAYER_2)
-			end
-		end
-		
-		local optionRow = params.Title:GetParent():GetParent();
-	
-		-- color the active optionrow's title appropriately
-		if optionRow:HasFocus(PLAYER_1) then
-			params.Title:diffuse(PlayerColor(PLAYER_1))
-		end
-	
-		if optionRow:HasFocus(PLAYER_2) then
-			params.Title:diffuse(PlayerColor(PLAYER_2))
-		end
-		
-		if CurrentRowIndex.P1 and CurrentRowIndex.P2 then
-			if CurrentRowIndex.P1 == CurrentRowIndex.P2 then
-				params.Title:diffuseshift()
-				params.Title:effectcolor1(PlayerColor(PLAYER_1))
-				params.Title:effectcolor2(PlayerColor(PLAYER_2))
-			end
-		end
-
 	end
 }
+
+t[#t+1] = LoadActor(THEME:GetPathB("ScreenPlayerOptions", "common"));
 
 
 for player in ivalues(Players) do
