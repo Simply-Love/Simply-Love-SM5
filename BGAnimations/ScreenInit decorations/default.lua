530f4be2e791b3b607050b2b64b848b08886e533
Dan Guzek <dguzek@gmail.com>
2014-07-30 18:18:54 -0400

    version bump

--- BGAnimations/ScreenInit decorations/default.lua
+++ BGAnimations/ScreenInit decorations/default.lua
@@ -1,4 +1,4 @@
-local slc = SimplyLoveColor();
+local slc = SimplyLoveColor()
 local arrowData = {
 	sleep = {0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9},
 	xPos = {-150, -100, -50, 0, 50, 100, 150},
@@ -8,41 +8,37 @@ local arrowData = {
 local function RainbowArrows( x )
 	
 	return Def.ActorFrame {
-		InitCommand=cmd(Center);
+		InitCommand=cmd(Center),
 		
 		LoadActor("white_logo.png")..{
-			InitCommand=cmd(zoom,0.1; diffuse, GetHexColor(arrowData.color[x]%12+1); diffusealpha,0; x, arrowData.xPos[x] );
-			OnCommand=cmd(sleep,arrowData.sleep[x]; linear,0.75; diffusealpha,1; linear,0.75;diffusealpha,0);
-		};
+			InitCommand=cmd(zoom,0.1; diffuse, GetHexColor(arrowData.color[x]%12+1); diffusealpha,0; x, arrowData.xPos[x] ),
+			OnCommand=cmd(sleep,arrowData.sleep[x]; linear,0.75; diffusealpha,1; linear,0.75;diffusealpha,0)
+		},
 		LoadActor("highlight.png")..{
-			InitCommand=cmd(zoom,0.1; diffusealpha,0; x, arrowData.xPos[x]);
-			OnCommand=cmd(sleep,arrowData.sleep[x]; linear,0.75; diffusealpha,0.75; linear,0.75;diffusealpha,0);
-		};
-	};
-	
+			InitCommand=cmd(zoom,0.1; diffusealpha,0; x, arrowData.xPos[x]),
+			OnCommand=cmd(sleep,arrowData.sleep[x]; linear,0.75; diffusealpha,0.75; linear,0.75;diffusealpha,0)
+		}
+	}
 end
 
-
-
-local t = Def.ActorFrame {};
-
+local t = Def.ActorFrame {}
 
 t[#t+1] = Def.ActorFrame {
-	InitCommand=cmd(Center);
+	InitCommand=cmd(Center),
 
 	Def.Quad {
-		InitCommand=cmd(zoomto,_screen.w,0; diffuse, Color.Black);
-		OnCommand=cmd( accelerate,0.3; zoomtoheight,128; diffusealpha,0.9; sleep,2.5; linear,0.25);
-		OffCommand=cmd(accelerate,0.3; zoomtoheight,0;)
-	};
+		InitCommand=cmd(zoomto,_screen.w,0; diffuse, Color.Black),
+		OnCommand=cmd( accelerate,0.3; zoomtoheight,128; diffusealpha,0.9; sleep,2.5; linear,0.25),
+		OffCommand=cmd(accelerate,0.3; zoomtoheight,0)
+	},
   		
 	LoadFont("_misoreg hires")..{
-		Text="Theme by " .. THEME:GetThemeAuthor();
-		InitCommand=cmd(diffuse,GetCurrentColor(); diffusealpha,0;);
-		OnCommand=cmd(sleep,3;linear,0.25;diffusealpha,1);
-		OffCommand=cmd(linear, 0.25; diffusealpha,0);
-	};
-};
+		Text="theme by " .. THEME:GetThemeAuthor(),
+		InitCommand=cmd(diffuse,GetCurrentColor(); diffusealpha,0;),
+		OnCommand=cmd(sleep,3;linear,0.25;diffusealpha,1),
+		OffCommand=cmd(linear, 0.25; diffusealpha,0)
+	}
+}
 
 for i=1,7 do
 	t[#t+1] = RainbowArrows(i)
--- Scripts/00 themeInfo.lua
+++ Scripts/00 themeInfo.lua
@@ -2,7 +2,7 @@
 themeInfo = {
 	ProductCode = "SL-001",
 	Name = "Simply Love",
-	Version = 0.4,
-	FriendlyVersion = "Release Candidate 1",
-	Date = "20140330 21:10",
-};
+	Version = 0.5,
+	FriendlyVersion = "Release Candidate 2",
+	Date = "20140730 18:12",
+}
