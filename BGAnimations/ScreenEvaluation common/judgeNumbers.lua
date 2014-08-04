b75fd622f6409bae1c5b8ffd3d80cfd64ae9f4e2
Dan Guzek <dguzek@gmail.com>
2014-08-04 00:58:41 -0400

    use RollingNumbers for ScreenEval
    The system I had been using was hackish at best.  This is more convenient and less likely to break.

--- BGAnimations/ScreenEvaluation common/judgeNumbers.lua
+++ BGAnimations/ScreenEvaluation common/judgeNumbers.lua
@@ -1,4 +1,4 @@
-local pn = ...;
+local pn = ...
 
 local TNSTypes = {
 	'TapNoteScore_W1',
@@ -7,139 +7,93 @@ local TNSTypes = {
 	'TapNoteScore_W4',
 	'TapNoteScore_W5',
 	'TapNoteScore_Miss'
-};
+}
 
-local labels2_RC = {'RadarCategory_Holds', 'RadarCategory_Mines', 'RadarCategory_Hands', 'RadarCategory_Rolls' };
+local labels2_RC = {'RadarCategory_Holds', 'RadarCategory_Mines', 'RadarCategory_Hands', 'RadarCategory_Rolls' }
 
+local n = Def.ActorFrame{
+	InitCommand=cmd(visible, GAMESTATE:IsPlayerEnabled(pn))
+}
+local stats = STATSMAN:GetCurStageStats():GetPlayerStageStats(pn)
 
-local function paddingZeros(number, howManyDigits, xpos, ypos, pn)
-
-	return LoadFont("_ScreenEvaluation numbers")..{
-		InitCommand=cmd(zoom,0.5; x, xpos; y, ypos; diffuse,color("#5A6166"); diffusealpha,0.75; horizalign, left;);
-		BeginCommand=function(self)
-						
-			local zeros = "";
-			
-			if howManyDigits == 4 then
-				if number < 10 then
-					zeros = "000";
-				elseif number >= 10 and number< 100 then
-					zeros = "00";
-				elseif number >= 100 and number < 1000 then
-					zeros = "0";
-				end
-			elseif howManyDigits == 3 then
-				if number < 10 then
-					zeros = "00";
-				elseif number >= 10 and number < 100 then
-					zeros = "0";
-				end
-			else
-				zeros = "";
-			end
-			
-			self:settext(zeros);
-			self:visible( GAMESTATE:IsPlayerEnabled(pn) );
-		end;
-	};
-end
-
-
-
-local n = Def.ActorFrame{};
-local stats = STATSMAN:GetCurStageStats():GetPlayerStageStats(pn);
-
-local performance_x1,paddng_x1,performance_x2,slash_x,possible_x,padding_x2,padding_x3;
+local performance_x1,paddng_x1,performance_x2,slash_x,possible_x,padding_x2,padding_x3
 
 -- this function is no longer modular; sorry about that, AJ :(
 -- ( There are definitely better ways to do this, but I'm lazy. )
 if pn == PLAYER_1 then
-	performance_x1 = 64;
-	paddng_x1 = -9;
-	performance_x2 = -180;
-	padding_x2 = -234
-	slash_x = -168;	
-	possible_x = -114;
-	padding_x3 = -168;
+	performance_x1 = 64
+	performance_x2 = -180
+	slash_x = -168
+	possible_x = -114
 elseif pn == PLAYER_2 then
-	performance_x1 = 94;
-	paddng_x1 = 20;	
-	performance_x2 = 218;
-	padding_x2 = 164;
-	slash_x = 230;
-	possible_x = 286;
-	padding_x3 = 230;
+	performance_x1 = 94
+	performance_x2 = 218
+	slash_x = 230
+	possible_x = 286
 end	
 
 
 -- do the normals first
 for i=1,#TNSTypes do
 	
-	local number = stats:GetTapNoteScores(TNSTypes[i]);
+	local number = stats:GetTapNoteScores(TNSTypes[i])
 	
 	-- actual numbers
-	n[#n+1] = LoadFont("_ScreenEvaluation numbers")..{
-		InitCommand=cmd(shadowlength,1;NoStroke; zoom,0.5; x,performance_x1;);
+	n[#n+1] = Def.RollingNumbers{
+		Font="_ScreenEvaluation numbers",
+		InitCommand=cmd(shadowlength,1; zoom,0.5; visible, GAMESTATE:IsPlayerEnabled(pn); Load, "RollingNumbersEvaluationA" ),
 		BeginCommand=function(self)
-			self:y((i-1)*35 -20);
-			self:settext(number);
-			self:diffuse(color("#FFFFFF"));
-			self:halign( 1 );
-			self:visible( GAMESTATE:IsPlayerEnabled(pn) );
-		end;
-	};
-
-	n[#n+1] = paddingZeros(number, 4, paddng_x1, ((i-1)*35 - 20), pn)
+			self:x( performance_x1 )
+			self:y((i-1)*35 -20)
+			self:targetnumber(number)
+			self:horizalign(right)
+		end
+	}
 	
-end;
+end
 	
 	
 
 for i=1,#labels2_RC do	
-		
-	-- player performace value
-	n[#n+1] = LoadFont("_ScreenEvaluation numbers")..{
-		InitCommand=cmd(shadowlength,1;NoStroke;zoom,0.5);
-		BeginCommand=function(self)			
-			self:y((i-1)*35 + 53);
-			self:x(performance_x2);			
-			self:settext(stats:GetRadarActual():GetValue(labels2_RC[i]));
-			self:halign( 1 );
-			self:visible( GAMESTATE:IsPlayerEnabled(pn) );
-		end;
-	};
 
-	-- gray zeros for padding the performance value
-	n[#n+1] = paddingZeros(stats:GetRadarActual():GetValue(labels2_RC[i]), 3, padding_x2, ((i-1)*35 + 53), pn);
+	local performance = stats:GetRadarActual():GetValue(labels2_RC[i])
+	local possible = stats:GetRadarPossible():GetValue(labels2_RC[i])
+
+	-- player performace value
+	n[#n+1] = Def.RollingNumbers{
+		Font="_ScreenEvaluation numbers",
+		InitCommand=cmd(shadowlength,1; zoom,0.5; Load, "RollingNumbersEvaluationB"),
+		BeginCommand=function(self)
+			self:y((i-1)*35 + 53)
+			self:x(performance_x2)
+			self:targetnumber(performance)
+			self:horizalign(right)
+		end
+	}
 	
 	--  slash
 	n[#n+1] = LoadFont("_misoreg hires")..{
-		Text="/";
-		InitCommand=cmd(NoStroke; diffuse,color("#5A6166"); zoom, 1.25);
+		Text="/",
+		InitCommand=cmd(diffuse,color("#5A6166"); zoom, 1.25),
 		BeginCommand=function(self)
-			self:y((i-1)*35 + 53);
-			self:x(slash_x);
-			self:halign( 1 );
-			self:visible( GAMESTATE:IsPlayerEnabled(pn) );
-		end;
-	};
+			self:y((i-1)*35 + 53)
+			self:x(slash_x)
+			self:horizalign(right)
+		end
+	}
 	
-	--possible value
-	n[#n+1] = LoadFont("_ScreenEvaluation numbers")..{
-		InitCommand=cmd(shadowlength,1;NoStroke;zoom,0.5);
+	-- possible value
+	n[#n+1] = Def.RollingNumbers{
+		Font="_ScreenEvaluation numbers",
+		InitCommand=cmd(shadowlength,1; zoom,0.5; Load, "RollingNumbersEvaluationC"),
 		BeginCommand=function(self)
-			self:y((i-1)*35 + 53);
-			self:x(possible_x);
-			self:settext(stats:GetRadarPossible():GetValue(labels2_RC[i]));
-			self:halign( 1 );
-			self:visible( GAMESTATE:IsPlayerEnabled(pn) );
-		end;
-	};
-
-	-- gray zeros for padding the possible value
-	n[#n+1] = paddingZeros(stats:GetRadarPossible():GetValue(labels2_RC[i]), 3, padding_x3, ((i-1)*35 + 53), pn);
+			self:y((i-1)*35 + 53)
+			self:x(possible_x)
+			self:targetnumber(possible)
+			self:horizalign(right)
+		end
+	}
 
 end	
 
-
-return n;
+return n
