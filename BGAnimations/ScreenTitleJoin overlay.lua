6e19c9595f9cb01dc0bc7e4f60c61cdc20e8ee45
Dan Guzek <dguzek@gmail.com>
2014-08-16 05:00:40 -0400

    hide "Press Start" text on TitleJoin's OffCommand
    The text should hide when a user presses Start.  Minor, but it was annoying me.

--- BGAnimations/ScreenTitleJoin overlay.lua
+++ BGAnimations/ScreenTitleJoin overlay.lua
@@ -1,18 +1,19 @@
 return Def.ActorFrame{
 	LoadFont("_wendy small")..{
-		Text=THEME:GetString("ScreenTitleJoin", "Press Start");
-		InitCommand=cmd(xy,_screen.cx,SCREEN_BOTTOM-100; visible,false; queuecommand,"Refresh");
-		OnCommand=cmd(zoom,0.95; diffuseblink; effectperiod,0.5; effectcolor1,1,1,1,0; effectcolor2,1,1,1,1);
-		CoinsChangedMessageCommand=cmd(playcommand,"Refresh");
-		RefreshCommand=function(self)			
+		Text=THEME:GetString("ScreenTitleJoin", "Press Start"),
+		InitCommand=cmd(xy,_screen.cx,SCREEN_BOTTOM-100; visible,false; queuecommand,"Refresh"),
+		OnCommand=cmd(zoom,0.95; diffuseblink; effectperiod,0.5; effectcolor1,1,1,1,0; effectcolor2,1,1,1,1),
+		OffCommand=cmd(visible,false),
+		CoinsChangedMessageCommand=cmd(playcommand,"Refresh"),
+		RefreshCommand=function(self)
 			if GAMESTATE:GetCoinMode() == "CoinMode_Pay" then
-				local Credits = GetCredits();
+				local Credits = GetCredits()
 				if Credits["Credits"] < 1 then
-					self:visible(false);
+					self:visible(false)
 				else
-					self:visible(true);
+					self:visible(true)
 				end
 			end
 		end
-	};
-};
+	}
+}
