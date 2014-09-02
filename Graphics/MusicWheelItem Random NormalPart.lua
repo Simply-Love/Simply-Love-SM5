f558c31de3ba8e5e0d816a91c36df362a8924f09
Dan Guzek <dguzek@gmail.com>
2014-09-01 22:16:08 -0400

    clean up MusicWheelItems

--- Graphics/MusicWheelItem Random NormalPart.lua
+++ Graphics/MusicWheelItem Random NormalPart.lua
@@ -1,6 +1,1 @@
-local t = Def.ActorFrame{
-	SetMessageCommand=function(self, params)
-	end;
-};
-
-return t;
+return Def.ActorFrame{}
--- Graphics/MusicWheelItem Song NormalPart.lua
+++ Graphics/MusicWheelItem Song NormalPart.lua
@@ -1,13 +1,11 @@
-local t = Def.ActorFrame{
-	InitCommand=cmd(fov,90);
+return Def.ActorFrame{
+	InitCommand=cmd(fov,90),
 
 	LoadActor("MusicWheelItem song.png")..{
-		Name="SongItemBG";
-		InitCommand=cmd(zoomto,_screen.w/2, _screen.h/15; horizalign,left; addx,-_screen.w/10);
+		Name="SongItemBG",
+		InitCommand=cmd(zoomto,_screen.w/2, _screen.h/15; horizalign,left; addx,-_screen.w/10),
 		SetMessageCommand=function(self,params)
 				
-		end;
-	};
-};
-
-return t;
+		end
+	}
+}
