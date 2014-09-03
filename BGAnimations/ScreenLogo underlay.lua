fe71c7b53fc22289581739d9915bd2f7188cda95
Dan Guzek <dguzek@gmail.com>
2014-09-03 19:13:51 -0400

    touch ScreenLogo and TitleJoin
    These screens now include instructions to assist novice players.

--- BGAnimations/ScreenLogo underlay.lua
+++ BGAnimations/ScreenLogo underlay.lua
@@ -1,17 +1,32 @@
-local t = Def.ActorFrame{};
-local game = GAMESTATE:GetCurrentGame():GetName();
+local t = Def.ActorFrame{}
+local game = GAMESTATE:GetCurrentGame():GetName()
 if game == "popn" or game == "beat" or game == "kb7" or game == "para" then
 	game = "techno"
 end
 
 t[#t+1] = LoadActor(THEME:GetPathG("", "_logos/" .. game))..{
-	InitCommand=cmd(x, _screen.cx; y, _screen.cy; diffusealpha, 0;);
-	OnCommand=cmd(linear,0.5; diffusealpha, 1);
-};
+	InitCommand=cmd(x, _screen.cx; y, _screen.cy; diffusealpha, 0),
+	OnCommand=cmd(linear,0.5; diffusealpha, 1)
+}
 
 t[#t+1] = LoadActor(THEME:GetPathB("ScreenTitleMenu","underlay/SimplyLove.png"))..{
-	InitCommand=cmd(x, _screen.cx; y, _screen.cy; diffusealpha, 0; zoom, 0.333);
-	OnCommand=cmd(linear,0.5; diffusealpha, 1);
-};
+	InitCommand=cmd(x, _screen.cx; y, _screen.cy; diffusealpha, 0; zoom, 0.333),
+	OnCommand=cmd(linear,0.5; diffusealpha, 1)
+}
 
-return t;
+t[#t+1] = LoadFont("_wendy small")..{
+	Text=THEME:GetString("ScreenLogo", "EnterCreditsToPlay"),
+	InitCommand=cmd(xy,_screen.cx,SCREEN_BOTTOM-100; visible,false; zoom,0.525),
+	OnCommand=function(self)
+		if GAMESTATE:GetCoinMode() == "CoinMode_Pay" then
+			self:visible(true)
+			self:diffuseshift()
+			self:effectperiod(1)
+			self:effectcolor1(1,1,1,0)
+			self:effectcolor2(1,1,1,1)
+		end
+	end,
+	OffCommand=cmd(visible,false)
+}
+
+return t
--- BGAnimations/ScreenTitleJoin overlay.lua
+++ BGAnimations/ScreenTitleJoin overlay.lua
@@ -1,19 +1,27 @@
 return Def.ActorFrame{
-	LoadFont("_wendy small")..{
-		Text=THEME:GetString("ScreenTitleJoin", "Press Start"),
-		InitCommand=cmd(xy,_screen.cx,SCREEN_BOTTOM-100; visible,false; queuecommand,"Refresh"),
-		OnCommand=cmd(zoom,0.95; diffuseblink; effectperiod,0.5; effectcolor1,1,1,1,0; effectcolor2,1,1,1,1),
-		OffCommand=cmd(visible,false),
-		CoinsChangedMessageCommand=cmd(playcommand,"Refresh"),
-		RefreshCommand=function(self)
-			if GAMESTATE:GetCoinMode() == "CoinMode_Pay" then
-				local Credits = GetCredits()
-				if Credits["Credits"] < 1 then
-					self:visible(false)
-				else
-					self:visible(true)
-				end
+	InitCommand=cmd(queuecommand,"Refresh"),
+	OnCommand=cmd(diffuseblink; effectperiod,1; effectcolor1,1,1,1,0; effectcolor2,1,1,1,1),
+	OffCommand=cmd(visible,false),
+	CoinsChangedMessageCommand=cmd(playcommand,"Refresh"),
+	RefreshCommand=function(self)
+		if GAMESTATE:GetCoinMode() == "CoinMode_Pay" then
+			local Credits = GetCredits()
+			if Credits["Credits"] < 1 then
+				self:visible(false)
+			else
+				self:visible(true)
 			end
 		end
+	end,
+
+	LoadFont("_wendy small")..{
+		Text=THEME:GetString("ScreenTitleJoin", "Press Start"),
+		InitCommand=cmd(xy,_screen.cx, _screen.h-80 ),
+		OnCommand=cmd(zoom,0.715),
+	},
+	
+	LoadFont("_wendy small")..{
+		Text=THEME:GetString("ScreenSelectMusic","Start Button");
+		InitCommand=cmd(x,_screen.cx - 12; y,_screen.h - 125; zoom,1.1);
 	}
 }
