77c87793adf65dd6298806126226b16eb36cf2bf
Dan Guzek <dguzek@gmail.com>
2014-09-03 11:34:57 -0400

    fix SortList's sick_wheel behavior
    Thanks, Kyzentun.

--- BGAnimations/ScreenSortList overlay.lua
+++ BGAnimations/ScreenSortList overlay.lua
@@ -18,7 +18,7 @@ local function input(event)
 		return false
 	end
 
-	if event.type == "InputEventType_FirstPress" then
+	if event.type ~= "InputEventType_Release" then
 		local overlay = SCREENMAN:GetTopScreen():GetChild("Overlay")
 
 		if event.GameButton == "MenuRight" then
@@ -30,12 +30,12 @@ local function input(event)
 			overlay:GetChild("change_sound"):play()
 
 		elseif event.GameButton == "Start" then
-			overlay:GetChild("start_sound"):play()			
-			MESSAGEMAN:Broadcast('Sort',{order=sort_wheel:get_actor_item_at_focus_pos().order})
+			overlay:GetChild("start_sound"):play()
+			MESSAGEMAN:Broadcast('Sort',{order=sort_wheel:get_actor_item_at_focus_pos().info})
 			SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
-			
+
 		elseif event.GameButton == "Back" then
-			SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")	
+			SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
 		end
 	end
 
@@ -48,10 +48,6 @@ local wheel_item_mt = {
 	__index = {
 		create_actors = function(self, name)
 			self.name=name
-			local index = tonumber((name:gsub("item","")))
-			self.index = index 
-			local order = sort_orders[index]
-			self.order = order
 
 			local af = Def.ActorFrame{
 				Name=name,
@@ -63,8 +59,11 @@ local wheel_item_mt = {
 			}
 
 			af[#af+1] = LoadFont("_wendy small")..{
-				Text=THEME:GetString("ScreenSortList", order),
-				InitCommand=cmd(diffusealpha,0;),
+				Text="",
+				InitCommand=function(subself)
+					subself:diffusealpha(0)
+					self.text= subself
+				end,
 				OnCommand=function(self)
 					self:sleep(0.13)
 					self:linear(0.05)
@@ -77,7 +76,7 @@ local wheel_item_mt = {
 
 		transform = function(self, item_index, num_items, has_focus)
 			self.container:finishtweening()
-			
+
 			if has_focus then
 				self.container:accelerate(0.15)
 				self.container:zoom(0.6)
@@ -91,9 +90,9 @@ local wheel_item_mt = {
 				self.container:glow(color("1,1,1,0"))
 			end
 
-			self.container:y(28 * item_index - (5*28))
-			
-			if item_index < 3 or  item_index > 7 then
+			self.container:y(28 * (item_index - math.ceil(num_items/2)))
+
+			if item_index <= 1 or  item_index >= num_items then
 				self.container:diffusealpha(0)
 			else
 				self.container:diffusealpha(1)
@@ -102,23 +101,25 @@ local wheel_item_mt = {
 
 		set = function(self, info)
 			self.info= info
-			if not info then return end
+			if not info then self.text:settext("") return end
+			self.text:settext(THEME:GetString("ScreenSortList", info))
 		end
 	}
 }
 
-
-
 local t = Def.ActorFrame {
 	InitCommand=function(self)
-		sort_wheel:set_info_set({""}, 1)
-		sort_wheel:scroll_by_amount(-4)
+		sort_wheel:set_info_set(sort_orders, 1)
+		-- override sick_wheel's default focus_pos, which is math.floor(num_items / 2) 
+		sort_wheel.focus_pos = 4
+		-- "scroll" the wheel (0 positions) just so that the override takes immediate effect
+		sort_wheel:scroll_by_amount(0)
 		self:queuecommand("Capture")
 	end,
 	CaptureCommand=function(self)
 		SCREENMAN:GetTopScreen():AddInputCallback(input)
 	end,
-	
+
 	-- slightly darken the entire screen
 	Def.Quad {
 		InitCommand=cmd(FullScreen; diffuse,Color.Black; diffusealpha,0.6)
@@ -128,12 +129,12 @@ local t = Def.ActorFrame {
 	Def.Quad {
 		InitCommand=cmd(Center; zoomto,202,152)
 	},
-	
+
 	-- BG of the sortlist box
 	Def.Quad {
 		InitCommand=cmd(Center; zoomto,200,150; diffuse,Color.Black)
 	},
-		
+
 	-- top mask
 	Def.Quad {
 		InitCommand=cmd(Center; zoomto,200,_screen.h/2; y,50; MaskSource )
@@ -142,9 +143,9 @@ local t = Def.ActorFrame {
 	Def.Quad {
 		InitCommand=cmd(zoomto,200,_screen.h/2 ; xy,_screen.cx,_screen.cy+195; MaskSource)
 	},
-	
-	sort_wheel:create_actors( "sort_wheel", #sort_orders, wheel_item_mt, _screen.cx, _screen.cy ),
-	
+
+	-- this returns an ActorFrame ( see: ./Scripts/Consensual-sick_wheel.lua )
+	sort_wheel:create_actors( "sort_wheel", 7, wheel_item_mt, _screen.cx, _screen.cy )
 }
 
 t[#t+1] = LoadActor( THEME:GetPathS("ScreenSelectMaster", "change") )..{ Name="change_sound", SupportPan = false }
