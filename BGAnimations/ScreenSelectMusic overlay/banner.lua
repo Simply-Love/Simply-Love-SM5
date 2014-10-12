7fdca5219ce59e532657da4d2d3525907f084027
Dan Guzek <dguzek@gmail.com>
2014-10-12 03:08:29 -0400

    fix banner sizing on SSM

--- BGAnimations/ScreenSelectMusic overlay/banner.lua
+++ BGAnimations/ScreenSelectMusic overlay/banner.lua
@@ -1,7 +1,7 @@
 local t = Def.ActorFrame{
 	OnCommand=function(self)	
 		if IsUsingWideScreen() then
-			self:zoom(0.775)
+			self:zoom(0.7655)
 			self:xy(_screen.cx - 173, 112)
 		else
 			self:zoom(0.74)
