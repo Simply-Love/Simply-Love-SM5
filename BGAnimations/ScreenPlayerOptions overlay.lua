Simply-Love/Simply-Love-SM5

commit d81f80be7ecf2514c19b6fb9977f01ce2a2708cc

improve SpeedMod OptionRow Title with ratemods

BGAnimations/ScreenPlayerOptions overlay.lua
@@ -28,7 +28,7 @@ local t = Def.ActorFrame{
 	end
 }
 
-t[#t+1] = LoadActor(THEME:GetPathB("ScreenPlayerOptions", "common"));
+t[#t+1] = LoadActor(THEME:GetPathB("ScreenPlayerOptions", "common"))
 
 
 for player in ivalues(Players) do
@@ -68,7 +68,7 @@ for player in ivalues(Players) do
 				self:queuecommand("Set" .. pn)
 				self:GetParent():GetChild(pn.."MusicRateHelper"):playcommand("Set")
 			end
-		end;
+		end,
 		
 		["Set" .. pn .. "Command"]=function(self)
 			local text = ""
@@ -83,14 +83,14 @@ for player in ivalues(Players) do
 			
 			SpeedModItems[pn]:settext( text )
 			self:GetParent():GetChild(pn .. "SpeedModHelper"):settext( DisplaySpeedMod(pn) )
-		end;
+		end,
 		
 		["MenuLeft" .. pn .. "MessageCommand"]=function(self)
 			if SCREENMAN:GetTopScreen():GetCurrentRowIndex(player) == 1 then
 				ChangeSpeedMod( pn, -1 )
 				self:queuecommand("Set"..pn)
 			end	
-		end;
+		end,
 		["MenuRight" .. pn .. "MessageCommand"]=function(self)
 			if SCREENMAN:GetTopScreen():GetCurrentRowIndex(player) == 1 then
 				ChangeSpeedMod( pn, 1 )
@@ -144,8 +144,8 @@ for player in ivalues(Players) do
 		end,
 		OnCommand=cmd(linear,0.4;diffusealpha,1),
 		SetCommand=function(self)
+			local musicrate = SL.Global.ActiveModifiers.MusicRate
 			if SL[pn].ActiveModifiers.SpeedModType == "x" then
-				local musicrate = SL.Global.ActiveModifiers.MusicRate
 				if musicrate == 1 then
 					self:settext("")
 				else
@@ -155,6 +155,15 @@ for player in ivalues(Players) do
 				self:settext("")
 			end
 			self:GetParent():GetChild(pn .. "SpeedModHelper"):settext( DisplaySpeedMod(pn) )
+			
+			local ScreenOptions = SCREENMAN:GetTopScreen()
+			local SpeedModTitle = ScreenOptions:GetOptionRow(1):GetChild(""):GetChild("Title")
+			local bpms = GAMESTATE:GetCurrentSong():GetDisplayBpms()
+			if bpms[1] == bpms[2] then
+				SpeedModTitle:settext( "Speed Mod (" .. bpms[1] * musicrate .. ")" )
+			else
+				SpeedModTitle:settext( "Speed Mod (" .. bpms[1] * musicrate ..  " - " .. bpms[2] * musicrate  .. ")" )
+			end
 		end,
 		MusicRateChangedMessageCommand=cmd(playcommand,"Set")
 	}

commit 177b395bf65c93e2a6290c9a844c18712f515778

fix BPM display for Course Mode
Course Mode's BPM display on ScreenPlayerOptions had been broken since d81f80b but I didn't realize until recently because no one plays Course Mode...

This commit fixes that and also adds functionality to the GetCourseModeBPMs() function to appropriately handle BPMs <= 0.

BGAnimations/ScreenPlayerOptions overlay.lua
@@ -9,16 +9,16 @@ local t = Def.ActorFrame{
 	OnCommand=cmd(diffusealpha,0; linear,0.2;diffusealpha,1; queuecommand,"Capture");
 	OffCommand=cmd(linear,0.2;diffusealpha,0);
 	CaptureCommand=function(self)
-		
+
 		local ScreenOptions = SCREENMAN:GetTopScreen()
-		
+
 		-- reset for ScreenEditOptions
 		SpeedModItems = {P1 = nil, P2 = nil}
-		
+
 		-- The bitmaptext actors for P1 and P2 speedmod are both named "Item"
 		SpeedModItems.P1 = ScreenOptions:GetOptionRow(1):GetChild(""):GetChild("Item")[1]
 		SpeedModItems.P2 = ScreenOptions:GetOptionRow(1):GetChild(""):GetChild("Item")[2]
-			
+
 		if SpeedModItems.P1 and GAMESTATE:IsPlayerEnabled(PLAYER_1) then
 			self:playcommand("SetP1")
 		end
@@ -37,15 +37,15 @@ for player in ivalues(Players) do
 
 		-- Commands for player speedmod
 		["SpeedModType" .. pn .. "SetMessageCommand"]=function(self,params)
-					
+
 			local oldtype = SL[pn].ActiveModifiers.SpeedModType
 			local newtype = params.SpeedModType
 			local song = GAMESTATE:GetCurrentSong()
-			
+
 			if oldtype ~= newtype then
 				local bpm
 				local oldspeed = SL[pn].ActiveModifiers.SpeedMod
-				
+
 				if GAMESTATE:IsCourseMode() then
 					bpm = GetCourseModeBPMs()
 				else
@@ -54,28 +54,28 @@ for player in ivalues(Players) do
 						bpm = song:GetTimingData():GetActualBPM()
 					end
 				end
-				
+
 				if oldtype == "x" and (newtype == "C" or newtype == "M") then
 					-- convert to the nearest MMod/CMod-appropriate integer by rounding to nearest 10
-					SL[pn].ActiveModifiers.SpeedMod = (round((oldspeed * bpm[2]) / 10)) * 10	
-				
+					SL[pn].ActiveModifiers.SpeedMod = (round((oldspeed * bpm[2]) / 10)) * 10
+
 				elseif newtype == "x" then
 					-- convert to the nearest XMod-appropriate integer by rounding to 2 decimal places
 					-- and then rounding that to the nearest 0.05 increment
 					SL[pn].ActiveModifiers.SpeedMod = (round(round(oldspeed / bpm[2], 2) / 0.05)) * 0.05
 				end
-		
+
 				SL[pn].ActiveModifiers.SpeedModType = newtype
-		
+
 				ApplySpeedMod(player)
 				self:queuecommand("Set" .. pn)
 				self:GetParent():GetChild(pn.."MusicRateHelper"):playcommand("Set")
 			end
 		end,
-		
+
 		["Set" .. pn .. "Command"]=function(self)
 			local text = ""
-			
+
 			if  SL[pn].ActiveModifiers.SpeedModType == "x" then
 				text = string.format("%.2f" , SL[pn].ActiveModifiers.SpeedMod ) .. "x"
 			elseif  SL[pn].ActiveModifiers.SpeedModType == "C" then
@@ -83,16 +83,16 @@ for player in ivalues(Players) do
 			elseif  SL[pn].ActiveModifiers.SpeedModType == "M" then
 				text = "M" .. tostring(SL[pn].ActiveModifiers.SpeedMod)
 			end
-			
+
 			SpeedModItems[pn]:settext( text )
 			self:GetParent():GetChild(pn .. "SpeedModHelper"):settext( DisplaySpeedMod(pn) )
 		end,
-		
+
 		["MenuLeft" .. pn .. "MessageCommand"]=function(self)
 			if SCREENMAN:GetTopScreen():GetCurrentRowIndex(player) == 1 then
 				ChangeSpeedMod( pn, -1 )
 				self:queuecommand("Set"..pn)
-			end	
+			end
 		end,
 		["MenuRight" .. pn .. "MessageCommand"]=function(self)
 			if SCREENMAN:GetTopScreen():GetCurrentRowIndex(player) == 1 then
@@ -101,7 +101,7 @@ for player in ivalues(Players) do
 			end
 		end
 	}
-	
+
 	-- the display that does math for you up at the top
 	t[#t+1] = LoadFont("_wendy small")..{
 		Name=pn.."SpeedModHelper",
@@ -119,7 +119,7 @@ for player in ivalues(Players) do
 		end,
 		OnCommand=cmd(linear,0.4;diffusealpha,1)
 	}
-	
+
 	t[#t+1] = LoadFont("_misoreg hires")..{
 		Name=pn.."MusicRateHelper",
 		Text="",
@@ -127,8 +127,8 @@ for player in ivalues(Players) do
 			self:shadowlength(0.4)
 			self:diffuse(PlayerColor(player))
 			self:zoom(0.9)
-			
-			if IsUsingWideScreen() then		
+
+			if IsUsingWideScreen() then
 				if player == PLAYER_1 then
 					self:x(-100)
 				elseif player == PLAYER_2 then
@@ -148,7 +148,7 @@ for player in ivalues(Players) do
 		OnCommand=cmd(linear,0.4;diffusealpha,1),
 		SetCommand=function(self)
 			local musicrate = SL.Global.ActiveModifiers.MusicRate
-			
+
 			-- settext on the musicrate helper
 			if SL[pn].ActiveModifiers.SpeedModType == "x" then
 				if musicrate == 1 then
@@ -159,28 +159,34 @@ for player in ivalues(Players) do
 			else
 				self:settext("")
 			end
-			
+
 			-- settext on the speedmod helper
 			self:GetParent():GetChild(pn .. "SpeedModHelper"):settext( DisplaySpeedMod(pn) )
-			
+
 			-------------------------------
 			-- variables to be used for setting the text in the "Speed Mod" OptionRow title
 			local ScreenOptions = SCREENMAN:GetTopScreen()
 			local SpeedModTitle = ScreenOptions:GetOptionRow(1):GetChild(""):GetChild("Title")
 			local song = GAMESTATE:GetCurrentSong()
-			
+			local bpms
+
 			-- get the song's native display BPM(s)
-			local bpms = song:GetDisplayBpms()
-			
-			-- if either display BPM is negative or 0, use the actual BPMs instead...
-			if bpms[1] <= 0 or bpms[2] <= 0 then
-				bpms = song:GetTimingData():GetActualBPM()
+			if song then
+				bpms = song:GetDisplayBpms()
+				-- if either display BPM is negative or 0, use the actual BPMs instead...
+				if bpms[1] <= 0 or bpms[2] <= 0 then
+					bpms = song:GetTimingData():GetActualBPM()
+				end
+			else
+				bpms = GetCourseModeBPMs()
 			end
-			
+
+
+
 			-- truncate possible decimal places
 			bpms[1] = ("%0.0f"):format(bpms[1])
 			bpms[2] = ("%0.0f"):format(bpms[2])
-			
+
 			-- settext on "Speed Mod" OptionRow title
 			if bpms[1] == bpms[2] then
 				SpeedModTitle:settext( THEME:GetString("OptionTitles", "SpeedModNew") .. " (" .. bpms[1] * musicrate .. ")" )

Scripts/SL-CustomSpeedMods.lua
@@ -14,7 +14,7 @@ function SpeedModsType()
 		end,
 		SaveSelections = function(self, list, pn)
 			local sSave
-			
+
 			for i=1, #list do
 				if list[i] then
 					sSave=modList[i]
@@ -29,7 +29,7 @@ end
 
 
 function SpeedModsNew()
-	
+
 	local blank = {"       "}
 
 	local t = {
@@ -40,11 +40,11 @@ function SpeedModsNew()
 		ExportOnChange = false,
 		Choices = blank,
 		LoadSelections = function(self, list, pn)
-			list[1] = true	
+			list[1] = true
 		end,
 		SaveSelections = function(self, list, pn)
 			ApplySpeedMod(pn)
-		end	
+		end
 	}
 	return t
 end
@@ -54,10 +54,10 @@ end
 
 
 function ChangeSpeedMod(pn, direction)
-		
+
 	-- if using an XMod
 	if SL[pn].ActiveModifiers.SpeedModType == "x" then
-		
+
 		if SL[pn].ActiveModifiers.SpeedMod + (0.05 * direction) >= 20 then
 			SL[pn].ActiveModifiers.SpeedMod = 0.05
 		elseif SL[pn].ActiveModifiers.SpeedMod + (0.05 * direction) <= 0 then
@@ -65,7 +65,7 @@ function ChangeSpeedMod(pn, direction)
 		else
 			SL[pn].ActiveModifiers.SpeedMod = SL[pn].ActiveModifiers.SpeedMod + (0.05 * direction)
 		end
-		
+
 	-- elseif using a CMod or an MMod
 	elseif SL[pn].ActiveModifiers.SpeedModType == "C" or SL[pn].ActiveModifiers.SpeedModType == "M" then
 
@@ -76,7 +76,7 @@ function ChangeSpeedMod(pn, direction)
 		else
 			SL[pn].ActiveModifiers.SpeedMod = SL[pn].ActiveModifiers.SpeedMod + (10 * direction)
 		end
-	end	
+	end
 end
 
 
@@ -87,12 +87,12 @@ function ApplySpeedMod(player)
 	local speed = SL[ToEnumShortString(player)].ActiveModifiers.SpeedMod or 1.00
 	local topscreen = SCREENMAN:GetTopScreen():GetName()
 	local modslevel = topscreen  == "ScreenEditOptions" and "ModsLevel_Stage" or "ModsLevel_Preferred"
-	
+
 	local playeroptions = GAMESTATE:GetPlayerState(player):GetPlayerOptions(modslevel)
-	
+
 	-- it's necessary to manually apply a speedmod of 1x first, otherwise speedmods stack?
 	playeroptions:XMod(1.00)
-	
+
 	if type == "x" then
 		playeroptions:XMod(speed)
 	elseif type == "C" then
@@ -107,7 +107,7 @@ function DisplaySpeedMod(pn)
 	local bpm
 	local display = ""
 	local speed = SL[pn].ActiveModifiers.SpeedMod
-	
+
 	if GAMESTATE:IsCourseMode() then
 		bpm = GetCourseModeBPMs()
 	else
@@ -117,20 +117,20 @@ function DisplaySpeedMod(pn)
 			bpm = GAMESTATE:GetCurrentSong():GetTimingData():GetActualBPM()
 		end
 	end
-		
+
 	-- if using an XMod
 	if SL[pn].ActiveModifiers.SpeedModType == "x" then
 		local musicrate = SL.Global.ActiveModifiers.MusicRate
-		
+
 		--if a single bpm suffices
 		if bpm[1] == bpm[2] then
 			display = string.format("%.2f", speed) .. "x (" .. round(speed * bpm[1] * musicrate) .. ")"
-			
+
 		-- if we have a range of bpms
 		else
 			display = string.format("%.2f", speed) .. "x (" .. round(speed * bpm[1] * musicrate) .. " - " .. round(speed * bpm[2] * musicrate) .. ")"
 		end
-	
+
 	-- elseif using a CMod or an MMod
 	elseif SL[pn].ActiveModifiers.SpeedModType == "C" or SL[pn].ActiveModifiers.SpeedModType == "M" then
 		display = SL[pn].ActiveModifiers.SpeedModType .. tostring(speed)
@@ -153,7 +153,13 @@ function GetCourseModeBPMs()
 			trailEntries = trail:GetTrailEntries()
 
 			for k,trailEntry in ipairs(trailEntries) do
-				local bpms = trailEntry:GetSong():GetDisplayBpms()
+				local song = trailEntry:GetSong()
+				local bpms = song:GetDisplayBpms()
+
+				-- if either display BPM is negative or 0, use the actual BPMs instead...
+				if bpms[1] <= 0 or bpms[2] <= 0 then
+					bpms = song:GetTimingData():GetActualBPM()
+				end
 
 				-- on the first iteration, lowest and highest will both be nil
 				-- so set lowest to this song's lower bpm
@@ -184,37 +190,37 @@ end
 
 function GetDisplayBPMs()
 	local text = ""
-	
+
 	-- if in "normal" mode
 	if not GAMESTATE:IsCourseMode() then
 		local song = GAMESTATE:GetCurrentSong()
-		
+
 		if song then
 			local bpm = song:GetDisplayBpms()
-			
+
 			-- handle DisplayBPMs that are <= 0
 			if bpm[1] <= 0 or bpm[2] <= 0 then
 				bpm = song:GetTimingData():GetActualBPM()
 			end
-			
+
 			--if a single bpm suffices
 			if bpm[1] == bpm[2] then
 				text = round(bpm[1])
-		
+
 			-- if we have a range of bpms
 			else
 				text = round(bpm[1]) .. " - " .. round(bpm[2])
 			end
 		end
-		
-	-- if we ARE in CourseMode		
+
+	-- if we ARE in CourseMode
 	else
 		local range = GetCourseModeBPMs()
 		if range then
 			local lowest = range[1]
 			local highest = range[2]
-				
-				
+
+
 			if lowest and highest then
 				if lowest == highest then
 					text = round(lowest)
@@ -224,6 +230,6 @@ function GetDisplayBPMs()
 			end
 		end
 	end
-	
+
 	return text
 end