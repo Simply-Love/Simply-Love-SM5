368b16fbd8eccbc7e146825a7fa3ad0c97cc6b1c
Dan Guzek <dguzek@gmail.com>
2015-01-24 13:41:00 -0500

    don't rely on SSM's FadingBanner
    A year later, I finally understand an almost-competent way of handling banners in Lua.
    
    This commit attempts to look to banners cached in memory first, then tries loading from disk, then resorts to the fallback "hearts" banner (which, technically, is always there).
    
    If you have your BannerCache setting in Preferences.ini set to "LowResPreload" you may find this commit makes your banners look worse if you are running SM at a high resolution.  Unfortunately, "Full" BannerCache is nigh-unusable due to how many banners will inevitably have dimensions that aren't multiples of 2.
    
    If there is ample negative feedback regarding this commit, I'll definitely consider reverting it.

--- BGAnimations/ScreenSelectMusic overlay/banner.lua
+++ BGAnimations/ScreenSelectMusic overlay/banner.lua
@@ -11,32 +11,53 @@ local t = Def.ActorFrame{
 
 	LoadActor("colored_banners/banner"..SimplyLoveColor()..".png")..{
 		Name="FallbackBanner",
-		OnCommand=function(self)
-			self:rotationy(180)
-			self:setsize(418,164)
-		end,
+		OnCommand=cmd(rotationy,180; setsize,418,164; diffuseshift; effectoffset,3; effectperiod, 6; effectcolor1, 1,1,1,0; effectcolor2, 1,1,1,1),
 		HideCommand=cmd(visible,false),
 		ShowCommand=cmd(visible,true)
 	},
 	
 	LoadActor("colored_banners/banner"..SimplyLoveColor()..".png")..{
 		Name="FallbackBanner",
-		OnCommand=function(self)
-			self:diffuseshift()
-			self:effectperiod(6)
-			self:effectcolor1(1,1,1,0)
-			self:effectcolor2(1,1,1,1)
-			self:setsize(418,164)
-		end,
+		OnCommand=cmd(diffuseshift; effectperiod, 6; effectcolor1, 1,1,1,0; effectcolor2, 1,1,1,1; setsize, 418,164),
 		HideCommand=cmd(visible,false),
 		ShowCommand=cmd(visible,true)
 	},
-	
-	Def.ActorProxy{
-		Name="BannerProxy",
-		BeginCommand=function(self)
-			local banner = SCREENMAN:GetTopScreen():GetChild('Banner')
-			self:SetTarget(banner)
+		
+	Def.Banner{
+		Name="Banner",
+		CurrentSongChangedMessageCommand=cmd(playcommand,"Set"),
+		CurrentCourseChangedMessageCommand=cmd(playcommand,"Set"),
+		SetCommand=function(self)
+			local Group = SCREENMAN:GetTopScreen():GetChild("MusicWheel"):GetSelectedSection()
+			local SongOrCourse
+
+			if GAMESTATE:IsCourseMode() then
+				SongOrCourse = GAMESTATE:GetCurrentCourse()
+			else
+				SongOrCourse = GAMESTATE:GetCurrentSong()
+			end
+			-- try to load a song banner first,
+			-- if one is not avaiable, try to load a group banner
+			-- if one is not available, hide this sprite, and rely
+			-- fallback banner(s) loaded above
+			if SongOrCourse then
+				local BannerPath = SongOrCourse:GetBannerPath()
+				-- attempt to read a cached banner out of memory first
+				-- if, for some reasons, it doesn't exist, try reading one from disk
+				if BannerPath then
+					self:LoadFromCachedBanner(BannerPath)
+				else
+					self:LoadFromSong(SongOrCourse)
+				end
+				self:visible(true)
+				self:setsize(418,164)
+			elseif Group then
+				self:visible(true)
+				self:LoadFromSongGroup(Group)
+				self:setsize(418,164)
+			else
+				self:visible(false)
+			end
 		end
 	}
 }
