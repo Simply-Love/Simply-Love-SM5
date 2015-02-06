3e5405813d39cae6e858b38419bd9fcb1367e650
Dan Guzek <dguzek@gmail.com>
2015-02-06 06:30:15 -0500

    add ScreenAttackMenu
    This is (yet another) variation on ScreenPlayerOptions customized for adding attacks (scripted modifiers) in Edit Mode.  I hadn't realized this screen existed until quite recently.

---
+++ BGAnimations/ScreenAttackMenu overlay.lua
@@ -0,0 +1,5 @@
+local t = Def.ActorFrame{}
+
+t[#t+1] = LoadActor(THEME:GetPathB("ScreenPlayerOptions", "overlay"))
+
+return t
---
+++ BGAnimations/ScreenAttackMenu underlay.lua
@@ -0,0 +1,4 @@
+return Def.ActorFrame {
+	LoadActor(THEME:GetPathB("","_black.lua")),
+	LoadActor(THEME:GetPathB("ScreenPlayerOptions", "underlay"))
+}
