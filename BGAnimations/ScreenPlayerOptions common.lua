a7c889bd3196f4267ac9c6159367309f15f4e088
Dan Guzek <dguzek@gmail.com>
2015-02-06 06:28:26 -0500

    ScreenEditOptions uses ScreenPlayerOptions
    ...rather than effectively duplicating its code.

--- BGAnimations/ScreenPlayerOptions common.lua
+++ BGAnimations/ScreenPlayerOptions common.lua
@@ -1,41 +1,34 @@
-local ScreenOptions = SCREENMAN:GetTopScreen()
-
 return Def.ActorFrame{
 	-- this is broadcast from [OptionRow] TitleGainFocusCommand in metrics.ini
 	-- we use it to color the active OptionRow's title appropriately by PlayerColor()
 	OptionRowChangedMessageCommand=function(self, params)
 		local CurrentRowIndex = {P1, P2}
-		
+
 		-- There is always the possibility that a diffuseshift is still active;
 		-- cancel it now (and re-apply below, if applicable).
 		params.Title:stopeffect()
-	
-		-- if ScreenOptions is nil, get it now
-		if not ScreenOptions then
-			ScreenOptions = SCREENMAN:GetTopScreen()
-		else
-			-- get the index of PLAYER_1's current row
-			if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
-				CurrentRowIndex.P1 = ScreenOptions:GetCurrentRowIndex(PLAYER_1)
-			end
-		
-			-- get the index of PLAYER_2's current row
-			if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
-				CurrentRowIndex.P2 = ScreenOptions:GetCurrentRowIndex(PLAYER_2)
-			end
+
+		-- get the index of PLAYER_1's current row
+		if GAMESTATE:IsPlayerEnabled(PLAYER_1) then
+			CurrentRowIndex.P1 = SCREENMAN:GetTopScreen():GetCurrentRowIndex(PLAYER_1)
 		end
-		
+
+		-- get the index of PLAYER_2's current row
+		if GAMESTATE:IsPlayerEnabled(PLAYER_2) then
+			CurrentRowIndex.P2 = SCREENMAN:GetTopScreen():GetCurrentRowIndex(PLAYER_2)
+		end
+
 		local optionRow = params.Title:GetParent():GetParent();
-	
+
 		-- color the active optionrow's title appropriately
 		if optionRow:HasFocus(PLAYER_1) then
 			params.Title:diffuse(PlayerColor(PLAYER_1))
 		end
-	
+
 		if optionRow:HasFocus(PLAYER_2) then
 			params.Title:diffuse(PlayerColor(PLAYER_2))
 		end
-		
+
 		if CurrentRowIndex.P1 and CurrentRowIndex.P2 then
 			if CurrentRowIndex.P1 == CurrentRowIndex.P2 then
 				params.Title:diffuseshift()
--- BGAnimations/ScreenPlayerOptions underlay.lua
+++ BGAnimations/ScreenPlayerOptions underlay.lua
@@ -1,12 +1,10 @@
-local t = Def.ActorFrame{
-	OnCommand=cmd(diffusealpha,0; linear,0.2;diffusealpha,1);
-	OffCommand=cmd(linear,0.2;diffusealpha,0);	
+return Def.ActorFrame{
+	OnCommand=cmd(diffusealpha,0; linear,0.2; diffusealpha,1),
+	OffCommand=cmd(linear,0.2; diffusealpha,0),
 	
 	Def.Quad{
-		Name="ExplanationBackground";
-		InitCommand=cmd(diffuse, color("0,0,0,0.5"); xy, _screen.cx, _screen.h-57; );
-		OnCommand=cmd(zoomto,_screen.w*0.935,_screen.h*0.08;);
+		Name="ExplanationBackground",
+		InitCommand=cmd(diffuse, color("0,0,0,0.5"); xy, _screen.cx, _screen.h-57 ),
+		OnCommand=cmd(zoomto, _screen.w*0.935, _screen.h*0.08 ),
 	}
-};
-
-return t;
+}
