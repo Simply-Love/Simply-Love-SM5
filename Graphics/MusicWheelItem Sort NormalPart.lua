e4a60b0bc7351923e58aaa9d4444ddd274d8a79e
Dan Guzek <dguzek@gmail.com>
2015-02-24 16:45:27 -0500

    MusicWheelItem sort cleanup

--- Graphics/MusicWheelItem Sort NormalPart.lua
+++ Graphics/MusicWheelItem Sort NormalPart.lua
@@ -1,9 +1,3 @@
-local t = Def.ActorFrame{
-	LoadActor("MusicWheelItem sort.png" )..{
-		InitCommand=cmd(zoomto,_screen.w/2,50; horizalign,left; addx,-_screen.w/10);
-		SetMessageCommand=function(self,param)
-		end;
-	};
-};
-
-return t;
+return LoadActor("MusicWheelItem sort.png" )..{
+	InitCommand=cmd(zoomto,_screen.w/2,50; horizalign,left; addx,-_screen.w/10)
+}
