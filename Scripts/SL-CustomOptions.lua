0bb39322ce81e6d120fd0ff2749709f2880b9302
Dan Guzek <dguzek@gmail.com>
2015-06-16 15:24:42 -0400

    add more MusicWheelSwitchSpeed options
    May the schwartz be with you.

--- Scripts/SL-CustomOptions.lua
+++ Scripts/SL-CustomOptions.lua
@@ -27,3 +27,43 @@ function OptionRowEditorNoteskin()
 		end,
 	}
 end
+
+function OptionRowMusicWheelSpeed()
+
+	local choices = { "Slow", "Normal", "Fast", "Faster", "Ridiculous", "Ludicrous", "Plaid" }
+	local values = { 5, 10, 15, 25, 30, 45, 100 }
+	local localized_choices = {}
+
+	for i=1, #choices do
+		localized_choices[i] = THEME:GetString("MusicWheelSpeed", choices[i] )
+	end
+
+	-- it's possible the user has manually edited Preferences.ini and set an arbitrary value
+	-- try to accommodate, rather than obliterating that custom setting
+	local user_setting = PREFSMAN:GetPreference("MusicWheelSwitchSpeed") or 15
+	if not FindInTable(user_setting, values) then
+		values[#values+1] = user_setting
+		choices[ #choices+1 ] = THEME:GetString("MusicWheelSpeed", "Custom")
+	end
+
+	return {
+		Name = "MusicWheelSpeed",
+		LayoutType = "ShowAllInRow",
+		SelectType = "SelectOne",
+		OneChoiceForAllPlayers = true,
+		ExportOnChange = false,
+		Choices = choices,
+		LoadSelections = function(self, list, pn)
+			local i = FindInTable(user_setting, values) or 1
+			list[i] = true
+		end,
+		SaveSelections = function(self, list, pn)
+			for i = 1, #values do
+				if list[i] then
+					PREFSMAN:SetPreference("MusicWheelSwitchSpeed", values[i] )
+					break
+				end
+			end
+		end
+	}
+end
