84e16dc025310f1f8fc2360e98fe4b7c08e3087e
Dan Guzek <dguzek@gmail.com>
2015-06-17 14:34:36 -0400

    make EditMode help text more helpful
    In English, anyway...
    
    This could still be improved upon, but it's WORLDS better than the old help text.

--- BGAnimations/ScreenEdit underlay.lua
+++ BGAnimations/ScreenEdit underlay.lua
@@ -1,100 +1,58 @@
-local bgChildren, textChildren;
+local t = Def.ActorFrame{
+	Name="Text",
 
-return Def.ActorFrame {
+	EditCommand=cmd(playcommand, "Show"),
 
+	PlayingCommand=cmd(playcommand, "Hide"),
+	RecordCommand=cmd(playcommand, "Hide"),
+	RecordPausedCommand=cmd(playcommand, "Hide"),
+
+	-- Info
 	Def.ActorFrame{
-		Name="Backgrounds",
-		InitCommand=cmd(y,_screen.cy),
-		BeginCommand=function(self)
-			bgChildren = self:GetChildren()
-		end,
-
-		EditCommand=cmd(playcommand,"Show"),
-		PlayingCommand=cmd(playcommand,"Hide"),
-		RecordCommand=cmd(playcommand,"Hide"),
-		RecordPausedCommand=cmd(playcommand,"Hide"),
-
-		ShowCommand=function(self)
-			if bgChildren then
-				self:linear(0.2)
-				bgChildren.HelpBG:linear(0.2)
-				bgChildren.HelpBG:addx(120)
-
-				bgChildren.InfoBG:linear(0.2)
-				bgChildren.InfoBG:addx(-120)
-
-				MESSAGEMAN:Broadcast("EditorShow")
-			end
-		end,
-		HideCommand=function(self)
-			if bgChildren then
-				bgChildren.HelpBG:linear(0.2)
-				bgChildren.HelpBG:addx(-120)
-
-				bgChildren.InfoBG:linear(0.2)
-				bgChildren.InfoBG:addx(120)
-
-				MESSAGEMAN:Broadcast("EditorHide")
-			end
-		end,
-
-		Def.Quad {
-			Name="HelpBG",
-			InitCommand=cmd(x,SCREEN_LEFT;horizalign,left;zoomtowidth,128;zoomtoheight,_screen.h;diffuse,color("#000000");diffuserightedge,color("#00000008");)
-		},
-		Def.Quad {
-			Name="InfoBG",
-			InitCommand=cmd(x,SCREEN_RIGHT;horizalign,right;zoomtowidth,128;zoomtoheight,_screen.h;diffuse,color("#000000");diffuseleftedge,color("#00000008");)
-		}
-	},
-
-	Def.ActorFrame{
-		Name="Text",
-		InitCommand=cmd(y,SCREEN_TOP+16;),
-
-		BeginCommand=function(self)
-			textChildren = self:GetChildren()
-		end,
-
-		EditCommand=cmd(playcommand,"Show"),
-		PlayingCommand=cmd(playcommand,"Hide"),
-		RecordCommand=cmd(playcommand,"Hide"),
-		RecordPausedCommand=cmd(playcommand,"Hide"),
-
-		ShowCommand=function(self)
-			if textChildren then
-				textChildren.HelpText:linear(0.2)
-				textChildren.HelpText:addx(120)
-
-				textChildren.InfoText:linear(0.2)
-				textChildren.InfoText:addx(-120)
-			end
-		end,
-		HideCommand=function(self)
-			if textChildren then
-				self:linear(0.2)
-				textChildren.HelpText:linear(0.2)
-				textChildren.HelpText:addx(-120)
-
-				textChildren.InfoText:linear(0.2)
-				textChildren.InfoText:addx(120)
-			end
-		end,
-
-		LoadFont("_misoreg hires") .. {
-			Name="HelpText",
-			InitCommand=cmd(x,SCREEN_LEFT+60;zoom,0.75;settext,THEME:GetString("ScreenEdit","Help");strokecolor,color("#00000077");shadowlength,0)
-		},
-		Def.Quad{
-			InitCommand=cmd(x,SCREEN_LEFT+60;y,12;zoomto,120,2;diffuse,color("1,1,1,0.8");shadowlength,1)
-		},
+		InitCommand=cmd(xy, _screen.w-60, 16),
+		ShowCommand=cmd(decelerate, 0.1; x, _screen.w-60),
+		HideCommand=cmd(accelerate, 0.1; x, _screen.w+60),
 
 		LoadFont("_misoreg hires") .. {
 			Name="InfoText",
-			InitCommand=cmd(x,SCREEN_RIGHT-60;zoom,0.75;settext,THEME:GetString("ScreenEdit","Info");strokecolor,color("#00000077");shadowlength,0)
+			Text=THEME:GetString("ScreenEdit","Info"),
+			InitCommand=cmd(zoom, 0.75)
 		},
 		Def.Quad{
-			InitCommand=cmd(x,SCREEN_RIGHT-60;y,12;zoomto,120,2;diffuse,color("1,1,1,0.8");shadowlength,1)
+			InitCommand=cmd(y,12; zoomto,120,1 )
 		}
 	}
 }
+
+local sections = {
+	NavigationHelp = 0,
+	NoteWritingHelp = 106,
+	MenuHelp = 254,
+	RecordModeHelp = 332,
+	KeyMappingHelp = 394,
+	MiscHelp = 430
+}
+
+for section, offset in pairs(sections) do
+	t[#t+1] = Def.ActorFrame{
+		Name=section,
+		InitCommand=cmd(xy, 0, offset; diffusealpha, 0),
+		OnCommand=cmd(queuecommand, "Show"),
+		ShowCommand=cmd(decelerate, 0.2; diffusealpha, 1),
+		HideCommand=cmd(diffusealpha, 0),
+
+		LoadFont("_wendy small")..{
+			Text=THEME:GetString("ScreenEdit", section.."Label"),
+			InitCommand=cmd(zoom, 0.265; horizalign, left; xy, 35, 10; diffuse, PlayerColor(PLAYER_1))
+		},
+		Def.Quad{
+			InitCommand=cmd(y,10; zoomto,30,1; horizalign, left; diffusealpha,0.75 )
+		},
+		LoadFont("_misoreg hires")..{
+			Text=THEME:GetString("ScreenEdit", section.."Text"),
+			InitCommand=cmd(y, 14; zoom, 0.6; horizalign, left; xy, 10, 20; vertalign, top; vertspacing, -1 ),
+		},
+	}
+end
+
+return t
