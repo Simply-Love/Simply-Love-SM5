2d9fd60f8add916585cc786f8bdd58c9b1bca4d8
Dan Guzek <dguzek@gmail.com>
2015-07-28 21:55:14 -0400

    update SL-Utilities.lua
    This commit adds a recursive table-to-string helper function to SL-Utilities which effectively replaces the old SMPairs() helper function.

--- Scripts/SL-Utilities.lua
+++ Scripts/SL-Utilities.lua
@@ -1,19 +1,60 @@
 -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
--- Development Utility Functions
+-- Utility Functions For Development
 -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-local function SMPairs(t)
-	local temp = ""
-	for k,v in pairs(t) do
-		temp = temp .. "key: " .. tostring(k) .. ", val: " .. tostring(v) .."\n"
+-- local helper functions first
+-- the handful of global utility functions below will depend on these
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+-- TableToString_Recursive() function via:
+-- http://www.hpelbers.org/lua/print_r
+-- Copyright 2009: hans@hpelbers.org
+local function TableToString_Recursive(t, name, indent)
+	local tableList = {}
+
+	function table_r (t, name, indent, full)
+		local id = not full and name or type(name)~="number" and tostring(name) or '['..name..']'
+		local tag = indent .. id .. ' = '
+		local out = {}	-- result
+
+		if type(t) == "table" then
+			if tableList[t] ~= nil then
+				table.insert(out, tag .. '{} -- ' .. tableList[t] .. ' (self reference)')
+			else
+				tableList[t]= full and (full .. '.' .. id) or id
+				if next(t) then -- Table not empty
+					table.insert(out, tag .. '{')
+					for key,value in pairs(t) do
+						table.insert(out,table_r(value,key,indent .. '|  ',tableList[t]))
+					end
+					table.insert(out,indent .. '}')
+				else
+					table.insert(out,tag .. '{}')
+				end
+			end
+		else
+			local val = type(t)~="number" and type(t)~="boolean" and '"'..tostring(t)..'"' or tostring(t)
+			table.insert(out, tag .. val)
+		end
+
+		return table.concat(out, '\n')
 	end
-	SCREENMAN:SystemMessage(temp)
+
+	return table_r(t,name or 'Value',indent or '')
 end
 
-function SM(str)
-	if type(str) == "table" then
-		SMPairs(str)
+
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- GLOBAL UTILITY FUNCTIONS
+-- use these to assist in theming/scripting efforts
+
+-- shorthand for SystemMessage()
+-- displays tables recursively until the message spills off the screen
+function SM( arg )
+	if type( arg ) == "table" then
+		SCREENMAN:SystemMessage( TableToString_Recursive(arg) )
 	else
-		SCREENMAN:SystemMessage(tostring(str))
+		SCREENMAN:SystemMessage( tostring(arg) )
 	end
 end
 
