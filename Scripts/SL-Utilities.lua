d8e8df7e2d71ed443b4dfb26a5b07633f16a265d
Dan Guzek <dguzek@gmail.com>
2015-08-04 15:17:17 -0400

    more comments + error-handling in SL-Utilities.lua

--- Scripts/SL-Utilities.lua
+++ Scripts/SL-Utilities.lua
@@ -51,11 +51,21 @@ end
 -- SM()
 -- Shorthand for SCREENMAN:SystemMessage(), this is useful for
 -- rapid iterative testing by allowing us to print variables to the screen.
--- If passed a table, SM() will use the recursive TableToString (from above)
+-- If passed a table, SM() will use the TableToString_Recursive (from above)
 -- to display children recursively until the SystemMessage spills off the screen.
 function SM( arg )
+
+	-- if a table has been passed in
 	if type( arg ) == "table" then
-		SCREENMAN:SystemMessage( TableToString_Recursive(arg) )
+
+		-- recurively print its contents to a string
+		local msg = TableToString_Recursive(arg)
+		-- and SystemMessage() that string
+		SCREENMAN:SystemMessage( msg )
+
+		-- tables as strings spill off SM's screen height quickly,
+		-- so we might as well also do a proper Trace() to ./Logs/log.txt
+		Trace( msg )
 	else
 		SCREENMAN:SystemMessage( tostring(arg) )
 	end
@@ -98,13 +108,22 @@ end
 
 -- stringify() accepts an indexed table, applies tostring() to each element,
 -- and returns the results.  sprintf style format can be provided via an
--- optional second argument.
+-- optional second argument.  Note that this function will ignores key/value pairs
+-- if any are passed in via "tbl".
+--
+-- Example:
+-- 		local blah = stringify( {10, true, "hey now", asdf=10} )
+-- Result:
+-- 		blah == { "10", "true", "hey now" }
+--
+-- For an example with range()
+-- see Mini in ./Scripts/SL-PlayerOptions.lua
 function stringify( tbl, form )
 	if not tbl then return end
 
 	local t = {}
-	for i in tbl do
-		t[#t+1] = ( form and form:format(i) ) or tostring(i)
+	for _,value in ipairs(tbl) do
+		t[#t+1] = (type(value)=="number" and form and form:format(value) ) or tostring(value)
 	end
 	return t
 end
