5264a8cdd3594b7cbdd79432aa9e3e45a64eda9a
Dan Guzek <dguzek@gmail.com>
2015-08-10 22:54:41 -0400

    fix typos in GraphDisplay Barely.lua

--- Graphics/GraphDisplay Barely.lua
+++ Graphics/GraphDisplay Barely.lua
@@ -1,18 +1,17 @@
 return Def.ActorFrame{
+	InitCommand=cmd(diffusealpha, 0),
+	OnCommand=cmd( y, -20; sleep, 2; accelerate,0.2; diffusealpha,1; y, 10; decelerate,0.2; y,-5; accelerate,0.2; y,10 ),
+
 
 	Def.BitmapText{
-		Font="_misoreg hires"),
+		Font="_misoreg hires",
 		Text="Barely!",
-		OnCommand=cmd(zoom,0.75;shadowlength,0;y,-4;diffusealpha,0;addy,-20;sleep,0.5;accelerate,.2;diffusealpha,1;addy,30 )
-	),
+		InitCommand=cmd(zoom, 0.75),
+	},
 
 	Def.Sprite{
 		Texture=THEME:GetPathB("ScreenSelectMusic", "overlay/PerPlayer/arrow.png"),
-		InitCommand=cmd(rotationz,90; zoom,0.5; y,10; diffusealpha,0; addy,-20 ),
-		OnCommand=cmd(sleep,0.5;
- 			accelerate,.2; diffusealpha,1; addy,30;
-			diffuseshift; effectcolor1,1,1,1,1; effectcolor2,1,1,1,0.2;
-			decelerate,.3; addy,-10; accelerate,.3; addy,10;
-		)
+		InitCommand=cmd(rotationz,90; zoom,0.5; y,10; ),
+		OnCommand=cmd(sleep,0.5; diffuseshift; effectcolor1,1,1,1,1; effectcolor2,1,1,1,0.2 )
 	}
 }
