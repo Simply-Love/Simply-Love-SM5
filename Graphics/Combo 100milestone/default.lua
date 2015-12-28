355cc8552bb8a6a21a11007061d02e8319aac0d2
Dan Guzek <dguzek@gmail.com>
2015-12-28 14:07:47 -0500

    fix typo

--- Graphics/Combo 100milestone/default.lua
+++ Graphics/Combo 100milestone/default.lua
@@ -16,7 +16,7 @@ return Def.ActorFrame{
 		MilestoneCommand=cmd(finishtweening; diffuse, GetCurrentColor();rotationz,10;zoom,.25;diffusealpha,0.6;decelerate,0.6;rotationz,0;zoom,2;diffusealpha,0)
 	},
 
-	LoadActor(iamge.."_minisplode.png")..{
+	LoadActor(image.."_minisplode.png")..{
 		InitCommand=cmd(diffusealpha,0; blend,"BlendMode_Add";),
 		MilestoneCommand=cmd(finishtweening; diffuse, GetCurrentColor();rotationz,10;zoom,.25;diffusealpha,1;linear,0.4;rotationz,0;zoom,1.8;diffusealpha,0)
 	}
