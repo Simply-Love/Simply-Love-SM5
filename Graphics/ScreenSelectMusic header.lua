a306622a405db4ecce100add0a8449ac89ac14e5
Dan Guzek <dguzek@gmail.com>
2015-12-29 23:11:03 -0500

    fix SSM's GameMode text x-pos when MenuTimer is on

--- Graphics/ScreenSelectMusic header.lua
+++ Graphics/ScreenSelectMusic header.lua
@@ -84,8 +84,16 @@ local t = Def.ActorFrame{
 	Def.BitmapText{
 		Name="GameModeText",
 		Font="_wendy small",
-		InitCommand=cmd(diffusealpha,0; zoom,WideScale(0.5,0.6); xy,_screen.w-10, 15; halign, 1),
-		OnCommand=cmd(sleep,0.1; decelerate,0.33; diffusealpha,1; settext, THEME:GetString("ScreenSelectPlayMode", SL.Global.GameMode)),
+		InitCommand=function(self)
+			self:diffusealpha(0):zoom( WideScale(0.5,0.6)):xy(_screen.w-70, 15):halign(1)
+			if not PREFSMAN:GetPreference("MenuTimer") then
+				self:x(_screen.w-10)
+			end
+		end,
+		OnCommand=function(self)
+			self:sleep(0.1):decelerate(0.33):diffusealpha(1)
+				:settext(THEME:GetString("ScreenSelectPlayMode", SL.Global.GameMode))
+		end
 	}
 }
 
