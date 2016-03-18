a55135071703827bbc6ec33a40e1d07d0f6b7e4f
Dan Guzek <dguzek@gmail.com>
2016-03-18 18:41:53 -0400

    save profile UserPrefs to .lua rather than .cfg
    Writing a lot of little plaintext .cfg files was problematic in that everything was read back in as a string.
    
    This commit replaces the .cfg code with code that write the contents of SL[pn].ActiveModifiers to a lua file and saves that lua file to the player's profile, if there is one.
    
    This is somewhat experimental yet, and deserves further testing.

--- Scripts/SL-CustomProfiles.lua
+++ Scripts/SL-CustomProfiles.lua
@@ -1,9 +1,9 @@
-local path =  "UserPrefs/" .. THEME:GetThemeDisplayName() .. "/"
+local path =  THEME:GetThemeDisplayName() .. " UserPrefs.lua"
 
 -- Hook called during profile load
 function LoadProfileCustom(profile, dir)
 
-	local PrefPath =  dir .. path
+	local fullFilename =  dir .. path
 	local pn
 
 	-- we've been passed a profile object as the variable "profile"
@@ -18,38 +18,10 @@ function LoadProfileCustom(profile, dir)
 		end
 	end
 
-
-	-- ...and then, if a player profile exists, read .cfg files from it
 	if pn then
-
-		local f = RageFileUtil.CreateRageFile()
-		local setting
-
-		for k,v in pairs( SL[pn].ActiveModifiers ) do
-
-			local fullFilename = PrefPath..k..".cfg"
-
-			if f:Open(fullFilename,1) then
-
-				-- RageFile's Read() method always returns a string
-				setting = f:Read()
-
-				-- but maybe we don't want a string; attempt to convert
-				if setting == "true" then setting = true
-				elseif setting == "false" then setting = false
-				end
-
-				SL[pn].ActiveModifiers[k] = setting
-			else
-				local fError = f:GetError()
-				Trace( "[FileUtils] Error reading ".. fullFilename ..": ".. fError )
-				f:ClearError()
-			end
+		if FILEMAN:DoesFileExist(fullFilename) then
+			SL[pn].ActiveModifiers = LoadActor(fullFilename)
 		end
-
-		-- don't destroy the RageFile until we've tried to load all custom options
-		-- and set them to the env table to make them accessible from anywhere in SM
-		f:destroy()
 	end
 
 	return true
@@ -58,7 +30,7 @@ end
 -- Hook called during profile save
 function SaveProfileCustom(profile, dir)
 
-	local PrefPath =  dir .. path
+	local fullFilename =  dir .. path
 	local pn
 
 	local Players = GAMESTATE:GetHumanPlayers()
@@ -70,30 +42,17 @@ function SaveProfileCustom(profile, dir)
 	end
 
 	if pn then
-		-- a generic ragefile (?)
+		-- a generic ragefile
 		local f = RageFileUtil.CreateRageFile()
 
-		-- then loop through the prefs, saving one .cfg file per available setting
-		-- if a particular value is nil, nothing gets written
-		for k,v in pairs( SL[pn].ActiveModifiers ) do
-
-			local fullFilename = PrefPath..k..".cfg"
-
-			if f:Open(fullFilename, 2) then
-
-				-- if a setting exists (it should) write that to the .cfg file
-				if v ~= nil then
-					f:Write( tostring( v ) )
-				end
-			else
-				local fError = f:GetError()
-				Trace( "[FileUtils] Error writing to ".. fullFilename ..": ".. fError )
-				f:ClearError()
-			end
+		if f:Open(fullFilename, 2) then
+			f:Write( "return " .. table.tostring(SL[pn].ActiveModifiers) )
+		else
+			local fError = f:GetError()
+			Trace( "[FileUtils] Error writing to ".. fullFilename ..": ".. fError )
+			f:ClearError()
 		end
 
-		-- again, don't destroy the file until after we're done looping
-		-- through all possible custom options
 		f:destroy()
 	end
 
--- Scripts/SL-Utilities.lua
+++ Scripts/SL-Utilities.lua
@@ -43,6 +43,42 @@ local function TableToString_Recursive(t, name, indent)
 end
 
 
+function table.val_to_str ( v )
+	if "string" == type( v ) then
+		v = string.gsub( v, "\n", "\\n" )
+
+		if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+			return "'" .. v .. "'"
+		end
+		return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+	else
+		return "table" == type( v ) and table.tostring( v ) or tostring( v )
+	end
+end
+
+function table.key_to_str ( k )
+	if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+		return k
+	else
+		return "[" .. table.val_to_str( k ) .. "]"
+	end
+end
+
+function table.tostring( tbl )
+	local result, done = {}, {}
+	for k, v in ipairs( tbl ) do
+		table.insert( result, table.val_to_str( v ) )
+    	done[ k ] = true
+	end
+	for k, v in pairs( tbl ) do
+		if not done[ k ] then
+			table.insert( result, "\t" .. table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+		end
+	end
+	return "{\n" .. table.concat( result, ",\n" ) .. "\n}"
+end
+
+
 -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 -- GLOBAL UTILITY FUNCTIONS
 -- use these to assist in theming/scripting efforts
