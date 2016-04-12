f5567ae346b70b92957e5b3c0d664dcdaf4f7dc2
Dan Guzek <dguzek@gmail.com>
2016-04-12 00:30:30 -0400

    fix ColumnFlashOnMiss to work with jumps and holds
    My previous means of getting the note column, params.FirstTrack, would (surprise!) only get the first column, which prevented missed jumps from flashing both columns.  It was better to use params.Notes, which is a table of notes that were just judged, and loop through.

--- BGAnimations/ScreenGameplay underlay/PerPlayer/ColumnFlashOnMiss.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/ColumnFlashOnMiss.lua
@@ -2,6 +2,7 @@ local NumColumns = GAMESTATE:GetCurrentStyle():ColumnsPerPlayer()
 local player = ...
 local pn = ToEnumShortString(player)
 local mods = SL[pn].ActiveModifiers
+local columns = {}
 
 if mods.ColumnFlashOnMiss then
 
@@ -16,33 +17,32 @@ if mods.ColumnFlashOnMiss then
 			-- a mini value like 150% to a zoom factor like 0.25
 			local zoom_factor = 1 - scale( mods.Mini:gsub("%%","")/100, 0, 2, 0, 1)
 			self:zoom( zoom_factor )
+		end,
+		JudgmentMessageCommand=function(self, params)
+			if params.Player == player and (params.Notes or params.Holds) then
+				for i,col in pairs(params.Notes or params.Holds) do
+					local tns = ToEnumShortString(params.TapNoteScore or params.HoldNoteScore)
+					if tns == "Miss" or tns == "MissedHold" then
+						columns[i]:playcommand("Flash")
+					end
+				end
+			end
 		end
 	}
 
-	for ColumnIndex=0,NumColumns-1 do
+	for ColumnIndex=1,NumColumns do
 		af[#af+1] = Def.Quad{
 			InitCommand=function(self)
+				columns[ColumnIndex] = self
+				local style = GAMESTATE:GetCurrentStyle(player)
+				local width = style:GetWidth(player)
 				self:diffuse(0,0,0,0)
-
-				if not mods.ColumnFlashOnMiss then
-					self:hibernate(math.huge)
-				else
-					local style = GAMESTATE:GetCurrentStyle(player)
-					local width = style:GetWidth(player)
-					self:x((ColumnIndex-1.5) * (width/NumColumns))
-						:setsize(width/NumColumns, _screen.h*100)
-				end
+					:x((ColumnIndex-2.5) * (width/NumColumns))
+					:setsize(width/NumColumns, _screen.h*100)
 	        end,
-			JudgmentMessageCommand=function(self, params)
-				if mods.ColumnFlashOnMiss then
-					if params.Player == player and params.FirstTrack == ColumnIndex then
-						local tns = ToEnumShortString(params.TapNoteScore)
-						if tns == "Miss" then
-							self:diffuse(1,0,0,0.66)
-								:accelerate(0.165):diffuse(0,0,0,0)
-						end
-					end
-				end
+			FlashCommand=function(self)
+				self:diffuse(1,0,0,0.66)
+					:accelerate(0.165):diffuse(0,0,0,0)
 			end
 		}
 	end
