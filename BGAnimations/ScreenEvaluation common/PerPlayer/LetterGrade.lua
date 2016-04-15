60c3dcdb45f891412232205a1d8d9cf972365728
Dan Guzek <dguzek@gmail.com>
2016-04-15 14:02:00 -0400

    add ScreenEval easter egg
    "I passd with a Q though."

--- BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua
@@ -4,6 +4,10 @@ if SL.Global.GameMode ~= "StomperZ" then
 	local playerStats = STATSMAN:GetCurStageStats():GetPlayerStageStats(pn)
 	local grade = playerStats:GetGrade()
 
+	-- "I passd with a q though."
+	local title = GAMESTATE:GetCurrentSong():GetDisplayFullTitle()
+	if title == "D" then grade = "Grade_Tier99" end
+
 	return LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
 		InitCommand=cmd(xy, 70, _screen.cy-134),
 		OnCommand=function(self)
