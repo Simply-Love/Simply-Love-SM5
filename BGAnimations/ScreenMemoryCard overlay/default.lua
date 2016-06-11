be986e968f1b1a39d3a8dc13f6ba86a578471c37
Dan Guzek <dguzek@gmail.com>
2016-06-11 15:08:43 -0400

    make some elements easier to see in RainbowMode
    This commit adds shadow to the text on ScreenMemoryCard and slightly changes the MusicWheel's highlight element.

--- BGAnimations/ScreenMemoryCard overlay/default.lua
+++ BGAnimations/ScreenMemoryCard overlay/default.lua
@@ -1,20 +1,23 @@
 return Def.ActorFrame{
+	InitCommand=function(self) self:Center() end,
 
 	LoadActor("usbicon.png")..{
-		OnCommand=cmd(zoom,0.6;glow,1,1,1,1;glowshift;x,_screen.cx;y,_screen.cy;diffusealpha,0;sleep,1.0;decelerate,2;diffusealpha,1;sleep,6;linear,0.75;diffusealpha,0);
-		OffCommand=cmd(stoptweening;accelerate,0.5;addx,-_screen.w*1.5);
-	};
-	
-	LoadFont("_miso")..{
-		Text="Use a USB card";
-		OnCommand=cmd(x,_screen.cx;y,_screen.cy-60;diffusealpha,0;sleep,2.0;decelerate,1;diffusealpha,1;sleep,6;linear,0.75;diffusealpha,0);
-		OffCommand=cmd(stoptweening;accelerate,0.5;addx,-_screen.w*1.5);
-	};
-	
-	LoadFont("_miso")..{
-		Text="to save scores and preferences.";
-		OnCommand=cmd(x,_screen.cx;y,_screen.cy+60;diffusealpha,0;sleep,3.0;decelerate,1;diffusealpha,1;sleep,5;linear,0.75;diffusealpha,0);
-		OffCommand=cmd(stoptweening;accelerate,0.5;addx,-_screen.w*1.5);
-	};
+		InitCommand=function(self) self:shadowlength(1) end,
+		OnCommand=cmd(zoom,0.6; glow,1,1,1,1; glowshift; diffusealpha,0; sleep,1.0; decelerate,2; diffusealpha,1;sleep,6;linear,0.75;diffusealpha,0),
+		OffCommand=cmd(stoptweening;accelerate,0.5;addx,-_screen.w*1.5)
+	},
 
-};
+	LoadFont("_miso")..{
+		Text=ScreenString("Top"),
+		InitCommand=function(self) self:shadowlength(1):y(-60):diffusealpha(0) end,
+		OnCommand=cmd(sleep,2.0; decelerate,1; diffusealpha,1; sleep,6; linear,0.75; diffusealpha,0),
+		OffCommand=cmd(stoptweening;accelerate,0.5;addx,-_screen.w*1.5)
+	},
+
+	LoadFont("_miso")..{
+		Text=ScreenString("Bottom"),
+		InitCommand=function(self) self:shadowlength(1):y(60):diffusealpha(0) end,
+		OnCommand=cmd(sleep,3.0; decelerate,1; diffusealpha,1; sleep,5; linear,0.75; diffusealpha,0),
+		OffCommand=cmd(stoptweening;accelerate,0.5;addx,-_screen.w*1.5)
+	}
+}
