88e58608b6e48f6f5f855d020afd616845f8a84b
Dan Guzek <dguzek@gmail.com>
2016-09-15 18:54:22 -0400

    gray surround LifeMeter for ECFA GameMode
    Previously, ECFA's surround LifeMeter had been colored in the style of StomperZ.  It should not have been, as those colors (purple for full life, for example) are associated with Rhythm Horizon only, not ECFA.
    
    This commit introduces a plain gray surround LifeMeter for ECFA.
    
    As always, there may be bugs lurking yet.

--- BGAnimations/ScreenGameplay underlay/PerPlayer/LifeMeter/default.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/LifeMeter/default.lua
@@ -1,12 +1,5 @@
 if SL.Global.GameMode ~= "Casual" then
 	local player = ...
-	local lifemeter
-	
-	if SL.Global.GameMode == "StomperZ" or SL.Global.GameMode == "ECFA" then
-		lifemeter = LoadActor("StomperZ.lua", player)
-	else
-		lifemeter = LoadActor("Competitive.lua", player)
-	end
-	
+	local lifemeter = LoadActor(SL.Global.GameMode .. ".lua", player)
 	return lifemeter
 end
