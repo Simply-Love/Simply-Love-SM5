b6f6bdb08a1e0df900b3f39d2713cb42048f5abf
Dan Guzek <dguzek@gmail.com>
2016-10-05 01:27:51 -0400

    get EditMode functioning again
    This needs some visual refinement, yet, but it at least it's working again.  Many thanks go to kyzentun for doing most of the work here.

--- BGAnimations/ScreenEditMenu underlay.lua
+++ BGAnimations/ScreenEditMenu underlay.lua
@@ -1,156 +1,220 @@
---HACK: Reset MusicRate to 1.0 every time this screen is accessed
--- because, otherwise, it can persist from editing one song with a
--- ratemod applied into another, different song.
---
--- see: OptionRowSongMusicRate() in ./Scripts/SL-PlayerOptions.lua
-GAMESTATE:ApplyGameCommand("mod,1.0xmusic")
-SL.Global.ActiveModifiers.MusicRate = 1
+local steps_item_space= _screen.h*.05
+local steps_item_width= steps_item_space * .75
+local steps_type_item_space= _screen.h*.06
 
-
--- down lower in this file, we loop seven times to create seven "rows" of grey quads
--- one for each: group, song, type, steps, fill type, fill steps, action
--- in that loop, rowYvalues will be filled with each row's Y value (defined in metrics)
-local rowYvalues = {};
-
-
-local t = Def.ActorFrame{
-	InitCommand=cmd(diffusealpha,0);
-	OnCommand=cmd(linear,0.15; diffusealpha,1; queuecommand, "Hax");
-	--playcommand seems more responsive than queuecommand, so use it here
-	--or else we see a frame or two where the cursor color hasn't been applied yet (HAX)
-	EditMenuChangeMessageCommand=cmd(playcommand, "Hax");
-	HaxCommand=function(self)
-
-		local topscreen = SCREENMAN:GetTopScreen();
-
-		if topscreen then
-			local editMenu = topscreen:GetChild("EditMenu");
-
-			local cursor = editMenu:GetChild("");
-			local cursorY = cursor:GetY();
-
-			local songTextBanner = editMenu:GetChild("SongTextBanner");
-			local rowHighlight = self:GetChild("RowHighlight");
-
-
-			for i=1,#rowYvalues do
-
-				-- if this row is "active"
-				if cursorY == rowYvalues[i] then
-					editMenu:GetChild("Label"..i):diffuse( Color.Black )
-					editMenu:GetChild("Label"..i):shadowlength(0)
-					editMenu:GetChild("Value"..i):diffuse( Color.Black )
-					editMenu:GetChild("Value"..i):shadowlength(0)
-
-					-- row2 has a textbanner and needs to be handled differently
-					if cursorY == rowYvalues[2] then
-						songTextBanner:GetChild("Title"):diffuse( Color.Black )
-						songTextBanner:GetChild("Title"):shadowlength(0)
-						songTextBanner:GetChild("Subtitle"):diffuse( Color.Black )
-						songTextBanner:GetChild("Subtitle"):shadowlength(0)
-					end
-
-					if cursorY == rowYvalues[4] then
-						editMenu:GetChild("StepsDisplay"):GetChild("Meter"):diffuse( Color.Black )
-					end
-					if cursorY == rowYvalues[6] then
-						editMenu:GetChild("StepsDisplaySource"):GetChild("Meter"):diffuse( Color.Black )
-					end
-
-				else
-					editMenu:GetChild("Label"..i):diffuse(color("#FFFFFF"))
-					editMenu:GetChild("Value"..i):diffuse(color("#FFFFFF"))
-					editMenu:GetChild("Label"..i):shadowlength(1)
-					editMenu:GetChild("Value"..i):shadowlength(1)
-
-					if cursorY ~= rowYvalues[2] then
-						songTextBanner:GetChild("Title"):diffuse(color("#FFFFFF"))
-						songTextBanner:GetChild("Title"):shadowlength(1)
-						songTextBanner:GetChild("Subtitle"):diffuse(color("#FFFFFF"))
-						songTextBanner:GetChild("Subtitle"):shadowlength(1)
-					end
-
-					if cursorY ~= rowYvalues[4] then
-						editMenu:GetChild("StepsDisplay"):GetChild("Meter"):diffuse(color("#FFFFFF"))
-					end
-					if cursorY ~= rowYvalues[6] then
-						editMenu:GetChild("StepsDisplaySource"):GetChild("Meter"):diffuse(color("#FFFFFF"))
-					end
-				end
-
-				rowHighlight:y(cursorY+1)
-				if cursorY == rowYvalues[7] then
-					rowHighlight:diffuse(PlayerColor(PLAYER_2))
-				else
-					rowHighlight:diffuse(GetCurrentColor())
-				end
-			end
-
-
-		end
-	end,
-	-- MeterSetMessage is broadcast from Metrics under [StepsDisplay] MeterSetCommand
-	-- I'm (ab)using it here to force the meter on Row4 to always be black
-	-- even when the user is sitting on Row4 flipping between difficulties
-	MeterSetMessageCommand=function(self)
-		local topscreen = SCREENMAN:GetTopScreen()
-
-		if topscreen then
-
-			local editMenu = topscreen:GetChild("EditMenu")
-
-			local cursor = editMenu:GetChild("")
-			if cursor then
-				local cursorY = cursor:GetY()
-				if cursorY == rowYvalues[4] then
-					editMenu:GetChild("StepsDisplay"):GetChild("Meter"):diffuse( Color.Black )
-				end
-			end
-		end
-	end
-}
-
-
--- the overall BG
-t[#t+1] = Def.Quad {
-	InitCommand=cmd(zoomto,_screen.w*0.9,_screen.h*0.725; diffuse,Color.Black),
-	OnCommand=cmd(xy,_screen.cx,_screen.cy)
-}
-
-
--- loop seven times to create seven dark-grey rows
-for i=1,7 do
-	-- a row
-	t[#t+1] = Def.Quad {
-		InitCommand=cmd(zoomto,_screen.w*0.745, _screen.h*0.0885; diffuse,color("#071016")),
-		OnCommand=cmd(xy, _screen.cx + WideScale(49,65), 40 + (i*45))
-	}
-
-	-- fill our table with each row's Y value from the Metrics
-	rowYvalues[#rowYvalues+1] = THEME:GetMetric("EditMenu", "Row"..i.."Y")
-
+local steps_display= setmetatable({disable_wrapping= true}, item_scroller_mt)
+local function steps_transform(self, item_index, num_items)
+	self.container:xy((item_index-.5) * steps_item_space, 15)
 end
+local function stype_transform(self, item_index, num_items)
+	self.container:y((item_index-1) * steps_type_item_space)
+end
+local stype_item_mt= edit_pick_menu_steps_display_item(
 
--- the grey BG for row labels
-t[#t+1] = Def.Quad {
-	InitCommand = cmd(Center;zoomto,_screen.w*0.15,_screen.h*0.725;diffuse,color("#212831"); x, _screen.cx-WideScale(240,320))
+	stype_transform,
+
+	-- StepsType text
+	Def.BitmapText{
+		Font= "Common Normal", InitCommand= function(self)
+			self:horizalign(left)
+		end,
+		SetCommand= function(self, param)
+			self:settext(THEME:GetString("LongStepsType", ToEnumShortString(param.stype)))
+		end,
+	},
+
+	steps_transform,
+
+	-- Steps meter text
+	Def.BitmapText{
+		Font= "Common Normal",
+		InitCommand= function(self)
+
+		end,
+		SetCommand= function(self, param)
+			self:settext(param.steps:GetMeter())
+				:diffuse(GameColor.Difficulty[param.steps:GetDifficulty()])
+				-- width_limit_text(self, steps_item_width, .5)
+		end,
+	}
+)
+
+local picker_width= _screen.w * .5
+local spacer= _screen.h * .05
+local jacket_size= _screen.h * .2
+local jacket_x= _screen.w - spacer - (jacket_size / 2)
+local jacket_y= _screen.h * .2
+local banner_width= _screen.w - picker_width - jacket_size - (spacer * 3)
+local banner_height= _screen.h * .2
+local banner_x= jacket_x - spacer - (jacket_size / 2) - (banner_width / 2)
+local banner_y= jacket_y
+local bpm_x= jacket_x + (jacket_size / 2)
+local bpm_y= jacket_y + (jacket_size / 2) + (spacer / 2)
+local length_x= bpm_x
+local length_y= bpm_y + (spacer / 1.5)
+local title_x= banner_x - (banner_width / 2)
+local title_y= bpm_y
+local artist_x= title_x
+local artist_y= length_y
+local steps_display_x= title_x
+local steps_display_y= title_y + (spacer * 1.75)
+local steps_display_items= (_screen.h - steps_display_y) / steps_type_item_space
+
+local menu_params= {
+	name= "menu", x= _screen.cx*.5, y= 10, width= _screen.cx,
+	translation_section= "ScreenEditMenu",
+	menu_sounds= {
+		pop= THEME:GetPathS("Common", "Cancel"),
+		push= THEME:GetPathS("_common", "row"),
+		act= THEME:GetPathS("Common", "start"),
+		move= THEME:GetPathS("_switch", "down"),
+		move_up= THEME:GetPathS("_next", "row"),
+		move_down= THEME:GetPathS("_prev", "row"),
+		inc= THEME:GetPathS("_switch", "up"),
+		dec= THEME:GetPathS("_switch", "down"),
+	},
+	num_displays= 1, el_height= 24, display_params= {
+		no_status= true,
+		height= _screen.h-32, el_zoom= menu_zoom,
+		item_mt= cons_option_item_mt, item_params= {
+			text_commands= {
+				Font= "Common Normal", OnCommand= function(self)
+					self:diffusealpha(0):linear(1):diffusealpha(1)
+				end,
+			},
+			text_width= .7,
+			value_text_commands= {
+				Font= "Common Normal", OnCommand= function(self)
+					self:diffusealpha(0):linear(1):diffusealpha(1)
+				end,
+			},
+			value_image_commands= {
+				OnCommand= function(self)
+					self:diffusealpha(0):linear(1):diffusealpha(1)
+				end,
+			},
+			value_width= .25,
+			type_images= {
+				bool= THEME:GetPathG("", "menu_icons/bool"),
+				choice= THEME:GetPathG("", "menu_icons/bool"),
+				menu= THEME:GetPathG("", "menu_icons/menu"),
+			},
+		},
+	},
 }
 
--- the grey BG for the instructions at the bottom
-t[#t+1] = Def.Quad {
-	InitCommand=cmd(zoomto,_screen.w*0.745,_screen.h*0.0725; diffuse,color("#212831")),
-	OnCommand=cmd(xy, _screen.cx + WideScale(49,65), 396)
+local frame= Def.ActorFrame{
+	edit_menu_selection_changedMessageCommand=edit_pick_menu_update_steps_display_info(steps_display),
+
+	-- semi-transparent fullscreen quad to darken the bg
+	Def.Quad{
+		InitCommand=function(self)
+			self:Center():FullScreen():diffuse(Color.Black):diffusealpha(0.8)
+		end
+	},
+
+	-- a simple line to divide the screen in half
+	Def.Quad{
+		InitCommand=function(self)
+			self:Center():zoomto(2, _screen.h):cropbottom(1)
+		end,
+		OnCommand=function(self) self:linear(0.5):cropbottom(0) end
+ 	},
+
+	edit_pick_menu_actor(menu_params),
+
+
+	Def.Sprite{
+		Name= "banner", InitCommand= function(self)
+			self:xy(banner_x, banner_y)
+		end,
+		edit_menu_selection_changedMessageCommand= function(self, params)
+			if params.group then
+				local path= SONGMAN:GetSongGroupBannerPath(params.group)
+				if #path > 0 then
+					self:visible(true)
+					self:LoadBanner(path)
+					scale_to_fit(self, banner_width, banner_height)
+				else
+					self:visible(false)
+				end
+			elseif params.song then
+				if params.song:HasBanner() then
+					self:visible(true)
+					self:LoadBanner(params.song:GetBannerPath())
+					scale_to_fit(self, banner_width, banner_height)
+				else
+					self:visible(false)
+				end
+			end
+		end,
+	},
+	Def.BitmapText{
+		Name= "length",
+		Font= "Common Normal",
+		InitCommand= function(self)
+			self:xy(length_x, length_y):horizalign(right):zoom(1)
+		end,
+		edit_menu_selection_changedMessageCommand= function(self, params)
+			if params.group then
+				self:visible(false)
+			elseif params.song then
+				self:settext(SecondsToMSS(params.song:MusicLengthSeconds()))
+				self:visible(true)
+			end
+		end,
+	},
+	Def.BitmapText{
+		Name= "bpm",
+		Font= "Common Normal",
+		InitCommand= function(self)
+			self:xy(bpm_x, bpm_y):horizalign(right):zoom(1)
+		end,
+		edit_menu_selection_changedMessageCommand= function(self, params)
+			if params.group then
+				self:visible(false)
+			elseif params.song then
+				local display_bpm= params.song:GetDisplayBpms()
+				if display_bpm[1] == display_bpm[2] then
+					self:settextf("%d BPM", display_bpm[1])
+				else
+					self:settextf("%d - %d BPM", math.round(display_bpm[1]), math.round(display_bpm[2]))
+				end
+				self:visible(true)
+			end
+		end,
+	},
+	Def.BitmapText{
+ 		Name= "title",
+		Font= "Common Normal",
+		InitCommand= function(self)
+			self:xy(title_x, title_y):horizalign(left):zoom(1)
+		end,
+		edit_menu_selection_changedMessageCommand= function(self, params)
+			if params.group then
+				self:visible(false)
+			elseif params.song then
+				self:settext(params.song:GetDisplayMainTitle())
+				self:visible(true)
+			end
+		end,
+	},
+	Def.BitmapText{
+ 		Name= "artist",
+		Font= "Common Normal",
+		InitCommand= function(self)
+			self:xy(artist_x, artist_y):horizalign(left):zoom(1)
+		end,
+		edit_menu_selection_changedMessageCommand= function(self, params)
+			if params.group then
+				self:visible(false)
+			elseif params.song then
+				self:settext(params.song:GetDisplayArtist())
+				self:visible(true)
+			end
+		end,
+	},
+	steps_display:create_actors("steps_display", steps_display_items, stype_item_mt, steps_display_x, steps_display_y),
 }
 
-t[#t+1] = Def.Quad {
-	Name="RowHighlight",
-	OnCommand=cmd(x, _screen.cx; setsize, _screen.w*0.9 - 4,_screen.h*0.088)
-}
-
-
--- white border
-t[#t+1] = Border(_screen.w*0.9, _screen.h*0.734, 2) .. {
-	InitCommand = cmd(xy,_screen.cx, _screen.cy),
-}
-
-return t
+return frame
