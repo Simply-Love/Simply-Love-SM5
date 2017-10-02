34b47f12095b6034f07e38e747185d0317c859ed
Horsey <cheval.de.jeanvaljean@gmail.com>
2017-10-01 20:26:58 -0700

    finish merging from Dguzek with banner.lua
    merged new banner.lua with positioning values from the Horsey branch

--- BGAnimations/ScreenSelectMusic overlay/banner.lua
+++ BGAnimations/ScreenSelectMusic overlay/banner.lua
@@ -1,4 +1,5 @@
 local banner_directory = ThemePrefs.Get("VisualTheme")
+local SongOrCourse, banner
 
 local t = Def.ActorFrame{
 	OnCommand=function(self)
@@ -6,25 +7,38 @@ local t = Def.ActorFrame{
 			self:zoom(0.7655)
 			self:xy(_screen.cx - 197, 102)
 		else
-			self:zoom(0.74)
-			self:xy(_screen.cx - 165, 102)
+			self:zoom(0.75)
+			self:xy(_screen.cx - 166, 102)
 		end
 	end,
 
-	LoadActor("colored_banners/".. banner_directory .."/banner"..SL.Global.ActiveColorIndex.." (doubleres).png" )..{
-		Name="FallbackBanner",
-		OnCommand=cmd(rotationy,180; setsize,418,164; diffuseshift; effectoffset,3; effectperiod, 6; effectcolor1, 1,1,1,0; effectcolor2, 1,1,1,1)
-	},
+	Def.ActorFrame{
+		CurrentSongChangedMessageCommand=function(self) self:playcommand("Set") end,
+		CurrentCourseChangedMessageCommand=function(self) self:playcommand("Set") end,
+		SetCommand=function(self)
+			SongOrCourse = GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentCourse() or GAMESTATE:GetCurrentSong()
+			if SongOrCourse and SongOrCourse:HasBanner() then
+				self:visible(false)
+			else
+				self:visible(true)
+			end
+		end,
 
-	LoadActor("colored_banners/".. banner_directory .."/banner"..SL.Global.ActiveColorIndex.." (doubleres).png" )..{
-		Name="FallbackBanner",
-		OnCommand=cmd(diffuseshift; effectperiod, 6; effectcolor1, 1,1,1,0; effectcolor2, 1,1,1,1; setsize, 418,164)
+		LoadActor("colored_banners/".. banner_directory .."/banner"..SL.Global.ActiveColorIndex.." (doubleres).png" )..{
+			Name="FallbackBanner",
+			OnCommand=cmd(rotationy,180; setsize,418,164; diffuseshift; effectoffset,3; effectperiod, 6; effectcolor1, 1,1,1,0; effectcolor2, 1,1,1,1)
+		},
+
+		LoadActor("colored_banners/".. banner_directory .."/banner"..SL.Global.ActiveColorIndex.." (doubleres).png" )..{
+			Name="FallbackBanner",
+			OnCommand=cmd(diffuseshift; effectperiod, 6; effectcolor1, 1,1,1,0; effectcolor2, 1,1,1,1; setsize, 418,164)
+		},
 	},
 
 	Def.ActorProxy{
 		Name="BannerProxy",
 		BeginCommand=function(self)
-			local banner = SCREENMAN:GetTopScreen():GetChild('Banner')
+			banner = SCREENMAN:GetTopScreen():GetChild('Banner')
 			self:SetTarget(banner)
 		end
 	}
