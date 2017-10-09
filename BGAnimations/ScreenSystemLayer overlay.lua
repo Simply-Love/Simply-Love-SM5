Simply-Love/Simply-Love-SM5

commit b97fec56547bc752fb13d75fd12efb92519965bf

cleanup SystemLayer overlay
This commit is for me and the few others who debug in Simply Love using SCREENMAN:SystemMessage(thing) or the shorthand SM(thing) function.

When doing rapid debugging with SM() to print the value of variables to the screen, there was previously a Quad that was hardcoded to be 30px in height behind the text.  If the text was taller than 30px, it spilled out, over the Quad.  This was (mildly) annoying to me, so I fixed it to re-zoom the Quad's to accommodate the height of the SystemMessage text.

This also removes a large if-else chain that was (for once) better handled with a few additional lines in Metrics.ini.

BGAnimations/ScreenSystemLayer overlay.lua
@@ -38,11 +38,12 @@ t[#t+1] = Def.ActorFrame {
 	CreditsText( PLAYER_2 );
 };
 
+local SystemMessageText = nil
 
 -- SystemMessage Text
 t[#t+1] = Def.ActorFrame {
 	SystemMessageMessageCommand=function(self, params)
-		self:GetChild("Text"):settext( params.Message )
+		SystemMessageText:settext( params.Message )
 		self:playcommand( "On" )
 		if params.NoAnimate then
 			self:finishtweening()
@@ -50,18 +51,28 @@ t[#t+1] = Def.ActorFrame {
 		self:playcommand( "Off" )
 	end,
 	HideSystemMessageMessageCommand=cmd(finishtweening),
-	
+
 	Def.Quad {
-		InitCommand=cmd(zoomto,_screen.w, 30; horizalign,left; vertalign,top; diffuse, Color.Black; diffusealpha,0 ),
-		OnCommand=cmd(finishtweening; diffusealpha,0.85 ),
-		OffCommand=cmd(sleep,3; linear,0.5; diffusealpha,0 )
+		InitCommand=function(self)
+			self:zoomto(_screen.w, 30):horizalign(left):vertalign(top)
+				:diffuse(Color.Black):diffusealpha(0)
+		end,
+		OnCommand=function(self)
+			self:finishtweening():diffusealpha(0.85)
+				:zoomto(_screen.w, (SystemMessageText:GetHeight() + 16) * 0.8 )
+		end,
+		OffCommand=function(self) self:sleep(3):linear(0.5):diffusealpha(0) end,
 	},
 
 	LoadFont("_miso")..{
 		Name="Text",
-		InitCommand=cmd(maxwidth,750; horizalign,left; vertalign,top; xy,SCREEN_LEFT+10, 10; diffusealpha,0),
-		OnCommand=cmd(finishtweening; diffusealpha,1; zoom,0.8 ),
-		OffCommand=cmd(sleep,3; linear,0.5; diffusealpha,0 )
+		InitCommand=function(self)
+			self:maxwidth(750):horizalign(left):vertalign(top)
+				:xy(SCREEN_LEFT+10, 10):diffusealpha(0):zoom(0.8)
+			SystemMessageText = self
+		end,
+		OnCommand=function(self) self:finishtweening():diffusealpha(1) end,
+		OffCommand=function(self) self:sleep(3):linear(0.5):diffusealpha(0) end,
 	}
 }
 
@@ -77,25 +88,16 @@ t[#t+1] = LoadFont("_wendy small")..{
 	RefreshCommand=function(self)
 
 		local screen = SCREENMAN:GetTopScreen()
-		local bShow = true
-		if screen then
-			local sClass = screen:GetName()
-			bShow = THEME:GetMetric( sClass, "ShowCreditDisplay" )
 
-			-- hide  just this centered credit text for certain screens,
-			-- where it would more likely just be distracting and superfluous
-			if sClass == "ScreenPlayerOptions"
-				or sClass == "ScreenPlayerOptions2"
-				or sClass == "ScreenEvaluationStage"
-				or sClass == "ScreenEvaluationNonstop"
-				or sClass == "ScreenEvaluationSummary"
-				or sClass == "ScreenNameEntryTraditional"
-				or sClass == "ScreenGameOver" then
-				bShow = false
+		if screen then
+			-- if this screen's Metric for ShowCreditDisplay=false, then hide this BitmapText actor
+			if not (THEME:GetMetric( screen:GetName(), "ShowCreditDisplay" )) then
+				self:settext(""):visible(false)
+				return
 			end
 		end
 
-		self:visible( bShow )
+		self:visible( true )
 
 		if PREFSMAN:GetPreference("EventMode") then
 			self:settext('EVENT MODE')

metrics.ini
@@ -326,6 +326,7 @@ FooterOnCommand=visible,false
 TimerSeconds=SL.Global.MenuTimer.ScreenNameEntry
 TimerOnCommand=hide_if,not PREFSMAN:GetPreference("MenuTimer");draworder,99;
 ForceTimer=false
+ShowCreditDisplay=false
 
 [ScreenProfileSaveSummary]
 Fallback="ScreenProfileSave"
@@ -338,6 +339,7 @@ Fallback="ScreenAttract"
 PrevScreen=Branch.AfterInit()
 NextScreen=Branch.AfterInit()
 HeaderOnCommand=visible,false
+ShowCreditDisplay=false
 TimerSeconds=30
 ForceTimer=true
 TimerOnCommand=visible,false
@@ -630,6 +632,7 @@ TimerOnCommand=hide_if,not PREFSMAN:GetPreference("MenuTimer");draworder,99;
 TimerOffCommand=
 ForceTimer=false
 ShowHeader=true
+ShowCreditDisplay=false
 
 OptionRowExitMetricsGroup="OptionRowExit"
 RowPositionTransformFunction=function(self,positionIndex,itemIndex,numItems) self:y( (_screen.cy-170) + (_screen.h*0.065*positionIndex) ); end
@@ -1462,6 +1465,7 @@ Fallback="ComboGraph"
 TimerSeconds=SL.Global.MenuTimer.ScreenEvaluation
 ShowHeader=true
 FooterOnCommand=visible,false
+ShowCreditDisplay=false
 
 BannerWidth=334
 BannerHeight=132
@@ -1486,6 +1490,7 @@ Fallback="ScreenEvaluation"
 PrevScreen=Branch.AllowScreenNameEntry()
 NextScreen=Branch.AllowScreenNameEntry()
 TimerSeconds=SL.Global.MenuTimer.ScreenEvaluationSummary
+ShowCreditDisplay=false
 
 # when true, this metric appears to sum all of the player's Window types together
 # for exmaple, all W1 values are summed, all W2 values are summed, etc.

commit 20805f402e80aecd3e1bda40b6c18a0b37207d1a

bring back player profile name(s) to ScreenEval
This bug was introduced in b97fec5.

BGAnimations/ScreenSystemLayer overlay.lua
@@ -2,41 +2,39 @@
 -- very minor modifications.
 
 local function CreditsText( pn )
-	local text = LoadFont("_miso") .. {
+	return LoadFont("_miso") .. {
 		InitCommand=function(self)
 			self:visible(false)
 			self:name("Credits" .. PlayerNumberToString(pn))
 			ActorUtil.LoadAllCommandsAndSetXY(self,Var "LoadingScreen")
-		end;
+		end,
 		UpdateTextCommand=function(self)
 			local str = ScreenSystemLayerHelpers.GetCreditsMessage(pn)
 			self:settext(str)
-		end;
+		end,
 		UpdateVisibleCommand=function(self)
 			local screen = SCREENMAN:GetTopScreen()
 			local bShow = true
-			if screen then
-				local sClass = screen:GetName()
-				bShow = THEME:GetMetric( sClass, "ShowCreditDisplay" )
+			-- we always want to show the CreditText for each player on ScreenEval, regardless of the ShowCreditDisplay metric
+			if screen and (screen:GetName() ~= "ScreenEvaluationStage") and (screen:GetName() ~= "ScreenEvaluationNonstop") then
+				bShow = THEME:GetMetric( screen:GetName(), "ShowCreditDisplay" )
 			end
-
 			self:visible( bShow )
 		end
 	}
-	return text
 end
 
 
-local t = Def.ActorFrame {}
+local t = Def.ActorFrame{}
 
 -- Aux
-t[#t+1] = LoadActor(THEME:GetPathB("ScreenSystemLayer","aux"));
+t[#t+1] = LoadActor(THEME:GetPathB("ScreenSystemLayer","aux"))
 
 -- Credits
 t[#t+1] = Def.ActorFrame {
  	CreditsText( PLAYER_1 );
 	CreditsText( PLAYER_2 );
-};
+}
 
 local SystemMessageText = nil
 
@@ -76,28 +74,23 @@ t[#t+1] = Def.ActorFrame {
 	}
 }
 
--- Centered Credit Text
+-- Wendy CreditText at lower-center of screen
 t[#t+1] = LoadFont("_wendy small")..{
 	InitCommand=cmd(xy, _screen.cx, _screen.h-16; zoom,0.5; horizalign,center ),
 
-	OnCommand=cmd(playcommand,"Refresh"),
-	ScreenChangedMessageCommand=cmd(playcommand,"Refresh"),
-	CoinModeChangedMessageCommand=cmd(playcommand,"Refresh"),
-	CoinsChangedMessageCommand=cmd(playcommand,"Refresh"),
+	OnCommand=function(self) self:playcommand("Refresh") end,
+	ScreenChangedMessageCommand=function(self) self:playcommand("Refresh") end,
+	CoinModeChangedMessageCommand=function(self) self:playcommand("Refresh") end,
+	CoinsChangedMessageCommand=function(self) self:playcommand("Refresh") end,
 
 	RefreshCommand=function(self)
 
 		local screen = SCREENMAN:GetTopScreen()
 
+		-- if this screen's Metric for ShowCreditDisplay=false, then hide this BitmapText actor
 		if screen then
-			-- if this screen's Metric for ShowCreditDisplay=false, then hide this BitmapText actor
-			if not (THEME:GetMetric( screen:GetName(), "ShowCreditDisplay" )) then
-				self:settext(""):visible(false)
-				return
-			end
+			self:visible( THEME:GetMetric( screen:GetName(), "ShowCreditDisplay" ) )
 		end
-
-		self:visible( true )
 
 		if PREFSMAN:GetPreference("EventMode") then
 			self:settext('EVENT MODE')