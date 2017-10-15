6659130a85a5bc70223215e551d9b838d5122382
Dan Guzek <dguzek@gmail.com>
2017-10-15 07:30:10 -0400

    fix bug with 1x speedmod on ScreenEvaluation
    Previously, if a player was using a speedmod of 1x, it would not show up on ScreenEvaluation's list modifiers for that player.  This fixes that by checking the values from the SL[pn].ActiveModifiers table, and manually adding "1x, " to start of the list if necessary.

--- BGAnimations/ScreenEvaluation common/PerPlayer/PlayerModifiers.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/PlayerModifiers.lua
@@ -1,11 +1,18 @@
 if SL.Global.GameMode ~= "Casual" then
 	local player = ...
+	local pn = ToEnumShortString(player)
 
 	local PlayerState = GAMESTATE:GetPlayerState(player)
-	-- grab the song options from this PlayerState.
+	-- grab the song options from this PlayerState
 	local PlayerOptions = PlayerState:GetPlayerOptionsArray(0)
 	-- start with an empty string...
-	local optionslist= ""
+	local optionslist = ""
+
+	-- if the player used an xMod of 1x, it won't be in PlayerOptions list
+	-- so check here, and add it in manually if necessary
+	if SL[pn].ActiveModifiers.SpeedModType == "x" and SL[pn].ActiveModifiers.SpeedMod == 1 then
+		optionslist = "1x, "
+	end
 
 	local TimingWindowScale = round(PREFSMAN:GetPreference("TimingWindowScale") * 100)
 
@@ -14,8 +21,9 @@ if SL.Global.GameMode ~= "Casual" then
 
 		-- these don't need to show up in the mods list
 		if option ~= "FailAtEnd" and option ~= "FailImmediateContinue" and option ~= "FailImmediate" then
+			-- 100% Mini will be in the PlayerOptions as just "Mini" so use the value from the SL table instead
 			if option:match("Mini") then
-				option = SL[ToEnumShortString(player)].ActiveModifiers.Mini .. " Mini"
+				option = SL[pn].ActiveModifiers.Mini .. " Mini"
 			end
 
 			if k < #PlayerOptions then
