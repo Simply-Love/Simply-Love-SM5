955fee2daadaf7e6c78ed379aa8cac7c7e14ec61
Dan Guzek <dguzek@gmail.com>
2017-10-15 13:15:19 -0400

    scale MusicRate Quad on ScreenEval more cleanly
    I noticed that the Quad behind the MusicRate text on ScreenEvaluation didn't cover the full width of the banner in 4:3 aspect ratio (but did in 16:9).
    
    This code should size both the Quad and the BitmapText absolutely and then scale down using a zoom factor of 0.7 on their parent ActorFrame.

--- BGAnimations/ScreenEvaluation common/RateMod.lua
+++ BGAnimations/ScreenEvaluation common/RateMod.lua
@@ -2,17 +2,17 @@ return Def.ActorFrame{
 	-- don't bother drawing the ActorFrame if MusicRate==1
 	InitCommand=function(self)
 		self:visible(SL.Global.ActiveModifiers.MusicRate ~= 1)
-			:xy(_screen.cx, 172)
+			:xy(_screen.cx, 172):zoom(0.7)
 	end,
 
 	--quad behind the MusicRate text
 	Def.Quad{
-		InitCommand=function(self) self:diffuse( color("#1E282FCC") ):zoomto(293,14) end,
+		InitCommand=function(self) self:diffuse( color("#1E282FCC") ):zoomto(418,20) end,
 	},
 
 	--the MusicRate text
 	LoadFont("_miso")..{
-		InitCommand=function(self) self:shadowlength(1):zoom(0.7) end,
+		InitCommand=function(self) self:shadowlength(1) end,
 		OnCommand=function(self)
 			self:settext( ("%g"):format(SL.Global.ActiveModifiers.MusicRate) .. " " .. THEME:GetString("OptionTitles", "MusicRate") )
 
