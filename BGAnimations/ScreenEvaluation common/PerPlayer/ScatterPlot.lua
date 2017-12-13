c508f45c6862210ffbce6868e75d51640a3dee99
Dan Guzek <dguzek@gmail.com>
2017-12-13 14:44:38 -0500

    cleanup ScatterPlot.lua
    This doesn't actually fix anything; it just moves the code for calculating/initializing the verts table outside of the AMV's OnCommand.

--- BGAnimations/ScreenEvaluation common/PerPlayer/ScatterPlot.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/ScatterPlot.lua
@@ -27,52 +27,47 @@ local num_judgments_available = (SL.Global.ActiveModifiers.DecentsWayOffs=="Dece
 local worst_window = SL.Preferences[SL.Global.GameMode]["TimingWindowSecondsW"..num_judgments_available]
 -- ---------------------------------------------
 
--- the scater plot will use an ActorMultiVertex in "Quads" mode
+for t in ivalues(sequential_offsets) do
+	CurrentSecond = t[1]
+	Offset = t[2]
+
+	x = scale(CurrentSecond-FirstSecond, 0 , TotalSeconds, 0, GraphWidth)
+
+	if Offset ~= "Miss" then
+		-- DetermineTimingWindow() is defined in ./Scripts/SL-Helpers.lua
+		TimingWindow = DetermineTimingWindow(Offset)
+		y = scale(Offset, worst_window, -worst_window, 0, GraphHeight)
+
+		-- get the appropriate color from the global SL table
+		c = SL.JudgmentColors[SL.Global.GameMode][TimingWindow]
+		-- get the red, green, and blue values from that color
+		r = c[1]
+		g = c[2]
+		b = c[3]
+
+		-- insert four datapoints into the verts tables, effectively generating a single quadrilateral
+		table.insert( verts, {{x, y, 0}, {r,g,b,0.666} } )
+		table.insert( verts, {{x+1.5, y, 0}, {r,g,b,0.666} } )
+		table.insert( verts, {{x+1.5, y+1.5, 0}, {r,g,b,0.666} } )
+		table.insert( verts, {{x, y+1.5, 0}, {r,g,b,0.666} } )
+	else
+		-- else, a miss should be a quadrilateral that is the height of the entire graph and red
+		table.insert( verts, {{x, 0, 0}, color("#ff000077")} )
+		table.insert( verts, {{x+1, 0, 0}, color("#ff000077")} )
+		table.insert( verts, {{x+1, GraphHeight, 0}, color("#ff000077")} )
+		table.insert( verts, {{x, GraphHeight, 0}, color("#ff000077")} )
+	end
+end
+
+-- the scatter plot will use an ActorMultiVertex in "Quads" mode
 -- this is more efficient than drawing n Def.Quads (one for each judgment)
 -- because the entire AMV will be a single Actor rather than n Actors with n unique Draw() calls.
---
--- This AMV is probably less efficient than using an ActorFrameTexture with a single Quad
--- and EnablePreserveTexture(true) set, but I don't understand AFTs well enough.  I was having
--- problems with texture garbage showing up and sporadic crashes...
 local amv = Def.ActorMultiVertex{
 	OnCommand=function(self)
 		self:x(-GraphWidth/2)
 			:y(_screen.cy + 151 - GraphHeight/2 - 1)
 			:SetDrawState{Mode="DrawMode_Quads"}
-
-		for t in ivalues(sequential_offsets) do
-			CurrentSecond = t[1]
-			Offset = t[2]
-
-			x = scale(CurrentSecond-FirstSecond, 0 , TotalSeconds, 0, GraphWidth)
-
-			if Offset ~= "Miss" then
-				-- DetermineTimingWindow() is defined in ./Scripts/SL-Helpers.lua
-				TimingWindow = DetermineTimingWindow(Offset)
-				y = scale(Offset, worst_window, -worst_window, 0, GraphHeight)
-
-				-- get the appropriate color from the global SL table
-				c = SL.JudgmentColors[SL.Global.GameMode][TimingWindow]
-				-- get the red, green, and blue values from that color
-				r = c[1]
-				g = c[2]
-				b = c[3]
-
-				-- insert four datapoints into the verts tables, effectively generating a single quadrilateral
-				table.insert( verts, {{x, y, 0}, {r,g,b,0.666} } )
-				table.insert( verts, {{x+1.5, y, 0}, {r,g,b,0.666} } )
-				table.insert( verts, {{x+1.5, y+1.5, 0}, {r,g,b,0.666} } )
-				table.insert( verts, {{x, y+1.5, 0}, {r,g,b,0.666} } )
-			else
-				-- else, a miss should be a quadrilateral that is the height of the entire graph and red
-				table.insert( verts, {{x, 0, 0}, color("#ff000077")} )
-				table.insert( verts, {{x+1, 0, 0}, color("#ff000077")} )
-				table.insert( verts, {{x+1, GraphHeight, 0}, color("#ff000077")} )
-				table.insert( verts, {{x, GraphHeight, 0}, color("#ff000077")} )
-			end
-		end
-
-		self:SetVertices(verts)
+			:SetVertices(verts)
 	end,
 }
 
