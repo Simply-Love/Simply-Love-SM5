8312d264438493a3f605d170bdfeb9ebe70c2d52
Dan Guzek <dguzek@gmail.com>
2018-01-07 17:16:52 -0500

    Revert "Merge pull request #78 from ianklatzco/nice"
    This reverts commit 112f35c9409d8e45cfb3aa9bda607a4d042cacb0, reversing
    changes made to 21159b44733de5e5bfe18e6852eba054b25929d7.

--- BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua
@@ -8,22 +8,13 @@ if SL.Global.GameMode ~= "StomperZ" then
 	local title = GAMESTATE:GetCurrentSong():GetDisplayFullTitle()
 	if title == "D" then grade = "Grade_Tier99" end
 
-	local t = Def.ActorFrame{
-
-		LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
-			InitCommand=cmd(xy, 70, _screen.cy-134),
-			OnCommand=function(self)
-				self:zoom(0.4)
-				if pn == PLAYER_1 then
-					self:x( self:GetX() * -1 )
-				end
+	return LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
+		InitCommand=cmd(xy, 70, _screen.cy-134),
+		OnCommand=function(self)
+			self:zoom(0.4)
+			if pn == PLAYER_1 then
+				self:x( self:GetX() * -1 )
 			end
-		},
-
-	    LoadActor("nice.lua",pn)
-
+		end
 	}
-
-	return t
-
 end
--- Graphics/_grades/star.lua
+++ Graphics/_grades/star.lua
@@ -1,7 +1,6 @@
 local pss = ...
 local t = Def.ActorFrame{}
 
--- flag (all fantastics except 1 ex): stars
 t[#t+1] = LoadActor("graphics/star.png")..{
 	OnCommand=function(self)
 		if pss ~= nil and pss:GetTapNoteScores('TapNoteScore_Miss') == 0 and
@@ -91,7 +90,6 @@ t[#t+1] = LoadActor("graphics/goldstar (stretch).png")..{
 	end,
 	OnCommand=function(self)
 		self:draworder(100)
-		-- if worse score than 1 great
 		if pss == nil or pss:GetTapNoteScores('TapNoteScore_Miss') > 0 or
 				pss:GetTapNoteScores('TapNoteScore_W5') > 0 or
 				pss:GetTapNoteScores('TapNoteScore_W4') > 0 or
@@ -99,11 +97,11 @@ t[#t+1] = LoadActor("graphics/goldstar (stretch).png")..{
 			-- Nothing special
 			self:visible(false)
 		elseif pss:GetTapNoteScores('TapNoteScore_W3') == 1 then
-			-- Black flag; one great
+			-- Black flag
 			self:visible(true)
 			self:diffuse(Color.Black)
 		elseif pss:GetTapNoteScores('TapNoteScore_W2') == 1 then
-			-- Lol; one ex
+			-- Lol
 			self:visible(true)
 			self:diffuse(Color.Black)
 			self:sleep(2)
