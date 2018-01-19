0beb3f989fec3d068bd53e798abbd0319970f0ba
Dan Guzek <dguzek@gmail.com>
2018-01-19 15:21:23 -0500

    cleanup ScreenSelectMusic header
    ...in anticipation of additional future work being done to it.

--- Graphics/ScreenSelectMusic header.lua
+++ Graphics/ScreenSelectMusic header.lua
@@ -1,60 +1,56 @@
-local StageText = ""
 local SongsPerPlay = PREFSMAN:GetPreference("SongsPerPlay")
 local SongCost = 1
 
-local t = Def.ActorFrame{
+local StageText = function()
 
-	InitCommand=cmd(queuecommand,"FigureStuffOut"),
-	FigureStuffOutCommand=function(self)
+	-- if EventMode is enabled
+	if PREFSMAN:GetPreference("EventMode") then
+		return THEME:GetString("Stage", "Event")
+	end
 
-		if PREFSMAN:GetPreference("EventMode") then
-			StageText = THEME:GetString("Stage", "Event")
+	-- if the continue system is enabled, don't worry about determining "Final Stage"
+	if ThemePrefs.Get("NumberOfContinuesAllowed") > 0 then
+		return THEME:GetString("Stage", "Stage") .. " " .. tostring(SL.Global.Stages.PlayedThisGame + 1)
+	end
 
-		else
+	local topscreen = SCREENMAN:GetTopScreen()
+	if topscreen then
 
-			-- if the continue system is enabled, don't worry about determining "Final Stage"
-			if ThemePrefs.Get("NumberOfContinuesAllowed") > 0 then
-				StageText = THEME:GetString("Stage", "Stage") .. " " .. tostring(SL.Global.Stages.PlayedThisGame + 1)
+		-- if we're on ScreenEval for normal gameplay
+		-- we might want to display the text for StageFinal, or we might want to
+		-- increment the Stages.PlayedThisGame by the cost of the song that was just played
+		if topscreen:GetName() == "ScreenEvaluationStage" then
+			local song = GAMESTATE:GetCurrentSong()
+			local Duration = song:GetLastSecond()
+			local DurationWithRate = Duration / SL.Global.ActiveModifiers.MusicRate
 
+			local LongCutoff = PREFSMAN:GetPreference("LongVerSongSeconds")
+			local MarathonCutoff = PREFSMAN:GetPreference("MarathonVerSongSeconds")
+
+			local IsMarathon = DurationWithRate/MarathonCutoff > 1 and true or false
+			local IsLong 	 = DurationWithRate/LongCutoff > 1 and true or false
+
+			local SongCost = IsLong and 2 or IsMarathon and 3 or 1
+
+			if SL.Global.Stages.PlayedThisGame + SongCost >= SongsPerPlay then
+				return THEME:GetString("Stage", "Final")
 			else
-				local topscreen = SCREENMAN:GetTopScreen()
-
-				if topscreen then
-
-					-- if we're on ScreenEval
-					if topscreen:GetName() == "ScreenEvaluationStage" then
-						local song = GAMESTATE:GetCurrentSong()
-						local Duration = song:GetLastSecond()
-						local DurationWithRate = Duration / SL.Global.ActiveModifiers.MusicRate
-
-						local LongCutoff = PREFSMAN:GetPreference("LongVerSongSeconds")
-						local MarathonCutoff = PREFSMAN:GetPreference("MarathonVerSongSeconds")
-
-						local IsMarathon = DurationWithRate/MarathonCutoff > 1 and true or false
-						local IsLong 	 = DurationWithRate/LongCutoff > 1 and true or false
-
-						local SongCost = IsLong and 2 or IsMarathon and 3 or 1
-
-						if SL.Global.Stages.PlayedThisGame + SongCost >= SongsPerPlay then
-							StageText = THEME:GetString("Stage", "Final")
-						else
-							StageText = THEME:GetString("Stage", "Stage") .. " " .. tostring(SL.Global.Stages.PlayedThisGame + SongCost)
-						end
-
-					elseif topscreen:GetName() == "ScreenEvaluationNonstop" then
-						StageText = THEME:GetString("ScreenSelectPlayMode", "Marathon")
-
-					-- else if we're on ScreenSelectMusic
-					else
-						StageText = THEME:GetString("Stage", "Stage") .. " " .. tostring(SL.Global.Stages.PlayedThisGame + 1)
-					end
-				end
-
+				return THEME:GetString("Stage", "Stage") .. " " .. tostring(SL.Global.Stages.PlayedThisGame + SongCost)
 			end
-		end
 
-		self:GetChild("Stage Number"):playcommand("Text")
-	end,
+		-- if we're on ScreenEval within Marathon Mode, generic text will suffice
+		elseif topscreen:GetName() == "ScreenEvaluationNonstop" then
+			return THEME:GetString("ScreenSelectPlayMode", "Marathon")
+
+		-- if we're on ScreenSelectMusic, display the number of Stages.PlayedThisGame + 1
+		-- the song the player actually selects may cost more than 1, but we cannot know that now
+		else
+			return THEME:GetString("Stage", "Stage") .. " " .. tostring(SL.Global.Stages.PlayedThisGame + 1)
+		end
+	end
+end
+
+local t = Def.ActorFrame{
 	OffCommand=function(self)
 		local topscreen = SCREENMAN:GetTopScreen()
 		if topscreen then
@@ -71,9 +67,11 @@ local t = Def.ActorFrame{
 
 	LoadFont("_wendy small")..{
 		Name="Stage Number",
-		InitCommand=cmd(diffusealpha,0; zoom,WideScale(0.5,0.6); xy,_screen.cx, 15 ),
-		TextCommand=cmd(settext, StageText),
-		OnCommand=cmd(sleep,0.1; decelerate,0.33; diffusealpha,1),
+		InitCommand=function(self) self:diffusealpha(0):zoom( WideScale(0.5,0.6) ):xy(_screen.cx, 15) end,
+		OnCommand=function(self)
+			self:settext( StageText() )
+				:sleep(0.1):decelerate(0.33):diffusealpha(1)
+		end,
 	},
 
 	Def.BitmapText{
@@ -86,8 +84,8 @@ local t = Def.ActorFrame{
 			end
 		end,
 		OnCommand=function(self)
-			self:sleep(0.1):decelerate(0.33):diffusealpha(1)
-				:settext(THEME:GetString("ScreenSelectPlayMode", SL.Global.GameMode))
+			self:settext(THEME:GetString("ScreenSelectPlayMode", SL.Global.GameMode))
+				:sleep(0.1):decelerate(0.33):diffusealpha(1)
 		end,
 		UpdateHeaderTextCommand=function(self)
 			self:settext(THEME:GetString("ScreenSelectPlayMode", SL.Global.GameMode))
