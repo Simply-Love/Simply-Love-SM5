493d6624522146b5dad5160a18d27e74328917f8
Horsey- <cheval.de.jeanvaljean@gmail.com>
2018-01-27 22:27:32 -0700

    Finish merging conflicts to Dan's latest commit
    merging conflicts fixed from latest commit

--- BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua
@@ -8,48 +8,13 @@ if SL.Global.GameMode ~= "StomperZ" then
 	local title = GAMESTATE:GetCurrentSong():GetDisplayFullTitle()
 	if title == "D" then grade = "Grade_Tier99" end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	return LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
-		InitCommand=cmd(xy, 70, _screen.cy-129),
+		InitCommand=cmd(xy, 70, _screen.cy-134),
 		OnCommand=function(self)
 			self:zoom(0.4)
 			if pn == PLAYER_1 then
 				self:x( self:GetX() * -1 )
-=======
-=======
->>>>>>> refs/remotes/dguzek/master
-	local t = Def.ActorFrame{
-
-		LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
-			InitCommand=cmd(xy, 70, _screen.cy-134),
-			OnCommand=function(self)
-				self:zoom(0.4)
-				if pn == PLAYER_1 then
-					self:x( self:GetX() * -1 )
-				end
-<<<<<<< HEAD
->>>>>>> upstream/master
-=======
->>>>>>> refs/remotes/dguzek/master
 			end
-		},
-
-	    LoadActor("nice.lua",pn)
-
+		end
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
 end
-=======
-
-	return t
-
-end
->>>>>>> upstream/master
-=======
-
-	return t
-
-end
->>>>>>> refs/remotes/dguzek/master
