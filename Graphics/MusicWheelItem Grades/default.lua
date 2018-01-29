6bedf2fc88bcca2cdd39be851fce9287b52f9335
Dan Guzek <dguzek@gmail.com>
2018-01-29 12:44:04 -0500

    don't hardcode GradeTiers lookup table
    This commit builds on 0e35f3d5a1091ae8ede2d59b6d5068d183c1d44f by dynamically creating the GradeTier lookup table based on values from the Metrics, rather than hardcoding key/value pairs.  My hope is that this will make this file easier to "drag and drop" into some new theme that some new themer is working on.  Fewer modifications to the file should now need to be made.
    
    I also added fairly copious comments to ./Graphics/MusicWheelItem Grades/default.lua to explain what is going on as it is now (without a hardcoded table that one can visually inspect) less apparent.

--- Graphics/MusicWheelItem Grades/default.lua
+++ Graphics/MusicWheelItem Grades/default.lua
@@ -1,33 +1,31 @@
+-- if we're in CourseMode or MusicWheel grades are unwanted, return an empty Actor now
 if GAMESTATE:IsCourseMode() or not ThemePrefs.Get("ShowGradesInMusicWheel") then
 	return Def.Actor{}
 end
 
-local grades = {
-	Grade_Tier01 = 0,
-	Grade_Tier02 = 1,
-	Grade_Tier03 = 2,
-	Grade_Tier04 = 3,
-	Grade_Tier05 = 4,
-	Grade_Tier06 = 5,
-	Grade_Tier07 = 6,
-	Grade_Tier08 = 7,
-	Grade_Tier09 = 8,
-	Grade_Tier10 = 9,
-	Grade_Tier11 = 10,
-	Grade_Tier12 = 11,
-	Grade_Tier13 = 12,
-	Grade_Tier14 = 13,
-	Grade_Tier15 = 14,
-	Grade_Tier16 = 15,
-	Grade_Tier17 = 16,
-	Grade_Failed = 17,
-}
+
+-- how many GradeTiers are defined in Metrics.ini?
+local num_grade_tiers = THEME:GetMetric("PlayerStageStats", "NumGradeTiersUsed")
+
+-- make a grades table, and dynamically fill it with key/value pairs that we'll use in the
+-- Def.Sprite below to set the Sprite to the appropriate state on the spritesheet of grades provided
+--
+-- keys will be in the format of "Grade_Tier01", "Grade_Tier02", "Grade_Tier03", etc.
+-- values will start at 0 and go to (num_grade_tiers-1)
+local grades = {}
+for i=1,num_grade_tiers do
+	grades[ "Grade_Tier"..string.format("%02d",i) ] = i-1
+end
+-- assign the "Grade_Failed" key a value equal to num_grade_tiers
+grades["Grade_Failed"] = num_grade_tiers
+
+local state
 
 return Def.Sprite{
 	Texture=THEME:GetPathG("MusicWheelItem","Grades/grades 1x18.png"),
 	InitCommand=function(self) self:zoom( WideScale(0.18, 0.3) ):animate(0) end,
 	SetGradeCommand=function(self, params)
-		local state = grades[params.Grade]
+		state = grades[params.Grade]
 
 		if state == nil then
 			self:visible(false)
