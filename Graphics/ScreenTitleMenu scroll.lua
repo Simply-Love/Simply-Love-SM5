b2d4b8333132439dbad952182fe05a7e83888898
Horsey- <cheval.de.jeanvaljean@gmail.com>
2018-02-19 23:27:26 -0700

    Theme menu for title screen
    I wanted a way to change themes without having to go deep into the
    settings menu. Granted people don't normally change themes that often,
    but I think it shouldn't be so difficult to find. One thing I will say
    though, is that if you use your imagination, the themes menu could also
    be a good place to put quick options for yourself like volume level.

--- Graphics/ScreenTitleMenu scroll.lua
+++ Graphics/ScreenTitleMenu scroll.lua
@@ -11,8 +11,8 @@ t[#t+1] = LoadFont("_wendy small")..{
 	OnCommand=cmd(diffusealpha,0; sleep,tonumber(index) * 0.075; linear,0.2;diffusealpha,1),
 	OffCommand=cmd(sleep,tonumber(index) * 0.075; linear,0.18; diffusealpha, 0),
 
-	GainFocusCommand=cmd(stoptweening; zoom,0.5; accelerate,0.15; diffuse, PlayerColor(PLAYER_2); glow,color("1,1,1,0.5");decelerate,0.05;glow,color("1,1,1,0.0")),
-	LoseFocusCommand=cmd(stoptweening; zoom,0.4; accelerate,0.2; diffuse,color("#888888"); glow,color("1,1,1,0.0"))
+	GainFocusCommand=cmd(stoptweening; zoom,0.45; accelerate,0.15; diffuse, PlayerColor(PLAYER_2); glow,color("1,1,1,0.5");decelerate,0.05;glow,color("1,1,1,0.0")),
+	LoseFocusCommand=cmd(stoptweening; zoom,0.35; accelerate,0.2; diffuse,color("#888888"); glow,color("1,1,1,0.0"))
 
 }
 
