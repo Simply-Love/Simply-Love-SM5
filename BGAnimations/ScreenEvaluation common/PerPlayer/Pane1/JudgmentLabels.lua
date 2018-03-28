cb4030262aa15b906b61969be5945b135cd6fd87
Ian Klatzco <iklatzco@gmail.com>
2018-03-28 05:08:39 -0500

    nice-2.0

--- BGAnimations/ScreenEvaluation common/PerPlayer/Pane1/JudgmentLabels.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Pane1/JudgmentLabels.lua
@@ -1,19 +1,37 @@
 local player = ...
 local pn = ToEnumShortString(player)
+local stats = STATSMAN:GetCurStageStats():GetPlayerStageStats(pn)
 
 local mode = ""
 if SL.Global.GameMode == "StomperZ" then mode = "StomperZ" end
 if SL.Global.GameMode == "ECFA" then mode = "ECFA" end
 
--- tap note types
+function firstToUpper(str)
+    return (str:gsub("^%l", string.upper))
+end
+
+function getStringFromTheme( arg )
+	return THEME:GetString("TapNoteScore" .. mode, arg);
+end
+
+local nice = PREFSMAN:GetPreference("EasterEggs")
+
+-- i'm learning haskell okay? map is nice
+function map(func, array)
+  local new_array = {}
+  for i,v in ipairs(array) do
+    new_array[i] = func(v)
+  end
+  return new_array
+end
+
 -- Iterating through the enum isn't worthwhile because the sequencing is so bizarre...
-local TNSNames = {
-	THEME:GetString("TapNoteScore" .. mode, "W1"),
-	THEME:GetString("TapNoteScore" .. mode, "W2"),
-	THEME:GetString("TapNoteScore" .. mode, "W3"),
-	THEME:GetString("TapNoteScore" .. mode, "W4"),
-	THEME:GetString("TapNoteScore" .. mode, "W5"),
-	THEME:GetString("TapNoteScore" .. mode, "Miss")
+local TapNoteScores = {
+	Types = { 'W1', 'W2', 'W3', 'W4', 'W5', 'Miss' },
+	-- dunno if it's possible to access another subtable from a subtable
+	-- i want TapNoteScores.Types in place of the {'W1',...} table below
+	-- but i couldn't figure it out so i just lazily pasted it back in.
+	Names = map (getStringFromTheme, { 'W1', 'W2', 'W3', 'W4', 'W5', 'Miss' } )
 }
 
 local RadarCategories = {
@@ -23,6 +41,11 @@ local RadarCategories = {
 	THEME:GetString("ScreenEvaluation", 'Rolls')
 }
 
+local scores_table = {}
+for index, window in ipairs(TapNoteScores.Types) do
+	local number = stats:GetTapNoteScores( "TapNoteScore_"..window )
+	scores_table[window] = number
+end
 
 local t = Def.ActorFrame{
 	InitCommand=cmd(xy, 50, _screen.cy-24),
@@ -35,9 +58,12 @@ local t = Def.ActorFrame{
 
 
 --  labels: W1 ---> Miss
-for index, label in ipairs(TNSNames) do
+for index, window in ipairs(TapNoteScores.Types) do
+
+	local label = getStringFromTheme ( window )
+
 	t[#t+1] = LoadFont("_miso")..{
-		Text=label:upper(),
+		Text=(nice and scores_table[window] == 69) and 'NICE' or label:upper();
 		InitCommand=cmd(zoom,0.833; horizalign,right ),
 		BeginCommand=function(self)
 			self:x( (player == PLAYER_1 and 28) or -28 )
@@ -70,8 +96,13 @@ end
 
 -- labels: holds, mines, hands, rolls
 for index, label in ipairs(RadarCategories) do
+
+	local performance = stats:GetRadarActual():GetValue( "RadarCategory_"..firstToUpper(label) )
+	local possible = stats:GetRadarPossible():GetValue( "RadarCategory_"..firstToUpper(label) )
+
 	t[#t+1] = LoadFont("_miso")..{
-		Text=label,
+		-- lua ternary operators are adorable
+		Text=(nice and (performance == 69 or possible == 69)) and 'nice' or label,
 		InitCommand=cmd(NoStroke;zoom,0.833; horizalign,right ),
 		BeginCommand=function(self)
 			self:x( (player == PLAYER_1 and -160) or 90 )
