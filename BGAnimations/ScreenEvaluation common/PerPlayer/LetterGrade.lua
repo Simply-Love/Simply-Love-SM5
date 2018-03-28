60077eafe9d2b19a0b4dc7a3018a15e257f4fe0b
Ian Klatzco <iklatzco@gmail.com>
2018-03-28 03:42:19 -0500

    Revert "Revert "Merge pull request #78 from ianklatzco/nice""
    This reverts commit 8312d264438493a3f605d170bdfeb9ebe70c2d52.

--- BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua
@@ -8,13 +8,22 @@ if SL.Global.GameMode ~= "StomperZ" then
 	local title = GAMESTATE:GetCurrentSong():GetDisplayFullTitle()
 	if title == "D" then grade = "Grade_Tier99" end
 
-	return LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
-		InitCommand=cmd(xy, 70, _screen.cy-134),
-		OnCommand=function(self)
-			self:zoom(0.4)
-			if pn == PLAYER_1 then
-				self:x( self:GetX() * -1 )
+	local t = Def.ActorFrame{
+
+		LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
+			InitCommand=cmd(xy, 70, _screen.cy-134),
+			OnCommand=function(self)
+				self:zoom(0.4)
+				if pn == PLAYER_1 then
+					self:x( self:GetX() * -1 )
+				end
 			end
-		end
+		},
+
+	    LoadActor("nice.lua",pn)
+
 	}
+
+	return t
+
 end
