0eb48df30bbfed698d9d15b03f201ca496f51ee5
Dan Guzek <dguzek@gmail.com>
2018-03-31 15:31:15 -0400

    fix 0-wrapping in SL-Colors.lua
    This commit adds the color fix recommended by GitHub user shockdude in GitHub issue #106.

--- Scripts/SL-Colors.lua
+++ Scripts/SL-Colors.lua
@@ -5,18 +5,15 @@ function PlayerColor( pn )
 end
 
 function GetHexColor( n )
-	if SL.Colors[n] then
-		return color(SL.Colors[n])
-
-	-- if passed an index that returns nil, wrap the index using modulus
-	elseif SL.Colors[(n % #SL.Colors) + 1] then
-		return color( SL.Colors[(n % #SL.Colors) + 1] )
-
+	local clr = ((n - 1) % #SL.Colors) + 1
+	if SL.Colors[clr] then
+		return color(SL.Colors[clr])
 	end
-
+	
 	-- if we were passed nil or a non-integer, return white
 	return Color.White
 end
+
 
 
 function GetCurrentColor()
@@ -48,8 +45,7 @@ function DifficultyIndexColor( i )
 	return GetHexColor(clr)
 end
 
-function ColorRGB ( n )
-	local clr = n + SL.Global.ActiveColorIndex + #SL.Colors - 1
-	clr = (clr % #SL.Colors) + 1
+function ColorRGB( n )
+	local clr = n + SL.Global.ActiveColorIndex
 	return GetHexColor(clr)
 end
