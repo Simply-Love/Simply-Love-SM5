7b893c6b428a9aeaa6deedbec135121de9bfd5c6
Dan Guzek <dguzek@gmail.com>
2018-04-25 23:31:11 -0400

    update ScreenInit to only list primary authors
    I intend to add some sort of an "About This Theme" screen to the operator menu later where I can properly credit the many, many people that have made SL for SM5 possible.

--- BGAnimations/ScreenInit decorations/default.lua
+++ BGAnimations/ScreenInit decorations/default.lua
@@ -1,21 +1,5 @@
 local slc = SL.Global.ActiveColorIndex
 
-local RainbowArrows = function( x )
-
-	return Def.ActorFrame {
-		InitCommand=function(self) self:Center() end,
-
-		LoadActor("white_logo.png")..{
-			InitCommand=cmd(zoom, 0.1; diffuse, GetHexColor(slc-x-3); diffusealpha,0; x, (x-4)*50 ),
-			OnCommand=cmd(sleep, x*0.1 + 0.2; linear,0.75; diffusealpha,1; linear,0.75;diffusealpha,0)
-		},
-		LoadActor("highlight.png")..{
-			InitCommand=cmd(zoom,0.1; diffusealpha,0; x, (x-4)*50),
-			OnCommand=cmd(sleep, x*0.1 + 0.2; linear,0.75; diffusealpha,0.75; linear,0.75;diffusealpha,0)
-		}
-	}
-end
-
 local af = Def.ActorFrame{}
 
 af[#af+1] = Def.Quad{
@@ -24,16 +8,28 @@ af[#af+1] = Def.Quad{
 	OffCommand=cmd(accelerate,0.3; zoomtoheight,0)
 }
 
-af[#af+1] = LoadFont("_miso")..{
-	Text="theme by " .. THEME:GetThemeAuthor(),
-	InitCommand=cmd(diffuse,GetHexColor(slc); diffusealpha,0; Center),
-	OnCommand=cmd(sleep,3;linear,0.25;diffusealpha,1),
-	OffCommand=cmd(linear, 0.25; diffusealpha,0)
-}
-
 -- loop to add 7 SM5 arrows to the primary ActorFrame
 for i=1,7 do
-	af[#af+1] = RainbowArrows(i)
+	af[#af+1] = Def.ActorFrame {
+		InitCommand=function(self) self:Center() end,
+
+		LoadActor("white_logo.png")..{
+			InitCommand=cmd(zoom, 0.1; diffuse, GetHexColor(slc-i-3); diffusealpha,0; x, (i-4)*50 ),
+			OnCommand=cmd(sleep, i*0.1 + 0.2; linear,0.75; diffusealpha,1; linear,0.75;diffusealpha,0)
+		},
+		LoadActor("highlight.png")..{
+			InitCommand=cmd(zoom,0.1; diffusealpha,0; x, (i-4)*50),
+			OnCommand=cmd(sleep, i*0.1 + 0.2; linear,0.75; diffusealpha,0.75; linear,0.75;diffusealpha,0)
+		}
+	}
 end
+
+af[#af+1] = Def.BitmapText{
+	Font="_miso",
+	Text=ScreenString("ThemeDesign"),
+	InitCommand=cmd(diffuse,GetHexColor(slc); diffusealpha,0; Center),
+	OnCommand=cmd(sleep,3;linear,0.25;diffusealpha,1),
+	OffCommand=cmd(linear, 0.25; diffusealpha,0),
+}
 
 return af
