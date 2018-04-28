5e52dc7fc90eecfd6bec0435c03bd8476fa15621
Dan Guzek <dguzek@gmail.com>
2018-04-28 19:52:55 -0400

    use _wendy monospace for EventMode timer in header
    The digits in the EventMode timer in SelectMusic's and Evaluation's header shouldn't bounce around any longer.
    
    This required me to add a colon bitmap to "_wendy monospace" using Photoshop and update the ini.  I'm not the most adept with Photoshop, but I don't think I broke anything else that relies on this font.

--- Graphics/ScreenSelectMusic header.lua
+++ Graphics/ScreenSelectMusic header.lua
@@ -85,11 +85,11 @@ local t = Def.ActorFrame{
 
 	LoadActor( THEME:GetPathG("", "_header.lua") ),
 
-	LoadFont("_wendy small")..{
+	LoadFont("_wendy monospace numbers")..{
 		Name="Stage Number",
 		InitCommand=function(self)
 			bmt_actor = self
-			self:diffusealpha(0):zoom( WideScale(0.5,0.6) ):xy(_screen.cx, 15)
+			self:diffusealpha(0):zoom( WideScale(0.305,0.365) ):xy(_screen.cx, WideScale(10,9))
 		end,
 		OnCommand=function(self)
 			if not PREFSMAN:GetPreference("EventMode") then
