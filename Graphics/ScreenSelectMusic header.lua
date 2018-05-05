Simply-Love/Simply-Love-SM5

commit 5e52dc7fc90eecfd6bec0435c03bd8476fa15621

use _wendy monospace for EventMode timer in header
The digits in the EventMode timer in SelectMusic's and Evaluation's header shouldn't bounce around any longer.

This required me to add a colon bitmap to "_wendy monospace" using Photoshop and update the ini.  I'm not the most adept with Photoshop, but I don't think I broke anything else that relies on this font.

Graphics/ScreenSelectMusic header.lua
@@ -85,11 +85,11 @@ local t = Def.ActorFrame{
 
 	LoadActor( THEME:GetPathG("", "_header.lua") ),
 
-	LoadFont("_wendy small")..{
+	LoadFont("_wendy monospace numbers")..{
 		Name="Stage Number",
 		InitCommand=function(self)
 			bmt_actor = self
-			self:diffusealpha(0):zoom( WideScale(0.5,0.6) ):xy(_screen.cx, 15)
+			self:diffusealpha(0):zoom( WideScale(0.305,0.365) ):xy(_screen.cx, WideScale(10,9))
 		end,
 		OnCommand=function(self)
 			if not PREFSMAN:GetPreference("EventMode") then

Fonts/_wendy monospace numbers.ini
@@ -4,11 +4,13 @@ Top=18
 Baseline=50
 
 [Char Widths]
-line 0=1234567890?!.
+line 0=1234567890?!.:
 
 # numbers are 48
 DefaultWidth=48
-# ! point is 20
+# exclamation point is 20
 11=20
 # . is 20
 12=20
+# colon is 24
+13=24

commit 09575d51c92cc60c2fe6f74d423d9a0dd947d7aa

fix SSM header text again
This fixes a small font bug introduced by 5e52dc7fc90eecfd6bec0435c03bd8476fa15621.

To keep texture sizes down, "_wendy monospace numbers" doesn't have alphabetical characters by design.  This is fine when the text being displayed only includes numbers (like SSM's timer header in EventMode), but can causes problems if we use a string that includes alphabetical characters (like "STAGE ").

This commit changes SSM's header to check whether EventMode is on or off, and uses different fonts accordingly.

Graphics/ScreenSelectMusic header.lua
@@ -85,11 +85,16 @@ local t = Def.ActorFrame{
 
 	LoadActor( THEME:GetPathG("", "_header.lua") ),
 
-	LoadFont("_wendy monospace numbers")..{
+	Def.BitmapText{
+		Font=PREFSMAN:GetPreference("EventMode") and "_wendy monospace numbers" or "_wendy small",
 		Name="Stage Number",
 		InitCommand=function(self)
 			bmt_actor = self
-			self:diffusealpha(0):zoom( WideScale(0.305,0.365) ):xy(_screen.cx, WideScale(10,9))
+			if PREFSMAN:GetPreference("EventMode") then
+				self:diffusealpha(0):zoom( WideScale(0.305,0.365) ):xy(_screen.cx, WideScale(10,9))
+			else
+				self:diffusealpha(0):zoom( WideScale(0.5,0.6) ):xy(_screen.cx, 15)
+			end
 		end,
 		OnCommand=function(self)
 			if not PREFSMAN:GetPreference("EventMode") then