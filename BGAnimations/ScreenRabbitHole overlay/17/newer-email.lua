c16bfe1317fdf773be85ecd9cc6354831761f4e8
Dan Guzek <dguzek@gmail.com>
2018-05-13 22:28:12 -0400

    more connection

--- BGAnimations/ScreenRabbitHole overlay/17/newer-email.lua
+++ BGAnimations/ScreenRabbitHole overlay/17/newer-email.lua
@@ -5,7 +5,8 @@ local delay = args.delay
 local song = args.song
 local max_width = args.width
 
-local font_zoom = 0.635
+local header_zoom = 0.55
+local font_zoom = 0.585
 local bgm_volume = 10
 
 local af = Def.ActorFrame{
@@ -29,38 +30,63 @@ af[#af+1] = Def.Sound{
 }
 
 af[#af+1] = Def.Quad{
-	InitCommand=function(self) self:FullScreen():Center():diffusealpha(0) end,
+	InitCommand=function(self) self:FullScreen():Center():diffuse(0.7,0.9,1,0) end,
 	StartSceneCommand=function(self) self:linear(1):diffusealpha(1) end
 }
 
-af[#af+1] = Def.BitmapText{
-	File=THEME:GetPathB("ScreenRabbitHole", "overlay/_shared/helvetica neue/_helvetica neue 20px.ini"),
-	Text=header,
-	InitCommand=function(self)
-		self:zoom(font_zoom)
-			:align(0,0):xy(_screen.cx - max_width/2, 10)
-			:diffuse(Color.Black):diffusealpha(0)
-			:wrapwidthpixels(max_width/font_zoom)
-	end,
-	StartSceneCommand=function(self) self:linear(1):diffusealpha(1) end
+af[#af+1] = Def.Sprite{
+	Texture=THEME:GetPathB("ScreenRabbitHole", "overlay/17/grass.png"),
+	InitCommand=function(self) self:Center():zoom(0.9) end
 }
 
-af[#af+1] = Def.BitmapText{
-	File=THEME:GetPathB("ScreenRabbitHole", "overlay/_shared/helvetica neue/_helvetica neue 20px.ini"),
-	InitCommand=function(self)
-		self:zoom(font_zoom)
-			:align(0,0):xy(_screen.cx - max_width/2, 88)
-			:diffuse(Color.Black)
-			:wrapwidthpixels(max_width/font_zoom)
-	end,
-	StartSceneCommand=function(self)
-		self:sleep(3):queuecommand("Type")
-	end,
-	TypeCommand=function(self)
-		if body:len() > self:GetText():len() then
-			self:settext( body:sub(0,self:GetText():len()+1) ):sleep( delay ):queuecommand("Type")
+
+af[#af+1] = Def.ActorFrame{
+
+	InitCommand=function(self) self:fov(90):rotationx(-3):zoom(0.9):xy(30,WideScale(32, 36)) end,
+
+	-- white background for email
+	Def.Quad{
+		InitCommand=function(self) self:xy(_screen.cx, _screen.h):zoomto(max_width+60, _screen.h-70):valign(1) end
+	},
+
+	Def.BitmapText{
+		File=THEME:GetPathB("ScreenRabbitHole", "overlay/_shared/helvetica neue/_helvetica neue 20px.ini"),
+		Text=header,
+		InitCommand=function(self)
+			self:zoom(header_zoom)
+				:align(0,0):xy(_screen.cx - max_width/2, 92)
+				:diffuse(Color.Black):diffusealpha(0)
+				:wrapwidthpixels(max_width/header_zoom)
+		end,
+		StartSceneCommand=function(self) self:linear(1):diffusealpha(1) end
+	},
+
+	Def.Quad{
+		InitCommand=function(self) self:xy(_screen.cx, 150):zoomto(max_width, 1):diffuse(0.75, 0.75, 0.75, 1) end
+	},
+
+	Def.BitmapText{
+		File=THEME:GetPathB("ScreenRabbitHole", "overlay/_shared/helvetica neue/_helvetica neue 20px.ini"),
+		InitCommand=function(self)
+			self:zoom(font_zoom)
+				:align(0,0):xy(_screen.cx - max_width/2, 160)
+				:diffuse(Color.Black)
+				:wrapwidthpixels(max_width/font_zoom)
+		end,
+		StartSceneCommand=function(self)
+			self:sleep(3):queuecommand("Type")
+		end,
+		TypeCommand=function(self)
+			if body:len() > self:GetText():len() then
+				self:settext( body:sub(0,self:GetText():len()+1) ):sleep( delay ):queuecommand("Type")
+			end
 		end
-	end
+	},
+
+	Def.Sprite{
+		Texture=THEME:GetPathB("ScreenRabbitHole", "overlay/17/laptop.png"),
+		InitCommand=function(self) self:valign(1):xy(_screen.cx, _screen.h):zoom(0.65) end
+	}
 }
 
 return af
