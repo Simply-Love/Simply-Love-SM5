c5c1ef1a88dfea0d1bc06f527a4b5979ee9b9f6e
Dan Guzek <dguzek@gmail.com>
2018-05-15 12:00:14 -0400

    updated music files

--- Sounds/_common menu music/default.lua
+++ Sounds/_common menu music/default.lua
@@ -1,7 +1,7 @@
 local songs = {
 	Arrows = "cloud break (loop).ogg",
 	Bears = "crystalis (loop).ogg",
-	Hearts = "simply love og theme (loop).ogg",
+	Hearts = "feel (loop).ogg",
 	Ducks = "Xuxa fami VRC6 (loop).ogg",
 	Gay = "Mystical Wheelbarrow Journey (loop).ogg",
 }
