a83508192f249ebb638ac61c399cb6cf88d7b9b1
Dan Guzek <dguzek@gmail.com>
2018-05-19 01:25:56 -0400

    fix input handling bug with SelectProfile
    Previously, there were some conflicts between ScreenSelectProfile (which I'd basically nabbed out of _fallback) and Simply Love's AutoStyle ThemePref.  It was possible to break the theme if AutoStyle was set to "single" or "double" and a player joined both players on ScreenSelectProfile.
    
    This commit should resolve that bug, and adds a few paltry comments explaining what I did.

--- BGAnimations/ScreenSelectProfile overlay.lua
+++ BGAnimations/ScreenSelectProfile overlay.lua
@@ -1,3 +1,9 @@
+-- AutoStyle is a Simply Love ThemePref that can allow players to always
+-- automatically have one of [single, double, versus] chosen for them.
+-- If AutoStyle is either "single" or "double", we don't want to load
+-- SelectProfileFrames for both PLAYER_1 and PLAYER_2, but only the MasterPlayerNumber
+local AutoStyle = ThemePrefs.Get("AutoStyle")
+
 function GetLocalProfiles()
 	local t = {}
 
@@ -27,28 +33,22 @@ function GetLocalProfiles()
 end
 
 function LoadCard(cColor)
-	local t = Def.ActorFrame {
-		LoadActor( THEME:GetPathG("ScreenSelectProfile","CardBackground") ) .. {
-			InitCommand=cmd(diffuse,cColor);
-		};
-		LoadActor( THEME:GetPathG("ScreenSelectProfile","CardFrame") );
-	};
-	return t
+	return Def.ActorFrame {
+		LoadActor( THEME:GetPathG("ScreenSelectProfile","CardBackground") )..{ InitCommand=function(self) self:diffuse(cColor) end },
+		LoadActor( THEME:GetPathG("ScreenSelectProfile","CardFrame") )
+	}
 end
 
 
 function LoadPlayerStuff(Player)
 	local t = {}
 
-	local pn = (Player == PLAYER_1) and 1 or 2
-
-
 	t[#t+1] = Def.ActorFrame {
-		Name = 'JoinFrame',
+		Name='JoinFrame',
 		LoadCard(Color.Black),
 
 		LoadFont("_miso") .. {
-			Text="Press &START; to join.",
+			Text=THEME:GetString("ScreenSelectProfile", "PressStartToJoin"),
 			OnCommand=cmd(diffuseshift;effectcolor1,Color('White');effectcolor2,color("0.5,0.5,0.5")),
 		},
 	}
@@ -59,7 +59,7 @@ function LoadPlayerStuff(Player)
 	}
 
 	t[#t+1] = Def.ActorFrame {
-		Name = 'SmallFrame',
+		Name='SmallFrame',
 		InitCommand=cmd(y,-2),
 
 		Def.Quad {
@@ -81,168 +81,174 @@ function LoadPlayerStuff(Player)
 		children = GetLocalProfiles()
 	}
 
-	-- t[#t+1] = Def.ActorFrame {
-	-- 	Name="EffectFrame"
-	-- }
-
 	t[#t+1] = LoadFont("_miso") .. {
 		Name='SelectedProfileText',
 		InitCommand=cmd(y,160;)
 	}
 
-	return t;
-end;
+	return t
+end
 
 function UpdateInternal3(self, Player)
-	local pn = (Player == PLAYER_1) and 1 or 2;
-	local frame = self:GetChild(string.format('P%uFrame', pn));
-	local scroller = frame:GetChild('Scroller');
-	local seltext = frame:GetChild('SelectedProfileText');
-	local joinframe = frame:GetChild('JoinFrame');
-	local smallframe = frame:GetChild('SmallFrame');
-	local bigframe = frame:GetChild('BigFrame');
+	local frame = self:GetChild(ToEnumShortString(Player) .. 'Frame')
+	local scroller = frame:GetChild('Scroller')
+	local seltext = frame:GetChild('SelectedProfileText')
+	local joinframe = frame:GetChild('JoinFrame')
+	local smallframe = frame:GetChild('SmallFrame')
+	local bigframe = frame:GetChild('BigFrame')
 
 	if GAMESTATE:IsHumanPlayer(Player) then
-		frame:visible(true);
+		frame:visible(true)
 		if MEMCARDMAN:GetCardState(Player) == 'MemoryCardState_none' then
 			--using profile if any
-			joinframe:visible(false);
-			smallframe:visible(true);
-			bigframe:visible(true);
-			seltext:visible(true);
-			scroller:visible(true);
-			local ind = SCREENMAN:GetTopScreen():GetProfileIndex(Player);
+			joinframe:visible(false)
+			smallframe:visible(true)
+			bigframe:visible(true)
+			seltext:visible(true)
+			scroller:visible(true)
+			local ind = SCREENMAN:GetTopScreen():GetProfileIndex(Player)
 			if ind > 0 then
-				scroller:SetDestinationItem(ind-1);
-				seltext:settext(PROFILEMAN:GetLocalProfileFromIndex(ind-1):GetDisplayName());
+				scroller:SetDestinationItem(ind-1)
+				seltext:settext(PROFILEMAN:GetLocalProfileFromIndex(ind-1):GetDisplayName())
 			else
 				if SCREENMAN:GetTopScreen():SetProfileIndex(Player, 1) then
-					scroller:SetDestinationItem(0);
-					self:queuecommand('UpdateInternal2');
+					scroller:SetDestinationItem(0)
+					self:queuecommand('UpdateInternal2')
 				else
-					joinframe:visible(true);
-					smallframe:visible(false);
-					bigframe:visible(false);
-					scroller:visible(false);
-					seltext:settext(ScreenString("NoProfile"));
-				end;
-			end;
+					joinframe:visible(true)
+					smallframe:visible(false)
+					bigframe:visible(false)
+					scroller:visible(false)
+					seltext:settext(ScreenString("NoProfile"))
+				end
+			end
 		else
 			--using card
-			smallframe:visible(false);
-			scroller:visible(false);
-			seltext:settext(ScreenString("Card"));
-			SCREENMAN:GetTopScreen():SetProfileIndex(Player, 0);
-		end;
+			smallframe:visible(false)
+			scroller:visible(false)
+			seltext:settext(ScreenString("Card"))
+			SCREENMAN:GetTopScreen():SetProfileIndex(Player, 0)
+		end
 	else
-		joinframe:visible(true);
-		scroller:visible(false);
-		seltext:visible(false);
-		smallframe:visible(false);
-		bigframe:visible(false);
-	end;
-end;
+		joinframe:visible(true)
+		scroller:visible(false)
+		seltext:visible(false)
+		smallframe:visible(false)
+		bigframe:visible(false)
+	end
+end
 
 local t = Def.ActorFrame {
 
 	StorageDevicesChangedMessageCommand=function(self, params)
-		self:queuecommand('UpdateInternal2');
-	end;
+		self:queuecommand('UpdateInternal2')
+	end,
 
-	CodeMessageCommand = function(self, params)
+	CodeMessageCommand=function(self, params)
+		if (AutoStyle=="single" or AutoStyle=="double") and params.PlayerNumber ~= GAMESTATE:GetMasterPlayerNumber() then
+			return
+		end
+
 		if params.Name == 'Start' or params.Name == 'Center' then
-			MESSAGEMAN:Broadcast("StartButton");
+			MESSAGEMAN:Broadcast("StartButton")
 			if not GAMESTATE:IsHumanPlayer(params.PlayerNumber) then
-				SCREENMAN:GetTopScreen():SetProfileIndex(params.PlayerNumber, -1);
+				SCREENMAN:GetTopScreen():SetProfileIndex(params.PlayerNumber, -1)
 			else
-				SCREENMAN:GetTopScreen():Finish();
-			end;
-		end;
+				SCREENMAN:GetTopScreen():Finish()
+			end
+		end
 		if params.Name == 'Up' or params.Name == 'Left' or params.Name == 'DownLeft' then
 			if GAMESTATE:IsHumanPlayer(params.PlayerNumber) then
-				local ind = SCREENMAN:GetTopScreen():GetProfileIndex(params.PlayerNumber);
+				local ind = SCREENMAN:GetTopScreen():GetProfileIndex(params.PlayerNumber)
 				if ind > 1 then
 					if SCREENMAN:GetTopScreen():SetProfileIndex(params.PlayerNumber, ind - 1 ) then
-						MESSAGEMAN:Broadcast("DirectionButton");
-						self:queuecommand('UpdateInternal2');
-					end;
-				end;
-			end;
-		end;
+						MESSAGEMAN:Broadcast("DirectionButton")
+						self:queuecommand('UpdateInternal2')
+					end
+				end
+			end
+		end
 		if params.Name == 'Down' or params.Name == 'Right' or params.Name == 'DownRight' then
 			if GAMESTATE:IsHumanPlayer(params.PlayerNumber) then
-				local ind = SCREENMAN:GetTopScreen():GetProfileIndex(params.PlayerNumber);
+				local ind = SCREENMAN:GetTopScreen():GetProfileIndex(params.PlayerNumber)
 				if ind > 0 then
 					if SCREENMAN:GetTopScreen():SetProfileIndex(params.PlayerNumber, ind + 1 ) then
-						MESSAGEMAN:Broadcast("DirectionButton");
-						self:queuecommand('UpdateInternal2');
-					end;
-				end;
-			end;
-		end;
+						MESSAGEMAN:Broadcast("DirectionButton")
+						self:queuecommand('UpdateInternal2')
+					end
+				end
+			end
+		end
 		if params.Name == 'Back' then
 			if GAMESTATE:GetNumPlayersEnabled()==0 then
-				SCREENMAN:GetTopScreen():Cancel();
+				SCREENMAN:GetTopScreen():Cancel()
 			else
-				MESSAGEMAN:Broadcast("BackButton");
-				SCREENMAN:GetTopScreen():SetProfileIndex(params.PlayerNumber, -2);
-			end;
-		end;
-	end;
+				MESSAGEMAN:Broadcast("BackButton")
+				SCREENMAN:GetTopScreen():SetProfileIndex(params.PlayerNumber, -2)
+			end
+		end
+	end,
 
 	PlayerJoinedMessageCommand=function(self, params)
-		self:queuecommand('UpdateInternal2');
-	end;
+		self:queuecommand('UpdateInternal2')
+	end,
 
 	PlayerUnjoinedMessageCommand=function(self, params)
-		self:queuecommand('UpdateInternal2');
-	end;
+		self:queuecommand('UpdateInternal2')
+	end,
 
 	OnCommand=function(self, params)
-		self:queuecommand('UpdateInternal2');
-	end;
+		self:queuecommand('UpdateInternal2')
+	end,
 
 	UpdateInternal2Command=function(self)
-		UpdateInternal3(self, PLAYER_1);
-		UpdateInternal3(self, PLAYER_2);
-	end;
+		if AutoStyle=="none" or AutoStyle=="versus" then
+			UpdateInternal3(self, PLAYER_1)
+			UpdateInternal3(self, PLAYER_2)
+		else
+			UpdateInternal3(self, GAMESTATE:GetMasterPlayerNumber())
+		end
+	end,
 
 	children = {
-		Def.ActorFrame {
-			Name = 'P1Frame';
-			InitCommand=cmd(x,SCREEN_CENTER_X-160;y,SCREEN_CENTER_Y);
-			OnCommand=cmd(zoom,0;bounceend,0.35;zoom,1);
-			OffCommand=cmd(bouncebegin,0.35;zoom,0);
-			PlayerJoinedMessageCommand=function(self,param)
-				if param.Player == PLAYER_1 then
-					(cmd(;zoom,1.15;bounceend,0.175;zoom,1.0;))(self);
-				end;
-			end;
-			children = LoadPlayerStuff(PLAYER_1);
-		};
-		Def.ActorFrame {
-			Name = 'P2Frame';
-			InitCommand=cmd(x,SCREEN_CENTER_X+160;y,SCREEN_CENTER_Y);
-			OnCommand=cmd(zoom,0;bounceend,0.35;zoom,1);
-			OffCommand=cmd(bouncebegin,0.35;zoom,0);
-			PlayerJoinedMessageCommand=function(self,param)
-				if param.Player == PLAYER_2 then
-					(cmd(zoom,1.15;bounceend,0.175;zoom,1.0;))(self);
-				end;
-			end;
-			children = LoadPlayerStuff(PLAYER_2);
-		};
 		-- sounds
 		LoadActor( THEME:GetPathS("Common","start") )..{
-			StartButtonMessageCommand=cmd(play);
-		};
+			StartButtonMessageCommand=cmd(play)
+		},
 		LoadActor( THEME:GetPathS("ScreenSelectMusic","select down") )..{
-			BackButtonMessageCommand=cmd(play);
-		};
+			BackButtonMessageCommand=cmd(play)
+		},
 		LoadActor( THEME:GetPathS("ScreenSelectMaster","change") )..{
-			DirectionButtonMessageCommand=cmd(play);
-		};
-	};
+			DirectionButtonMessageCommand=cmd(play)
+		},
+	}
 }
+
+local PlayerFrame = function(player)
+	return Def.ActorFrame {
+		Name=ToEnumShortString(player) .. "Frame",
+		InitCommand=function(self) self:xy(_screen.cx+(160*(player==PLAYER_1 and -1 or 1)), _screen.cy):rotationy(-90) end,
+		OnCommand=function(self) self:smooth(0.35):rotationy(0) end,
+		OffCommand=function(self) self:bouncebegin(0.35):zoom(0) end,
+
+		PlayerJoinedMessageCommand=function(self,param)
+			if param.Player == player then
+				self:zoom(1.15):bounceend(0.175):zoom(1)
+			end
+		end,
+		children = LoadPlayerStuff(player)
+	}
+end
+
+-- load SelectProfileFrames for both
+if AutoStyle=="none" or AutoStyle=="versus" then
+	for i, player in ipairs({PLAYER_1, PLAYER_2}) do
+		t.children[#t.children+1] = PlayerFrame(player)
+	end
+
+-- load only for the MasterPlayerNumber
+else
+	t.children[#t.children+1] = PlayerFrame(GAMESTATE:GetMasterPlayerNumber())
+end
 
 return t
