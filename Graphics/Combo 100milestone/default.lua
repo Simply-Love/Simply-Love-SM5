d1d4d9649fd15749ed96c1666015559cf9563684
Dan Guzek <dguzek@gmail.com>
2018-05-24 21:03:25 -0400

    fix ComboMilestone size
    Oops.

--- Graphics/Combo 100milestone/default.lua
+++ Graphics/Combo 100milestone/default.lua
@@ -11,12 +11,12 @@ return Def.ActorFrame{
 		MilestoneCommand=cmd(finishtweening; rotationz,0;zoom,2;diffusealpha,0.5;linear,0.5;rotationz,-90;zoom,1;diffusealpha,0)
 	},
 
-	LoadActor(THEME:GetPathB("ScreenGameplay", "in/"..image.."_splode"))..{
+	LoadActor(image.."_splode")..{
 		InitCommand=cmd(diffusealpha,0; blend,"BlendMode_Add";),
 		MilestoneCommand=cmd(finishtweening; diffuse, GetCurrentColor();rotationz,10;zoom,.25;diffusealpha,0.6;decelerate,0.6;rotationz,0;zoom,2;diffusealpha,0)
 	},
 
-	LoadActor(THEME:GetPathB("ScreenGameplay", "in/"..image.."_minisplode"))..{
+	LoadActor(image.."_minisplode")..{
 		InitCommand=cmd(diffusealpha,0; blend,"BlendMode_Add";),
 		MilestoneCommand=cmd(finishtweening; diffuse, GetCurrentColor();rotationz,10;zoom,.25;diffusealpha,1;linear,0.4;rotationz,0;zoom,1.8;diffusealpha,0)
 	}
