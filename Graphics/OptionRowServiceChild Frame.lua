ce8b1450557822e091de94400bcaa0084806cb48
Dan Guzek <dguzek@gmail.com>
2018-05-28 14:30:51 -0400

    update OperatorMenu layout for widescreen
    The explanation pane is narrower in widescreen now for aesthetic.  It should be unchanged in 4:3.

--- Graphics/OptionRowServiceChild Frame.lua
+++ Graphics/OptionRowServiceChild Frame.lua
@@ -1,10 +1,11 @@
-local t = Def.ActorFrame{}
 local title_bg_width = _screen.w*WideScale(0.18,0.15)
+local t = Def.ActorFrame{}
+t.InitCommand=function(self) self:x(WideScale(12,30)) end
 
 -- a row
 t[#t+1] = Def.Quad {
 	Name="RowBackgroundQuad",
-	InitCommand=function(self) self:zoomto(_screen.w * WideScale(0.475,0.45), _screen.h*0.0625) end
+	InitCommand=function(self) self:zoomto(_screen.w * WideScale(0.475,0.54), _screen.h*0.0625):halign(0) end
 }
 
 -- black quad behind the title
@@ -12,7 +13,7 @@ t[#t+1] = Def.Quad {
 	Name="TitleBackgroundQuad",
 	OnCommand=function(self)
 		self:zoomto(title_bg_width, _screen.h*0.0625)
-			:x( -WideScale(94,128) )
+			:halign(0):x(0)
 			:diffuse(Color.Black):diffusealpha( BrighterOptionRows() and 0.75 or 0.25)
 	end
 }
