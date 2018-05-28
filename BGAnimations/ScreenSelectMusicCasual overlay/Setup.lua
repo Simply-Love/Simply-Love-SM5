0a566a83b169cf24504449e050c1e83ce875067f
Dan Guzek <dguzek@gmail.com>
2018-05-28 12:25:14 -0400

    allow DefaultSong(s) for SSMCasual
    Machine operators can now specify one or more default songs for ScreenSelectMusicCasual.  If one song is specified, SSMCasual will default to that when for the first round.  If multiple valid songs are specified, one will be randomly selected for each new casual mode game session.
    
    I'm going to ship the next release with CasualMode-DefaultSong.txt already filled in with some songs.  If none of those songs exist on a given machine, SSMCasual will default to the first song of the first pack found in ./Other/CasualMode-Groups.txt
    
    If ./Other/CasualMode-Groups.txt is empty or contains only invalid groups, SSMCasual will default to the first song from the first valid pack found on the machine.

--- BGAnimations/ScreenSelectMusicCasual overlay/Setup.lua
+++ BGAnimations/ScreenSelectMusicCasual overlay/Setup.lua
@@ -43,7 +43,6 @@ local row = {
 	h = ((_screen.h - (margin.h*(numRows-2))) / (numRows-2)),
 }
 
-
 ---------------------------------------------------------------------------
 -- a steps_type like "StepsType_Dance_Single" is needed so we can filter out steps that aren't suitable
 -- there has got to be a better way to do this...
@@ -54,20 +53,19 @@ else
 	steps_type = steps_type .. "Single"
 end
 
+
 ---------------------------------------------------------------------------
--- parse ./Other/CasualMode-Groups.txt to create a fully custom Casual MusicWheel
+-- helper function used by GetGroups() and GetDefaultSong()
+-- returns the contents of a txt file as an indexed table, split on newline
 
-local GetGroups = function()
-
-	local contents
-
-	-- the second argument here (the 1) signifies
-	-- that we are opening the file in read-only mode
-	local path = THEME:GetCurrentThemeDirectory() .. "Other/CasualMode-Groups.txt"
+local GetFileContents = function(path)
+	local contents = ""
 
 	if FILEMAN:DoesFileExist(path) then
 		-- create a generic RageFile that we'll use to read the contents
 		local file = RageFileUtil.CreateRageFile()
+		-- the second argument here (the 1) signifies
+		-- that we are opening the file in read-only mode
 		if file:Open(path, 1) then
 			contents = file:Read()
 		end
@@ -76,19 +74,32 @@ local GetGroups = function()
 		file:destroy()
 	end
 
-	if contents == nil or contents == "" then
+	-- split the contents of the file on newline
+	-- to create a table of lines as strings
+	local lines = {}
+	for line in contents:gmatch("[^\r\n]+") do
+		lines[#lines+1] = line
+	end
+
+	return lines
+end
+
+---------------------------------------------------------------------------
+-- parse ./Other/CasualMode-Groups.txt to find which groups will appear in SSMCasual
+-- returns an indexed table of group names as strings
+
+local GetGroups = function()
+	local path = THEME:GetCurrentThemeDirectory() .. "Other/CasualMode-Groups.txt"
+	local preliminary_groups = GetFileContents(path)
+
+	-- if the file didn't exist or was empty or contained no valid groups,
+	-- return the full list of groups available to SM
+	if preliminary_groups == nil or #preliminary_groups == 0 then
 		return SONGMAN:GetSongGroupNames()
 	end
 
-	-- split the contents of the file on newline and create a table of "preliminary groups"
-	-- some may not actually exist due to human error, typos, etc.
-	local preliminary_groups = {}
-	for line in contents:gmatch("[^\r\n]+") do
-		preliminary_groups[#preliminary_groups+1] = line
-	end
-
 	local groups = {}
-
+	-- some Groups found in the file may not actually exist due to human error, typos, etc.
 	for prelim_group in ivalues(preliminary_groups) do
 		-- if this group exists
 		if SONGMAN:DoesSongGroupExist( prelim_group ) then
@@ -104,39 +115,91 @@ local GetGroups = function()
 	end
 end
 
+---------------------------------------------------------------------------
+-- parse ./Other/CasualMode-DefaultSong.txt to find one or more songs to
+-- default to when SSNCasual first loads
+-- returns a song object
+
+local GetDefaultSong = function(Groups)
+
+	local path = THEME:GetCurrentThemeDirectory() .. "Other/CasualMode-DefaultSong.txt"
+	local preliminary_songs = GetFileContents(path)
+
+	-- the file was empty or doesn't exist, return the first song in the first group
+	if preliminary_songs == nil or #preliminary_songs == 0 then
+		return SONGMAN:GetSongsInGroup(Groups[1])[1]
+	end
+
+	-- verify that the song(s) specified actually exist
+	local songs = {}
+	for prelim_song in ivalues(preliminary_songs) do
+		-- parse the group out of the prelim_song string to verify this song
+		-- exists within a permitted group
+		local _group = prelim_song:gsub("/[%w%s]*", "")
+
+		-- if this song exists and is part of a group returned by PruneGroups()
+		if SONGMAN:FindSong( prelim_song ) and FindInTable(_group, Groups) then
+			-- add this prelim_song to the table of songs that do exist
+			songs[#songs+1] = prelim_song
+		end
+	end
+
+
+	-- if multiple valid songs were found, randomly select and return one
+	if #songs >= 2 then
+		local song = SONGMAN:FindSong( songs[math.random(1, #songs)] )
+		if song then return song end
+
+	-- if DefaultSong.txt only contained one song, return that
+	elseif #songs == 1 then
+		local song = SONGMAN:FindSong( songs[1] )
+		if song then return song end
+	end
+
+	-- fall back on first song from first group if needed
+	return SONGMAN:GetSongsInGroup(Groups[1])[1]
+end
 
 
 ---------------------------------------------------------------------------
--- here, we prune out packs that have no valid steps
+-- prune out groups that have no valid steps
+-- returns an indexed table of group names as strings
+
+local PruneGroups = function()
+	local groups = {}
+
+	for group in ivalues( GetGroups() ) do
+		local group_has_been_added = false
+
+		for song in ivalues(SONGMAN:GetSongsInGroup(group)) do
+			if song:HasStepsType(steps_type) then
+
+				for steps in ivalues(song:GetStepsByStepsType(steps_type)) do
+					if steps:GetMeter() < ThemePrefs.Get("CasualMaxMeter") then
+						groups[#groups+1] = group
+						group_has_been_added = true
+						break
+					end
+				end
+			end
+			if group_has_been_added then break end
+		end
+	end
+	return groups
+end
+---------------------------------------------------------------------------
 
 local current_song = GAMESTATE:GetCurrentSong()
 local group_index = 1
-local Groups = {}
+local Groups = PruneGroups()
 
-for group in ivalues( GetGroups() ) do
-	local group_has_been_added = false
-
-	for song in ivalues(SONGMAN:GetSongsInGroup(group)) do
-		if song:HasStepsType(steps_type) then
-
-			for steps in ivalues(song:GetStepsByStepsType(steps_type)) do
-				if steps:GetMeter() < ThemePrefs.Get("CasualMaxMeter") then
-					Groups[#Groups+1] = group
-					group_has_been_added = true
-					break
-				end
-			end
-		end
-		if group_has_been_added then break end
-	end
-end
-
+-- there will be a current_song if this is stage 2 or later
 if current_song then
 	group_index = FindInTable(current_song:GetGroupName(), Groups) or 1
 
--- if no current_song, choose the first song in the first pack as a last resort...
+-- if no current_song, check ./Other/CasualMode-DefaultSong.txt
 else
-	current_song = SONGMAN:GetSongsInGroup(Groups[1])[1]
+	current_song = GetDefaultSong(Groups)
 	GAMESTATE:SetCurrentSong(current_song)
 end
 
