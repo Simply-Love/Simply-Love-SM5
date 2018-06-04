1126f6a21e7ed94b69b45f4a4061b889ab7c963a
Dan Guzek <dguzek@gmail.com>
2018-06-04 11:53:46 -0400

    minor cleanup to snow.lua

--- BGAnimations/_shared background normal/snow.lua
+++ BGAnimations/_shared background normal/snow.lua
@@ -16,6 +16,7 @@ local verts = {}
 local velocities = {}
 local x, y, size, alpha, index
 local amv
+local delta_x, delta_y
 
 local Update = function(self, delta)
 
@@ -24,26 +25,30 @@ local Update = function(self, delta)
 	for i=1, num_particles*4, 4 do
 		index = math.floor(i/4)+1
 
-		-- update y coordinates
-		verts[i+0][1][2] = verts[i+0][1][2] + velocities[index][1]*delta
-		verts[i+1][1][2] = verts[i+1][1][2] + velocities[index][1]*delta
-		verts[i+2][1][2] = verts[i+2][1][2] + velocities[index][1]*delta
-		verts[i+3][1][2] = verts[i+3][1][2] + velocities[index][1]*delta
-		-- update x coordinates
-		verts[i+0][1][1] = verts[i+0][1][1] + velocities[index][2]*delta
-		verts[i+1][1][1] = verts[i+1][1][1] + velocities[index][2]*delta
-		verts[i+2][1][1] = verts[i+2][1][1] + velocities[index][2]*delta
-		verts[i+3][1][1] = verts[i+3][1][1] + velocities[index][2]*delta
+		-- update the 4 x coordinates belonging to this particle
+		delta_x = velocities[index][1]*delta
+		verts[i+0][1][1] = verts[i+0][1][1] + delta_x
+		verts[i+1][1][1] = verts[i+1][1][1] + delta_x
+		verts[i+2][1][1] = verts[i+2][1][1] + delta_x
+		verts[i+3][1][1] = verts[i+3][1][1] + delta_x
+
+		-- update the 4 y coordinates belonging to this particle
+		delta_y = velocities[index][2]*delta
+		verts[i+0][1][2] = verts[i+0][1][2] + delta_y
+		verts[i+1][1][2] = verts[i+1][1][2] + delta_y
+		verts[i+2][1][2] = verts[i+2][1][2] + delta_y
+		verts[i+3][1][2] = verts[i+3][1][2] + delta_y
 
 		-- if the top of this particular quadrilateral within the AMV has gone off
-		-- the bottom of the screen, re-randomize its x and y velocities, size, and
-		-- starting x position, and reset its starting y position to be just above
-		-- the top of the screen
+		-- the bottom of the screen, reset its properties to reuse it
 		if (verts[i+0][1][2] > _screen.h+(verts[i+2][1][2]-verts[i+0][1][2])) then
-			velocities[index] = {math.random(min_vy,max_vy), math.random(min_vx,max_vx)}
+			-- re-randomize velocities as {vx, vy}
+			velocities[index] = {math.random(min_vx,max_vx), math.random(min_vy,max_vy)}
+			-- re-randomize particle size
 			size = math.random(min_size, max_size)
+			-- re-randomize starting x position
 			x = math.random(_screen.w + size*2)
-
+			-- reset starting y position to be just above the top of the screen
 			verts[i+0][1] = {x-size, -size, 0}
 			verts[i+1][1] = {x, -size, 0}
 			verts[i+2][1] = {x, 0, 0}
@@ -61,7 +66,7 @@ for i=1, num_particles do
 	size = math.random(min_size, max_size)
 	x = math.random(_screen.w + size*2)
 	y = math.random(_screen.h + size*2)
-	velocities[i] = {math.random(min_vy,max_vy), math.random(min_vx,max_vx)}
+	velocities[i] = {math.random(min_vx,max_vx), math.random(min_vy,max_vy)}
 	alpha = math.random(6, 10)/10
 
 	table.insert( verts, {{x-size, y-size, 0}, {1,1,1,alpha}, {0,0} } )
