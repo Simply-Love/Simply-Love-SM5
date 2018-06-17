24afc4ada699913bb55e10c4dd99d76895395aa5
Dan Guzek <dguzek@gmail.com>
2018-06-17 13:51:35 -0400

    more rh15
    Two households.

--- BGAnimations/ScreenRabbitHole overlay/15/default.lua
+++ BGAnimations/ScreenRabbitHole overlay/15/default.lua
@@ -1,9 +1,9 @@
 -- Connection: Chapter 2
 
 local scenes = {}
-local duration = { 10, 15, 26, 22, 49 }
+local duration = { 10, 20, 26, 22, 49 }
 
-local scene2 = { date="May 25, 2008", body="\"Hey! Will you take our picture?\"\n\nWith tripod and camera in hand, I spun around to see three teens approaching me. They were apparently on their way out of the carnival that I, too, had just left.\n\n\"Why?\" I asked, somewhat stupefied by their request.\n\n\"Why not?\" one of the girls responded with a giggle and a bubbly smile.\n\nI paused to consider this, but rapidly concluded that any contemplation of the matter was inane.\n\n\"Absolutely. I'll take your picture.\"\n\n\"Now you can remember us forever!\" They let out a collective giggle before disappearing into the night." }
+local scene2 = { date="March 25, 2008", body="When the Intro to Networking assignment started with the phrase \"Imagine two households...\" and proceeded to develop a TCP metaphor involving packets and pipes and three-way handshakes, the only thing that came to my mind was:\n\nTwo households, both alike in dignity,\nIn fair Verona, where we lay our scene\n\nI do often worry I'm not cut out to be a programmer.  I see such innate brilliance in my peers while I struggle with the most simple concepts daily.  I fear I'm more taken by code as a means rather than an end.\n\n// Lost in nested loops\n// I write haiku in the grey\n// comments of my code..." }
 local scene3 = { song="15/dreams.ogg", date="August 3, 2008", body="I had another dream about her last night.\n\nWhen I first entered, she smiled and gave me a hug.  I could see the corners of her lips curling upward in a sign of affection and feel her arms pressing into my back, holding me close to her body for the embrace.  I could sense the warmth from her torso diffusing into mine.  I could smell the fragrance from her shampoo in her hair.  When she kissed me, I could taste the flavor of her lip gloss and feel the warmth of her lips pressed against my own.  And when she whispered I love you into my ear, I not only heard it, but felt the words tickling the inner recesses of my ear.\n\nBeing there with her, in that moment, I believed it." }
 
 local scene4 = {
@@ -29,7 +29,7 @@ af[#af+1] = LoadActor(THEME:GetPathB("ScreenRabbitHole", "overlay/14/title.lua")
 }
 
 
--- scene 2: Will you take our picture?
+-- scene 2: Two households
 af[#af+1] = LoadActor("./blog.lua", scene2)..{
 	InitCommand=function(self)
 		scenes[2] = self
