7ede2334757daf34401c7fb642d0f8b7348dd513
Dan Guzek <dguzek@gmail.com>
2018-06-23 13:05:53 -0400

    rh stuff

--- BGAnimations/ScreenRabbitHole overlay/default.lua
+++ BGAnimations/ScreenRabbitHole overlay/default.lua
@@ -1,5 +1,5 @@
 local af
-local count = 17
+local count = 18
 
 local InputHandler = function(event)
 	if count ~= 17 and (not event.PlayerNumber or not event.button) then return false end
@@ -10,6 +10,6 @@ return Def.ActorFrame{
 	InitCommand=function(self) af=self end,
 	OnCommand=function(self) SCREENMAN:GetTopScreen():AddInputCallback(InputHandler) end,
 	OffCommand=function(self) ThemePrefs.Set( "RabbitHole", ThemePrefs.Get("RabbitHole")+1 ) end,
-	-- LoadActor(THEME:GetPathB("ScreenRabbitHole", "overlay/"..(ThemePrefs.Get("RabbitHole")%count+1).."/default.lua"))
-	LoadActor(THEME:GetPathB("ScreenRabbitHole", "overlay/"..count.."/default.lua"))
+	LoadActor(THEME:GetPathB("ScreenRabbitHole", "overlay/"..(ThemePrefs.Get("RabbitHole")%count+1).."/default.lua"))
+	-- LoadActor(THEME:GetPathB("ScreenRabbitHole", "overlay/"..count.."/default.lua"))
 }
