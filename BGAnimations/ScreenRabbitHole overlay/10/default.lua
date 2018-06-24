20e46670629e774e7b91599fd37355c1c6f45adc
Dan Guzek <dguzek@gmail.com>
2018-06-23 20:24:22 -0400

    auto-transition out of rh10

--- BGAnimations/ScreenRabbitHole overlay/10/default.lua
+++ BGAnimations/ScreenRabbitHole overlay/10/default.lua
@@ -3,14 +3,18 @@
 local af = Def.ActorFrame{}
 af.InputEventCommand=function(self, event)
 	if event.type == "InputEventType_FirstPress" and (event.GameButton=="Start" or event.GameButton=="Back") then
-		SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
+		self:queuecommand("Transition")
 	end
 end
 af.InitCommand=function(self) self:diffuse( Color.Black ) end
 af.OnCommand=function(self)
 	self:sleep(2):smooth(1):diffuse( Color.White )
-		:sleep(2*60 + 50):linear(6):diffuse( Color.Black )
+		:sleep(2*60 + 50):linear(6):diffuse( Color.Black ):sleep(13):queuecommand("Transition")
 end
+af.TransitionCommand=function(self)
+	SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
+end
+
 
 af[#af+1] = Def.Sound{
 	File=THEME:GetPathB("ScreenRabbitHole", "overlay/10/13 Ghosts II.ogg"),
