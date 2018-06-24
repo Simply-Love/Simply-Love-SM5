cfc0a55e8bf6f8ae9a03eef4bddf44e266e01d51
Dan Guzek <dguzek@gmail.com>
2018-06-23 22:40:42 -0400

    more rh11

--- BGAnimations/ScreenRabbitHole overlay/11/default.lua
+++ BGAnimations/ScreenRabbitHole overlay/11/default.lua
@@ -11,11 +11,11 @@ local quotes = {
 	"But the wicked are like a troubled sea\nthat knows no rest\nwhose waves cast up mire and mud.",
 	"I always want to retroactively apply the question \"why,\" despite what actually occurs in the moment. After waking, I try to re-imagine myself asking why.\n\nWhy are you doing this?\nWhy can't I move?\nWhy are you holding me down? \nWhy are you putting your mouth on me? \nWhy can't I scream?\nWhy?",
 	"The truth is that in the moment, I don't pause to ask why. Maybe there isn't time, or maybe I'm too taken by panic to reason. I don't know.\n\nThe reality is that there are only raw emotions and non-verbal feelings.",
-	"It starts as discomfort as I gain awareness that you are holding me down. It doesn't matter how I got here; I am here again.\n\nThe discomfort quickly rises within me, transforming to fear as I understand that I cannot move a muscle. That fear wells up in me so quickly, as though I am a drinking glass being filled with a tumultuous ocean.\n\nIs it instantaneous? It hardly makes sense to ask, as there is no understanding of time here. The previous fear gives way to new terror. What was the previous thing that happened? The previous feeling I experienced? They are gone, one moment violently torn away by the furious storm as a new one is swept in to replace it.",
+	"It starts as discomfort as I gain awareness that you are holding me down. It doesn't matter how I got here; I am here again.\n\nThe discomfort quickly rises within me, transforming to fear as I understand that I cannot move a muscle. That fear overwhelms me so quickly, as though I am a drinking glass being filled with a tumultuous ocean.\n\nIs it instantaneous? The question is meaningless. There is no understanding of time here. The previous fear gives way to new terror. What was the previous thing that happened? The previous feeling I experienced? They are gone, one moment violently torn away by the furious storm as a new one is swept in to replace it.",
 	"You are on me, sucking on me, pulling me as an undercurrent does, and it is now that language finally breaks through into my consciousness: no.\n\nNo no no no no no NO.\n\nI cannot speak it, I cannot control the muscles in my lips to scream it, but that is how it takes form in my mind. I am motionless, powerless, I lack bodily autonomy. I want to scream, but I cannot. I want to swim away, but I cannot.\n\nFear is now wholly consuming, and all I can comprehend. I understand that I'm going to die like this, drowning in the still-rising sea inside me, and I cannot bear any more.",
 	"And I don't.\n\nI am suddenly free.\n\nAwake, in bed, vaguely aware that I have just screamed, I take note of how wet my face is, doused in a mixture of sweat and saliva.\n\nI am shaken, but alive.",
 	"The effects of the adrenaline remain noticeable for ten to fifteen minutes, and I am aware of this passing of time.",
-	"I am aware that I was very literally just fighting for my survival.",
+	"I am aware that I was just fighting for my survival.",
 	"I am aware\nthat my life\nmust still\nmean something\nto me."
 }
 
@@ -28,6 +28,7 @@ local af = Def.ActorFrame{
 			if quotes[count+1] then
 				count = count + 1
 				quote_bmts[count]:queuecommand("FadeIn")
+				if count == 5 then self:queuecommand("Fear") end
 				if count == 6 then self:queuecommand("WakeUp") end
 			else
 				self:sleep(1.5):queuecommand("Transition")
@@ -52,6 +53,12 @@ af[#af+1] = LoadActor("./storm.ogg")..{
 			self:sleep(0.1):queuecommand("FadeOutAudio")
 		end
 	end,
+}
+
+-- fear
+af[#af+1] = LoadActor("./fear.ogg")..{
+	FearCommand=function(self) self:play() end,
+	WakeUpCommand=function(self) self:stop() end
 }
 
 -- thunder
@@ -99,7 +106,7 @@ af[#af+1] = Def.Quad{
 -- lightning
 af[#af+1] = Def.Quad{
 	InitCommand=function(self) self:FullScreen():Center():diffuse(1,1,1,0) end,
-	WakeUpCommand=function(self) self:diffusealpha(1):decelerate(10):diffusealpha(0) end
+	WakeUpCommand=function(self) self:diffusealpha(1):sleep(0.25):decelerate(10):diffusealpha(0) end
 }
 
 return af
