52d5e8ea89ef43cc5f1152c07656cd5470e69b29
Dan Guzek <dguzek@gmail.com>
2018-06-25 16:10:02 -0400

    simplify Eval HelpText in CasualMode
    This simplification was made possible by andrewipark's recent pull request.

--- BGAnimations/ScreenEvaluation common/CasualHelpText.lua
+++ BGAnimations/ScreenEvaluation common/CasualHelpText.lua
@@ -1,24 +1,15 @@
 if SL.Global.GameMode ~= "Casual" then return end
 
-return Def.ActorFrame{
+return Def.BitmapText{
+	Font="_wendy small",
+	Text=THEME:GetString("ScreenEvaluation", "PressStartToContinue"),
 	InitCommand=function(self)
-		self:diffusealpha(0):sleep(3):diffusealpha(1)
-			:diffuseshift():effectperiod(3)
-			:effectcolor1(0,0,0,0):effectcolor2(1,1,1,1)
+		self:xy(_screen.cx, _screen.cy + 170):zoom(0.55)
+			:diffusealpha(0)
 	end,
-
-	Def.BitmapText{
-		Font="_wendy small",
-		Text=THEME:GetString("ScreenEvaluation", "PressStartToContinue"),
-		InitCommand=function(self)
-			self:xy(_screen.cx, _screen.cy + 170):zoom(0.55)
-		end,
-	},
-	Def.BitmapText{
-		Font="_miso",
-		Text="&START;",
-		InitCommand=function(self)
-			self:xy(_screen.cx-35, _screen.cy + 170):zoom(1)
-		end,
-	},
+	OnCommand=function(self)
+		self:sleep(3):diffusealpha(1)
+			:diffuseshift():effectperiod(3)
+			:effectcolor1(1,1,1,0):effectcolor2(1,1,1,1)
+	end
 }
