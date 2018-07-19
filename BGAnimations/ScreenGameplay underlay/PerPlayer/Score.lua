eabe0321a72c9b6804ecfdc4c43b39144543ea41
Dan Guzek <dguzek@gmail.com>
2018-07-19 18:10:26 -0400

    Revert "Make score truly centered"
    This reverts commit c788b3d1926020e040c8609dd2ef22cb11160832.

--- BGAnimations/ScreenGameplay underlay/PerPlayer/Score.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/Score.lua
@@ -13,33 +13,17 @@ return Def.BitmapText{
 	InitCommand=function(self)
 		self:valign(1):halign(1)
 
-		-- figure out where the font should be placed
-		-- maximum width is for the text 100.00
-		local max_width = (5 * 48) + 20 - 4
-		-- respectively: all chars, decimal point,
-		-- compensation for 1 appearing narrower than a 0
-		-- 0 is 25
-		-- (since P1 has the 1 on the edge and P2 has the 0)
-
-		-- set y-position and scaling now
-		local scale = 0.5
-		self:y(56)
-
 		if SL.Global.GameMode == "StomperZ" then
-			scale = 0.4
-			self:y(20)
+			self:zoom(0.4):x( WideScale(160, 214) ):y(20)
+			if player == PLAYER_2 then
+				self:x( _screen.w - WideScale(50, 104) )
+			end
+		else
+			self:zoom(0.5):x( _screen.cx - _screen.w/4.3 ):y(56)
+			if player == PLAYER_2 then
+				self:x( _screen.cx + _screen.w/2.75 )
+			end
 		end
-
-		self:zoom(scale)
-
-		-- position the text just inwards of the difficulty meter with padding
-		local push = WideScale(27, 84) + 18
-
-		self:x(push + (max_width * scale))
-		if (player == PLAYER_2) then
-			self:x( _screen.w - push)
-		end
-
 	end,
 	JudgmentMessageCommand=function(self) self:queuecommand("RedrawScore") end,
 	RedrawScoreCommand=function(self)
