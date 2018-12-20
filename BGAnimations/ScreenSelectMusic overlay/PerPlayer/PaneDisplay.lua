Simply-Love/Simply-Love-SM5

commit 9cd7b583c1fd1b2a3f96fb0f5ae27a7b5639319b

selectively diffuse BMTs on SSM
This is (hopefully) a better version of the previous commit, ad35bad1fbb68b98d13aad212db986b134dd35b0.

Some strings on ScreenSelectMusic might contain emojis now.
	• song title/subtitle
	• song artist
	• stepartist
	• machine highscore name
	• personal highscore name

The first two are already diffused white, so emojis aren't colored by them.  The final three need to be handled somehow or else they'll display as all black.

My solution here is to loop through each character in each of those strings and diffuse the string character-by-character using the oft-overlooked BitmapText method addattribute().

More efficient solutions probably exist, but this should work for now.

BGAnimations/ScreenSelectMusic overlay/PerPlayer/PaneDisplay.lua
@@ -156,19 +156,28 @@ local pd = Def.ActorFrame{
 	SetCommand=function(self)
 		local machine_score, machine_name = GetNameAndScore( PROFILEMAN:GetMachineProfile() )
 
-		local c = {0,0,0,1}
-		if string.byte(machine_name) and string.byte(machine_name) >= 240 then c = {1,1,1,1} end
-
 		self:GetChild("MachineHighScore"):settext(machine_score)
-		self:GetChild("MachineHighScoreName"):settext(machine_name):diffuse(c)
+		self:GetChild("MachineHighScoreName"):settext(machine_name):diffuse({0,0,0,1})
+
+		-- loop through each char in the string, checking for emojis; if any are found
+		-- don't diffuse that char to be any specific color by selectively diffusing it to be {1,1,1,1}
+		for i=1, machine_name:utf8len() do
+			if machine_name:utf8sub(i,i):byte() >= 240 then
+				self:GetChild("MachineHighScoreName"):AddAttribute(i-1, { Length=1, Diffuse={1,1,1,1} } )
+			end
+		end
 
 		if PROFILEMAN:IsPersistentProfile(player) then
 			local player_score, player_name = GetNameAndScore( PROFILEMAN:GetProfile(player) )
-			c = {0,0,0,1}
-			if string.byte(player_name) and string.byte(player_name) >= 240 then c = {1,1,1,1} end
 
 			self:GetChild("PlayerHighScore"):settext(player_score)
-			self:GetChild("PlayerHighScoreName"):settext(player_name):diffuse(c)
+			self:GetChild("PlayerHighScoreName"):settext(player_name):diffuse({0,0,0,0})
+
+			for i=1, player_name:utf8len() do
+				if player_name:utf8sub(i,i):byte() >= 240 then
+					self:GetChild("PlayerHighScoreName"):AddAttribute(i-1, { Length=1, Diffuse={1,1,1,1} } )
+				end
+			end
 		end
 	end
 }

BGAnimations/ScreenSelectMusic overlay/PerPlayer/StepArtist.lua
@@ -80,11 +80,15 @@ return Def.ActorFrame{
 			if not SongOrCourse then
 				self:settext("")
 			elseif StepsOrCourse then
-				local stepartist = GAMESTATE:IsCourseMode() and StepsOrCourse:GetScripter() or StepsOrCourse:GetAuthorCredit()
-				local c = {0,0,0,1}
-				if string.byte(stepartist) and string.byte(stepartist) >= 240 then c = {1,1,1,1} end
 
-				self:settext(stepartist or ""):diffuse(c)
+				local stepartist = GAMESTATE:IsCourseMode() and StepsOrCourse:GetScripter() or StepsOrCourse:GetAuthorCredit()
+				self:settext(stepartist or ""):diffuse( color("#1e282f") )
+
+				for i=1, stepartist:utf8len() do
+					if stepartist:utf8sub(i,i):byte() >= 240 then
+						self:AddAttribute(i-1, { Length=1, Diffuse={1,1,1,1} } )
+					end
+				end
 			end
 		end
 	}

commit bd03fba86bfbae5e800c6caf1e94ae8897b43cdd

actually show profile HighScore name in SSM
This fixes a bug introduced in 9cd7b583c1fd1b2a3f96fb0f5ae27a7b5639319b in which profile high score names would be drawn by default with an alpha of 0.  Their alpha is now 1 by default, meaning we can actually see them again!

BGAnimations/ScreenSelectMusic overlay/PerPlayer/PaneDisplay.lua
@@ -171,7 +171,7 @@ local pd = Def.ActorFrame{
 			local player_score, player_name = GetNameAndScore( PROFILEMAN:GetProfile(player) )
 
 			self:GetChild("PlayerHighScore"):settext(player_score)
-			self:GetChild("PlayerHighScoreName"):settext(player_name):diffuse({0,0,0,0})
+			self:GetChild("PlayerHighScoreName"):settext(player_name):diffuse({0,0,0,1})
 
 			for i=1, player_name:utf8len() do
 				if player_name:utf8sub(i,i):byte() >= 240 then