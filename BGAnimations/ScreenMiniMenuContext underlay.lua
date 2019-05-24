c13e3d3ad9db9d868d6259a8582b11f7de5f1297
Dan Guzek <dguzek@gmail.com>
2019-05-24 13:46:12 -0400

    fix ManageProfiles scroller for 4:3 displays

--- BGAnimations/ScreenMiniMenuContext underlay.lua
+++ BGAnimations/ScreenMiniMenuContext underlay.lua
@@ -1,26 +1,26 @@
 -- this is only used for the Screen that manages local profiles so far
 
-local NumRows
+local num_rows
+local row_height = 28
 
-local t = Def.ActorFrame {
-	InitCommand=cmd(xy,_screen.cx-_screen.w/6, -16; queuecommand, "Capture"),
+return Def.ActorFrame {
+	InitCommand=cmd(xy,_screen.cx-WideScale(146, 143), -16; queuecommand, "Capture"),
 	CaptureCommand=function(self)
 		-- how many rows do we need to accommodate?
-		NumRows = #SCREENMAN:GetTopScreen():GetChild("Container"):GetChild("")
-		-- If there are more than 10 rows, they collapse into a scroller anyway,
-		-- so limit it to 10 if so.
-		if NumRows > 10 then NumRows = 10 end
+		num_rows = #SCREENMAN:GetTopScreen():GetChild("Container"):GetChild("")
+		-- If there are more than 10 rows, they collapse via scroller anyway
+		-- so don't accommodate the decorative border for more than 10
+		num_rows = math.min(10, num_rows)
 		self:queuecommand("Size")
 	end,
 
-
-	-- white border
+	-- decorative border
 	Def.Quad{
-		SizeCommand=cmd(zoomto, 240, 28*NumRows)
+		SizeCommand=cmd(zoomto, 240, row_height*num_rows)
 	},
 
 	LoadFont("_miso")..{
-		InitCommand=cmd(xy,-80, -118; halign,0; diffuse, Color.Black ),
+		InitCommand=cmd(xy,-99, -118; halign,0; diffuse, Color.Black ),
 		BeginCommand=function(self)
 			local profile = GAMESTATE:GetEditLocalProfile()
 			if profile then
@@ -29,5 +29,3 @@ local t = Def.ActorFrame {
 		end
 	}
 }
-
-return t
