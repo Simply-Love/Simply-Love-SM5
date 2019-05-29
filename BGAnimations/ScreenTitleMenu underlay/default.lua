47c53994322312152174a248747940eb1f1acdd2
Dan Guzek <dguzek@gmail.com>
2019-05-29 17:44:35 -0400

    display a more accurate theme name on title screen

--- BGAnimations/ScreenTitleMenu underlay/default.lua
+++ BGAnimations/ScreenTitleMenu underlay/default.lua
@@ -5,11 +5,19 @@ SongStats = SongStats .. SONGMAN:GetNumSongGroups() .. " groups, "
 SongStats = SongStats .. #SONGMAN:GetAllCourses(PREFSMAN:GetPreference("AutogenGroupCourses")) .. " courses"
 
 -- - - - - - - - - - - - - - - - - - - - -
-
 local game = GAMESTATE:GetCurrentGame():GetName();
 if game ~= "dance" and game ~= "pump" then
 	game = "techno"
 end
+
+-- - - - - - - - - - - - - - - - - - - - -
+-- People commonly have multiple copies of SL installed – sometimes different forks with unique features
+-- sometimes due to concern that an update will cause them to lose data, sometimes accidentally, etc.
+
+-- It is important to display the current theme's name to help users quickly assess what version of SL
+-- they are using right now.  THEME:GetCurThemeName() provides the name of the theme folder from the
+-- filesystem, so we'll show that.  It is guaranteed to be unique and users are likely to recognize it.
+local sl_name = THEME:GetCurThemeName()
 
 -- - - - - - - - - - - - - - - - - - - - -
 local sm_version = ""
@@ -27,6 +35,7 @@ if ProductVersion():find("git") then
 else
 	sm_version = ProductID() .. sm_version
 end
+
 -- - - - - - - - - - - - - - - - - - - - -
 local style = ThemePrefs.Get("VisualTheme")
 local image = "TitleMenu"
@@ -49,7 +58,7 @@ local af = Def.ActorFrame{
 		OnCommand=function(self) self:sleep(0.2):linear(0.4):diffusealpha(1) end,
 
 		LoadFont("_miso")..{
-			Text=sm_version .. (sl_version and ("       Simply Love v"..sl_version) or ""),
+			Text=sm_version .. "       " .. sl_name .. (sl_version and (" v" .. sl_version) or ""),
 			InitCommand=function(self) self:y(-20):diffuse(TextColor) end,
 		},
 		LoadFont("_miso")..{
