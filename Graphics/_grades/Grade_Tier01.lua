2a4310606e5dc2729b47eb3279894889b5f2028b
Lamar Cooley <30600688+CrashCringle12@users.noreply.github.com>
2019-07-02 17:16:20 -0400

    Add files via upload

--- Graphics/_grades/Grade_Tier01.lua
+++ Graphics/_grades/Grade_Tier01.lua
@@ -33,7 +33,6 @@ local t = Def.ActorFrame{
 		OnCommand=cmd(x,46;y,-46;zoom,0.5;effectoffset,0.2;pulse;effectmagnitude,0.9,1,0;sleep,3;queuecommand,"Spin";);
 		SpinCommand=function(self) Spin(self) end;
 	};
-
 	-- bottom left
 	LoadActor("star.lua", pss)..{
 		OnCommand=cmd(x,-46;y,46;zoom,0.5;effectoffset,0.4;pulse;effectmagnitude,0.9,1,0;sleep,11;queuecommand,"Spin";);
@@ -43,8 +42,12 @@ local t = Def.ActorFrame{
 	--  bottom right
 	LoadActor("star.lua", pss)..{
 		OnCommand=cmd(x,46;y,46;zoom,0.5;effectoffset,0.6;pulse;effectmagnitude,1,0.9,0;sleep,48;queuecommand,"Spin";);
-		SpinCommand=function(self) Spin(self) end;
+		SpinCommand=function(self) Spin(self) end;	
 	};
+		--Ponies in between
+	LoadActor("graphics/rhyme (stretch).png", pss)..{
+		OnCommand=cmd(x,0;y,0;zoom,0.9;effectoffset,0.6;pulse;effectmagnitude,2,0.9,0;sleep,48;queuecommand,"Spin";);
+		};
 };
 
 return t;
