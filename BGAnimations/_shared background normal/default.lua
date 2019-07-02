8bc9c22492f71b71c4668ea629a36b016ddb7c9e
Lamar Cooley <30600688+CrashCringle12@users.noreply.github.com>
2019-07-02 17:35:24 -0400

    Update default.lua

--- BGAnimations/_shared background normal/default.lua
+++ BGAnimations/_shared background normal/default.lua
@@ -2,6 +2,10 @@
 if PREFSMAN:GetPreference("EasterEggs") and MonthOfYear()==11 then
 	return LoadActor( THEME:GetPathB("", "_shared background normal/snow-simple.lua") )
 end
+-- the best way school spirit! We are..?
+if PREFSMAN:GetPreference("EasterEggs") and ThemePrefs.Get("VisualTheme") == "PSU" then
+	return LoadActor( THEME:GetPathB("", "_shared background normal/spirit.lua") )
+end
 
 local file = THEME:GetPathG("", "_VisualStyles/" .. ThemePrefs.Get("VisualTheme") .. "/SharedBackground.png")
 
