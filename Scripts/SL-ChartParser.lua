cda4cdb4329a77532db499b7f55404e8991a958e
Dan Guzek <dguzek@gmail.com>
2019-07-12 01:23:22 -0400

    it seems unreal, but so do you
    The walls are stripped of plasterboard, revealing rotting structure. Bundles of electrical wiring lie strewn about. The stairs are surprisingly solid, only groaning a little under your weight. Silence envelops otherwise.
    
    Humans used to live here, now it's just dust and dirt, mice and various insects.  And you.
    
    Why do you keep returning to these same abandoned structures over and over?  What are you hoping to find?

--- Scripts/SL-ChartParser.lua
+++ Scripts/SL-ChartParser.lua
@@ -116,8 +116,8 @@ local function getStreamMeasures(measuresString, notesPerMeasure)
 
 	-- How many
 
-	-- Loop through each line in our string of measures
-	for line in measuresString:gmatch("[^\r\n]+")
+	-- Loop through each line in our string of measures, trimming potential leading whitespace (thanks, TLOES/Mirage Garden)
+	for line in measuresString:gmatch("[^%s*\r\n]+")
 	do
 		-- If we hit a comma or a semi-colon, then we've hit the end of our measure
 		if(line:match("^[,;]%s*")) then
@@ -131,7 +131,7 @@ local function getStreamMeasures(measuresString, notesPerMeasure)
 			measureCount = measureCount + 1
 			measureNotes = {}
 		else
-			-- Iterate the measure timing
+			-- increment the measure timing
 			measureTiming = measureTiming + 1
 
 			-- Is this a note?
@@ -240,8 +240,8 @@ function GetNPSperMeasure(Song, Steps)
 	local NPSforThisMeasure, PeakNPS, BPM = 0, 0, 0
 	local TimingData = Steps:GetTimingData()
 
-	-- Loop through each line in our string of measures
-	for line in ChartString:gmatch("[^\r\n]+") do
+	-- Loop through each line in our string of measures, trimming potential leading whitespace (thanks, TLOES/Mirage Garden)
+	for line in ChartString:gmatch("[^%s*\r\n]+") do
 
 		-- If we hit a comma or a semi-colon, then we've hit the end of our measure
 		if(line:match("^[,;]%s*")) then
