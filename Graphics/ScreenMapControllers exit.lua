c71f8ff0f58e32a97acaf1ca66f1f57875ba8023
Dan Guzek <dguzek@gmail.com>
2019-08-01 00:00:09 -0400

    without the wall, at least the floor feels solid

--- Graphics/ScreenMapControllers exit.lua
+++ Graphics/ScreenMapControllers exit.lua
@@ -1,10 +1,6 @@
-return LoadFont("_wendy small") .. {
-	Text="Exit";
-	InitCommand=cmd(x,_screen.cx;zoom,0.5;shadowlength,0;diffuse,GetCurrentColor();NoStroke);
-	OnCommand=cmd(diffusealpha,0;decelerate,0.5;diffusealpha,1);
-	OffCommand=cmd(stoptweening;accelerate,0.3;diffusealpha,0;queuecommand,"Hide");
-	HideCommand=cmd(visible,false);
-
-	GainFocusCommand=cmd(diffuseshift;effectcolor1,GetHexColor((SL.Global.ActiveColorIndex-1)%12);effectcolor2,GetHexColor((SL.Global.ActiveColorIndex+1)%12););
-	LoseFocusCommand=cmd(stopeffect);
-};
+return LoadFont("_wendy small")..{
+	Text=THEME:GetString("ScreenMapControllers", "Exit"),
+	InitCommand=function(self) self:x(_screen.cx):zoom(0.5):diffuse(GetCurrentColor()) end,
+	GainFocusCommand=function(self) self:diffuseshift():effectcolor1(PlayerColor(PLAYER_1)):effectcolor2(PlayerColor(PLAYER_2)) end,
+	LoseFocusCommand=function(self) self:stopeffect() end
+}
