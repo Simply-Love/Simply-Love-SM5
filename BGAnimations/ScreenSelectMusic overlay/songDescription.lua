Simply-Love/Simply-Love-SM5

commit f1dcf9a587b000ea4fccec4526729985fac14b1a

clean up GetDisplayBPMs()
Okay, this helper function is slightly less bad now.

It was previously sometimes returning numbers, sometimes returning strings, and I'd already spotted a few places throughout the theme where the same bleh logic was being recreated.

I've split the functionality into 2 functions now: GetDisplayBPMs() and StringifyDisplayBPMs()

GetDisplayBPMs() will do its best to return a table of number values like {low, high}.  It handles CourseMode and normal gameplay, split bpms and song timing, and factors in MusicRate.

StringifyDisplayBPMs() will format the numbers GetDisplayBPMs() returns into a string that can presented in a BitmapText actor.  As before, decimal precision in BPM values is hidden unless a MusicRate modifier is in effect.

I've updated old calls to GetDisplayBPMs() to now use StringifyDisplayBPMs() and removed some now-unnecessarily duplicated code in ScreenPlayerOptions to use GetDisplayBPMs() instead.

BGAnimations/ScreenSelectMusic overlay/songDescription.lua
@@ -100,7 +100,7 @@ local t = Def.ActorFrame{
 				InitCommand=function(self) self:horizalign(left):xy(5,8):diffuse(1,1,1,1) end,
 				SetCommand=function(self)
 					--defined in ./Scipts/SL-BPMDisplayHelpers.lua
-					local text = GetDisplayBPMs()
+					local text = StringifyDisplayBPMs()
 					self:settext(text or "")
 				end
 			},

BGAnimations/ScreenEvaluation common/BPM_RateMod.lua
@@ -17,7 +17,7 @@ return Def.ActorFrame{
 				self:settext( ("%g"):format(SL.Global.ActiveModifiers.MusicRate) .. "x " .. THEME:GetString("OptionTitles", "MusicRate") )
 			end
 
-			local bpm = GetDisplayBPMs()
+			local bpm = StringifyDisplayBPMs()
 			if bpm then
 				self:settext(self:GetText() .. " (" .. bpm .. " BPM)" )
 			end

BGAnimations/ScreenPlayerOptions overlay/default.lua
@@ -22,18 +22,7 @@ local song = GAMESTATE:GetCurrentSong()
 -- the low and high values from #BPMS will be used
 local player_bpms = {}
 for player in ivalues(GAMESTATE:GetHumanPlayers()) do
-	if GAMESTATE:IsCourseMode() then
-		player_bpms[player] = GetCourseModeBPMs() or GetTrailBPMs(player)
-
-	else
-		local range = song:GetDisplayBpms()
-		-- handle DisplayBPMs that are <= 0
-		if range[1] <= 0 or range[2] <= 0 then
-			player_bpms[player] = song:GetTimingData():GetActualBPM()
-		else
-			player_bpms[player] = range
-		end
-	end
+	player_bpms[player] = GetDisplayBPMs(player)
 end
 
 ------------------------------------------------------------
@@ -148,9 +137,9 @@ local t = Def.ActorFrame{
 		if MusicRateRowIndex then
 			local musicrate = SL.Global.ActiveModifiers.MusicRate
 			local title_bmt = screen:GetOptionRow(MusicRateRowIndex):GetChild(""):GetChild("Title")
-			-- FIXME: GetDisplayBPMs() is awful and needs to be rewritten
-			local bpms = GetDisplayBPMs()
-			title_bmt:settext( THEME:GetString("OptionTitles", "MusicRate") .. "\nbpm: " .. bpms )
+
+			local bpms = StringifyDisplayBPMs()
+			title_bmt:settext( ("%s\nbpm: %s"):format(THEME:GetString("OptionTitles", "MusicRate"), bpms) )
 		end
 	end
 }

BGAnimations/ScreenSelectMusicCasual overlay/SongWheelShared.lua
@@ -144,7 +144,7 @@ af[#af+1] = Def.ActorFrame{
 			InitCommand=function(self) self:zoom(0.65):diffuse(Color.White):y(0):horizalign(left) end,
 			CurrentSongChangedMessageCommand=function(self, params)
 				if params.song then
-					self:settext( THEME:GetString("ScreenSelectMusic", "BPM") .. ": " .. GetDisplayBPMs() )
+					self:settext( ("%s: %s"):format(THEME:GetString("ScreenSelectMusic", "BPM"), StringifyDisplayBPMs() or "") )
 				end
 			end,
 		},

Scripts/SL-BPMDisplayHelpers.lua
@@ -31,7 +31,7 @@ local GetCourseOrTrailBPMs = function(entries)
 end
 
 GetCourseModeBPMs = function(course)
-	local course = course or GAMESTATE:GetCurrentCourse( GAMESTATE:GetMasterPlayerNumber() )
+	course = course or GAMESTATE:GetCurrentCourse( GAMESTATE:GetMasterPlayerNumber() )
 	if not course then return false end
 
 	local courseEntries = course:GetCourseEntries()
@@ -48,56 +48,70 @@ GetTrailBPMs = function(player)
 end
 
 
-GetDisplayBPMs = function()
-	local text = ""
+-- GetDisplayBPMs() will attempt to return a table of {lower, upper} DISPLAYBPM values
+-- it handles CourseMode and normal gameplay and factors in the current MusicRate
+--
+-- if a player is provided, it will prefer steps timing in case the ssc file has "split BPMs"
+-- if a player is not provided, song timing will be used
+--
+-- the SM engine does not allow bpm values <= 0, but it does allow stepartists to
+-- manually specify DISPLAYBPM values <= 0; if such a DISPLAYBPM value is found,
+-- this function will use actual bpm values instead to preserve sanity
+
+GetDisplayBPMs = function(player)
+	player = player or GAMESTATE:GetMasterPlayerNumber()
+
 	local MusicRate = SL.Global.ActiveModifiers.MusicRate
+	local bpms
 
-	-- if in "normal" mode
-	if not GAMESTATE:IsCourseMode() then
-		local song = GAMESTATE:GetCurrentSong()
+	-- if in CourseMode
+	if GAMESTATE:IsCourseMode() then
+		bpms = GetCourseModeBPMs() or GetTrailBPMs(GAMESTATE:GetMasterPlayerNumber())
+		if not bpms then return end
 
-		if song then
-			local bpm = song:GetDisplayBpms()
-
-			-- handle DisplayBPMs that are <= 0
-			if bpm[1] <= 0 or bpm[2] <= 0 then
-				bpm = song:GetTimingData():GetActualBPM()
-			end
-
-			--if a single bpm suffices
-			if bpm[1] == bpm[2] then
-				if MusicRate == 1 then
-					text = round(bpm[1])
-				else
-					text = round(bpm[1] * MusicRate, 1)
-				end
-
-			-- if we have a range of bpms
-			else
-				if MusicRate == 1 then
-					text = round(bpm[1]) .. " - " .. round(bpm[2])
-				else
-					text = round(bpm[1] * MusicRate, 1) .. " - " .. round(bpm[2] * MusicRate, 1)
-				end
-			end
-		end
-
-	-- if we are in CourseMode
+	-- otherwise, we are not in CourseMode, i.e. in "normal" mode
 	else
-		local range = GetCourseModeBPMs() or GetTrailBPMs(GAMESTATE:GetMasterPlayerNumber())
-		if range then
-			local lowest = range[1]
-			local highest = range[2]
+		-- prefer the simfile's provided DISPLAYBPM values for this stepchart (possible with .ssc files)
+		-- if steps aren't available, try getting DISPLAYBPM values at the song level (normal for .sm files)
+		local StepsOrSong = player and GAMESTATE:GetCurrentSteps(player) or GAMESTATE:GetCurrentSong()
+		if not StepsOrSong then return end
 
-			if lowest and highest then
-				if lowest == highest then
-					text = round(lowest * MusicRate)
-				else
-					text = round(lowest * MusicRate) .. " - " .. round(highest * MusicRate)
-				end
-			end
-		end
+		bpms = StepsOrSong:GetDisplayBpms()
 	end
 
-	return text
+	-- ensure there are 2 values before attempting to index them
+	if not bpms[1] or not bpms[2] then return end
+
+	-- if the stepartist has specified a DISPLAYBPM <= 0, that's cute but
+	-- 1. the engine doens't actually permit that and will ignore it
+	-- 2. trying to accommodate it themeside is complicated and error-prone
+	-- so get the honest BPM data from the step's TimingData
+	if bpms[1] <= 0 or bpms[2] <= 0 then
+		bpms = song:GetTimingData():GetActualBPM()
+		-- again, ensure there are 2 values
+		if not bpms[1] or not bpms[2] then return end
+	end
+
+	return {
+		math.round( bpms[1] * MusicRate, 1 ),
+		math.round( bpms[2] * MusicRate, 1 )
+	}
+end
+
+StringifyDisplayBPMs = function(player)
+	player = player or GAMESTATE:GetMasterPlayerNumber()
+	local MusicRate = SL.Global.ActiveModifiers.MusicRate
+
+	local bpms = GetDisplayBPMs(player)
+	if not (bpms and bpms[1] and bpms[2]) then return end
+
+	-- format DisplayBPMs to not show decimals unless a musicrate
+	-- modifier is in effect, in which case show 1 decimal of precision
+	local fmt = MusicRate==1 and "%.0f" or "%.1f"
+
+	if bpms[1] == bpms[2] then
+		return fmt:format(bpms[1])
+	end
+
+	return ( ("%s - %s"):format(fmt, fmt) ):format(bpms[1], bpms[2])
 end

commit 684713ecf65ac3f4233be19013cdf462bf65274e

fix update of BPM value text on SelectMusic
The BPM text in SelectMusic was not updating to be an empty string when scrolling over group items in the MusicWheel.  It was retaining the BPM value of the last-seen song.

This bug was introduced in f1dcf9a587b000ea4fccec4526729985fac14b1a.

It seems that GAMESTATE:GetCurrentSong() will return nil if the MusicWheel is on a group item, but that GAMESTATE:GetCurrentSteps(player) will return the last-seen steps object in the same circumstance.

This behavior seems inconsistent, but changing anything engine-side (especially ScreenSelectMusic) is fraught with complications and community fallout, so I'm fixing it here, theme-side.

BGAnimations/ScreenSelectMusic overlay/songDescription.lua
@@ -99,6 +99,15 @@ local t = Def.ActorFrame{
 			LoadFont("Common Normal")..{
 				InitCommand=function(self) self:horizalign(left):xy(5,8):diffuse(1,1,1,1) end,
 				SetCommand=function(self)
+					-- if the active MusicWheel item is a group, GAMESTATE:GetCurrentSong() will return nil, but
+					-- GAMESTATE:GetCurrentSteps(player) will return the last-seen steps before wheel focus changed to the group
+					-- SL's StringifyDisplayBPMs() cannot reliably be used because of this, so if GetCurrentSong() returns nil
+					-- set this BPM value to an empty string and return out of this function early
+					if GAMESTATE:GetCurrentSong() == nil then
+						self:settext("")
+						return
+					end
+
 					--defined in ./Scipts/SL-BPMDisplayHelpers.lua
 					local text = StringifyDisplayBPMs()
 					self:settext(text or "")