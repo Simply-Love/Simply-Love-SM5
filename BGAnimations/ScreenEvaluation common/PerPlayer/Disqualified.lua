8afd9803f834c22e249489f1f6ad6635c4f45e8b
Dan Guzek <dguzek@gmail.com>
2020-03-12 10:21:45 -0400

    add inline comments to Disqualified.lua
    I set out to address the confusion in point 4 here
    https://www.reddit.com/r/Stepmania/comments/d5saey/what_are_all_the_mods_that_give_you_a/f0pgffe/ by adding inline comments.  Minor additional cleanup occurred
    along the way.

--- BGAnimations/ScreenEvaluation common/PerPlayer/Disqualified.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Disqualified.lua
@@ -1,16 +1,20 @@
+-- I don't want disqualification to apply to Casual mode; it's too discouraging
+-- for novice players and this game is already offputting enough as-is.
+-- If we're in Casual mode, return early; don't evaluate the rest of this file.
 if SL.Global.GameMode == "Casual" then return end
+-- -----------------------------------------------------------------------
 
 local player = ...
 
 local stats = STATSMAN:GetCurStageStats():GetPlayerStageStats(player)
 local disqualified = stats:IsDisqualified()
 
-return LoadFont("_wendy small")..{
-	Name="Disqualified"..ToEnumShortString(player),
-	InitCommand=function(self) self:diffusealpha(0.7):zoom(0.23):y(_screen.cy+138) end,
-	OnCommand=function(self)
-		if disqualified then
-			self:settext(THEME:GetString("ScreenEvaluation","Disqualified"))
-		end
-	end
-}
+-- If the player was disqualified, return a BitmapText actor with localized text
+-- like "Disqualified for Ranking".
+if disqualified then
+	return LoadFont("_wendy small")..{
+		Name="Disqualified"..ToEnumShortString(player),
+		Text=THEME:GetString("ScreenEvaluation","Disqualified"),
+		InitCommand=function(self) self:diffusealpha(0.7):zoom(0.23):y(_screen.cy+138) end,
+	}
+end
