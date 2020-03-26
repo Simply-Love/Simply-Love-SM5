417f5bd44aa8c365f03baae47bd2c47f256b5575
quietly-turning <quietly.turning@gmail.com>
2020-03-25 23:35:22 -0400

    tidy SSM header code
    This is cleaner and easier for me to work with.  There should be no
    observable differences for players from these changes.

--- Graphics/ScreenSelectMusic header.lua
+++ Graphics/ScreenSelectMusic header.lua
@@ -11,63 +11,75 @@ local Update = function(af, dt)
 	end
 end
 
-local t = Def.ActorFrame{
-	InitCommand=function(self)
-		if PREFSMAN:GetPreference("EventMode") and SL.Global.GameMode ~= "Casual" then
-			-- TimeAtSessionStart will be reset to nil between game sessions
-			-- thus, if it's currently nil, we're loading ScreenSelectMusic
-			-- for the first time this particular game session
-			if SL.Global.TimeAtSessionStart == nil then
-				SL.Global.TimeAtSessionStart = GetTimeSinceStart()
-			end
+local af = Def.ActorFrame{ OffCommand=function(self) self:linear(0.1):diffusealpha(0) end }
 
-			self:SetUpdateFunction( Update )
+-- only add this InitCommand to the main ActorFrame in EventMode
+if PREFSMAN:GetPreference("EventMode") then
+	af.InitCommand=function(self)
+		-- TimeAtSessionStart will be reset to nil between game sessions
+		-- thus, if it's currently nil, we're loading ScreenSelectMusic
+		-- for the first time this particular game session
+		if SL.Global.TimeAtSessionStart == nil then
+			SL.Global.TimeAtSessionStart = GetTimeSinceStart()
 		end
-	end,
-	OffCommand=function(self)
-		self:linear(0.1):diffusealpha(0)
-	end,
 
-	-- generic header elements (background Def.Quad, left-aligned screen name)
-	LoadActor( THEME:GetPathG("", "_header.lua") ),
+		self:SetUpdateFunction( Update )
+	end
+end
 
-	LoadFont(PREFSMAN:GetPreference("EventMode") and "_wendy monospace numbers" or "_wendy small")..{
-		Name="Stage Number",
+
+-- generic header elements (background Def.Quad, left-aligned screen name)
+af[#af+1] = LoadActor( THEME:GetPathG("", "_header.lua") )
+
+-- centered text
+-- session timer in EventMode
+if PREFSMAN:GetPreference("EventMode") then
+
+	af[#af+1] = LoadFont("_wendy monospace numbers")..{
+		Name="Session Timer",
 		InitCommand=function(self)
 			bmt_actor = self
-			if PREFSMAN:GetPreference("EventMode") then
-				self:zoom( WideScale(0.3,0.36) ):y( WideScale(3.15,3.5)/self:GetZoom() )
-			else
-				self:zoom( WideScale(0.5,0.6) ):y( WideScale(7.5,9)/self:GetZoom() )
-			end
-
+			self:zoom( WideScale(0.3,0.36) ):y( WideScale(3.15,3.5)/self:GetZoom() )
 			self:diffusealpha(0):x(_screen.cx)
 		end,
 		OnCommand=function(self)
-			if not PREFSMAN:GetPreference("EventMode") then
-				self:settext( SSM_Header_StageText() )
-			end
-
 			self:sleep(0.1):decelerate(0.33):diffusealpha(1)
 		end,
-	},
+	}
 
-	LoadFont("_wendy small")..{
-		Name="GameModeText",
+-- stage number when not EventMode
+else
+
+	af[#af+1] = LoadFont("_wendy small")..{
+		Name="Stage Number",
+		Text=SSM_Header_StageText(),
 		InitCommand=function(self)
-			self:diffusealpha(0):zoom( WideScale(0.5,0.6)):xy(_screen.w-70, 15):halign(1)
-			if not PREFSMAN:GetPreference("MenuTimer") then
-				self:x(_screen.w-10)
-			end
+			self:zoom( WideScale(0.5,0.6) ):y( WideScale(7.5,9)/self:GetZoom() )
+			self:diffusealpha(0):x(_screen.cx)
 		end,
 		OnCommand=function(self)
-			self:settext(THEME:GetString("ScreenSelectPlayMode", SL.Global.GameMode))
-				:sleep(0.1):decelerate(0.33):diffusealpha(1)
+			self:sleep(0.1):decelerate(0.33):diffusealpha(1)
 		end,
-		UpdateHeaderTextCommand=function(self)
-			self:settext(THEME:GetString("ScreenSelectPlayMode", SL.Global.GameMode))
-		end
 	}
+
+end
+
+-- "ITG" or "FA+" or "StomperZ"; aligned to right of screen
+af[#af+1] = LoadFont("_wendy small")..{
+	Name="GameModeText",
+	Text=THEME:GetString("ScreenSelectPlayMode", SL.Global.GameMode),
+	InitCommand=function(self)
+		self:diffusealpha(0):zoom( WideScale(0.5,0.6)):xy(_screen.w-70, 15):halign(1)
+		if not PREFSMAN:GetPreference("MenuTimer") then
+			self:x(_screen.w-10)
+		end
+	end,
+	OnCommand=function(self)
+		self:sleep(0.1):decelerate(0.33):diffusealpha(1)
+	end,
+	UpdateHeaderTextCommand=function(self)
+		self:settext(THEME:GetString("ScreenSelectPlayMode", SL.Global.GameMode))
+	end
 }
 
-return t
+return af
