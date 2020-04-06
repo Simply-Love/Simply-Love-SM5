0ee6e9946076580c9e085355b0c95e42c6a5bf80
quietly-turning <quietly.turning@gmail.com>
2020-04-06 08:13:36 -0400

    don't show HoldJudgment OptRow when game is Pump
    HoldJudgments don't appear in Pump so there's no point in showing
    players the OptionRow to set it.
    
    This also repositions the preview sprites to be more centered.

--- BGAnimations/ScreenPlayerOptions overlay/HoldJudgmentPreviews.lua
+++ BGAnimations/ScreenPlayerOptions overlay/HoldJudgmentPreviews.lua
@@ -5,14 +5,15 @@ for hj_filename in ivalues( GetHoldJudgments() ) do
 		Name="HoldJudgment_"..StripSpriteHints(hj_filename),
 		InitCommand=function(self) self:visible(false) end,
 
+		-- held
 		Def.Sprite{
 			Texture=THEME:GetPathG("", "_HoldJudgments/" .. hj_filename),
-			InitCommand=function(self) self:animate(false):setstate(0) end
+			InitCommand=function(self) self:animate(false):setstate(0):addx(-self:GetWidth()*0.4) end
 		},
+		-- let go
 		Def.Sprite{
-			Name="HoldJudgment_"..StripSpriteHints(hj_filename),
 			Texture=THEME:GetPathG("", "_HoldJudgments/" .. hj_filename),
-			InitCommand=function(self) self:animate(false):setstate(1):addx(self:GetWidth()*0.75) end
+			InitCommand=function(self) self:animate(false):setstate(1):addx(self:GetWidth()*0.4) end
 		}
 	}
 end
