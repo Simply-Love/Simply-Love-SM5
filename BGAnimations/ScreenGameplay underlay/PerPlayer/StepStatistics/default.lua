55ef16bf5ee8caf072118b8b314593ba284eaade
quietly-turning <quietly.turning@gmail.com>
2020-06-11 16:55:29 -0400

    minor cleanup

--- BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/default.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/default.lua
@@ -5,7 +5,7 @@ local ar = GetScreenAspectRatio()
 -- if the conditions aren't right, don't bother
 if SL[pn].ActiveModifiers.DataVisualizations ~= "Step Statistics"
 or SL.Global.GameMode == "Casual"
-or (ar < 21/9-- less wide than ultrawide
+or (ar < 21/9 -- less wide than ultrawide
 and GAMESTATE:GetCurrentStyle():GetName() ~= "single"
 and (PREFSMAN:GetPreference("Center1Player") and not IsUsingWideScreen()))
 then
@@ -66,8 +66,6 @@ af[#af+1] = Def.ActorFrame{
 		if ar < 21/9 then
 			if (PREFSMAN:GetPreference("Center1Player") and IsUsingWideScreen()) then
 				local zoom = scale(GetScreenAspectRatio(), 16/10, 16/9, zoomfactor.sixteen_ten, zoomfactor.sixteen_nine)
-				-- handle aspect ratios wider than 16:9 (e.g. 21:9) by clamping zoom
-				zoom = clamp(zoom, zoomfactor.sixteen_ten, zoomfactor.sixteen_nine )
 				self:zoom( zoom )
 			end
 
