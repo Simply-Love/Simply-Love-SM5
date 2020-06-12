Simply-Love/Simply-Love-SM5

commit 6b1fa989c68edcf20a487c2d3a6dccfb869bc924

use steps:GetFilename() in stepchart parsing
I found that steps objects have a GetFilename() method that "Returns the
Steps filename from the Cache."

https://quietly-turning.github.io/Lua-For-SM5/LuaAPI#Actors-Steps-GetFilename

I'd previously been recreating this in-theme with a combination of the
filepath to the current song and some assumptions that ssc files should
be preferred over sm files for parsing if both are found.  It makes more
sense to use whatever file (ssc or sm) the cache file specifies as that
is (hopefully!) where gameplay will get its steps from.

This changes GetSimfileString() in SL-ChartParser.lua to use
steps:GetFilename() and updates other functions that interact with it
accordingly.

Scripts/SL-ChartParser.lua
@@ -1,25 +1,12 @@
-function GetSimfileString(path)
+GetSimfileString = function(steps)
 
-	local filename, filetype
-	local files = FILEMAN:GetDirListing(path)
+	-- steps:GetFilename() returns the filename of the sm or ssc file, including path, as it is stored in SM's cache
+	local filename = steps:GetFilename()
+	if not filename then return end
 
-	for file in ivalues(files) do
-		if file:find(".+%.[sS][sS][cC]$") then
-			-- Finding a .ssc file is preferable.
-			-- If we find one, stop looking.
-			filename = file
-			filetype = "ssc"
-			break
-		elseif file:find(".+%.[sS][mM]$") then
-			-- Don't break if we find a .sm file first;
-			-- there might still be a .ssc file waiting.
-			filename = file
-			filetype = "sm"
-		end
-	end
-
+	local filetype = filename:match("[^.]+$")
 	-- if neither a .ssc nor a .sm file were found, bail now
-	if not (filename and filetype) then return end
+	if not (filetype=="ssc" or filetype=="sm") then return end
 
 	-- create a generic RageFile that we'll use to read the contents
 	-- of the desired .ssc or .sm file
@@ -28,7 +15,7 @@ function GetSimfileString(path)
 
 	-- the second argument here (the 1) signifies
 	-- that we are opening the file in read-only mode
-	if f:Open(path .. filename, 1) then
+	if f:Open(filename, 1) then
 		contents = f:Read()
 	end
 
@@ -217,7 +204,7 @@ function GetNPSperMeasure(Song, Steps)
 	if Song==nil or Steps==nil then return end
 
 	local SongDir = Song:GetSongDir()
-	local SimfileString, Filetype = GetSimfileString( SongDir )
+	local SimfileString, Filetype = GetSimfileString( Steps )
 	if not SimfileString then return end
 
 	-- StepsType, a string like "dance-single" or "pump-double"
@@ -294,9 +281,9 @@ end
 
 
 
-function GetStreams(SongDir, StepsType, Difficulty, NotesPerMeasure, MeasureSequenceThreshold)
+function GetStreams(Steps, StepsType, Difficulty, NotesPerMeasure, MeasureSequenceThreshold)
 
-	local SimfileString, Filetype = GetSimfileString( SongDir )
+	local SimfileString, Filetype = GetSimfileString( Steps )
 	if not SimfileString then return end
 
 	-- Parse out just the contents of the notes

BGAnimations/ScreenEvaluation common/PerPlayer/Pane6/GrooveStatsURL.lua
@@ -8,10 +8,11 @@ score = tostring(tonumber(score:gsub("%%", "") * 100)):gsub("%.", "")
 local failed = stats:GetFailed() and "1" or "0"
 local rate = tostring(SL.Global.ActiveModifiers.MusicRate * 100):gsub("%.", "")
 
-local currentSteps = GAMESTATE:GetCurrentSteps(player)
+local steps = GAMESTATE:GetCurrentSteps(player)
 local difficulty = ""
-if currentSteps then
-	difficulty = currentSteps:GetDifficulty()
+
+if steps then
+	difficulty = steps:GetDifficulty()
 	-- GetDifficulty() returns a value from the Difficulty Enum
 	-- "Difficulty_Hard" for example.
 	-- Strip the characters up to and including the underscore.
@@ -25,7 +26,8 @@ if GAMESTATE:GetCurrentStyle():GetStyleType() == "StyleType_OnePlayerTwoSides" t
 else
 	style = "dance-single"
 end
-local hash = GenerateHash(style, difficulty):sub(1, 12)
+
+local hash = GenerateHash(steps, style, difficulty):sub(1, 12)
 
 -- ************* CURRENT QR VERSION *************
 -- * Update whenever we change relevant QR code *

BGAnimations/ScreenGameplay in/MeasureCounterAndModsLevel.lua
@@ -13,13 +13,12 @@ return function(SongNumberInCourse)
 		local mods = SL[pn].ActiveModifiers
 		if mods.MeasureCounter and mods.MeasureCounter ~= "None" then
 
-			local song_dir, steps
+			local steps = nil
+
 			if GAMESTATE:IsCourseMode() then
 				local trail = GAMESTATE:GetCurrentTrail(player):GetTrailEntries()[SongNumberInCourse+1]
-				song_dir = trail:GetSong():GetSongDir()
 				steps = trail:GetSteps()
 			else
-				song_dir = GAMESTATE:GetCurrentSong():GetSongDir()
 				steps = GAMESTATE:GetCurrentSteps(player)
 			end
 
@@ -29,13 +28,14 @@ return function(SongNumberInCourse)
 			local threshold_to_be_stream = 2
 
 			-- if any of these don't match what we're currently looking for...
-			if SL[pn].Streams.SongDir ~= song_dir or SL[pn].Streams.StepsType ~= steps_type or SL[pn].Streams.Difficulty ~= difficulty then
+			if SL[pn].Streams.Steps ~= steps or SL[pn].Streams.StepsType ~= steps_type or SL[pn].Streams.Difficulty ~= difficulty then
 
 				-- ...then parse the simfile, given the current parameters
-				SL[pn].Streams.Measures = GetStreams(song_dir, steps_type, difficulty, notes_per_measure, threshold_to_be_stream)
+				SL[pn].Streams.Measures = GetStreams(steps, steps_type, difficulty, notes_per_measure, threshold_to_be_stream)
+
 				-- and set these so we can check again next time.
-				SL[pn].Streams.SongDir = song_dir
-				SL[pn].Streams.StepsType = steps_type
+				SL[pn].Streams.Steps      = steps
+				SL[pn].Streams.StepsType  = steps_type
 				SL[pn].Streams.Difficulty = difficulty
 			end
 		end

Scripts/SL-HashGenerator.lua
@@ -123,8 +123,8 @@ end
 --
 -- stepsType is usually either 'dance-single' or 'dance-double'
 -- difficulty is usually one of {'Beginner', 'Easy', 'Medium', 'Hard', 'Challenge'}
-function GenerateHash(stepsType, difficulty)
-	local msdFile = ParseMsdFile(GAMESTATE:GetCurrentSong():GetSongDir())
+function GenerateHash(steps, stepsType, difficulty)
+	local msdFile = ParseMsdFile(steps)
 
 	if #msdFile == 0 then return ''	end
 

Scripts/SL-MsdFileParser.lua
@@ -15,7 +15,7 @@
 -- #VALUE2:PARAM2
 -- we'll recover.
 
-function ParseMsdFile(SongDir)
+function ParseMsdFile(steps)
 	local function AddParam(t, p, plen)
 		-- table.concat(table_name, separator, start, end)
 		local param = table.concat(p, '', 1, plen)
@@ -47,7 +47,7 @@ function ParseMsdFile(SongDir)
 		return s:sub(i, i)
 	end
 
-	local SimfileString, FileType = GetSimfileString(SongDir)
+	local SimfileString, FileType = GetSimfileString(steps)
 	if not SimfileString then return {} end
 	if FileType ~= 'sm' then return {} end
 
@@ -152,7 +152,7 @@ function ParseMsdFile(SongDir)
 			continue = true
 		end
 
-		-- We've gone through all the control characters.  All that is left is either an escaped character, 
+		-- We've gone through all the control characters.  All that is left is either an escaped character,
 		-- ie \#, \\, \:, etc., or a regular character.
 		-- NOTE: There is usually an 'unescape' bool passed to this top level function,
 		-- but when reading SM/SSC files it's always set to true so we assume that.

commit 2dbbcb0b7d444c29cd9f0d9e63e9f4c8da901b38

normalize simfile extension to lowercase in parser
SL-ChartParser supports parsing ssc and sm files, but not other formats
that SM supports (e.g. dwi), and should once again support permutations
on capitalization in "ssc" and "sm".  Load Futurepack's simfiles are
".SM" for example.

This fixes a regression introduced in
6b1fa989c68edcf20a487c2d3a6dccfb869bc924.

Scripts/SL-ChartParser.lua
@@ -4,8 +4,11 @@ GetSimfileString = function(steps)
 	local filename = steps:GetFilename()
 	if not filename then return end
 
-	local filetype = filename:match("[^.]+$")
-	-- if neither a .ssc nor a .sm file were found, bail now
+	-- get the file extension like "sm" or "SM" or "ssc" or "SSC" or "sSc" or etc.
+	-- convert to lowercase
+	local filetype = filename:match("[^.]+$"):lower()
+	-- if file doesn't match "ssc" or "sm", it was (hopefully) something else (.dwi, .bms, etc.)
+	-- that isn't supported by SL-ChartParser
 	if not (filetype=="ssc" or filetype=="sm") then return end
 
 	-- create a generic RageFile that we'll use to read the contents
@@ -13,7 +16,7 @@ GetSimfileString = function(steps)
 	local f = RageFileUtil.CreateRageFile()
 	local contents
 
-	-- the second argument here (the 1) signifies
+	-- the second argument here (the 1) signals
 	-- that we are opening the file in read-only mode
 	if f:Open(filename, 1) then
 		contents = f:Read()
2dbbcb0b7d444c29cd9f0d9e63e9f4c8da901b38
quietly-turning <quietly.turning@gmail.com>
2020-06-12 12:13:23 -0400

    normalize simfile extension to lowercase in parser
    SL-ChartParser supports parsing ssc and sm files, but not other formats
    that SM supports (e.g. dwi), and should once again support permutations
    on capitalization in "ssc" and "sm".  Load Futurepack's simfiles are
    ".SM" for example.
    
    This fixes a regression introduced in
    6b1fa989c68edcf20a487c2d3a6dccfb869bc924.

--- Scripts/SL-ChartParser.lua
+++ Scripts/SL-ChartParser.lua
@@ -4,8 +4,11 @@ GetSimfileString = function(steps)
 	local filename = steps:GetFilename()
 	if not filename then return end
 
-	local filetype = filename:match("[^.]+$")
-	-- if neither a .ssc nor a .sm file were found, bail now
+	-- get the file extension like "sm" or "SM" or "ssc" or "SSC" or "sSc" or etc.
+	-- convert to lowercase
+	local filetype = filename:match("[^.]+$"):lower()
+	-- if file doesn't match "ssc" or "sm", it was (hopefully) something else (.dwi, .bms, etc.)
+	-- that isn't supported by SL-ChartParser
 	if not (filetype=="ssc" or filetype=="sm") then return end
 
 	-- create a generic RageFile that we'll use to read the contents
@@ -13,7 +16,7 @@ GetSimfileString = function(steps)
 	local f = RageFileUtil.CreateRageFile()
 	local contents
 
-	-- the second argument here (the 1) signifies
+	-- the second argument here (the 1) signals
 	-- that we are opening the file in read-only mode
 	if f:Open(filename, 1) then
 		contents = f:Read()
