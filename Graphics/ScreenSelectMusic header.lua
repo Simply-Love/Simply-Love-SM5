5e7524d3bdaf4bb03802c14fa001af791c348f47
quietly-turning <quietly.turning@gmail.com>
2020-06-13 20:07:57 -0400

    SecondsToHMMSS() for SelectMusic header
    Game sessions longer than 1 hour but shorter than 10 hours would
    previously display with two digits in the hours segment.  1 hour, 15
    minutes, and 45 seconds would display as "01:15:45"
    
    The SM5 engine does not (yet) provide a formatting function for HMMSS,
    so this creates one in-theme.  Players would now see "1:15:45"
    
    If SecondsToHMMSS is added to the engine in the future, this Lua is
    written to use that instead.  This is my attempt to be both forward- and
    backwards-compatible.

--- Graphics/ScreenSelectMusic header.lua
+++ Graphics/ScreenSelectMusic header.lua
@@ -1,16 +1,39 @@
 local bmt_actor
 local DrawNinePanelPad = LoadActor( THEME:GetPathB("ScreenSelectStyle", "underlay/pad.lua") )
 
-local Update = function(af, dt)
+-- -----------------------------------------------------------------------
+
+local hours, mins, secs
+local hmmss = "%d:%02d:%02d"
+
+-- prefer the engine's SecondsToHMMSS()
+-- but define it ourselves if it isn't provided by this version of SM5
+local SecondsToHMMSS = SecondsToHMMSS or function(s)
+	-- native floor division sounds nice but isn't available in Lua 5.1
+	hours = math.floor(s/3600)
+	mins  = math.floor((s % 3600) / 60)
+	secs  = s - (hours * 3600) - (mins * 60)
+	return hmmss:format(hours, mins, secs)
+end
+
+local UpdateTimer = function(af, dt)
 	local seconds = GetTimeSinceStart() - SL.Global.TimeAtSessionStart
 
 	-- if this game session is less than 1 hour in duration so far
 	if seconds < 3600 then
 		bmt_actor:settext( SecondsToMMSS(seconds) )
+
+	-- somewhere between 1 and 10 hours
+	elseif seconds >= 3600 and seconds < 36000 then
+		bmt_actor:settext( SecondsToHMMSS(seconds) )
+
+	-- in it for the long haul
 	else
 		bmt_actor:settext( SecondsToHHMMSS(seconds) )
 	end
 end
+
+-- -----------------------------------------------------------------------
 
 local af = Def.ActorFrame{ OffCommand=function(self) self:linear(0.1):diffusealpha(0) end }
 
@@ -24,7 +47,7 @@ if PREFSMAN:GetPreference("EventMode") then
 			SL.Global.TimeAtSessionStart = GetTimeSinceStart()
 		end
 
-		self:SetUpdateFunction( Update )
+		self:SetUpdateFunction( UpdateTimer )
 	end
 end
 
