bdf514cfd876abbf94677f54cf80d0a37abab9cd
quietly-turning <quietly.turning@gmail.com>
2020-06-17 15:58:45 -0400

    don't hardcode SL gamemodes during judgment lookup
    We can use FILEMAN:GetDirListing() to get a list like { "ITG", "FA+" }
    rather than hardcoding those strings when looking up judgment graphics
    for ScreenSelectProfile.

--- BGAnimations/ScreenSelectProfile underlay/JudgmentGraphicPreviews.lua
+++ BGAnimations/ScreenSelectProfile underlay/JudgmentGraphicPreviews.lua
@@ -3,28 +3,31 @@ local af = args.af
 
 local already_loaded = {}
 
+-- get a table like { "ITG", "FA+" }
+local judgment_dirs = FILEMAN:GetDirListing(THEME:GetCurrentThemeDirectory().."/Graphics/_judgments/", true, false)
+
 for profile in ivalues(args.profile_data) do
 	if profile.judgment ~= nil and profile.judgment ~= "" and not FindInTable(profile.judgment, already_loaded) then
 
-		if FILEMAN:DoesFileExist(THEME:GetCurrentThemeDirectory().."/Graphics/_judgments/ITG/"..profile.judgment) then
-			af[#af+1] = LoadActor(THEME:GetPathG("","_judgments/ITG/"..profile.judgment))..{
-				Name="JudgmentGraphic_"..StripSpriteHints(profile.judgment),
-				InitCommand=function(self)
-					self:y(-50):animate(false)
-					-- why is the original Love judgment asset so... not aligned?
-					-- it throws the aesthetic off as-is, so fudge a little
-					if profile.judgment == "Love 2x6.png" then self:y(-55) end
-				end
-			}
-			table.insert(already_loaded, profile.judgment)
+		for dir in ivalues(judgment_dirs) do
 
-		elseif FILEMAN:DoesFileExist(THEME:GetCurrentThemeDirectory().."/Graphics/_judgments/FA+/"..profile.judgment) then
-			af[#af+1] = LoadActor(THEME:GetPathG("","_judgments/FA+/"..profile.judgment))..{
-				Name="JudgmentGraphic_"..StripSpriteHints(profile.judgment),
-				InitCommand=function(self) self:y(-50):animate(false) end
-			}
-			table.insert(already_loaded, profile.judgment)
+			local path = ("/%s/Graphics/_judgments/%s/%s"):format(THEME:GetCurrentThemeDirectory(), dir, profile.judgment)
+			if FILEMAN:DoesFileExist(path) then
 
+				af[#af+1] = Def.Sprite{
+					Name="JudgmentGraphic_"..StripSpriteHints(profile.judgment),
+					Texture=path,
+					InitCommand=function(self)
+						self:y(-50):animate(false)
+						-- why is the original Love judgment asset so... not aligned?
+						-- it throws the aesthetic off as-is, so fudge a little
+						if dir=="ITG" and profile.judgment == "Love 2x6.png" then self:y(-55) end
+					end
+				}
+
+				table.insert(already_loaded, profile.judgment)
+				break
+			end
 		end
 	end
 end
