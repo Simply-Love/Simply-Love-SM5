31f4d57a9b8d273f4cda846d2875a7fbfac3943e
quietly-turning <quietly.turning@gmail.com>
2020-07-02 14:00:33 -0400

    show unmapped input events on ScreenTestInput
    This should help people during the process of mapping dance pads to game
    buttons.
    
    If the input event is mapped to a game button, the pad visualization
    will light up like before.  If the input event is not mapped, show some
    text saying what the event is being registered as, and note that it is
    not mapped to any game button.

--- BGAnimations/ScreenTestInput underlay.lua
+++ BGAnimations/ScreenTestInput underlay.lua
@@ -1,5 +1,11 @@
+local held = {}
+local unmapped_list
+local game = GAMESTATE:GetCurrentGame():GetName()
+
+-- -----------------------------------------------------------------------
+
 local InputHandler = function(event)
-	if not (event and event.PlayerNumber and event.button) then return false end
+	if not (event and event.button) then return false end
 
 	-- allow players to back out of ScreenTestInput by pressing Escape on their keyboard
 	if event.GameButton == "Back" then
@@ -10,24 +16,46 @@ local InputHandler = function(event)
 		MESSAGEMAN:Broadcast("TestInputEvent", event)
 	end
 
+	if event.button == "" then
+		local key = ("%s %s"):format(ToEnumShortString(event.DeviceInput.device), ToEnumShortString(event.DeviceInput.button))
+
+		if event.type == "InputEventType_Release" then
+			held[key] = false
+		else
+			held[key] = true
+		end
+
+		unmapped_list:playcommand("Update")
+	end
+
 	return false
 end
 
+-- -----------------------------------------------------------------------
+
 local af = Def.ActorFrame {
 	OnCommand=function(self)
-		SCREENMAN:GetTopScreen():AddInputCallback(InputHandler)
+		-- only add a Lua input callback handler for dance, pump, and techno
+		-- where we load in custom visuals
+		if (game=="dance" or game=="pump" or game=="techno") then
+			SCREENMAN:GetTopScreen():AddInputCallback(InputHandler)
+		end
 	end,
 	OffCommand=function(self) self:sleep(0.4) end,
 
 	Def.DeviceList {
 		Font=THEME:GetPathF("","Common Normal"),
-		InitCommand=function(self) self:xy(_screen.cx,_screen.h-60):zoom(0.8) end
+		InitCommand=function(self)
+			if ThemePrefs.Get("RainbowMode") then self:diffuse(0,0,0,1) end
+			self:xy(_screen.cx,_screen.h-60):zoom(0.8)
+		end
 	}
 }
 
-local game = GAMESTATE:GetCurrentGame():GetName()
-
+-- for these specific games
 if (game=="dance" or game=="pump" or game=="techno") then
+
+	-- load custom visuals to show which inputs are mapped to game buttons
 	for player in ivalues( PlayerNumber ) do
 		local pad = LoadActor(THEME:GetPathB("", "_modules/TestInput Pad"), {Player=player, ShowMenuButtons=true, ShowPlayerLabel=true})
 
@@ -37,10 +65,35 @@ if (game=="dance" or game=="pump" or game=="techno") then
 
 		af[#af+1] = pad
 	end
+
+	-- and add a custom BitmapText to show which inputs are not mapped to any game buttons
+	af[#af+1] = Def.BitmapText{
+		Font="Common Normal",
+		InitCommand=function(self)
+			if ThemePrefs.Get("RainbowMode") then self:diffuse(0,0,0,1) end
+			self:xy(_screen.cx, _screen.cy+32):vertalign(top):vertspacing(-3)
+			unmapped_list = self
+		end,
+		UpdateCommand=function(self)
+			local s = ""
+			for k, v in pairs(held) do
+				if v then
+					s = s .. ("%s (%s)\n"):format(k, THEME:GetString("ScreenTestInput", "not mapped"))
+				end
+			end
+			self:settext(s)
+		end
+
+	}
+
+-- for other games (para, kb7), just use a standard InputList provided by the engine
 else
 	af[#af+1] = Def.InputList{
-		Font="Common normal",
-		InitCommand=function(self) self:xy(_screen.cx-250, 50):horizalign(left):vertalign(0):vertspacing(0) end
+		Font="Common Normal",
+		InitCommand=function(self)
+			if ThemePrefs.Get("RainbowMode") then self:diffuse(0,0,0,1) end
+			self:xy(_screen.cx-250, 50):horizalign(left):vertalign(top):vertspacing(0)
+		end
 	}
 end
 
