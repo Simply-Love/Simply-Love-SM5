8db65f50a4e35a52a20ce322179f1a71f17a1b97
quietly-turning <quietly.turning@gmail.com>
2020-07-02 03:01:22 -0400

    slightly darken bg of ManageLocalProfiles MiniMenu
    This commit slightly darkens the background when viewing the modal
    MiniMenu for managing a specific local profile (Set P1, Set P2, Edit,
    Rename, Delete, Merge, etc.).
    
    I'd really been hoping to change the options presented (wanted to remove
    choices for "Change To Guest", "Change To Normal", and "Change To Test"
    as the engine's use of those terms is different than SL's) but the
    MiniMenu choices appear to be hardcoded into
    ScreenOptionsManageProfiles.cpp.  The necessary Lua hooks to recreate a
    similar, reduced menu in Lua are not available, so this task will have
    to wait for now.

--- BGAnimations/ScreenMiniMenuContext underlay.lua
+++ BGAnimations/ScreenMiniMenuContext underlay.lua
@@ -7,8 +7,8 @@ local num_rows
 local row_height  = 28
 local container_w = 240
 
-return Def.ActorFrame {
-	InitCommand=function(self) self:xy(_screen.cx-container_w, -16):queuecommand("Capture") end,
+local underlay = Def.ActorFrame {
+	InitCommand=function(self) self:queuecommand("Capture") end,
 	CaptureCommand=function(self)
 		-- how many rows do we need to accommodate?
 		num_rows = #SCREENMAN:GetTopScreen():GetChild("Container"):GetChild("")
@@ -16,13 +16,30 @@ return Def.ActorFrame {
 		-- so don't accommodate the decorative border for more than 10
 		num_rows = math.min(10, num_rows)
 		self:queuecommand("Size")
-	end,
+	end
+}
+
+-- darken the background behind the modal
+underlay[#underlay+1] = Def.Quad{
+	SizeCommand=function(self)
+		self:x(_screen.cx)
+		self:zoomto(_screen.w*2, _screen.h*2):diffuse(0,0,0,0.75)
+	end
+}
+
+
+-- modal of choices for managing this local profile
+-- the MiniMenu choices (set P1, set P2, edit, rename, delete, merge, move, etc.)
+-- are hardcoded by the SM5 engine for now
+underlay[#underlay+1] = Def.ActorFrame{
+	InitCommand=function(self) self:xy(_screen.cx-container_w, -16) end,
 
 	-- decorative border
 	Def.Quad{
 		SizeCommand=function(self) self:zoomto(container_w, row_height*num_rows) end,
 	},
 
+	-- local profile's display name at top of modal
 	LoadFont("Common Normal")..{
 		InitCommand=function(self) self:xy(-99, -118):halign(0):diffuse(Color.Black) end,
 		BeginCommand=function(self)
@@ -33,3 +50,5 @@ return Def.ActorFrame {
 		end
 	}
 }
+
+return underlay
