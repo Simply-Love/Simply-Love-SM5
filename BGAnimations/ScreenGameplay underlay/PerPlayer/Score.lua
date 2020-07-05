026b1cbce3590bbcb12c0e6e28e95c91787803ed
quietly-turning <quietly.turning@gmail.com>
2020-07-05 00:37:03 -0400

    align score in StepStats with judgment counts
    When StepStats pane and DensityGraphAtTop are both active, the score
    moves down into the StepStats pane, right-aligned to the judgment count
    text, assuming 4 digits of judgments.
    
    Some very long songs have total tapnote counts with 5 or 6 digits.  This
    accommodates those scenarios for widescreen aspect ratios.
    
    4:3 displays retain the behavior of aligning the score to the 4th digit
    of the judgment counts.

--- BGAnimations/ScreenGameplay underlay/PerPlayer/Score.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/Score.lua
@@ -26,6 +26,16 @@ local pos = {
 local dance_points, percent
 local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(player)
 
+local StepsOrTrail = (GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentTrail(player)) or GAMESTATE:GetCurrentSteps(player)
+local total_tapnotes = StepsOrTrail:GetRadarValues(player):GetValue( "RadarCategory_Notes" )
+
+-- determine how many digits are needed to express the number of notes in base-10
+local digits = (math.floor(math.log10(total_tapnotes)) + 1)
+-- subtract 4 from the digit count; we're only really interested in how many digits past 4
+-- this stepcount is so we can use it to align the score actor in the StepStats pane if needed
+-- aligned-with-4-digits is the default
+digits = clamp(math.max(4, digits) - 4, 0, 3)
+
 -- -----------------------------------------------------------------------
 
 return LoadFont("Wendy/_wendy monospace numbers")..{
@@ -49,7 +59,14 @@ return LoadFont("Wendy/_wendy monospace numbers")..{
 				local judgmentnumbers = step_stats:GetChild("BannerAndData"):GetChild("JudgmentNumbers"):GetChild("")[1]
 				-- padding is a lazy fix for multiple ActorFrames having zoom applied and
 				-- me not feeling like recursively crawling the AF tree to factor each in
-				local padding = IsUltraWide and -4 or 37.5
+				local padding = IsUltraWide and -4 or 37
+
+				if IsUsingWideScreen() then
+					-- pad with an additional ~14px for each digit past 4 the stepcount goes
+					-- this keeps the score right-aligned with the right edge of the judgment
+					-- counts in the StepStats pane
+					padding = padding + (digits * 14)
+				end
 
 
 				self:x(step_stats:GetX() + judgmentnumbers:GetX() + padding)
