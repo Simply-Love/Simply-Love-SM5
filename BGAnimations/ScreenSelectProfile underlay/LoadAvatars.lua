ae5fa7ebb812dfb7d18ec9ac4533fe6c1a482d2a
quietly-turning <quietly.turning@gmail.com>
2020-07-07 19:27:29 -0400

    include avatar file intended for prev commit
    Oops.

---
+++ BGAnimations/ScreenSelectProfile underlay/LoadAvatars.lua
@@ -0,0 +1,27 @@
+local args = ...
+local af = args.af
+
+local textures = {}
+
+af[#af+1] = Def.Sprite{
+	InitCommand=function(self)
+		for profile in ivalues(args.profile_data) do
+			if profile.dir and profile.displayname then
+				local path = ActorUtil.ResolvePath(profile.dir .. "avatar", 1, true)
+				          or ActorUtil.ResolvePath("/Appearance/Avatars/" .. profile.displayname, 1, true)
+
+				if path then
+					-- limited to basic Bitmaps (png, jpg, jpeg, bpm, gif) for now
+					-- maybe support movie files or animated sprite textures in the future
+					if ActorUtil.GetFileType(path) == "FileType_Bitmap" then
+						self:Load(path)
+						textures[profile.displayname] = self:GetTexture()
+					end
+				end
+			end
+		end
+		self:visible(false):hibernate(math.huge)
+	end
+}
+
+return textures
