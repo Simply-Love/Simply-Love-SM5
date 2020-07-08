936ecbe36a52c075acdc75b383dc8654917d827b
quietly-turning <quietly.turning@gmail.com>
2020-07-08 08:40:13 -0400

    more sensible (7+3) HighScore list rules for pane3
    This is hopefully a more sensible implementation of what
    648a7fd37492d44f84fa8cd8e429f562aeda88c5 set out to do.
    
    The underlying motivation is the same:
    Novice players frequently improve their own score while struggling to
    break into an overall leaderboard. The lack of *visible* leaderboard
    progress can be demoralizing, so let's do what we can to alleviate that.
    
    The previous implementation check if a profile (local or USB) was in use
    and displayed top 7 machine high scores and then top 3 personal high
    scores, if so.
    
    This 7-then-3 layout would always show up when a profile was in use,
    even if the entire leader was a single high score name because it was a
    privately-owned setup that only one person played on.
    
    The new implementation checks:
    Is this score not good enough to be a top-10 machine score?
    Is it good enough to be a top-2 personal score?
    
    If both of those^, show 8-then-2.
    
    Some things to consider:
    1. If the player isn't using a profile, personal HighScores aren't
    possible, so those conditions will never be true, and we'll just show
    top-10 machine scores.
    
    2. This should help private machine owners who play by themselves (or
    mostly by themselves).  It's unlikely a score would be top-2 personal
    but somehow not top-10 machine for them.

--- BGAnimations/ScreenEvaluation common/PerPlayer/Pane3/default.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Pane3/default.lua
@@ -2,6 +2,17 @@
 
 local player = ...
 
+local pss = STATSMAN:GetCurStageStats():GetPlayerStageStats(player)
+local HighScoreIndex = {
+	Machine =  pss:GetMachineHighScoreIndex(),
+	Personal = pss:GetPersonalHighScoreIndex()
+}
+local NumHighScores = math.min(10, PREFSMAN:GetPreference("MaxHighScoresPerListForMachine"))
+local EarnedMachineRecord = HighScoreIndex.Machine  >= 0
+local EarnedTop2Personal  = (HighScoreIndex.Personal >= 0 and HighScoreIndex.Personal < 2)
+
+-- -----------------------------------------------------------------------
+
 local pane = Def.ActorFrame{
 	Name="Pane3",
 	InitCommand=function(self)
@@ -14,29 +25,52 @@ local pane = Def.ActorFrame{
 local rh
 local args = { Player=player, RoundsAgo=1, RowHeight=rh}
 
--- if the player is using a profile (local or USB)
-if PROFILEMAN:IsPersistentProfile(player) then
+
+-- Novice players frequently improve their own score while struggling to
+-- break into an overall leaderboard.  The lack of *visible* leaderboard
+-- progress can be frustrating/demoralizing, so let's do what we can to
+-- alleviate that.
+--
+-- If this score is not high enough to be a machine record, but it *is*
+-- good enough to be a top-2 personal record, show two HighScore lists:
+-- 1-8 machine HighScores, then 1-2 personal HighScores
+--
+-- If the player isn't using a profile (local or USB), there won't be any
+-- personal HighScores to compare against.
+--
+-- Also, this 8+2 shouldn't show up on privately owned machines where only
+-- one person plays, which is a common scenario in 2020.
+--
+-- This idea of showing both machine and personal HighScores to help new players
+-- track progress is based on my experiences maintaining a heavily-used
+-- public SM5 machine for several years while away at school.
+
+
+if (not EarnedMachineRecord and EarnedTop2Personal) then
 
 	-- less line spacing between HighScore rows to fit the horizontal line
 	rh = 20.25
 	args.RowHeight = rh
 
 	-- top 7 machine HighScores
-	args.NumHighScores = 7
+	args.NumHighScores = 8
 	pane[#pane+1] = LoadActor(THEME:GetPathB("", "_modules/HighScoreList.lua"), args)
 
 	-- horizontal line visually separating machine HighScores from player HighScores
-	pane[#pane+1] = Def.Quad{ InitCommand=function(self) self:zoomto(100, 1):y(rh*8):diffuse(1,1,1,0.33) end }
+	pane[#pane+1] = Def.Quad{ InitCommand=function(self) self:zoomto(100, 1):y(rh*9):diffuse(1,1,1,0.33) end }
 
-	-- top 3 player HighScores
-	args.NumHighScores = 3
+	-- top 2 player HighScores
+	args.NumHighScores = 2
 	args.Profile = PROFILEMAN:GetProfile(player)
 	pane[#pane+1] = LoadActor(THEME:GetPathB("", "_modules/HighScoreList.lua"), args)..{
-		InitCommand=function(self) self:y(rh*8) end
+		InitCommand=function(self) self:y(rh*9) end
 	}
 
--- else the player is not using a profile
+
+-- the player did not meet the conditions to show the 8+2 HighScores
+-- just show top 10 machine HighScores
 else
+
 	-- more breathing room between HighScore rows
 	rh = 22
 	args.RowHeight = rh
