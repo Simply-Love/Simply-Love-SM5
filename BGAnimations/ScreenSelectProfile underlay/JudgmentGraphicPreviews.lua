3e7dde1270d585f020887e15eb3f89a301cc9e3c
JapanYoshi <japanyoshi777@gmail.com>
2020-07-15 22:40:53 +1000

    Recreated Love judgment at doubleres

--- BGAnimations/ScreenSelectProfile underlay/JudgmentGraphicPreviews.lua
+++ BGAnimations/ScreenSelectProfile underlay/JudgmentGraphicPreviews.lua
@@ -19,9 +19,6 @@ for profile in ivalues(args.profile_data) do
 					Texture=path,
 					InitCommand=function(self)
 						self:y(-50):animate(false)
-						-- why is the original Love judgment asset so... not aligned?
-						-- it throws the aesthetic off as-is, so fudge a little
-						if dir=="ITG" and profile.judgment == "Love 2x6.png" then self:y(-55) end
 					end
 				}
 
