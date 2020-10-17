906645e8fc0ab1bc97edc083d52024122b9658bb
quietly-turning <quietly.turning@gmail.com>
2020-10-17 16:41:00 -0400

    hitd#5

--- BGAnimations/ScreenHereInTheDarkness overlay/5/default.lua
+++ BGAnimations/ScreenHereInTheDarkness overlay/5/default.lua
@@ -3,49 +3,59 @@
 -- D  C   D  Em
 -- D
 
-local intro = "Walking on the balls of my feet\nI led her down a dark hallway."
+local intro = {"Walking on the balls of my feet", "I led her down a dark hallway."}
 local footsteps = { "our","feet","gently","tip","tap","tapping","on","the","cold","dark","floor","as we","walked","hand in hand","together" }
-local outro = "Where were we going?\nHow would we know when we got there?\nI gripped her hand more tightly."
+local outro = {"Where were we going?", "How would we know when we got there?", "I gripped her hand more tightly."}
+
+local line_height = 18
+local font_path = THEME:GetPathB("ScreenHereInTheDarkness", "overlay/_shared/helvetica neue/_helvetica neue 20px.ini")
+
+local line = function(text, i, num_lines, wait)
+	return Def.BitmapText{
+		File=font_path,
+		Text=text,
+		InitCommand=function(self)
+			self:xy( _screen.cx, _screen.cy-50-((num_lines-i)*line_height) ):diffusealpha(0)
+		end,
+		OnCommand=function(self)
+			self:sleep(wait + (i-1)*2):smooth(2):diffusealpha(1)
+			self:sleep(3):smooth(2):diffusealpha(0)
+		end,
+	}
+end
 
 local af = Def.ActorFrame{}
 af.InputEventCommand=function(self, event)
 	if event.type == "InputEventType_FirstPress" and event.GameButton=="Back" then
-		SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
+		self:queuecommand("NextScreen")
 	end
 end
 af.OnCommand=function(self)
 	self:queuecommand("Intro")
-		:sleep(11):queuecommand("Walk")
-		:sleep(#footsteps/1.475):queuecommand("Outro")
+	self:sleep(11):queuecommand("Walk")
+	self:sleep(#footsteps/1.475):queuecommand("Hide")
+	self:sleep(14):queuecommand("NextScreen")
 end
-
+af.NextScreenCommand=function(self)
+	SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
+end
 
 af[#af+1] = LoadActor("hallways-v1.ogg")..{
 	OnCommand=function(self) self:play() end
 }
 
-af[#af+1] = Def.BitmapText{
-	File=THEME:GetPathB("ScreenHereInTheDarkness", "overlay/_shared/helvetica neue/_helvetica neue 20px.ini"),
-	Text=intro,
-	InitCommand=function(self) self:xy( _screen.cx, _screen.cy-self:GetHeight()/2 ):diffusealpha(0) end,
-	IntroCommand=function(self)
-		self:sleep(2):linear(2):diffusealpha(1):sleep(5):linear(2):diffusealpha(0)
-		if self:GetText()==outro then
-			self:sleep(3):queuecommand("Transition")
-		end
-	end,
-	OutroCommand=function(self)
-		self:settext(outro):queuecommand("Intro")
-	end,
-	TransitionCommand=function(self)
-		SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
-	end
-}
+for i=1, #intro do
+	af[#af+1] = line(intro[i], i, #intro, 2)
+end
+
+for i=1, #outro do
+	af[#af+1] = line(outro[i], i, #outro, 22.125)
+end
 
 local hallway = Def.ActorFrame{
 	InitCommand=function(self) self:y(_screen.cy+20):fov(90):rotationx(-80) end,
 	WalkCommand=function(self) self:linear(#footsteps/1.475) end,
-	OutroCommand=function(self) self:visible(false) end
+	HideCommand=function(self) self:visible(false) end
 }
 
 for i=#footsteps, 1, -1 do
@@ -54,10 +64,11 @@ for i=#footsteps, 1, -1 do
 		WalkCommand=function(self) self:sleep(i*0.5):accelerate(1.25):diffuse(1,1,1,1) end,
 
 		Def.BitmapText{
-			File=THEME:GetPathB("ScreenHereInTheDarkness", "overlay/_shared/helvetica neue/_helvetica neue 20px.ini"),
+			File=font_path,
 			Text=footsteps[i],
 			InitCommand=function(self) self:xy(_screen.cx+(i%2==0 and -20 or 20), -70):rotationx(82.5) end,
-			WalkCommand=function(self) self:sleep(i*0.5):linear(2.4):y(_screen.h * 0.75) end
+			WalkCommand=function(self) self:sleep(i*0.5):linear(2.4):y(_screen.h * 0.75):linear(0.5):diffusealpha(0):queuecommand("Hide") end,
+			HideCommand=function(self) self:visible(false) end
 		}
 	}
 end
