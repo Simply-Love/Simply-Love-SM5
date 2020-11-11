02fcee2681a6d761b269f3f5bc573e1ba33032fa
Martin Natano <natano@natano.net>
2020-11-11 23:22:30 +0100

    Allow to enable the SL theme on StepMania 5.3 (outfox)
    This commit also introduces the new IsSMVersion() function, which will
    be used in further commits.

--- Scripts/SL-SupportHelpers.lua
+++ Scripts/SL-SupportHelpers.lua
@@ -1,44 +1,51 @@
+function getSMVersion()
+	-- get the version string, e.g. "5.0.11" or "5.1.0" or "5.2-git-96f9771" or etc.
+	local version = ProductVersion()
+	if type(version) ~= "string" then return {} end
+
+	-- remove the build suffix from the version string
+	-- debug build are suffixed with "-git-$something" or "-UNKNOWN" if the
+	-- git hash is not available for some reason
+	version = version:gsub("-.*", "")
+
+	-- parse the version string into a table
+	local v = {}
+	for i in version:gmatch("[^%.]+") do
+		table.insert(v, tonumber(i))
+	end
+
+	return v
+end
+
+
+function IsSMVersion(...)
+	local version = getSMVersion()
+
+	for i = 1, select('#', ...) do
+		if select(i, ...) ~= version[i] then
+			return false
+		end
+	end
+
+	return true
+end
+
+
 -- -----------------------------------------------------------------------
 -- use StepManiaVersionIsSupported() to check if Simply Love supports the version of SM5 in use
 
 StepManiaVersionIsSupported = function()
-
-	-- ensure that we're using StepMania
+	-- sanity checks to make sure we're running StepMania
 	if type(ProductFamily) ~= "function" or ProductFamily():lower() ~= "stepmania" then return false end
-
-	-- ensure that a global ProductVersion() function exists before attempting to call it
 	if type(ProductVersion) ~= "function" then return false end
+	if type(ProductVersion()) ~= "string" then return false end
 
-	-- get the version string, e.g. "5.0.11" or "5.1.0" or "5.2-git-96f9771" or etc.
-	local version = ProductVersion()
-	if type(version) ~= "string" then return false end
-
-	-- remove the git hash if one is present in the version string
-	version = version:gsub("-git-.+", "")
-
-	-- split the remaining version string on periods; store each segment in a temp table
-	local t = {}
-	for i in version:gmatch("[^%.]+") do
-		table.insert(t, tonumber(i))
-	end
-
-	-- if we didn't detect SM5.x.x then Something Is Terribly Wrong.
-	if not (t[1] and t[1]==5) then return false end
-
-	-- SM5.0.x is supported
+	-- SM5.0.12 is supported (latest stable release)
 	-- SM5.1.x is supported
-	-- SM5.2 is not supported because it saw significant backwards-incompatible API changes and is now abandoned
-	-- SM5.3 is not supported for now because it is not open source
-	if not (t[2] and (t[2]==0 or t[2]==1)) then return false end
-
-	-- if we're in SM5.0.x, then check for a third segment
-	if t[2]==0 then
-		-- SM5.0.12 is supported because SM5.1 is "still in beta" and many users are reluctant to install beta software
-		-- anything older than SM5.0.12 is not supported
-		if not (t[3] and t[3]==12) then return false end
-	end
-
-	return true
+	-- SM5.2 is not supported because it saw significant
+	--       backwards-incompatible API changes and is now abandoned
+	-- SM5.3.x is supported (beta status because it's not open source yet)
+	return IsSMVersion(5, 0, 12) or IsSMVersion(5, 1) or IsSMVersion(5, 3)
 end
 
 -- -----------------------------------------------------------------------
