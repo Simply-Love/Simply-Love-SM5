Simply-Love/Simply-Love-SM5

commit 5de3718984da287b16970e65cd1ae3974f208557

redo layout of StepStats side pane
This layout is hopefully more helpful at-a-glance to players.

It moves judgment counts closer to the NoteField, better accommodates P1
vs P2 (by flipping horizontal alignment appropriately), takes
Center1Player into consideration, and scales for 4:3, 16:9, 16:10, and
21:9.

The code is overall cleaner, makes better use of ActorFrames to
position groups of actors, and is less likely to induce anxiety in
anyone who has to work with it in the future.

Up next: fixing score positioning 😬

BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/HoldsMinesRolls.lua
@@ -1,0 +1,118 @@
+local player = ...
+
+local IsUltraWide = (GetScreenAspectRatio() > 21/9)
+local NoteFieldIsCentered = (GetNotefieldX(player) == _screen.cx)
+
+local RadarCategories = { 'Holds', 'Mines', 'Rolls' }
+local RCJudgments = { Holds=0, Mines=0, Rolls=0 }
+
+local row_height = 28
+
+-- -----------------------------------------------------------------------
+
+local af = Def.ActorFrame{}
+af.InitCommand=function(self)
+	self:x(player==PLAYER_1 and 155 or -85)
+	self:y(-124)
+
+	if NoteFieldIsCentered and IsUsingWideScreen() then
+		self:x( player==PLAYER_1 and 155 or -88 )
+	end
+
+	-- adjust for smaller panes when ultrawide and both players joined
+	if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+		self:x( player==PLAYER_1 and 14 or 50 )
+	end
+end
+
+
+-- then handle holds, mines, hands, rolls
+for index, RCType in ipairs(RadarCategories) do
+
+	-- player performance value
+	af[#af+1] = LoadFont("Wendy/_ScreenEvaluation numbers")..{
+		Text="000",
+		InitCommand=function(self)
+			self:zoom(0.4)
+			self:halign(PlayerNumber:Reverse()[OtherPlayer[player]])
+			self:x( 0 )
+			self:y((index-1)*row_height - 22)
+		end,
+		BeginCommand=function(self)
+			leadingZeroAttr = { Length=2, Diffuse=color("#5A6166") }
+			self:AddAttribute(0, leadingZeroAttr )
+		end,
+		JudgmentMessageCommand=function(self, params)
+			if params.Player ~= player then return end
+			if not params.TapNoteScore then return end
+
+			if RCType=="Mines" and params.TapNoteScore == "TapNoteScore_AvoidMine" then
+				RCJudgments.Mines = RCJudgments.Mines + 1
+				self:settext( string.format("%03d", RCJudgments.Mines) )
+
+			elseif RCType=="Holds" and params.TapNote and params.TapNote:GetTapNoteSubType() == "TapNoteSubType_Hold" then
+				RCJudgments.Holds = RCJudgments.Holds + 1
+				self:settext( string.format("%03d", RCJudgments.Holds) )
+
+			elseif RCType=="Rolls" and params.TapNote and params.TapNote:GetTapNoteSubType() == "TapNoteSubType_Roll" then
+				RCJudgments.Rolls = RCJudgments.Rolls + 1
+				self:settext( string.format("%03d", RCJudgments.Rolls) )
+			end
+
+			leadingZeroAttr = { Length=(3-tonumber(tostring(RCJudgments[RCType]):len())), Diffuse=color("#5A6166") }
+			self:AddAttribute(0, leadingZeroAttr )
+		end
+	}
+
+	--  slash
+	af[#af+1] = LoadFont("Common Normal")..{
+		Text="/",
+		InitCommand=function(self)
+			self:diffuse(color("#5A6166")):zoom(1.25)
+			self:halign(PlayerNumber:Reverse()[OtherPlayer[player]])
+			self:x( 45 * (player==PLAYER_1 and -1 or 1) )
+			self:y((index-1)*row_height - 22)
+		end
+	}
+
+	-- possible value
+	af[#af+1] = LoadFont("Wendy/_ScreenEvaluation numbers")..{
+		InitCommand=function(self)
+			self:zoom(0.4)
+			self:halign(PlayerNumber:Reverse()[player])
+			self:x( 100 * (player==PLAYER_1 and -1 or 1) )
+			self:y((index-1)*row_height - 22)
+		end,
+		BeginCommand=function(self)
+
+			possible = 0
+			StepsOrTrail = (GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentTrail(player)) or GAMESTATE:GetCurrentSteps(player)
+
+			if StepsOrTrail then
+				rv = StepsOrTrail:GetRadarValues(OtherPlayer[player])
+				possible = rv:GetValue( RCType )
+				-- non-static courses (for example, "Most Played 1-4") will return -1 here
+				if possible < 0 then possible = 0 end
+			end
+
+			self:settext( string.format("%03d", possible) )
+			local leadingZeroAttr = { Length=3-tonumber(tostring(possible):len()); Diffuse=color("#5A6166") }
+			self:AddAttribute(0, leadingZeroAttr )
+		end
+	}
+end
+
+-- labels: holds, mines, rolls
+for i, label in ipairs(RadarCategories) do
+	af[#af+1] = LoadFont("Common Normal")..{
+		Text=THEME:GetString("ScreenEvaluation",label),
+		InitCommand=function(self)
+			self:zoom(0.833)
+			self:x( player==PLAYER_1 and -110 or -10 )
+			self:y((i-1)*row_height - 18)
+			self:horizalign( right )
+		end
+	}
+end
+
+return af

BGAnimations/ScreenGameplay underlay/PerPlayer/Score.lua
@@ -80,7 +80,7 @@ return LoadFont("Wendy/_wendy monospace numbers")..{
 				-- but current conditions might be such that it won't actually appear.
 				-- Ensure the StepStats ActorFrame is present before trying to traverse it.
 				if step_stats then
-					local judgmentnumbers = step_stats:GetChild("BannerAndData"):GetChild("JudgmentNumbers"):GetChild("")[1]
+					local judgmentnumbers = step_stats:GetChild("BannerAndData"):GetChild("TapNoteJudgments"):GetChild("")[1]
 
 					-- -----------------------------------------------------------------------
 					-- FIXME: "padding" is a lazy fix for multiple nested ActorFrames having zoom applied and

BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/Banner.lua
@@ -1,7 +1,21 @@
+local player = ...
+local NoteFieldIsCentered = (GetNotefieldX(player) == _screen.cx)
+local IsUltraWide = (GetScreenAspectRatio() > 21/9)
+
 return Def.Banner{
 	CurrentSongChangedMessageCommand=function(self)
 		self:LoadFromSong( GAMESTATE:GetCurrentSong() )
-			:setsize(418,164):zoom(0.4)
-			:xy(-70, -200)
+		self:setsize(418,164):zoom(0.4)
+		self:xy(70 * (player==PLAYER_1 and 1 or -1), -200)
+
+		-- offset a bit more when NoteFieldIsCentered
+		if NoteFieldIsCentered and IsUsingWideScreen() then
+			self:x( 72 * (player==PLAYER_1 and 1 or -1) )
+		end
+
+		-- ultrawide and both players joined
+		if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+			self:x(self:GetX() * -1)
+		end
 	end
 }

BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/JudgmentLabels.lua
@@ -1,50 +1,0 @@
-local player = ...
-local pn = ToEnumShortString(player)
-
-local TapNoteScores = { Types={'W1', 'W2', 'W3', 'W4', 'W5', 'Miss'}, Names={} }
-local tns_string = "TapNoteScore" .. (SL.Global.GameMode=="ITG" and "" or SL.Global.GameMode)
--- get TNS names appropriate for the current GameMode, localized to the current language
-for i, judgment in ipairs(TapNoteScores.Types) do
-	TapNoteScores.Names[#TapNoteScores.Names+1] = THEME:GetString(tns_string, judgment)
-end
-
-local RadarCategories = {
-	THEME:GetString("ScreenEvaluation", 'Holds'),
-	THEME:GetString("ScreenEvaluation", 'Mines'),
-	THEME:GetString("ScreenEvaluation", 'Rolls')
-}
-
-local row_height = 28
-local windows = SL.Global.ActiveModifiers.TimingWindows
-
-local af = Def.ActorFrame{}
-
---  labels: W1, W2, W3, W4, W5, Miss
-for i, label in ipairs(TapNoteScores.Names) do
-
-	-- no need to add BitmapText actors for TimingWindows that were turned off
-	if windows[i] or i==#TapNoteScores.Names then
-
-		af[#af+1] = LoadFont("Common Normal")..{
-			Text=label:upper(),
-			InitCommand=function(self) self:zoom(0.833):horizalign(right):maxwidth(72) end,
-			BeginCommand=function(self)
-				self:x(80):y((i-1)*row_height - 226)
-				    :diffuse( SL.JudgmentColors[SL.Global.GameMode][i] )
-			end
-		}
-	end
-end
-
--- labels: holds, mines, rolls
-for i, label in ipairs(RadarCategories) do
-	af[#af+1] = LoadFont("Common Normal")..{
-		Text=label,
-		InitCommand=function(self) self:zoom(0.833):horizalign(right) end,
-		BeginCommand=function(self)
-			self:x(-94):y((i-1)*row_height - 143)
-		end
-	}
-end
-
-return af

BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/JudgmentNumbers.lua
@@ -1,156 +1,0 @@
-local player = ...
-
-local IsUltraWide = (GetScreenAspectRatio() > 21/9)
-local NoteFieldIsCentered = (GetNotefieldX(player) == _screen.cx)
-
-local possible, rv, pss
-local StepsOrTrail = (GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentTrail(player)) or GAMESTATE:GetCurrentSteps(player)
-local total_tapnotes = StepsOrTrail:GetRadarValues(player):GetValue( "RadarCategory_Notes" )
-
--- determine how many digits are needed to express the number of notes in base-10
-local digits = (math.floor(math.log10(total_tapnotes)) + 1)
--- display a minimum 4 digits for aesthetic reasons
-digits = math.max(4, digits)
-
--- generate a Lua string pattern that will be used to leftpad with 0s
-local pattern = ("%%0%dd"):format(digits)
-
-
-local TapNoteScores = { 'W1', 'W2', 'W3', 'W4', 'W5', 'Miss' }
-local TapNoteJudgments = { W1=0, W2=0, W3=0, W4=0, W5=0, Miss=0 }
-local RadarCategories = { 'Holds', 'Mines', 'Rolls' }
-local RadarCategoryJudgments = { Holds=0, Mines=0, Rolls=0 }
-
-local leadingZeroAttr
-local row_height = 35
-
-local t = Def.ActorFrame{
-	Name="JudgmentNumbers",
-	InitCommand=function(self)
-		self:zoom(0.8)
-	end
-}
-
--- do "regular" TapNotes first
-for index, window in ipairs(TapNoteScores) do
-
-	-- player performance value
-	t[#t+1] = LoadFont("Wendy/_ScreenEvaluation numbers")..{
-		Text=(pattern):format(0),
-		InitCommand=function(self)
-			self:zoom(0.5):horizalign(left)
-
-			if SL.Global.ActiveModifiers.TimingWindows[index] or index==#TapNoteScores then
-				self:diffuse( SL.JudgmentColors[SL.Global.GameMode][index] )
-				leadingZeroAttr = { Length=(digits-1), Diffuse=Brightness(self:GetDiffuse(), 0.35) }
-				self:AddAttribute(0, leadingZeroAttr )
-			else
-				self:diffuse(Brightness({1,1,1,1},0.25))
-			end
-		end,
-		BeginCommand=function(self)
-			self:x( 108 )
-			self:y((index-1)*row_height - 282)
-
-			-- horizontally squishing the numbers isn't pretty, but I'm not sure what else to do
-			-- when people want to play "24 hours of 100 bpm stream" on a 16:9 display with Center1Player enabled  :(
-			if (not IsUsingWideScreen() and digits > 5)
-			or (NoteFieldIsCentered and digits > 4)
-			then
-				self:x(104):maxwidth(WideScale(140,185))
-			end
-
-			if IsUltraWide and (#GAMESTATE:GetHumanPlayers() > 1) and (digits > 4) then
-				self:x(104):maxwidth(165)
-			end
-		end,
-		JudgmentMessageCommand=function(self, params)
-			if params.Player ~= player then return end
-			if params.HoldNoteScore then return end
-
-			if params.TapNoteScore and ToEnumShortString(params.TapNoteScore) == window then
-				TapNoteJudgments[window] = TapNoteJudgments[window] + 1
-				self:settext( (pattern):format(TapNoteJudgments[window]) )
-
-				leadingZeroAttr = {
-					Length=(digits - (math.floor(math.log10(TapNoteJudgments[window]))+1)),
-					Diffuse=Brightness(SL.JudgmentColors[SL.Global.GameMode][index], 0.35)
-				}
-				self:AddAttribute(0, leadingZeroAttr )
-			end
-		end
-	}
-
-end
-
--- then handle holds, mines, hands, rolls
-for index, RCType in ipairs(RadarCategories) do
-
-	-- player performance value
-	t[#t+1] = LoadFont("Wendy/_ScreenEvaluation numbers")..{
-		Text="000",
-		InitCommand=function(self) self:zoom(0.5):horizalign(right) end,
-		BeginCommand=function(self)
-			self:y((index-1)*row_height - 178)
-			self:x( -54 )
-
-			leadingZeroAttr = { Length=2, Diffuse=color("#5A6166") }
-			self:AddAttribute(0, leadingZeroAttr )
-		end,
-		JudgmentMessageCommand=function(self, params)
-			if params.Player ~= player then return end
-			if not params.TapNoteScore then return end
-
-			if RCType=="Mines" and params.TapNoteScore == "TapNoteScore_AvoidMine" then
-				RadarCategoryJudgments.Mines = RadarCategoryJudgments.Mines + 1
-				self:settext( string.format("%03d", RadarCategoryJudgments.Mines) )
-
-			elseif RCType=="Holds" and params.TapNote and params.TapNote:GetTapNoteSubType() == "TapNoteSubType_Hold" then
-				RadarCategoryJudgments.Holds = RadarCategoryJudgments.Holds + 1
-				self:settext( string.format("%03d", RadarCategoryJudgments.Holds) )
-
-			elseif RCType=="Rolls" and params.TapNote and params.TapNote:GetTapNoteSubType() == "TapNoteSubType_Roll" then
-				RadarCategoryJudgments.Rolls = RadarCategoryJudgments.Rolls + 1
-				self:settext( string.format("%03d", RadarCategoryJudgments.Rolls) )
-			end
-
-			leadingZeroAttr = { Length=(3-tonumber(tostring(RadarCategoryJudgments[RCType]):len())), Diffuse=color("#5A6166") }
-			self:AddAttribute(0, leadingZeroAttr )
-		end
-	}
-
-	--  slash
-	t[#t+1] = LoadFont("Common Normal")..{
-		Text="/",
-		InitCommand=function(self) self:diffuse(color("#5A6166")):zoom(1.25):horizalign(right) end,
-		BeginCommand=function(self)
-			self:y((index-1)*row_height - 178)
-			self:x(-40)
-		end
-	}
-
-	-- possible value
-	t[#t+1] = LoadFont("Wendy/_ScreenEvaluation numbers")..{
-		InitCommand=function(self) self:zoom(0.5):horizalign(right) end,
-		BeginCommand=function(self)
-
-			possible = 0
-			StepsOrTrail = (GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentTrail(player)) or GAMESTATE:GetCurrentSteps(player)
-
-			if StepsOrTrail then
-				rv = StepsOrTrail:GetRadarValues(player)
-				possible = rv:GetValue( RCType )
-				-- non-static courses (for example, "Most Played 1-4") will return -1 here
-				if possible < 0 then possible = 0 end
-			end
-
-			self:y((index-1)*row_height - 178)
-			self:x( 16 )
-			self:settext( string.format("%03d", possible) )
-			local leadingZeroAttr = { Length=3-tonumber(tostring(possible):len()); Diffuse=color("#5A6166") }
-			self:AddAttribute(0, leadingZeroAttr )
-		end
-	}
-end
-
-return t

BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/TapNoteJudgments.lua
@@ -1,0 +1,123 @@
+local player = ...
+
+local IsUltraWide = (GetScreenAspectRatio() > 21/9)
+local NoteFieldIsCentered = (GetNotefieldX(player) == _screen.cx)
+
+local StepsOrTrail = (GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentTrail(player)) or GAMESTATE:GetCurrentSteps(player)
+local total_tapnotes = StepsOrTrail:GetRadarValues(player):GetValue( "RadarCategory_Notes" )
+
+-- determine how many digits are needed to express the number of notes in base-10
+local digits = (math.floor(math.log10(total_tapnotes)) + 1)
+-- display a minimum 4 digits for aesthetic reasons
+digits = math.max(4, digits)
+
+-- generate a Lua string pattern that will be used to leftpad with 0s
+local pattern = ("%%0%dd"):format(digits)
+
+
+local TNS = {
+	Types = { 'W1', 'W2', 'W3', 'W4', 'W5', 'Miss' },
+	Judgments = { W1=0, W2=0, W3=0, W4=0, W5=0, Miss=0 },
+	Names = {},
+}
+
+local tns_string = "TapNoteScore" .. (SL.Global.GameMode=="ITG" and "" or SL.Global.GameMode)
+-- get TNS names appropriate for the current GameMode, localized to the current language
+for i, judgment in ipairs(TNS.Types) do
+	TNS.Names[#TNS.Names+1] = THEME:GetString(tns_string, judgment)
+end
+
+local leadingZeroAttr
+local row_height = 35
+
+local windows = SL.Global.ActiveModifiers.TimingWindows
+
+-- -----------------------------------------------------------------------
+
+local af = Def.ActorFrame{}
+af.Name="TapNoteJudgments"
+af.InitCommand=function(self)
+	self:zoom(0.8)
+	self:x( SL_WideScale(152,204) * (player==PLAYER_1 and -1 or 1))
+
+	if NoteFieldIsCentered and IsUsingWideScreen() then
+		self:x( 156 * (player==PLAYER_1 and -1 or 1))
+	end
+
+	-- adjust for smaller panes when ultrawide and both players joined
+	if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+		self:x( 154 * (player==PLAYER_1 and 1 or -1))
+	end
+end
+
+
+for index, window in ipairs(TNS.Types) do
+
+	-- TNS value
+	-- i.e. how many W1s the player has earned so far, how many W2s, etc.
+	af[#af+1] = LoadFont("Wendy/_ScreenEvaluation numbers")..{
+		Text=(pattern):format(0),
+		InitCommand=function(self)
+			self:zoom(0.5)
+			self:y((index-1)*row_height - 280)
+			self:halign( PlayerNumber:Reverse()[player] )
+
+			-- flip alignment when ultrawide and both players joined
+			if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+				self:halign( PlayerNumber:Reverse()[OtherPlayer[player]] )
+			end
+
+			if SL.Global.ActiveModifiers.TimingWindows[index] or index==#TNS.Types then
+				self:diffuse( SL.JudgmentColors[SL.Global.GameMode][index] )
+				leadingZeroAttr = { Length=(digits-1), Diffuse=Brightness(self:GetDiffuse(), 0.35) }
+				self:AddAttribute(0, leadingZeroAttr )
+			else
+				self:diffuse(Brightness({1,1,1,1},0.25))
+			end
+		end,
+		JudgmentMessageCommand=function(self, params)
+			if params.Player ~= player then return end
+			if params.HoldNoteScore then return end
+
+			if params.TapNoteScore and ToEnumShortString(params.TapNoteScore) == window then
+				TNS.Judgments[window] = TNS.Judgments[window] + 1
+				self:settext( (pattern):format(TNS.Judgments[window]) )
+
+				leadingZeroAttr = {
+					Length=(digits - (math.floor(math.log10(TNS.Judgments[window]))+1)),
+					Diffuse=Brightness(SL.JudgmentColors[SL.Global.GameMode][index], 0.35)
+				}
+				self:AddAttribute(0, leadingZeroAttr )
+			end
+		end
+	}
+
+	-- TNS label
+	-- no need to add BitmapText actors for TimingWindows that were turned off
+	if windows[index] or index==#TNS.Names then
+
+		af[#af+1] = LoadFont("Common Normal")..{
+			Text=TNS.Names[index]:upper(),
+			InitCommand=function(self)
+				self:zoom(0.833):maxwidth(72)
+				self:halign( PlayerNumber:Reverse()[player] )
+				if player==PLAYER_1 then
+					self:x( 80 + (digits-4)*16)
+				else
+					self:x(-80 - (digits-4)*16)
+				end
+				self:y((index-1) * row_height - 279)
+				self:diffuse( SL.JudgmentColors[SL.Global.GameMode][index] )
+
+				-- flip alignment when ultrawide and both players joined
+				if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+					self:halign( PlayerNumber:Reverse()[OtherPlayer[player]] )
+					self:x(self:GetX() * -1)
+				end
+			end,
+		}
+	end
+
+end
+
+return af

BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/Time.lua
@@ -3,13 +3,17 @@ local PlayerState  = GAMESTATE:GetPlayerState(player)
 local SongPosition = GAMESTATE:GetPlayerState(player):GetSongPosition()
 local rate = SL.Global.ActiveModifiers.MusicRate
 
+local NoteFieldIsCentered = (GetNotefieldX(player) == _screen.cx)
+local IsUltraWide = (GetScreenAspectRatio() > 21/9)
+
 -- -----------------------------------------------------------------------
 -- reference to the BitmapText actor that will display remaining time
 local remBMT
+-- how wide (in visual pixels) the total time is, used to offset the label
+local total_width
 
 -- simple flag used in the Update function to stop updating remBMT once the player runs out of life
 local alive = true
-
 
 -- -----------------------------------------------------------------------
 -- prefer the engine's SecondsToHMMSS()
@@ -130,7 +134,17 @@ end
 local af = Def.ActorFrame{}
 af.InitCommand=function(self)
 	self:SetUpdateFunction(Update)
-	self:xy(-85,-50)
+	self:x(SL_WideScale(150,202) * (player==PLAYER_1 and -1 or 1))
+	self:y(-40)
+
+	if NoteFieldIsCentered and IsUsingWideScreen() then
+		self:x( 154 * (player==PLAYER_1 and -1 or 1) )
+	end
+
+	-- flip alignment when ultrawide and both players joined
+	if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+		self:x(self:GetX() * -1)
+	end
 end
 
 af.CurrentSongChangedMessageCommand=function(self,params)
@@ -151,18 +165,19 @@ af.CurrentSongChangedMessageCommand=function(self,params)
 end
 
 -- -----------------------------------------------------------------------
--- remaining time label
-
-af[#af+1] = LoadFont("Common Normal")..{
-	Text=("%s "):format( THEME:GetString("ScreenGameplay", "Remaining") ),
-	InitCommand=function(self) self:horizalign(right):xy(-6, 0):zoom(0.833) end
-}
-
 -- remaining time number
 af[#af+1] = LoadFont("Common Normal")..{
 	InitCommand=function(self)
 		remBMT = self
-		self:horizalign(left):xy(0,0)
+		self:x(0)
+		self:halign(PlayerNumber:Reverse()[player]):vertalign(bottom)
+
+		-- flip alignment and adjust for smaller pane size
+		-- when ultrawide and both players joined
+		if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+			self:halign( PlayerNumber:Reverse()[OtherPlayer[player]] )
+			self:x(50 * (player==PLAYER_1 and -1 or 1))
+		end
 	end,
 
 	-- HealthStateChanged is going to be broadcast quite a bit by the engine.
@@ -178,27 +193,87 @@ af[#af+1] = LoadFont("Common Normal")..{
 	end
 }
 
--- -----------------------------------------------------------------------
--- total time label
--- "song" in normal gameplay, "course" in CourseMode
-
+-- remaining time label
 af[#af+1] = LoadFont("Common Normal")..{
+	Text=("%s "):format( THEME:GetString("ScreenGameplay", "Remaining") ),
 	InitCommand=function(self)
-		self:horizalign(right):xy(-6, 20):zoom(0.833)
+		self:halign(PlayerNumber:Reverse()[player]):vertalign(bottom)
+		self:zoom(0.833)
 
-		local s = GAMESTATE:IsCourseMode() and THEME:GetString("ScreenGameplay", "Course") or THEME:GetString("ScreenGameplay", "Song")
-		self:settext( ("%s "):format(s) )
+		-- flip alignment and adjust for smaller pane size
+		-- when ultrawide and both players joined
+		if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+			self:halign( PlayerNumber:Reverse()[OtherPlayer[player]] )
+			self:x(50 * (player==PLAYER_1 and -1 or 1))
+		end
+	end,
+	OnCommand=function(self)
+		if player==PLAYER_1 then
+			self:x( 32 + (total_width-28))
+		else
+			self:x(-32 - (total_width-28))
+		end
+
+		-- flip offset when ultrawide and both players
+		if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+			if player==PLAYER_1 then
+				self:x(-86 - (total_width-28))
+			else
+				self:x( 86 + (total_width-28))
+			end
+		end
 	end
 }
 
+-- -----------------------------------------------------------------------
 -- total time number
 -- song duration in normal gameplay, overall course duration in CourseMode
 af[#af+1] = LoadFont("Common Normal")..{
 	InitCommand=function(self)
-		self:horizalign(left):xy(0,20)
+		self:xy(0,20)
+		self:halign(PlayerNumber:Reverse()[player]):vertalign(bottom)
+		if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+			self:halign( PlayerNumber:Reverse()[OtherPlayer[player]] )
+			self:x(50 * (player==PLAYER_1 and -1 or 1))
+		end
+
 		self:settext( fmt(totalseconds) )
+		total_width = self:GetWidth()
 	end
 }
+
+-- total time label
+-- "song" in normal gameplay, "course" in CourseMode
+af[#af+1] = LoadFont("Common Normal")..{
+	InitCommand=function(self)
+		self:zoom(0.833)
+		self:halign(PlayerNumber:Reverse()[player]):vertalign(bottom)
+		if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+			self:halign( PlayerNumber:Reverse()[OtherPlayer[player]] )
+		end
+
+		local s = GAMESTATE:IsCourseMode() and THEME:GetString("ScreenGameplay", "Course") or THEME:GetString("ScreenGameplay", "Song")
+		self:settext( ("%s "):format(s) )
+	end,
+	OnCommand=function(self)
+		if player==PLAYER_1 then
+			self:x(32 + (total_width-28))
+		else
+			self:x(-32 - (total_width-28))
+		end
+		self:y(20)
+
+		-- flip offset when ultrawide and both players
+		if IsUltraWide and #GAMESTATE:GetHumanPlayers() > 1 then
+			if player==PLAYER_1 then
+				self:x(-86 - (total_width-28))
+			else
+				self:x( 86 + (total_width-28))
+			end
+		end
+	end
+}
+
 -- -----------------------------------------------------------------------
 
 return af

BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/default.lua
@@ -28,7 +28,7 @@ local sidepane_width  = _screen.w/2
 local sidepane_pos_x  = _screen.w * (player==PLAYER_1 and 0.75 or 0.25)
 
 if not IsUltraWide then
-	if NoteFieldIsCentered and IsUsingWideScreen()  then
+	if NoteFieldIsCentered and IsUsingWideScreen() then
 		sidepane_width = (_screen.w - GetNotefieldWidth()) / 2
 
 		if player==PLAYER_1 then
@@ -49,7 +49,6 @@ else
 		end
 	end
 end
-
 
 -- -----------------------------------------------------------------------
 
@@ -87,8 +86,8 @@ af[#af+1] = Def.ActorFrame{
 	end,
 
 	LoadActor("./Banner.lua", player),
-	LoadActor("./JudgmentLabels.lua", player),
-	LoadActor("./JudgmentNumbers.lua", player),
+	LoadActor("./TapNoteJudgments.lua", player),
+	LoadActor("./HoldsMinesRolls.lua", player),
 	LoadActor("./Time.lua", player),
 }
 

commit 1e6dc7ab884b508523361cff3f110a50b521c139

fix HoldsMinesRolls BMT layout in StepStats
This addresses a small bug introduced in
5de3718984da287b16970e65cd1ae3974f208557 with the way BitmapText actors
for Holds, Mines, and Rolls values were laid out.

The BMTs should be laid out from left to right like:
  player_performance slash possible

Following 5de3718984da287b16970e65cd1ae3974f208557, PLAYER_1's layout
was:
  possible slash player_performance

This addresses that bug so that the layouts match and are correct for
both players.

BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/HoldsMinesRolls.lua
@@ -34,8 +34,8 @@ for index, RCType in ipairs(RadarCategories) do
 		Text="000",
 		InitCommand=function(self)
 			self:zoom(0.4)
-			self:halign(PlayerNumber:Reverse()[OtherPlayer[player]])
-			self:x( 0 )
+			self:horizalign(left)
+			self:x( player==PLAYER_1 and -100 or 0 )
 			self:y((index-1)*row_height - 22)
 		end,
 		BeginCommand=function(self)
@@ -79,8 +79,8 @@ for index, RCType in ipairs(RadarCategories) do
 	af[#af+1] = LoadFont("Wendy/_ScreenEvaluation numbers")..{
 		InitCommand=function(self)
 			self:zoom(0.4)
-			self:halign(PlayerNumber:Reverse()[player])
-			self:x( 100 * (player==PLAYER_1 and -1 or 1) )
+			self:horizalign(right)
+			self:x( player==PLAYER_1 and 0 or 100 )
 			self:y((index-1)*row_height - 22)
 		end,
 		BeginCommand=function(self)