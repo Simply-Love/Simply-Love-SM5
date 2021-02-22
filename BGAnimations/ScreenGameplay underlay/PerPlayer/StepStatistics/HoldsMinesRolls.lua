abda8b528061c02e07f8c5c2280b3a1cbe2f75f1
quietly-turning <quietly.turning@gmail.com>
2021-02-22 14:37:15 -0500

    fix format issues w/ HoldsMinesRolls in StepStats
    This implements several changes to Holds, Mines, and Rolls counts in
    gameplay's StepStats side pane:
    
    1. I've adjusted the formatting of Holds, Mines, and Rolls counts to expand to
    accommodate 4 digits if any one of the counts is > 999
    
    The current formatting (three digits with leading 0s) is maintained if
    all counts are <= 999
    
    If the largest value is > 999 and < 10000, the formatting of Holds,
    Mines, and Rolls will all accommodate 4 digits. This could result in a
    Mines count like "1052/1080" with a Holds count like "0270/0300".
    
    2. If the largest value is > 9999, the "possible" value will be
    formatted to display as "10k+", while the "player performance" will be
    allowed to visually "spill over" into values larger than 9999.
    
    BitmapTexts will initially be formatted like "0000/10k+", they'll grow
    as gameplay continues, and the eventually look like "10123/10k+" if the
    player reaches that point.  Since the BMTs are right-aligned, the text
    would grow out to the left in this (admittedly uncommon) situation.
    
    3. If the SM5 engine returns nil or -1 RadarValues as it currently does
    with non-static courses (e.g. Player's Best 01-04) the possible value
    will be formatted as "???"
    
    4. I've nudged the overall ActorFrame down by 6px for cleaner horizontal
    alignment with nearby BMTs for decents, way offs, and misses.

--- BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/HoldsMinesRolls.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/StepStatistics/HoldsMinesRolls.lua
@@ -3,17 +3,93 @@ local player = ...
 local IsUltraWide = (GetScreenAspectRatio() > 21/9)
 local NoteFieldIsCentered = (GetNotefieldX(player) == _screen.cx)
 
-local RadarCategories = { 'Holds', 'Mines', 'Rolls' }
-local RCJudgments = { Holds=0, Mines=0, Rolls=0 }
-
+-- gray is used for leading 0s
+local gray = color("#5A6166")
 local row_height = 28
+
+local StepsOrTrail = (GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentTrail(player)) or GAMESTATE:GetCurrentSteps(player)
+
+-- -----------------------------------------------------------------------
+-- helper function
+
+-- accepts a non-negative number
+-- returns how many digits are needed to express that number in base-10
+local Base10Digits = function(val)
+	if type(val) ~= "number" then return nil end
+	if val  < 0 then return nil end
+	if val == 0 then return 1   end
+	return (math.floor(math.log10(val)) + 1)
+end
+
+-- -----------------------------------------------------------------------
+-- SETUP: get numerical counts for Holds, Mines, and Rolls using the SM5
+--        engine's RadarValues interface
+
+local RadarCategories = { 'Holds', 'Mines', 'Rolls' }
+
+-- use RCJudgments to track player performance during gameplay
+-- use RCPossible to contain the total numbers for each type
+local RCJudgments = { Holds=0,  Mines=0,  Rolls=0  }
+local RCPossible  = { Holds={}, Mines={}, Rolls={} }
+
+-- get number values for RCPossible
+for i, category in ipairs(RadarCategories) do
+	RCPossible[category].count = StepsOrTrail:GetRadarValues( player ):GetValue( category )
+end
+
+-- how many digits do we need the UI to accommodate?
+local largest = math.max( RCPossible.Holds.count, RCPossible.Mines.count, RCPossible.Rolls.count )
+-- fall back on 0 if our helper function returns nil
+local digits_needed = Base10Digits(largest) or 0
+
+-- digits_needed might be 1, 2, 3, 4, etc.
+-- ensure we show at least 3 for aesthetic purposes
+local digits_to_fmt = clamp(digits_needed, 3, 4)
+
+-- generate a Lua string pattern to leftpad a number with 0s
+local patternPfrm = ("%%0%dd"):format( digits_to_fmt )
+local patternPsbl = "%s"
+
+-- a pattern for formatting performance/possible numbers with an appropriate quantity of leading 0s
+local fmt = ("%s/%s"):format(patternPfrm, patternPsbl)
+
+-- -----------------------------------------------------------------------
+-- MORE SETUP: with RadarValue counts and string patterns handled, we can set:
+--      display, a player-facing string representing the possible RadarValue
+--      dimLen,  how many characters to dim the brightness of using AddAttribute()
+--               typically for leading 0s
+
+for i, category in ipairs(RadarCategories) do
+	-- non-static courses (e.g. Player's Best 1-4) will have -1
+	-- use a question mark to indicate we don't know the count
+	-- FIXME: patch this in the SM5 engine to not return -1
+	if RCPossible[category].count < 0 then
+		-- display is a string to display
+		-- dimLen is how many characters to dim the brightness of using AddAttribute()
+		RCPossible[category].display = "???"
+		RCPossible[category].dimLen  = 3
+
+	-- very long courses may have Mines, Hold, and/or Rolls counts larger than 9999
+	-- but there's not enough space in the layout to support more than five digits
+	-- so use "10k+" to represent "a lot"
+	elseif RCPossible[category].count > 9999 then
+		RCPossible[category].display = "10k+"
+		RCPossible[category].dimLen  = 0
+
+	else
+		RCPossible[category].display = ( ("%%0%dd"):format(digits_to_fmt) ):format(RCPossible[category].count)
+		RCPossible[category].dimLen  = digits_to_fmt - Base10Digits(RCPossible[category].count)
+	end
+end
 
 -- -----------------------------------------------------------------------
 
-local af = Def.ActorFrame{}
+local af = Def.ActorFrame{ Name="HoldsMinesRolls" }
+
+-- position the ActorFrame
 af.InitCommand=function(self)
 	self:x(player==PLAYER_1 and 155 or -85)
-	self:y(-124)
+	self:y(-140)
 
 	if NoteFieldIsCentered and IsUsingWideScreen() then
 		self:x( player==PLAYER_1 and 155 or -88 )
@@ -26,91 +102,109 @@ af.InitCommand=function(self)
 end
 
 
--- then handle holds, mines, hands, rolls
-for index, RCType in ipairs(RadarCategories) do
+-- loop through our RadarCategories table, adding
+-- two BitmapText actors for each: a label and a value
+for i, category in ipairs(RadarCategories) do
 
-	-- player performance value
-	af[#af+1] = LoadFont("Wendy/_ScreenEvaluation numbers")..{
-		Text="000",
+	local possibleAttr = { Diffuse=gray }
+
+	-- labels: holds, mines, rolls
+	af[#af+1] = LoadFont("Common Normal")..{
+		Name=("%s_Label"):format(category),
+		Text=THEME:GetString("ScreenEvaluation", category),
 		InitCommand=function(self)
-			self:zoom(0.4)
-			self:horizalign(left)
-			self:x( player==PLAYER_1 and -100 or 0 )
-			self:y((index-1)*row_height - 22)
+			self:zoom(0.833):horizalign( right )
+			self:y( (i-1)*row_height )
+		end,
+		PositionCommand=function(self, params)
+			self:x((player==PLAYER_1 and -10 or 90) - (params.Offset or 0))
+		end
+	}
+
+	-- player performance value / possible value
+	af[#af+1] = LoadFont("Wendy/_ScreenEvaluation numbers")..{
+		Name=("%s_Values"):format(category),
+		InitCommand=function(self)
+			self:zoom(0.4):horizalign( right )
+			self:x( player==PLAYER_1 and 0 or 100 )
+			self:y( (i-1)*row_height )
 		end,
 		BeginCommand=function(self)
-			leadingZeroAttr = { Length=2, Diffuse=color("#5A6166") }
-			self:AddAttribute(0, leadingZeroAttr )
+
+			self:settext( (fmt):format(0, RCPossible[category].display) )
+
+			-----------------------------------------------------------------
+			-- diffuse the leading 0s for the player performance value
+			local performanceAttr = {
+				Length=digits_to_fmt - 1,
+				Diffuse=gray
+			}
+			self:AddAttribute(0, performanceAttr)
+
+			-----------------------------------------------------------------
+			-- diffuse the slash and leading 0s for the possible value
+
+			possibleAttr.Length = RCPossible[category].dimLen + 1 -- +1 to include the / character
+			self:AddAttribute(digits_to_fmt, possibleAttr)
+
+			-----------------------------------------------------------------
+			-- position the label now that we know how many digits there are
+			-- 36 comes from the width of characters in this Wendy ScreenEval font
+			-- it's not immediately portable to other themes with different fonts, but easy enough to change
+			local offset = (self:GetText():len() * 36) * self:GetZoom()
+			self:GetParent():GetChild(("%s_Label"):format(category)):playcommand("Position", {Offset=offset})
+
 		end,
 		JudgmentMessageCommand=function(self, params)
 			if params.Player ~= player then return end
 			if not params.TapNoteScore then return end
 
-			if RCType=="Mines" and params.TapNoteScore == "TapNoteScore_AvoidMine" then
+			if category=="Mines" and params.TapNoteScore == "TapNoteScore_AvoidMine" then
 				RCJudgments.Mines = RCJudgments.Mines + 1
-				self:settext( string.format("%03d", RCJudgments.Mines) )
 
-			elseif RCType=="Holds" and params.TapNote and params.TapNote:GetTapNoteSubType() == "TapNoteSubType_Hold" then
+			elseif category=="Holds" and params.TapNote and params.TapNote:GetTapNoteSubType() == "TapNoteSubType_Hold" then
 				RCJudgments.Holds = RCJudgments.Holds + 1
-				self:settext( string.format("%03d", RCJudgments.Holds) )
 
-			elseif RCType=="Rolls" and params.TapNote and params.TapNote:GetTapNoteSubType() == "TapNoteSubType_Roll" then
+			elseif category=="Rolls" and params.TapNote and params.TapNote:GetTapNoteSubType() == "TapNoteSubType_Roll" then
 				RCJudgments.Rolls = RCJudgments.Rolls + 1
-				self:settext( string.format("%03d", RCJudgments.Rolls) )
+
+			else
+				-- if Mines, Holds, or Rolls didn't change, return early from the function now
+				-- no need to settext, calculate digits, and diffuse leading 0s if nothing changed
+				return
 			end
 
-			leadingZeroAttr = { Length=(3-tonumber(tostring(RCJudgments[RCType]):len())), Diffuse=color("#5A6166") }
-			self:AddAttribute(0, leadingZeroAttr )
-		end
-	}
+			self:settext( (fmt):format(RCJudgments[category], RCPossible[category].display) )
 
-	--  slash
-	af[#af+1] = LoadFont("Common Normal")..{
-		Text="/",
-		InitCommand=function(self)
-			self:diffuse(color("#5A6166")):zoom(1.25)
-			self:halign(PlayerNumber:Reverse()[OtherPlayer[player]])
-			self:x( 45 * (player==PLAYER_1 and -1 or 1) )
-			self:y((index-1)*row_height - 22)
-		end
-	}
+			-- determine how many digits are needed to express the current player performance value in base-10
+			local digits = Base10Digits(RCJudgments[category])
+			local length = digits_to_fmt - digits
 
-	-- possible value
-	af[#af+1] = LoadFont("Wendy/_ScreenEvaluation numbers")..{
-		InitCommand=function(self)
-			self:zoom(0.4)
-			self:horizalign(right)
-			self:x( player==PLAYER_1 and 0 or 100 )
-			self:y((index-1)*row_height - 22)
-		end,
-		BeginCommand=function(self)
+			-----------------------------------------------------------------
+			-- make leading 0s a dim gray; this is the normal case for values like "042/125"
+			-- the leading "0" in "042" should be a dimmed gray color, as well as the slash
+			if length >= 0 then
+				-- diffuse the leading 0s for the player performance value
+				self:AddAttribute(0, { Length=length; Diffuse=gray } )
 
-			possible = 0
-			StepsOrTrail = (GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentTrail(player)) or GAMESTATE:GetCurrentSteps(player)
+				-- diffuse the slash and any leading 0s for the possible value
+				self:AddAttribute(digits_to_fmt, possibleAttr)
 
-			if StepsOrTrail then
-				rv = StepsOrTrail:GetRadarValues(OtherPlayer[player])
-				possible = rv:GetValue( RCType )
-				-- non-static courses (for example, "Most Played 1-4") will return -1 here
-				if possible < 0 then possible = 0 end
+
+			-- if length is negative, it means the player performance value has
+			-- exceeded 9999, and we have a value like "10053/10k+"
+			-- the only thing that should be dimmed now is the slash.
+			--
+			-- Note that, for aesthetic reasons, this is initially formatted
+			-- like "0000/10k+", it counts up to 9999 as gameplay progresses, and
+			-- eventually visually spills over like "10053/10k+".  This is not a perfect
+			-- system, but it's not awful, and situations where it shows up should at
+			-- least be uncommon.
+			else
+				-- diffuse just the slash
+				self:AddAttribute( self:GetText():len()-5, { Length=1; Diffuse=gray } )
 			end
-
-			self:settext( string.format("%03d", possible) )
-			local leadingZeroAttr = { Length=3-tonumber(tostring(possible):len()); Diffuse=color("#5A6166") }
-			self:AddAttribute(0, leadingZeroAttr )
-		end
-	}
-end
-
--- labels: holds, mines, rolls
-for i, label in ipairs(RadarCategories) do
-	af[#af+1] = LoadFont("Common Normal")..{
-		Text=THEME:GetString("ScreenEvaluation",label),
-		InitCommand=function(self)
-			self:zoom(0.833)
-			self:x( player==PLAYER_1 and -110 or -10 )
-			self:y((i-1)*row_height - 18)
-			self:horizalign( right )
+			-----------------------------------------------------------------
 		end
 	}
 end
