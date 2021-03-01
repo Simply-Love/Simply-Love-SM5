Simply-Love/Simply-Love-SM5

commit 19d3317514a7864a92dfb048de66b9ad6b187175

track last selected song in Edit Mode
GitHub issue #249 requested a way to initialize ScreenEditMenu with the
song that was most recently being worked on in EditMode, rather than
always initializing to the first song in the first pack.

This implements that request.  A string in the format of

/group_name/song_name

is saved in ThemePrefs.ini during ScreenEditMenu's OffCommand.  That
value is likewise used to set GAMESTATE's CurrentSong during
ScreenEditMenu's InitCommand.

Conveniently, the RefreshAll() method in EditMenu.cpp will use
GAMESTATE's CurrentSong, and RefreshAll() is called once after the
EditMenu finishes loading.

Storing a custom string like this in ThemePrefs required me to extend
ThemePrefs validation.  It's now possible for SL ThemePrefs to specify
their own custom Validation functions, instead of just validating on
variable type and presence in a hardcoded table.

Scripts/99 SL-ThemePrefs.lua
@@ -102,6 +102,19 @@ SL_CustomPrefs.Get = function()
 		},
 
 		-- - - - - - - - - - - - - - - - - - - -
+		-- Save the last seen song in Edit Mode to disk so that ScreenEditMenu
+		-- can load with it already selected, instead of the first song in the
+		-- first pack.  See: ./BGAnimations/ScreenEditMenu underlay.lua
+		EditModeLastSeenSong =
+		{
+			Default = "",
+			Validation = function(val)
+				if SONGMAN:FindSong(val) then return true end
+				return false
+			end
+		},
+
+		-- - - - - - - - - - - - - - - - - - - -
 		-- MenuTimer values for various screens
 		ScreenSelectMusicMenuTimer =
 		{
@@ -210,8 +223,10 @@ SL_CustomPrefs.Validate = function()
 		for k,v in pairs( file[theme_name] ) do
 			if sl_prefs[k] then
 				-- if we reach here, the setting exists in both the master definition as well as the user's ThemePrefs.ini
-				-- so perform some rudimentary validation; check for both type mismatch and presence in sl_prefs
-				if type( v ) ~= type( sl_prefs[k].Default )
+				-- if the ThemePref has its own validation function, use that
+				-- otherwise check for type mismatch and presence in sl_prefs
+				if (type(sl_prefs[k].Validation)=="function" and sl_prefs[k].Validation(v) ~= true)
+				or type( v ) ~= type( sl_prefs[k].Default )
 				or not FindInTable(v, (sl_prefs[k].Values or sl_prefs[k].Choices))
 				then
 					-- overwrite the user's erroneous setting with the default value

BGAnimations/ScreenEditMenu underlay.lua
@@ -104,6 +104,32 @@ local t = Def.ActorFrame{
 	end
 }
 
+t[#t+1] = Def.Actor{
+	InitCommand=function()
+		local str = ThemePrefs.Get("EditModeLastSeenSong")
+
+		if str ~= "" then
+			local song = SONGMAN:FindSong( str )
+			if song then
+				-- If a song was saved in ThemePrefs, set GAMESTATE's CurrentSong now
+				-- during Init.  In the engine's code, EditMenu::RefreshAll() is called
+				-- once at the end of EditMode::Load(), and uses GAMESTATE's current song.
+				GAMESTATE:SetCurrentSong( song )
+			end
+		end
+	end,
+	OffCommand=function()
+		local song = GAMESTATE:GetCurrentSong()
+
+		if song then
+			-- the string returned by song:GetSongDir() isn't usable by SONGMAN:FindSong()
+			local path = ("/%s/%s"):format(song:GetGroupName(), Basename(song:GetSongDir()))
+			ThemePrefs.Set("EditModeLastSeenSong", path)
+			ThemePrefs.Save()
+		end
+	end
+}
+
 
 -- the overall BG
 t[#t+1] = Def.Quad {

commit c4bee73cc397390a5d2008a81a0b1a14fccfd10c

fix if-else flow for ThemePrefs validation
This fixes a logical flow bug I introduced in
19d3317, which added custom validation
functions for SL's ThemePrefs.

In that commit, I'd used the statement:

if (type(sl_prefs[k].Validation)=="function" and sl_prefs[k].Validation(v) ~= true)
or type( v ) ~= type( sl_prefs[k].Default )
or not FindInTable(v, (sl_prefs[k].Values or sl_prefs[k].Choices))
then
     -- overwrite setting because it wasn't valid
end

Since not all of SL's ThemePrefs have custom Validation functions, I'd
wanted this statement to:

A. prefer the Validation function if there is one
 A1. overwrite user setting if validation fails
 A2. allow user setting if validation passes

B. validate using Lua type() and setting presence in a predefined table
 B1. overwrite user setting if validation fails
 B2. allow user setting if validation fails

I'd wanted it to validate using A if available or B if not, but not
both.  I'd failed to consider that if A was available, and it passed
validation, the evaluation would proceed to B where it might throw a Lua
error.

This commit rewrites the single if block into an if-else block that
meets my original goals.

Scripts/99 SL-ThemePrefs.lua
@@ -222,15 +222,29 @@ SL_CustomPrefs.Validate = function()
 		-- loop through key/value pairs retrieved and do some basic validation
 		for k,v in pairs( file[theme_name] ) do
 			if sl_prefs[k] then
-				-- if we reach here, the setting exists in both the master definition as well as the user's ThemePrefs.ini
-				-- if the ThemePref has its own validation function, use that
-				-- otherwise check for type mismatch and presence in sl_prefs
-				if (type(sl_prefs[k].Validation)=="function" and sl_prefs[k].Validation(v) ~= true)
-				or type( v ) ~= type( sl_prefs[k].Default )
-				or not FindInTable(v, (sl_prefs[k].Values or sl_prefs[k].Choices))
-				then
-					-- overwrite the user's erroneous setting with the default value
-					ThemePrefs.Set(k, sl_prefs[k].Default)
+				-- If we reach here, the setting exists in both the master definition
+				-- as well as the user's ThemePrefs.ini, so at this point,
+				-- perform validation on the value retrieved for this setting.
+				-- Maybe the user edited ThemePrefs.ini by hand and
+				-- introduced a value that isn't valid; this happens a lot.
+
+				-- if the ThemePref has its own custom validation function, prefer that
+				if type(sl_prefs[k].Validation) == "function" then
+					if not sl_prefs[k].Validation(v) then
+						-- the ThemePref value retrieved from disk failed validation,
+						-- so overwrite it with the default value
+						ThemePrefs.Set(k, sl_prefs[k].Default)
+					end
+
+				-- if this ThemePref doesn't have a custom validation function,
+				-- check for type mismatch and presence in sl_prefs
+				else
+					if (type( v ) ~= type( sl_prefs[k].Default ))
+					or (not FindInTable(v, (sl_prefs[k].Values or sl_prefs[k].Choices)))
+					then
+						-- Rudimentary validaiton failed, so overwrite with the default value.
+						ThemePrefs.Set(k, sl_prefs[k].Default)
+					end
 				end
 
 			-- It's possible a setting exists in the ThemePrefs.ini file, but does not exist