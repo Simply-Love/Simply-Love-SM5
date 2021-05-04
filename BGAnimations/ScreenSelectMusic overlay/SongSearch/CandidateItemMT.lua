7cb582cec7b671bb5cfca52dcb23c37b8481038d
teejusb <5017202+teejusb@users.noreply.github.com>
2021-05-04 00:15:21 -0700

    Woo Song Search is working

--- BGAnimations/ScreenSelectMusic overlay/SongSearch/CandidateItemMT.lua
+++ BGAnimations/ScreenSelectMusic overlay/SongSearch/CandidateItemMT.lua
@@ -1,3 +1,6 @@
+local paneHeight = 319
+local paneWidth = 319
+
 -- the metatable for an item in the song search results.
 return {
 	__index = {
@@ -20,7 +23,7 @@ return {
 				Name="Song",
 				InitCommand=function(subself)
 					self.song_name = subself
-					subself:y(0):diffusealpha(0)
+					subself:y(0):diffusealpha(0):maxwidth(155)
 				end,
 				OnCommand=function(subself)
 					subself:sleep(0.13):linear(0.05):diffusealpha(1)
@@ -32,36 +35,42 @@ return {
 
 		transform = function(self, item_index, num_items, has_focus)
 			self.container:finishtweening()
-
+	
 			if has_focus then
-				self.container:accelerate(0.15)
+				self.container:accelerate(0.1)
 				self.container:diffuse( GetCurrentColor() )
 				self.container:glow(color("1,1,1,0.5"))
+				MESSAGEMAN:Broadcast("UpdateSearchResult", {songOrExit=self.song_name.songOrExit})
 			else
 				self.container:glow(color("1,1,1,0"))
-				self.container:accelerate(0.15)
+				self.container:accelerate(0.1)
 				self.container:diffuse(color("#888888"))
 				self.container:glow(color("1,1,1,0"))
 			end
 
 			self.container:y(30 * item_index)
 
-			if item_index < 1 or item_index > num_items then
+			if item_index <= 1 or item_index >= num_items then
 				self.container:diffusealpha(0)
 			else
 				self.container:diffusealpha(1)
 			end
+
+			self.container:diffusealpha(1)
+
 		end,
 
-		set = function(self, songOrExit)
-			if songOrExit == nil then self.song_name:settext("") return end
+		set = function(self, info)
+			if info == nil then self.song_name:settext("") return end
+
+			local songOrExit = info.songOrExit
 
 			-- We need some way to differentiate between Songs and the "Exit" text.
 			-- Don't want to run into the issue of someone searching for "Exit".
 			if type(songOrExit) == "string" then
-				self.song_name:settext(songOrExit)
+				self.song_name:settext(songOrExit):diffuse(Color.Red)
 			else
-				self.song_name:settext(songOrExit:GetDisplayMainTitle())
+				self.song_name:settext(songOrExit:GetDisplayMainTitle()):diffuse(Color.White)
 			end
 			self.song_name.songOrExit = songOrExit
 		end
