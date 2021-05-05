3e9eede7049a2112250b041b7d510a2423cca8e0
teejusb <5017202+teejusb@users.noreply.github.com>
2021-05-04 23:44:04 -0700

    Import the function that computes tech. SM the results in SSM

--- Scripts/SL-ChartParser.lua
+++ Scripts/SL-ChartParser.lua
@@ -37,7 +37,7 @@ local Bin2Hex = type(BinaryToHex)=="function" and BinaryToHex or function(s)
 end
 
 -- Reduce the chart to it's smallest unique representable form.
-local MinimizeChart = function(ChartString)
+local MinimizeChart = function(chartString)
 	local function MinimizeMeasure(measure)
 		local minimal = false
 		-- We can potentially minimize the chart to get the most compressed
@@ -89,7 +89,7 @@ local MinimizeChart = function(ChartString)
 
 	local finalChartData = {}
 	local curMeasure = {}
-	for line in ChartString:gmatch('[^\n]+') do
+	for line in chartString:gmatch('[^\n]+') do
 		-- If we hit a comma, that denotes the end of a measure.
 		-- Try to minimize it, and then add it to the final chart data with
 		-- the delimiter.
@@ -268,8 +268,9 @@ local GetSimfileChartString = function(SimfileString, StepsType, Difficulty, Ste
 	return NoteDataString, BPMs
 end
 
+-- ----------------------------------------------------------------
 -- Figure out which measures are considered a stream of notes
-local GetMeasureInfo = function(Steps, measuresString)
+local GetMeasureInfo = function(Steps, chartString)
 	-- Stream Measures Variables
 	-- Which measures are considered a stream?
 	local notesPerMeasure = {}
@@ -282,7 +283,7 @@ local GetMeasureInfo = function(Steps, measuresString)
 	local timingData = Steps:GetTimingData()
 
 	-- Loop through each line in our string of measures, trimming potential leading whitespace (thanks, TLOES/Mirage Garden)
-	for line in measuresString:gmatch("[^%s*\r\n]+") do
+	for line in chartString:gmatch("[^%s*\r\n]+") do
 		-- If we hit a comma or a semi-colon, then we've hit the end of our measure
 		if(line:match("^[,;]%s*")) then
 			-- Does the number of notes in this measure meet our threshold to be considered a stream?
@@ -327,6 +328,332 @@ local GetMeasureInfo = function(Steps, measuresString)
 
 	return notesPerMeasure, peakNPS, NPSperMeasure
 end
+-- ----------------------------------------------------------------
+
+local GetTechniques = function(chartString)
+	local RegexStep = "[124]"
+	local RegexAny = "." -- "[%dM]" -- performance, i think
+
+	local RegexL = "^" .. RegexStep .. RegexAny .. RegexAny .. RegexAny
+	local RegexD = "^" .. RegexAny .. RegexStep .. RegexAny .. RegexAny
+	local RegexU = "^" .. RegexAny .. RegexAny .. RegexStep .. RegexAny
+	local RegexR = "^" .. RegexAny .. RegexAny .. RegexAny .. RegexStep
+
+	-- Output counters
+	local NumCrossovers = 0
+	local NumFootswitches = 0
+	local NumSideswitches = 0
+	local NumJacks = 0
+	local NumBrackets = 0
+
+	-- Transient algorithm state
+	local LastFoot = false -- false = left, true = right
+	local WasLastStreamFlipped = false
+	local LastStep -- Option<LDUR>
+	local LastRepeatedFoot -- Option<LDUR>
+	-- TODO: Microoptimize(?) by counting `NumLRCrossed` explicitly here,
+	-- and maybe even eg `NumConsecutiveFlipped`. test on long trancemania songs
+	local StepsLR = {}
+	local AnyStepsSinceLastCommitStream = false
+
+	-- Used for Brackets
+	-- Tracks the last arrow(s) (can be plural in the case of brackets themselves)
+	-- that each of the L and R feet was last on, respectively; However,
+	-- note that this can be flipped while recording a (unflipped?) stream...
+	local LastArrowL = "X"
+	local LastArrowR = "X"
+	-- ...so these track the true state of what happened (which become known only
+	-- after deciding whether to flip, hence are updated every CommitStream)
+	-- NB: These are "X" not "" b/c we use them in `match()`, and "" shouldn't match
+	local TrueLastArrowL = "X"
+	local TrueLastArrowR = "X"
+	local TrueLastFoot = nil
+	local JustBracketed = false
+
+	-- TODO(bracket) - Figure out what corner cases this is needed for...?
+	-- local justBracketed = false -- used for tiebreaks
+
+	function CommitStream(tieBreakFoot)
+		local ns = #StepsLR
+		local nx = 0
+		for step in ivalues(StepsLR) do
+			-- Count crossed-over steps given initial footing
+			if not step then nx = nx + 1 end
+		end
+
+		local needFlip = false
+		if nx * 2 > ns then
+			-- Easy case - more than half the L/R steps in this stream were crossed over,
+			-- so we guessed the initial footing wrong and need to flip the stream.
+			needFlip = true
+		elseif nx * 2 == ns then
+			-- Exactly half crossed over. Note that flipping the stream will introduce
+			-- a jack (i.e. break the alternating-feet assumption) on the first note,
+			-- whereas leaving it as is will footswitch that note. Break the tie by
+			-- looking at history to see if the chart is already more jacky or switchy.
+			-- (OTOH, the reverse applies if the previous stream was also flipped.)
+			if tieBreakFoot then
+				-- But first, as a higher priority tiebreaker -- if this stream is followed
+				-- by a bracketable jump, choose whichever flipness lets us bracket it.
+				if JustBracketed then
+					-- (However, don't get too overzealous -- if also preceded by a
+					-- bracket jump, that forces the footing, so we can't bracket both)
+					needFlip = false
+				elseif LastFoot then
+					needFlip = (tieBreakFoot == "R")
+				else
+					needFlip = (tieBreakFoot == "L")
+				end
+			elseif NumFootswitches > NumJacks then
+				needFlip = LastFlip -- Match flipness of last chunk -> footswitch
+			else
+				needFlip = not LastFlip -- Don't match -> jack
+			end
+		end
+
+		-- Now that we know the correct flip, see if the stream needs split.
+		-- If "too much" of the stream is *completely* crossed over, force a
+		-- double-step there by splitting the stream to stay facing forward.
+		-- Heuristic value (9) chosen by inspection on Subluminal - After Hours.
+		local splitIndex -- Option<int>
+		local splitFirstUncrossedStepIndex -- Option<Int>
+		local numConsecutiveCrossed = 0
+		for i, step in ipairs(StepsLR) do
+			local stepIsCrossed = step == needFlip
+			if not splitIndex then -- lua doesn't have `break` huh. ok
+				if stepIsCrossed then
+					numConsecutiveCrossed = numConsecutiveCrossed + 1
+					if numConsecutiveCrossed == 9 then
+						splitIndex = i - 8 -- beware the 1-index
+					end
+				else
+					numConsecutiveCrossed = 0
+				end
+			elseif not splitFirstUncrossedStepIndex then
+				-- Also search for the first un-crossed step after the fux section,
+				-- which will be used below in the `splitIndex == 1` case.
+				if not stepIsCrossed then
+					splitFirstUncrossedStepIndex = i
+				end
+			end
+		end
+
+		if splitIndex then
+			-- Note that since we take O(n) to compute `needFlip`, and then we might
+			-- do repeated work scanning already-analyzed ranges of `StepsLR` during
+			-- the recursive call here, it's technically possible for a worst case
+			-- performance of O(n^2 / 18) if the whole chart fits this pattern.
+			-- But this is expected to be pretty rare to happen even once so probably ok.
+			-- TODO: Optimize the above by using a separate explicit counter for `nx`.
+			if splitIndex == 1 then
+				-- Prevent infinite splittage if the fux section starts immediately.
+				-- In that case split instead at the first non-crossed step.
+				-- The next index is guaranteed to be set in this case.
+				splitIndex = splitFirstUncrossedStepIndex -- .unwrap()
+			end
+
+			local StepsLR1 = {}
+			local StepsLR2 = {}
+			for i, step in ipairs(StepsLR) do
+				if i < splitIndex then
+					StepsLR1[#StepsLR1+1] = step
+				else
+					StepsLR2[#StepsLR2+1] = step
+				end
+			end
+			-- Recurse for each split half
+			StepsLR = StepsLR1
+			CommitStream(nil)
+			LastRepeatedFoot = nil
+			StepsLR = StepsLR2
+			CommitStream(tieBreakFoot)
+		else
+			-- No heuristic doublestep splittage necessary. Update the stats.
+			if needFlip then
+				NumCrossovers = NumCrossovers + ns - nx
+			else
+				NumCrossovers = NumCrossovers + nx
+			end
+
+			if LastRepeatedFoot then
+				if needFlip == LastFlip then
+					NumFootswitches = NumFootswitches + 1
+					if LastRepeatedFoot == "L" or LastRepeatedFoot == "R" then
+						NumSideswitches = NumSideswitches + 1
+					end
+				else
+					NumJacks = NumJacks + 1
+				end
+			end
+
+			StepsLR = {}
+			LastFlip = needFlip
+
+			-- Merge the (flip-ambiguous) last-arrow tracking into the source of truth
+			-- TODO(bracket) - Do we need to check if the `LastArrow`s are empty, like
+			-- the hs version does? I hypothesize it actually makes no difference.
+			-- NB: This can't just be `ns > 0` bc we want to update the TrueLastFoot
+			-- even when there were just U/D steps (whereupon StepsLR would be empty).
+			if AnyStepsSinceLastCommitStream then
+				if needFlip then
+					-- TrueLastFoot is a tristate so can't just copy the bool
+					if LastFoot then TrueLastFoot = "L" else TrueLastFoot = "R" end
+					TrueLastArrowL = LastArrowR
+					TrueLastArrowR = LastArrowL
+					-- TODO: "If we had to flip a stream right after a bracket jump,
+					-- that'd make it retroactively unbracketable; if so cancel it"
+					-- (...do i even believe this anymore? probs not...)
+				else
+					if LastFoot then TrueLastFoot = "R" else TrueLastFoot = "L" end
+					TrueLastArrowL = LastArrowL
+					TrueLastArrowR = LastArrowR
+				end
+			end
+			AnyStepsSinceLastCommitStream = false
+			LastArrowL = ""
+			LastArrowR = ""
+			JustBracketed = false
+		end
+	end
+
+	for line in chartString:gmatch("[^%s*\r\n]+") do
+		if line:match(RegexStep) then
+			local step = ""
+			if line:match(RegexL) then step = step .. "L" end
+			if line:match(RegexD) then step = step .. "D" end
+			if line:match(RegexU) then step = step .. "U" end
+			if line:match(RegexR) then step = step .. "R" end
+
+			if step:len() == 1 then
+				-- Normal step
+				if LastStep and step == LastStep then
+					-- Jack or footswitch
+					CommitStream(nil)
+					LastRepeatedFoot = step
+				end
+
+				-- A normal streamy step
+				LastStep = step
+				-- Switch feet
+				LastFoot = not LastFoot
+				-- Record whether we stepped on a matching or crossed-over L/R arrow
+				-- TODO: Check yes/not true/false left/right parity here (vs .hs/.cpps)
+				if step == "L" then
+					StepsLR[#StepsLR+1] = not LastFoot
+				elseif step == "R" then
+					StepsLR[#StepsLR+1] = LastFoot
+				end
+				AnyStepsSinceLastCommitStream = true
+				-- Regardless, record what arrow the foot stepped on (for brackets l8r)
+				if LastFoot then
+					LastArrowR = step
+				else
+					LastArrowL = step
+				end
+			elseif step:len() > 1 then
+				-- Jump
+				-- TODO(bracket) - Make stream able to continue thru a bracket jump
+
+				if step:len() == 2 then
+					local isBracketLeft  = step:match("L[^R]")
+					local isBracketRight = step:match("[^L]R")
+
+					local tieBreakFoot = nil
+					if isBracketLeft then
+						tieBreakFoot = "L"
+					elseif isBracketRight then
+						tieBreakFoot = "R"
+					end
+					CommitStream(tieBreakFoot)
+					LastStep = nil
+					LastRepeatedFoot = nil
+
+					if isBracketLeft or isBracketRight then
+						-- Possibly bracketable
+						if isBracketLeft and (not TrueLastFoot or TrueLastFoot == "R") then
+							-- Check for interference from the right foot
+							-- NB: This should be `intersect` in case of eg LU <-> LR
+							-- But we dodge this by taking sub(2)/sub(1,1) down below.
+							if not step:match(TrueLastArrowR) then
+								NumBrackets = NumBrackets + 1
+								-- Allow subsequent brackets to stream
+								TrueLastFoot = "L"
+								LastFoot = false
+								-- This prevents e.g. "LD bracket, DR also bracket"
+								-- NB: Take only the U or D arrow (cf above NB)
+								TrueLastArrowL = step:sub(2)
+								JustBracketed = true
+							else
+								-- Right foot is in the way; we have to step with both feet
+								TrueLastFoot = nil
+								TrueLastArrowL = "L"
+								TrueLastArrowR = step:sub(2)
+								LastArrowR = TrueLastArrowR
+							end
+							LastArrowL = TrueLastArrowL
+						elseif isBracketRight and (not TrueLastFoot or TrueLastFoot == "L") then
+							-- Check for interference from the left foot
+							-- Symmetric logic; see comments above
+							if not step:match(TrueLastArrowL) then
+								NumBrackets = NumBrackets + 1
+								TrueLastFoot = "R"
+								LastFoot = true
+								TrueLastArrowR = step:sub(1,1)
+								JustBracketed = true
+							else
+								TrueLastFoot = nil
+								TrueLastArrowL = step:sub(1,1)
+								TrueLastArrowR = "R"
+								LastArrowL = TrueLastArrowL
+							end
+							LastArrowR = TrueLastArrowR
+						end
+					else
+						-- LR or DU
+						if step == "DU" then
+							-- Past footing influences which way the player can
+							-- comfortably face while jumping DU
+							local leftD  = TrueLastArrowL:match("D")
+							local leftU  = TrueLastArrowL:match("U")
+							local rightD = TrueLastArrowR:match("D")
+							local rightU = TrueLastArrowR:match("U")
+							-- The haskell version of this (decideDUFacing) is a
+							-- little more strict, and asserts each foot can't be
+							-- be on both D and U at once, but whatever.
+							if (leftD and not rightD) or (rightU and not leftU) then
+								TrueLastArrowL = "D"
+								TrueLastArrowR = "U"
+							elseif (leftU and not rightU) or (rightD and not leftD) then
+								TrueLastArrowL = "U"
+								TrueLastArrowR = "D"
+							else
+								TrueLastArrowL = "X"
+								TrueLastArrowR = "X"
+							end
+						else
+							-- Not going to bother thinking about spin-jumps ><
+							TrueLastArrowL = "X"
+							TrueLastArrowR = "X"
+						end
+						TrueLastFoot = nil
+					end
+				else
+					CommitStream()
+					LastStep = nil
+					LastRepeatedFoot = nil
+					-- Triple/quad - always gotta bracket these
+					NumBrackets = NumBrackets + 1
+					TrueLastFoot = nil
+				end
+			end
+		end
+	end
+	CommitStream(nil)
+
+	return NumCrossovers, NumFootswitches, NumSideswitches, NumJacks, NumBrackets
+end
+
+-- ----------------------------------------------------------------
 
 local MaybeCopyFromOppositePlayer = function(pn, filename, stepsType, difficulty, description)
 	local opposite_player = pn == "P1" and "P2" or "P1"
@@ -341,6 +668,12 @@ local MaybeCopyFromOppositePlayer = function(pn, filename, stepsType, difficulty
 		SL[pn].Streams.PeakNPS = SL[opposite_player].Streams.PeakNPS
 		SL[pn].Streams.NPSperMeasure = SL[opposite_player].Streams.NPSperMeasure
 		SL[pn].Streams.Hash = SL[opposite_player].Streams.Hash
+
+		SL[pn].Crossovers = SL[opposite_player].Crossovers
+		SL[pn].Footswitches = SL[opposite_player].Footswitches
+		SL[pn].Sideswitches = SL[opposite_player].Sideswitches
+		SL[pn].Jacks = SL[opposite_player].Jacks
+		SL[pn].Brackets = SL[opposite_player].Brackets
 
 		SL[pn].Streams.Filename = SL[opposite_player].Streams.Filename
 		SL[pn].Streams.StepsType = SL[opposite_player].Streams.StepsType
@@ -393,6 +726,13 @@ ParseChartInfo = function(steps, pn)
 				SL[pn].Streams.PeakNPS = PeakNPS
 				SL[pn].Streams.NPSperMeasure = NPSperMeasure
 				SL[pn].Streams.Hash = Hash
+
+				local Crossovers, Footswitches, Sideswitches, Jacks, Brackets = GetTechniques(chartString)
+				SL[pn].Streams.Crossovers = Crossovers
+				SL[pn].Streams.Footswitches = Footswitches
+				SL[pn].Streams.Sideswitches = Sideswitches
+				SL[pn].Streams.Jacks = Jacks
+				SL[pn].Streams.Brackets = Brackets
 
 				SL[pn].Streams.Filename = filename
 				SL[pn].Streams.StepsType = stepsType
