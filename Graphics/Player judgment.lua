6c99b6191a3c32a1f35bf5641ddb2f0e5ed50738
Lars <lars.d.j@hotmail.com>
2021-08-17 14:42:46 +0200

    oops, judges don't seem to dissapear in etterna :)

--- Graphics/Player judgment.lua
+++ Graphics/Player judgment.lua
@@ -60,7 +60,7 @@ return Def.ActorFrame{
 		--sprite:zoom(0.8):decelerate(0.1):zoom(0.75):sleep(0.6):accelerate(0.2):zoom(0)
 
 		-- this should match the behaviour of Etterna
-		sprite:zoom(0.75):sleep(0.6):accelerate(0.2):zoom(0)
+		sprite:zoom(0.75)
 
 	end,
 
