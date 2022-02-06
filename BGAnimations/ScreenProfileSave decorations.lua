c159d848f7b41dbd0316144b9f6b1f18c93f8c1e
Martin Natano <natano@natano.net>
2022-02-06 21:58:59 +0100

    Remove duplicate profile saving
    Player profiles are already saved by `Continue()`.

--- BGAnimations/ScreenProfileSave decorations.lua
+++ BGAnimations/ScreenProfileSave decorations.lua
@@ -1,11 +1,6 @@
 return Def.ActorFrame{
 	Def.Actor{
 		BeginCommand=function(self)
-			if SCREENMAN:GetTopScreen():HaveProfileToSave() then
-				for player in ivalues( GAMESTATE:GetHumanPlayers() ) do
-					PROFILEMAN:SaveProfile(player)
-				end
-			end
 			self:queuecommand("Load")
 		end,
 		LoadCommand=function()
