86d76af65899212dba514035391b3a251ee876fb
Martin Natano <natano@natano.net>
2022-03-28 22:12:37 +0200

    Use GaveUp() when available
    In OutFox and newer versions of SM 5.1 there is the GaveUp() function
    available. For SM 5.0.12 and older SM 5.1 versions we continue to
    emulate the functionality by checking the song position.

--- BGAnimations/ScreenGameplay overlay/FailOnHoldStart.lua
+++ BGAnimations/ScreenGameplay overlay/FailOnHoldStart.lua
@@ -1,10 +1,19 @@
--- We need a way to check if the player gave up before the song properly ended.
--- It doesn't look like the engine broadcasts any messages that would be helpful here,
--- so we do the best we can by checking the song position.
+-- Check if the player gave up before the song properly ended.
 
 local af = Def.ActorFrame{
 	OffCommand=function(self)
-		local fail = (GAMESTATE:GetCurMusicSeconds() < GAMESTATE:GetCurrentSong():GetLastSecond())
+		-- In OutFox and newer versions of SM 5.1 there is the GaveUp()
+		-- function available. For SM 5.0.12 and older SM 5.1 versions
+		-- we do the best we can by checking the song position. It
+		-- doesn't look like the engine broadcasts any messages that
+		-- would be helpful here.
+		local stage_stats = STATSMAN:GetCurStageStats()
+		local fail = false
+		if stage_stats.GaveUp then
+			fail = stage_stats:GaveUp()
+		else
+			fail = (GAMESTATE:GetCurMusicSeconds() < GAMESTATE:GetCurrentSong():GetLastSecond())
+		end
 
 		-- In course mode always fail if we're not already on the last
 		-- song. If we are on the last song, then we fall back to the
