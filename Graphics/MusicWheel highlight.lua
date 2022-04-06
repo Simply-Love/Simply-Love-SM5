895b7452197945a13907dc08c62aac78ef8c4aad
hayoreo <nimjero@gmail.com>
2022-04-06 14:29:52 -0700

    Remove engine side music wheel code.
    Since we don't use the engine's music/course wheel remove all code related to it.

--- Graphics/MusicWheel highlight.lua
+++
@@ -1,8 +0,0 @@
-local num_items = THEME:GetMetric("MusicWheel", "NumWheelItems")
--- subtract 2 from the total number of MusicWheelItems
--- one MusicWheelItem will be offsceen above, one will be offscreen below
-local num_visible_items = num_items - 2
-
-local item_width = _screen.w / 2.125
-
-return Def.Quad{ InitCommand=function(self) self:horizalign(left):x(WideScale(28,33)):zoomto(item_width,_screen.h/num_visible_items-1) end }
