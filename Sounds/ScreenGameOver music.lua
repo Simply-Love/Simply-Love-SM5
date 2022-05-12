99e7c1b9a048f9aff9b14970d721eeb40bb1bf44
teejusb <5017202+teejusb@users.noreply.github.com>
2022-05-11 20:44:19 -0700

    Update SRPG6 gameover audio

--- Sounds/ScreenGameOver music.lua
+++ Sounds/ScreenGameOver music.lua
@@ -2,7 +2,7 @@ local audio_file = "serenity in ruin.ogg"
 
 local style = ThemePrefs.Get("VisualStyle")
 if style == "SRPG6" then
-	audio_file = "dreams of will arrange.ogg"
+	audio_file = "SRPG6-GameOver.ogg"
 end
 
 return THEME:GetPathS("", audio_file)
