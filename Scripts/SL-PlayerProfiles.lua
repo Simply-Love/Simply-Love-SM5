eff17a477affbf3946a7e186608cf7ab0eef69e7
Ivana Kellyerova <ivana.kellyerova+git@amarion.net>
2022-06-22 19:09:17 +0200

    Fix ScreenEvaluationSummary
    The screen has been broken after introducing the profile switcher.
    
    The fix consists of two parts:
    
    1) Making sure the current session history survives any profile switches.
    2) Tweaking the screen to be less confusing when multiple profiles are involved.
       In case of no profile switches (i.e., both P1 and P2 only used up to one
       unique profile each), the screen behaves as usual. If either P1 or P2 (or
       both) used multiple profiles over the course of the session, we now show the
       display name of the profile that obtained a specific score.

--- Scripts/SL-PlayerProfiles.lua
+++ Scripts/SL-PlayerProfiles.lua
@@ -78,6 +78,22 @@ local permitted_profile_settings = {
 local theme_name = THEME:GetThemeDisplayName()
 local filename =  theme_name .. " UserPrefs.ini"
 
+
+-- Function called when a [GUEST] joins during SSM, either by late joining or via the fast
+-- profile switcher. It does two things:
+-- 1) properly reset profile state (e.g. modifiers), and
+-- 2) persist any state that should survive a profile switch (e.g., session history
+--    in SL[pn].Stages with songs played for displaying on ScreenEvaluationSummary).
+-- LoadProfileCustom takes care of this for persistent profiles.
+LoadGuest = function(player)
+	GAMESTATE:ResetPlayerOptions(player)
+	local pn = ToEnumShortString(player)
+	local stages = SL[pn].Stages
+	SL[pn]:initialize()
+	SL[pn].Stages = stages
+end
+
+
 -- function assigned to "CustomLoadFunction" under [Profile] in metrics.ini
 LoadProfileCustom = function(profile, dir)
 
@@ -95,9 +111,14 @@ LoadProfileCustom = function(profile, dir)
 	end
 
 	if pn then
+		-- Remember and persist stats about songs played across profile switches
+		local stages = SL[pn].Stages
+
 		SL[pn]:initialize()
 		ParseGrooveStatsIni(player)
 		ReadItlFile(player)
+
+		SL[pn].Stages = stages
 	end
 
 	if pn and FILEMAN:DoesFileExist(path) then
