a18e087faae800304cee2d1cc711223bbe289c8b
SugoiFactory <tony.f.cheng@gmail.com>
2022-08-03 09:38:23 -0700

    Update default.lua
    Accidentally added duplicate FolderStats call

--- BGAnimations/ScreenSelectMusic overlay/PerPlayer/default.lua
+++ BGAnimations/ScreenSelectMusic overlay/PerPlayer/default.lua
@@ -15,7 +15,6 @@ end
 -- Bouncing cursor inside the grid of difficulty blocks. These should be on top of both of the other elements.
 for player in ivalues( PlayerNumber ) do
 	t[#t+1] = LoadActor("./Cursor.lua", player)
-	t[#t+1] = LoadActor("./FolderStats.lua", player)
 end
 
 return t
