4912a0c6ac5e1c6fd48d8108bccdd54e297fe0a6
quietly-turning <quietly.turning@gmail.com>
2022-10-18 20:16:01 -0400

    ensure Casual Mode respects #SELECTABLE:NO;
    This fixes CasualMode to not show songs that are considered "locked" by
    StepMania.  This includes one or more of:
    
      #SELECTABLE:NO is set in the simfile and StepMania's HiddenSongs preference is
        enabled.
      the song is locked using UNLOCKMAN
      the song is disabled via ScreenOptionsToggleSongs
    
    UNLOCKMAN:SongIsLocked(song) will return an int between 0 to 15 inclusive, the
    result of bitwise ORing powers-of-2 representing reasons a song could be locked,
    defined in UnlockManager.h.
    
    0 means the song is not locked for any reason, so let's ensure
    UNLOCKMAN:SongIsLocked(song) returns 0 in ScreenSelectMusicCasual.  If not,
    prune the song.

--- BGAnimations/ScreenSelectMusicCasual overlay/Setup.lua
+++ BGAnimations/ScreenSelectMusicCasual overlay/Setup.lua
@@ -4,8 +4,9 @@
 -- The idea is basically to just throw setup-related stuff
 -- in here that we don't want cluttering up default.lua
 ---------------------------------------------------------------------------
+
 -- because no one wants "Invalid PlayMode 7"
-GAMESTATE:SetCurrentPlayMode(0)
+GAMESTATE:SetCurrentPlayMode('PlayMode_Regular')
 
 ---------------------------------------------------------------------------
 -- local junk
@@ -112,7 +113,10 @@ local PruneSongsFromGroup = function(group)
 		-- this should be guaranteed by this point, but better safe than segfault
 		if song:HasStepsType(steps_type)
 		-- respect StepMania's cutoff for 1-round songs
-		and song:MusicLengthSeconds() < PREFSMAN:GetPreference("LongVerSongSeconds") then
+		and song:MusicLengthSeconds() < PREFSMAN:GetPreference("LongVerSongSeconds")
+		-- respect the #SELECTABLE tag in this song's simfile
+		and UNLOCKMAN:IsSongLocked(song) == 0
+		then
 			-- ensure that at least one stepchart has a meter ≤ CasualMaxMeter (10, by default)
 			for steps in ivalues(song:GetStepsByStepsType(steps_type)) do
 				if steps:GetMeter() <= ThemePrefs.Get("CasualMaxMeter") then
