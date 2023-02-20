5db53268fb83be0edac200089e97eb5f075284fa
zarzob <73825889+zarzob@users.noreply.github.com>
2023-02-20 20:14:33 +1300

    Add difficulty name to block in ScreenGameplay

--- BGAnimations/ScreenGameplay underlay/PerPlayer/DifficultyMeter.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/DifficultyMeter.lua
@@ -28,6 +28,7 @@ return Def.ActorFrame{
 		InitCommand=function(self)
 			self:diffuse( Color.Black )
 			self:zoom( 0.4 )
+			self:y(-4)
 		end,
 		CurrentSongChangedMessageCommand=function(self) self:queuecommand("Begin") end,
 		BeginCommand=function(self)
@@ -38,5 +39,47 @@ return Def.ActorFrame{
 				self:settext(meter)
 			end
 		end
+	},
+
+	LoadFont("Common Normal")..{
+		InitCommand=function(self)
+			self:diffuse( Color.Black )
+			self:y(9.5)
+			self:zoom( 0.5 )
+			--self:y(_screen.cy-82)
+			--self:x(149 * (player==PLAYER_1 and -1 or 1))
+			--self:halign(pn)
+			
+			-- self:y(_screen.cy-83)
+			-- self:x(129.5 * (player==PLAYER_1 and -1 or 1))
+			-- --self:halign(0.5)
+			-- :zoom(0.5)
+			
+			-- self:y(_screen.cy-61)
+			-- self:x(129.5 * (player==PLAYER_1 and -1 or 1))
+			-- self:halign(0.5):zoom(0.5)
+
+			local textColor = Color.Black
+			local shadowLength = 0
+			if ThemePrefs.Get("RainbowMode") and not HolidayCheer() then
+				textColor = Color.Black
+			end
+			self:diffuse(textColor)
+			self:shadowlength(shadowLength)
+
+			local steps = GAMESTATE:GetCurrentSteps(player)
+			-- GetDifficulty() returns a value from the Difficulty Enum such as "Difficulty_Hard"
+			-- ToEnumShortString() removes the characters up to and including the
+			-- underscore, transforming a string like "Difficulty_Hard" into "Hard"
+			local difficulty = ToEnumShortString( steps:GetDifficulty() )
+			difficulty = THEME:GetString("Difficulty", difficulty)
+			--if difficulty == "Challenge" or difficulty == "Expert" then difficulty = "X"
+			--else difficulty = difficulty:sub(1,1) end
+			
+
+			self:settext(difficulty)
+		end
 	}
+
+
 }
