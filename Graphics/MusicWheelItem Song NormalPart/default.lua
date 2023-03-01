a2b6ead8e5111f84b280280cd364638814c34bcd
teejusb <5017202+teejusb@users.noreply.github.com>
2023-03-01 12:39:17 -0800

    Add ITL EX score to the wheel

--- Graphics/MusicWheelItem Song NormalPart/default.lua
+++ Graphics/MusicWheelItem Song NormalPart/default.lua
@@ -19,10 +19,50 @@ af[#af+1] = Def.Sprite{
 	end
 }
 
-for pn in ivalues(GAMESTATE:GetEnabledPlayers()) do
+for player in ivalues(GAMESTATE:GetEnabledPlayers()) do
 	-- Only use lamps if a profile is found
-	if PROFILEMAN:IsPersistentProfile(pn) then
-		af[#af+1] = LoadActor("GetLamp.lua", pn)..{}
+	if PROFILEMAN:IsPersistentProfile(player) then
+		af[#af+1] = LoadActor("GetLamp.lua", player)..{}
+
+		-- Add ITL EX scores to the song wheel as well.
+		-- It will be centered to the item if only one player is enabled, and stacked otherwise.
+		af[#af+1] = Def.BitmapText{
+			Font="Wendy/_wendy monospace numbers",
+			Text="",
+			InitCommand=function(self)
+				self:visible(false)
+				self:zoom(0.2)
+				self:x( _screen.w/(WideScale(2.15, 2.14)) - self:GetWidth()*self:GetZoom() - 40 )
+				if GAMESTATE:GetNumSidesJoined() == 2 then
+					if player == PLAYER_1 then
+						self:addy(-11)
+					else
+						self:addy(4)
+					end
+				else
+					self:addy(-4)
+				end
+				self:diffuse(SL.JudgmentColors["FA+"][1])
+			end,
+			SetCommand=function(self, params)
+				local pn = ToEnumShortString(player)
+				if params.Song ~= nil then
+					local song = params.Song
+					local song_dir = song:GetSongDir()
+					if song_dir ~= nil and #song_dir ~= 0 then
+						if SL[pn].ITLData["pathMap"][song_dir] ~= nil then
+							local hash = SL[pn].ITLData["pathMap"][song_dir]
+							if SL[pn].ITLData["hashMap"][hash] ~= nil then
+								self:settext(tostring(SL[pn].ITLData["hashMap"][hash]["ex"] / 100))
+								self:visible(true)
+								return
+							end
+						end
+					end
+				end
+				self:visible(false)
+			end,
+		}
 	end
 end
 
