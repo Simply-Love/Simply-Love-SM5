b0a082a1f932017ce64d5416a7f15327c0c542e4
teejusb <5017202+teejusb@users.noreply.github.com>
2023-03-09 01:48:20 -0800

    Implement scoring function

--- Scripts/SL_ITL.lua
+++ Scripts/SL_ITL.lua
@@ -132,8 +132,39 @@ local DataForSong = function(player)
 
 	-- EX score is a number like 92.67
 	local GetPointsForSong = function(maxPoints, exScore)
-		-- TODO: implement scoring function.
-		return 420
+		local thresholdEx = 50.0
+		local percentPoints = 40.0
+
+		-- Helper function to take the logarithm with a specific base.
+		local logn = function(x, y)
+			return math.log(x) / math.log(y)
+		end
+
+		-- The first half (logarithmic portion) of the scoring curve.
+		local first = logn(
+			math.min(exScore, thresholdEx) + 1,
+			math.pow(thresholdEx + 1, 1 / percentPoints)
+		)
+
+		-- The seconf half (exponential portion) of the scoring curve.
+		local second = math.pow(
+			100 - percentPoints + 1,
+			math.max(0, exScore - thresholdEx) / (100 - thresholdEx)
+		) - 1
+
+		-- Helper function to round to a specific number of decimal places.
+		-- We want 100% EX to actually grant 100% of the points.
+		-- We don't want to  lose out on any single points if possible. E.g. If
+		-- 100% EX returns a number like 0.9999999999999997 and the chart points is
+		-- 6500, then 6500 * 0.9999999999999997 = 6499.99999999999805, where
+		-- flooring would give us 6499 which is wrong.
+		local roundPlaces = function(x, places)
+			local factor = 10 ^ places
+			return math.floor(x * factor + 0.5) / factor
+		end
+
+		local percent = roundPlaces((first + second) / 100.0, 6)
+		return math.floor(maxPoints * percent)
 	end
 
 	local pn = ToEnumShortString(player)
