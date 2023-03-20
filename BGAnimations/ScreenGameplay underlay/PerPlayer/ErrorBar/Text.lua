14167c9038b34042c8517c3898a3c6e39609d7ea
teejusb <5017202+teejusb@users.noreply.github.com>
2023-03-20 01:21:44 -0700

    Display Early/Late text when FA+ is enabled even in ITG mode

--- BGAnimations/ScreenGameplay underlay/PerPlayer/ErrorBar/Text.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/ErrorBar/Text.lua
@@ -2,12 +2,18 @@
 -- later modified for SL.
 
 local player, layout = ...
+local pn = ToEnumShortString(player)
+local mods = SL[pn].ActiveModifiers
 local gmods = SL.Global.ActiveModifiers
 
 local threshold = nil
 for i = 1, NumJudgmentsAvailable() do
     if gmods.TimingWindows[i] then
-        threshold = GetTimingWindow(i)
+        if i == 1 and mods.ShowFaPlusWindow then
+            threshold = GetTimingWindow(1, "FA+")
+        else
+            threshold = GetTimingWindow(i)
+        end
         break
     end
 end
