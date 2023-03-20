cee88225e2251f24a0ad6c9d0cea6271201ce950
SugoiFactory <tony.f.cheng@gmail.com>
2023-03-19 18:51:05 -0700

    10ms blue window support for ITL

--- BGAnimations/ScreenEvaluation common/Shared/AutoSubmitScore.lua
+++ BGAnimations/ScreenEvaluation common/Shared/AutoSubmitScore.lua
@@ -30,8 +30,8 @@ end
 local GetJudgmentCounts = function(player)
 	local counts = GetExJudgmentCounts(player)
 	local translation = {
-		["W0"] = "fantasticPlus",
-		["W1"] = "fantastic",
+		["W015"] = "fantasticPlus",
+		["W115"] = "fantastic",
 		["W2"] = "excellent",
 		["W3"] = "great",
 		["W4"] = "decent",
