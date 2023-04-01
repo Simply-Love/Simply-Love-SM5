2825058e2567cde7a41401f77ae6044814ea600f
SugoiFactory <tony.f.cheng@gmail.com>
2023-04-01 11:48:02 -0700

    Fix Event Overlay for 2P mode
    Include check for event progress pane actually existing before trying to update it.

--- BGAnimations/ScreenEvaluation common/Shared/EventOverlay.lua
+++ BGAnimations/ScreenEvaluation common/Shared/EventOverlay.lua
@@ -467,7 +467,9 @@ local GetItlPaneFunctions = function(eventAf, itlData, player)
 			end
 		end
 		ItlPane = SCREENMAN:GetTopScreen():GetChild("Overlay"):GetChild("ScreenEval Common"):GetChild(ToEnumShortString(player).."_AF_Upper"):GetChild("ItlProgress"..ToEnumShortString(player))
-		ItlPane:playcommand("ItlBox",{ box_score=box_score, box_rp=box_rp, box_tp=box_tp, box_clearType=box_clearType, box_quests=box_quests })
+		if ItlPane ~= nil then
+			ItlPane:playcommand("ItlBox",{ box_score=box_score, box_rp=box_rp, box_tp=box_tp, box_clearType=box_clearType, box_quests=box_quests })
+		end
 	end
 
 	table.insert(paneTexts, string.format(
