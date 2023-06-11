2a1a1fc4fae2c3ba353584ca3466eff1d9006bd9
SugoiFactory <tony.f.cheng@gmail.com>
2023-06-10 22:49:37 -0700

    Error Bar Trim
    Split error bar trim option into its own row, with off, Great, and Excellent as possible choices.
    
    Switch to String options

--- Scripts/SL_Init.lua
+++ Scripts/SL_Init.lua
@@ -39,7 +39,7 @@ local PlayerDefaults = {
 				ErrorBar = "None",
 				ErrorBarUp = false,
 				ErrorBarMultiTick = false,
-				ErrorBarTrim = false,
+				ErrorBarTrim = "Off",
 
 				ShowFaPlusWindow = false,
 				ShowEXScore = false,
