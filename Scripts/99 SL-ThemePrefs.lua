a8af3fec8f93eb68efbff59d1cfd57103e08367e
zarzob <73825889+zarzob@users.noreply.github.com>
2023-08-10 11:31:44 +1200

    Separate Zmod options in operator menu (#65)

--- Scripts/99 SL-ThemePrefs.lua
+++ Scripts/99 SL-ThemePrefs.lua
@@ -61,23 +61,6 @@ SL_CustomPrefs.Get = function()
 			Default = "ITG",
 			Choices = { "ITG", "IIDX" }
 		},
-		SampleMusicLoop =
-		{
-			Default = true,
-			Choices = { THEME:GetString("ThemePrefs","Yes"), THEME:GetString("ThemePrefs", "No") },
-			Values  = { true, false }
-		},
-		MusicWheelGS =
-		{
-			Default = "Scorebox",
-			Choices = { "Scorebox", "Pane", "Off" }
-		},
-		FolderStats =
-		{
-			Default = true,
-			Choices = { THEME:GetString("ThemePrefs", "Show"), THEME:GetString("ThemePrefs", "Hide") },
-			Values  = { true, false }
-		},
 		AllowDanceSolo =
 		{
 			Default = false,
@@ -278,11 +261,7 @@ SL_CustomPrefs.Get = function()
 			Values  = { true, false }
 		},
 
-		EnableBoogieStats = {
-			Default = false,
-			Choices =  { THEME:GetString("ThemePrefs","Yes"), THEME:GetString("ThemePrefs", "No") },
-			Values  = { true, false }
-		},
+
 
 		AutoDownloadUnlocks = {
 			Default = false,
@@ -295,15 +274,43 @@ SL_CustomPrefs.Get = function()
 			Choices =  { THEME:GetString("ThemePrefs","Yes"), THEME:GetString("ThemePrefs", "No") },
 			Values  = { true, false }
 		},
+
+		-- - - - - - - - - - - - - - - - - - - -
+		-- new Zmod stuff
+		-- - - - - - - - - - - - - - - - - - - -
+		TournamentMode = {
+			Default = "Off",
+			Choices = { THEME:GetString("ThemePrefs","Off"), THEME:GetString("ThemePrefs", "EX"), THEME:GetString("ThemePrefs", "ITG") },
+			Values  = { "Off", "EX", "ITG" }
+		},
+
+		EnableBoogieStats = {
+			Default = false,
+			Choices =  { THEME:GetString("ThemePrefs","Yes"), THEME:GetString("ThemePrefs", "No") },
+			Values  = { true, false }
+		},
+
 		ThemeFont = {
 			Default = "Common",
 			Choices = { THEME:GetString("ThemePrefs","Wendy"), THEME:GetString("ThemePrefs", "Mega") },
 			Values = { "Common", "Mega" }
 		},
-		TournamentMode = {
-			Default = "Off",
-			Choices = { THEME:GetString("ThemePrefs","Off"), THEME:GetString("ThemePrefs", "EX"), THEME:GetString("ThemePrefs", "ITG") },
-			Values  = { "Off", "EX", "ITG" }
+		SampleMusicLoop =
+		{
+			Default = true,
+			Choices = { THEME:GetString("ThemePrefs","Yes"), THEME:GetString("ThemePrefs", "No") },
+			Values  = { true, false }
+		},
+		MusicWheelGS =
+		{
+			Default = "Scorebox",
+			Choices = { "Scorebox", "Pane", "Off" }
+		},
+		FolderStats =
+		{
+			Default = true,
+			Choices = { THEME:GetString("ThemePrefs", "Show"), THEME:GetString("ThemePrefs", "Hide") },
+			Values  = { true, false }
 		},
 	}
 end
