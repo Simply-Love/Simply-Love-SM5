5aca3a27607e526f9bbda18ea373cd6dc48f21ba
Michael Sundqvist <michael.sundqvist1@gmail.com>
2023-08-19 01:56:24 +0300

    Optimize QR code URL and utilize alphanumeric mode, add additional data

--- BGAnimations/ScreenEvaluation common/Panes/Pane7/GrooveStatsURL.lua
+++ BGAnimations/ScreenEvaluation common/Panes/Pane7/GrooveStatsURL.lua
@@ -2,10 +2,33 @@ local player = ...
 local pn = ToEnumShortString(player)
 
 local stats = STATSMAN:GetCurStageStats():GetPlayerStageStats(player)
-local PercentDP = stats:GetPercentDancePoints()
+local percent_dp = stats:GetPercentDancePoints()
 
-local score = FormatPercentScore(PercentDP)
+local score = FormatPercentScore(percent_dp)
 score = tostring(tonumber(score:gsub("%%", "") * 100)):gsub("%.", "")
+local ex_score = CalculateExScore(player)
+ex_score = tostring(tonumber(ex_score:gsub("%%", "") * 100)):gsub("%.", "")
+
+local judgmentCounts = GetExJudgmentCounts(player)
+
+local fantastic_plus = judgmentCounts["W0"]
+local fantastic = judgmentCounts["W1"]
+local excellent = judgmentCounts["W2"]
+local great = judgmentCounts["W3"]
+local decent = judgmentCounts["W4"]
+local wayOff = judgmentCounts["W5"]
+local miss = judgmentCounts["Miss"]
+local total_steps = judgmentCounts["totalSteps"]
+local holds_held = judgmentCounts["Holds"]
+local total_holds = judgmentCounts["totalHolds"]
+local mines_hit = judgmentCounts["Mines"]
+local total_mines = judgmentCounts["totalMines"]
+local rolls_held = judgmentCounts["Rolls"]
+local total_rolls = judgmentCounts["totalRolls"]
+
+local cmod = GAMESTATE:GetPlayerState(pn):GetPlayerOptions("ModsLevel_Preferred"):CMod()
+local used_cmod = cmod ~= nil and "1" or "0"
+
 local failed = stats:GetFailed() and "1" or "0"
 local rate = tostring(SL.Global.ActiveModifiers.MusicRate * 100):gsub("%.", "")
 
@@ -24,4 +47,7 @@ for i = 1, NumJudgmentsAvailable() do
 end
 dec_wo_enabled = dec_wo_enabled and "1" or "0"
 
-return ("https://groovestats.com/qr.php?h=%s&s=%s&f=%s&r=%s&v=%d&b=%s"):format(hash, score, failed, rate, hash_version, dec_wo_enabled)
+return ("HTTPS://GROOVESTATS.COM/QR/%d/%s/T%iP%iF%iE%iG%iD%iW%iM%iH%iT%iM%iT%iR%iT%i/F%sR%sC%sB%s"):format(
+        hash_version, hash:upper(), total_steps, fantastic_plus, fantastic, excellent, great, decent, wayOff, miss,
+        holds_held, total_holds, mines_hit, total_mines, rolls_held, total_rolls,
+        failed, rate, used_cmod, dec_wo_enabled)
