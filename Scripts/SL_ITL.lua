fa06045173332264ef1080064d891a7262929605
SugoiFactory <tony.f.cheng@gmail.com>
2023-11-16 17:21:07 -0800

    Update ITL date constraints
    ITL 2023 is re-opened, so let's allow event scripts to be active again.

--- Scripts/SL_ITL.lua
+++ Scripts/SL_ITL.lua
@@ -25,7 +25,7 @@ IsItlActive = function()
 	-- The file is only written to while the event is active.
 	-- These are just placeholder dates.
 	local startTimestamp = 20230317
-	local endTimestamp = 20230619
+	local endTimestamp = 20240215
 
 	local year = Year()
 	local month = MonthOfYear()+1
