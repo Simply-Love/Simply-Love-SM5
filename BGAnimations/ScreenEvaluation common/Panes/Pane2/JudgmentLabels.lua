c72f5b5a7e5942963b1ec412b26a0ccb8f10e4d0
teejusb <5017202+teejusb@users.noreply.github.com>
2023-11-25 19:17:54 -0800

    Swap ITG with EX score in Pane2 if ShowExScore is enabled

--- BGAnimations/ScreenEvaluation common/Panes/Pane2/JudgmentLabels.lua
+++ BGAnimations/ScreenEvaluation common/Panes/Pane2/JudgmentLabels.lua
@@ -79,13 +79,26 @@ end
 -- labels: hands/ex, holds, mines, rolls
 for index, label in ipairs(RadarCategories) do
 	if index == 1 then
+		text = nil
+		if SL[pn].ActiveModifiers.ShowEXScore then
+			text = "ITG"
+		else
+			text = "EX"
+		end
+
+
 		t[#t+1] = LoadFont("Wendy/_wendy small")..{
-			Text="EX",
+			Text=text,
 			InitCommand=function(self) self:zoom(0.5):horizalign(right) end,
 			BeginCommand=function(self)
 				self:x( (controller == PLAYER_1 and -160) or 90 )
 				self:y(38)
-				self:diffuse( SL.JudgmentColors[SL.Global.GameMode][1] )
+
+				if SL[pn].ActiveModifiers.ShowEXScore then
+					self:diffuse(Color.White)
+				else
+					self:diffuse( SL.JudgmentColors[SL.Global.GameMode][1] )
+				end
 			end
 		}
 	end
