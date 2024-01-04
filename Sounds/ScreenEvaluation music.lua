e7c5204cc237667d8d3a8d77d3c39d2e1bfeb210
teejusb <5017202+teejusb@users.noreply.github.com>
2024-01-03 17:42:17 -0800

    Play random ScreenEvaluation music

---
+++ Sounds/ScreenEvaluation music.lua
@@ -0,0 +1,16 @@
+local dir = THEME:GetPathS("", "ScreenEvaluationMusic/")
+
+local dirFiles = FILEMAN:GetDirListing(dir)
+local soundFiles = {}
+
+for k, file in ipairs(dirFiles) do
+  if string.match(file, "%.ogg") then
+    table.insert(soundFiles, THEME:GetPathS("", "ScreenEvaluationMusic/" ..file))
+  end
+end
+
+SM(soundFiles)
+
+if #soundFiles > 0 then
+  return soundFiles[math.random(#soundFiles)]
+end
