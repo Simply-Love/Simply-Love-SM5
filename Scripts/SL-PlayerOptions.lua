3a00c772b07bdf5525ad9f00ff7017cd424e982b
Michael Sundqvist <michael.sundqvist1@gmail.com>
2024-03-11 18:43:26 +0200

    Increase ranges of NotefieldOffsetX and NotefieldOffsetY

--- Scripts/SL-PlayerOptions.lua
+++ Scripts/SL-PlayerOptions.lua
@@ -736,7 +736,7 @@ local Overrides = {
 		ExportOnChange = true,
 		Choices = function()
 			local first	= 0
-			local last 	= 50
+			local last 	= 150
 			local step 	= 1
 
 			return range(first, last, step)
@@ -765,8 +765,8 @@ local Overrides = {
 		LayoutType = "ShowOneInRow",
 		ExportOnChange = true,
 		Choices = function()
-			local first	= -50
-			local last 	= 50
+			local first	= -100
+			local last 	= 100
 			local step 	= 1
 
 			return range(first, last, step)
