06cceef0cf27baedd72187fd7f958125d8110fd3
Michael Sundqvist <michael.sundqvist1@gmail.com>
2024-03-11 19:24:13 +0200

    Fix lane cover positioning

--- BGAnimations/ScreenGameplay overlay/LaneCover.lua
+++ BGAnimations/ScreenGameplay overlay/LaneCover.lua
@@ -8,8 +8,16 @@ local filter = Def.Quad{
         local headerHeight = 80
         local percentage = mods.LaneCover:gsub("%%","") / 100
 
+        local halfScreen = SCREEN_CENTER_X
+        local quarterScreen = halfScreen / 2
+
+        -- P1 notefield is at 25% and P2 notefield is at 75% X-position and moved from there
+        local playerNotefieldPosition = player == PLAYER_1 and quarterScreen or halfScreen + quarterScreen
+        local adjusted_offset_x = mods.NoteFieldOffsetX * (player == PLAYER_1 and -1 or 1)
+
 		self:diffuse(Color.Black)
-			:xy(GetNotefieldX(player) + (mods.NoteFieldOffsetX * 2), _screen.cy + (headerHeight / 2))
+		    :addx(playerNotefieldPosition + adjusted_offset_x)
+			:y(_screen.cy + (headerHeight / 2))
             :zoomto(GetNotefieldWidth(player), _screen.h - headerHeight)
 
         if (playeroptions:UsingReverse()) then
