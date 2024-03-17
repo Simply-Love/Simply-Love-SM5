a0fa997f1c4915cf1ed66e553263176b0a614940
SugoiFactory <tony.f.cheng@gmail.com>
2024-03-17 12:37:17 -0700

    Bring back CMOD warning

--- BGAnimations/ScreenGameplay underlay/PerPlayer/NoteField/DisplayMods.lua
+++ BGAnimations/ScreenGameplay underlay/PerPlayer/NoteField/DisplayMods.lua
@@ -24,4 +24,47 @@ af[#af+1] = LoadFont("Common Normal")..{
   end,
 }
 
+-- For tournament packs that have No CMOD rules 
+local song = GAMESTATE:GetCurrentSong()
+local song_dir = song:GetSongDir()
+local group = string.lower(song:GetGroupName())
+local tourneyPack = false
+local tourneyPacks = {"itl", "rip"}
+for pack in ivalues(tourneyPacks) do
+	if string.find(group,pack) ~= nil then tourneyPack = true end
+end
+
+-- Tourney specific only
+-- for tournament packs with rules, give a more obvious warning when cmod is on when it is not allowed
+local subtitle = song:GetDisplaySubTitle()
+if tourneyPack 
+	and string.find(string.upper(subtitle), "(NO CMOD)") 
+	and GAMESTATE:GetPlayerState(ToEnumShortString(player)):GetPlayerOptions("ModsLevel_Preferred"):CMod() then
+		af[#af+1] = Def.ActorFrame{
+			InitCommand=function(self)
+				self:y(15+15*#values)
+			end,
+			Name="CModWarning",
+			Def.Quad{
+				Name="BGCmodWarning",
+				InitCommand=function(self)
+					self:diffuse(0,0,0,0.8)
+						:x(0)
+						:setsize(90, 30)
+				end,
+			},
+			Def.BitmapText {
+				Name="CModWarningText",
+				Font="Common Normal",
+				Text="CMod On",
+				InitCommand=function(self)
+					self:zoom(1.5)
+						:diffuse(1,0,0,1)
+						:horizalign(center)						
+				end,
+			}
+		}
+		
+end
+
 return af
