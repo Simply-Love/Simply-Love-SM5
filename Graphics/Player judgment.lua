686df6589bd68cc28b78727ea82a48300e79bc9c
Arttu Ylä-Outinen <aryla@users.noreply.github.com>
2024-05-16 20:06:30 +0300

    Fix judgment flashes
    Fixes W1 judgment flashes appearing on the other player's side when that
    player is using the None judgment font and has FA+ window enabled.

--- Graphics/Player judgment.lua
+++ Graphics/Player judgment.lua
@@ -58,6 +58,8 @@ if file_to_load == "None" then
 	return Def.Actor{
 		InitCommand=function(self) self:visible(false) end,
 		JudgmentMessageCommand=function(self,param)
+			if param.Player ~= player then return end
+
 			if ToEnumShortString(param.TapNoteScore) == "W1" and mods.ShowFaPlusWindow then
 				if not IsW0Judgment(param, player) and not IsAutoplay(player) then
 					frame = 1
