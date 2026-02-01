13d1f138a39e6d4178515af80b26aec8c4cb477e
Crash Cringle <30600688+CrashCringle12@users.noreply.github.com>
2026-01-31 23:49:36 -0500

    Condense to 1 InputHandler

--- BGAnimations/ScreenSelectPlayMode underlay/default.lua
+++ BGAnimations/ScreenSelectPlayMode underlay/default.lua
@@ -31,6 +31,23 @@ local Update = function(af, delta)
 	end
 end
 
+local InputHandler = function(event)
+	if not event.PlayerNumber or not event.button then return false end
+
+	if event.type == "InputEventType_FirstPress" then
+		if event.GameButton == "Start" then
+			if ScreenName=="ScreenSelectPlayMode" or ScreenName=="ScreenSelectPlayModeThonk" then
+				SL.Global.GameMode = choices[cursor.index+1]
+				-- now that a GameMode has been selected, set related preferences
+				SetGameModePreferences()
+				-- and reload the theme's Metrics
+				THEME:ReloadMetrics()
+				SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
+			end
+		end
+	end
+end
+
 local t = Def.ActorFrame{
 	InitCommand=function(self)
 		self:SetUpdateFunction( Update )
@@ -49,24 +66,9 @@ local t = Def.ActorFrame{
 			choices[#choices+1] = choice
 			choice_actors[#choice_actors+1] = TopScreen:GetChild("IconChoice"..choice)
 		end
+    	SCREENMAN:GetTopScreen():AddInputCallback(InputHandler)
 
 		self:queuecommand("Update")
-	end,
-	MenuStartP1MessageCommand=function(self)
-		if ScreenName=="ScreenSelectPlayMode" or ScreenName=="ScreenSelectPlayModeThonk" then
-			-- set the GameMode now; we'll use it throughout the theme
-			-- to set certain Gameplay settings and determine which screen comes next
-			SL.Global.GameMode = choices[cursor.index+1]
-		end
-		SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
-	end,
-	MenuStartP2MessageCommand=function(self)
-		if ScreenName=="ScreenSelectPlayMode" or ScreenName=="ScreenSelectPlayModeThonk" then
-			-- set the GameMode now; we'll use it throughout the theme
-			-- to set certain Gameplay settings and determine which screen comes next
-			SL.Global.GameMode = choices[cursor.index+1]
-		end
-		SCREENMAN:GetTopScreen():StartTransitioningScreen("SM_GoToNextScreen")
 	end,
 	-- side mask
 	Def.Quad{
