ecb406539a19e811099d6d0eccc976d729957270
ChanceRbrts <roberts.chance@gmail.com>
2026-06-01 10:14:40 -0600

    Add Names to TestInput and ScreenSelectMusic out

--- BGAnimations/ScreenSelectMusic out.lua
+++ BGAnimations/ScreenSelectMusic out.lua
@@ -1,12 +1,15 @@
 return Def.ActorFrame{
+	Name="SplashScreen",
 	InitCommand=function(self) self:draworder(200) end,
 
 	Def.Quad{
+		Name="Background",
 		InitCommand=function(self) self:diffuse(0,0,0,0):FullScreen():cropbottom(1):fadebottom(0.5) end,
 		OffCommand=function(self) self:linear(0.3):cropbottom(-0.5):diffusealpha(1) end
 	},
 
 	LoadFont("Common Bold")..{
+		Name="OptionsText",
 		Text=THEME:GetString("ScreenSelectMusic", "Press Start for Options"),
 		InitCommand=function(self) self:visible(false):Center():zoom(0.75) end,
 		ShowPressStartForOptionsCommand=function(self) self:visible(true) end,
