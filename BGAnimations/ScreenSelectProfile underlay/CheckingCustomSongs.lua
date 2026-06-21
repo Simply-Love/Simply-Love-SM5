41ca04e4cda142ab4bcb76c5660cbd4b305d9458
Valentina Meneghello <meneghello.valentina16@gmail.com>
2026-06-21 19:32:38 +0200

    Re-apply USB Custom Songs edits

---
+++ BGAnimations/ScreenSelectProfile underlay/CheckingCustomSongs.lua
@@ -0,0 +1,41 @@
+if not PREFSMAN:GetPreference("CustomSongsEnable") then return end
+
+return Def.ActorFrame {
+    Name = "CheckingCustomSongs",
+
+    InitCommand = function(self)
+        self:Center():diffusealpha(0)
+    end,
+
+    OffCommand = function(self)
+        for player in ivalues(PlayerNumber) do
+            if GAMESTATE:IsHumanPlayer(player) and
+                MEMCARDMAN:GetCardState(player) ~= 'MemoryCardState_none' then
+                self:linear(0.5):diffusealpha(1)
+            end
+        end
+    end,
+
+    Def.Quad {
+        InitCommand = function(self)
+            self:zoomto(_screen.w, 100)
+                :diffuse(Color.Black)
+                :diffusealpha(0.8)
+        end
+    },
+
+    Def.Sprite {
+        Texture = THEME:GetPathB("ScreenMemoryCard", "overlay/usbicon.png"),
+        InitCommand = function(self)
+            self:x(-150):zoom(0.45)
+        end
+    },
+
+    Def.BitmapText {
+        Font = "Common Normal",
+        Text = ScreenString("CheckingCustomSongs"),
+        InitCommand = function(self)
+            self:x(80):zoom(1.5)
+        end
+    },
+}
