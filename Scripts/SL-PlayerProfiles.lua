bbf9b9b195502c0fec9124633a246187714c8864
Chris Cluskey <cjc12358@hotmail.com>
2026-06-21 15:00:04 -0700

    Add Vocalization Option

--- Scripts/SL-PlayerProfiles.lua
+++ Scripts/SL-PlayerProfiles.lua
@@ -66,6 +66,8 @@ local permitted_profile_settings = {
 	ShowFaPlusWindow     = "boolean",
 	ShowExScore          = "boolean",
 	ShowFaPlusPane       = "boolean",
+	
+	Vocalize             = "string",
 
 	HideEarlyDecentWayOffJudgments = "boolean",
 	HideEarlyDecentWayOffFlash     = "boolean",
