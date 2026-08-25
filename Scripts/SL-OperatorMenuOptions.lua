0ee7b823b63082f8333caf7d9cdfd6540c762201
Tater <Mr.Potatooh@gmail.com>
2026-08-24 23:55:41 -0500

    Adjust USB "Song Load Timeout" choices

--- Scripts/SL-OperatorMenuOptions.lua
+++ Scripts/SL-OperatorMenuOptions.lua
@@ -445,9 +445,7 @@ OperatorMenuOptionRows.CustomSongsMaxMegabytes = function()
 end
 
 OperatorMenuOptionRows.CustomSongsLoadTimeout = function()
-	-- first, define a reasonable range of integers from [3,10]
-	local choices = range(3,10)
-	table.insert(choices, 60)
+	local choices = {3, 5, 7, 10, 15, 30, 60, 90, 120}
 
 	-- accommodate custom values rather than steamrolling over them
 	local pref = PREFSMAN:GetPreference("CustomSongsLoadTimeout")
